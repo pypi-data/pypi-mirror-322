9b3bb0148a0a89b9382ffe3895375f47a466cd29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 14:03:59 +0000

    Add package agno/0.0.2/agno-0.0.2-py3-none-any.whl

---
+++ packages/agno/agno-0.0.2-py3-none-any.whl/agno/memory/summary.py
@@ -0,0 +1,13 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class SessionSummary(BaseModel):
+    """Model for Session Summary."""
+
+    summary: str = Field(
+        ...,
+        description="Summary of the session. Be concise and focus on only important information. Do not make anything up.",
+    )
+    topics: Optional[List[str]] = Field(None, description="Topics discussed in the session.")
