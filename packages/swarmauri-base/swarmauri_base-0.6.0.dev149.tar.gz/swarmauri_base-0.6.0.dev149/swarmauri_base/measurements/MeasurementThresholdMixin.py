473332acae22a0eb7c6fbb0c9efb9a6d6a052ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:52 +0000

    Add package swarmauri-base/0.6.0.dev149/swarmauri_base-0.6.0.dev149.tar.gz

---
+++ packages/swarmauri-base/swarmauri_base-0.6.0.dev149.tar.gz/swarmauri_base-0.6.0.dev149/swarmauri_base/measurements/MeasurementThresholdMixin.py
@@ -0,0 +1,8 @@
+from typing import Literal
+from pydantic import BaseModel
+
+from swarmauri_core.measurements.IThreshold import IThreshold
+
+class MeasurementThresholdMixin(IThreshold, BaseModel):
+    k: int
+    
