20dc1ebebc9ffe57c8c3411a73c2f627caedf75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:39 +0000

    Add package hashicorp-vault-django/0.0.3/hashicorp_vault_django-0.0.3.tar.gz

---
+++ packages/hashicorp-vault-django/hashicorp_vault_django-0.0.3.tar.gz/hashicorp_vault_django-0.0.3/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup, find_packages
+
+README = ""
+readme_path = os.path.join(os.path.dirname(__file__), 'readme.md')
+if os.path.exists(readme_path):
+    with open(readme_path, encoding="utf-8") as f:
+        README = f.read()
+
+setup(
+    name='hashicorp_vault_django',
+    version='0.0.3',
+    packages=find_packages(exclude=["tests*", "docs*"]),
+    include_package_data=True,
+    license='MIT License',
+    description='A Django app to manage the application secrets using hashicorp.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/icedreamer-praveen/hashicorp-vault',
+    author='icedreamer-praveen',
+    author_email='prabinchy1234@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    python_requires=">=3.10",
+    install_requires=[
+        "django>=3.2",
+        "hvac>=0.11.2"
+    ],
+    extras_require={
+        "dev": ['twine>=4.0.2'],
+    }
+)
