5cec6e2715babee25f3204b57346f383b86e8293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:04:27 +0000

    Add package shakhbanov/0.0.10/shakhbanov-0.0.10.tar.gz

---
+++ packages/shakhbanov/shakhbanov-0.0.10.tar.gz/shakhbanov-0.0.10/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shakhbanov',
+    version='0.0.10',
+    author='Zurab Shakhbanov',
+    author_email='zurab@shakhbanov.ru',
+    description='ML tools for time series',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://shakhbanov.org',
+    project_urls={
+        'GitHub': 'https://github.com/shakhbanov/shakhbanov',
+    },
+    download_url='https://github.com/shakhbanov/shakhbanov/archive/refs/heads/main.zip',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'tqdm',
+        'joblib',
+        'holidays',
+        'scikit-learn',
+        'scipy',
+        'utilsforecast',
+        'statsmodels'
+    ],
+)
---
+++ packages/shakhbanov/shakhbanov-0.0.10.tar.gz/shakhbanov-0.0.10/shakhbanov/__init__.py
@@ -0,0 +1,16 @@
+"""
+shakhbanov - ML инструменты для временного ряда
+
+Автор: Zurab Shakhbanov
+Email: zurab@shakhbanov.ru
+Сайт: https://shakhbanov.org
+"""
+
+from .metrics import accuracy, precision, recall, f_score, roc_auc, mae, mse, rmse, mape, wape, smape, mase
+from .preprocessing import Detector, DataFiller, DataCleaner, ColumnCleaner
+
+__all__ = [
+    'accuracy', 'precision', 'recall', 'f_score', 'roc_auc',
+    'mae', 'mse', 'rmse', 'mape', 'wape', 'smape', 'mase',
+    'Detector', 'DataFiller', 'DataCleaner', 'ColumnCleaner'
+]
