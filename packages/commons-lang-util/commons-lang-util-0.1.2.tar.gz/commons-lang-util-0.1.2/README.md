2923a546adfc8fce2ddc2ff122e427bff8e8be39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:05:15 +0000

    Add package commons-lang-util/0.1.2/commons-lang-util-0.1.2.tar.gz

---
+++ packages/commons-lang-util/commons-lang-util-0.1.2.tar.gz/commons-lang-util-0.1.2/README.md
@@ -0,0 +1,11 @@
+# commons_lang_util
+
+A utility package for common operations.
+
+## Installation
+```
+pip install commons_lang_util
+```
+
+
+
---
+++ packages/commons-lang-util/commons-lang-util-0.1.2.tar.gz/commons-lang-util-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='commons-lang-util',
+    version='0.1.2',
+    packages=['commons_lang_util'],
+    install_requires=[],
+    author='JiaBao Gao',
+    author_email='gaojiabao1991@gmail.com',
+    description='A utility package for common language operations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gaojiabao1991/commons_lang_util',
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    python_requires='>=3.0',
+) 
+
