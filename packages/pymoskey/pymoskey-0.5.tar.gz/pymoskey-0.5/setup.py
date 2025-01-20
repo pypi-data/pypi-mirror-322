fb019ea1e9ea3e50a4357200c66438fe0c609a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 14:04:23 +0000

    Add package pymoskey/0.5/pymoskey-0.5.tar.gz

---
+++ packages/pymoskey/pymoskey-0.5.tar.gz/pymoskey-0.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pymoskey",
+    version="0.5",
+    packages=find_packages(),
+    install_requires=["pyautogui"],  # Ajoutez ici les dépendances nécessaires
+)
