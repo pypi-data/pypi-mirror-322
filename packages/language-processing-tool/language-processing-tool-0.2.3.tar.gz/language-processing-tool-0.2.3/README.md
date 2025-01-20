5bf8e97254578e5cb234d4cdfef566cc602f670b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:48 +0000

    Add package language-processing-tool/0.2.3/language-processing-tool-0.2.3.tar.gz

---
+++ packages/language-processing-tool/language-processing-tool-0.2.3.tar.gz/language-processing-tool-0.2.3/README.md
@@ -0,0 +1,20 @@
+# Language Processing Tool
+
+This Python package provides functionality to process PDFs and detect languages in the documents.
+
+## Installation
+
+You can install the package using pip:
+
+
+## Usage
+
+For batch processing:
+
+process-pdfs /path/to/input/ /path/to/csv_file.csv /path/to/output/
+
+
+
+For single PDF processing:
+
+process-pdfs path.pdf
---
+++ packages/language-processing-tool/language-processing-tool-0.2.3.tar.gz/language-processing-tool-0.2.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="language-processing-tool",
+    version="0.2.3",  # Updated version
+    packages=find_packages(),
+    install_requires=[
+        "pytesseract",  # Example dependencies
+        "langdetect",
+        "pandas",
+        "PyMuPDF",
+        "icecream",
+        "Pillow",
+        "argparse",
+        # Add other dependencies as needed
+    ],
+    entry_points={
+        'console_scripts': [
+            'process-pdfs = language_processing_tool.process_pdfs:main',  # Define your CLI entry point
+        ],
+    },
+    description="A PDF language detection and OCR tool",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="Harish kumar S",
+    author_email="harishkumar56278@gmail.com",
+    url="https://github.com/Harish-nika/language-processing-tool-0.2.0.git",  # Update this URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Specify Python version
+)
