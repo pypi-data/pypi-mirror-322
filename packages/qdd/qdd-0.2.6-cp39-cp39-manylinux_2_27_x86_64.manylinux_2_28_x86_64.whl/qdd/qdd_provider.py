44ab00b6b8bcd52160a34fa8b85abef794f27fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:04:30 +0000

    Add package qdd/0.2.6/qdd-0.2.6-cp39-cp39-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/qdd/qdd-0.2.6-cp39-cp39-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/qdd/qdd_provider.py
@@ -0,0 +1,27 @@
+from qiskit.providers import ProviderV1 as Provider
+from qiskit.providers.providerutils import filter_backends
+
+from .qdd_backend import QddBackend
+from .qdd_sv_backend import QddSVBackend
+
+
+class QddProvider(Provider):
+
+    def __init__(self, token=None):
+        super().__init__()
+        self.token = token
+        self._backends = [QddBackend(provider=self), QddSVBackend(provider=self)]
+
+    def backends(self, name=None, **kwargs):
+        if name:
+            backend_candidates = [
+                backend for backend in self._backends if backend.name() == name
+            ]
+        else:
+            backend_candidates = self._backends.copy()
+        return filter_backends(backend_candidates, **kwargs)
+
+    def get_backend(self, name=None, **kwargs) -> QddBackend:
+        if name == None:
+            name = "qasm_simulator"
+        return super().get_backend(name, **kwargs)
