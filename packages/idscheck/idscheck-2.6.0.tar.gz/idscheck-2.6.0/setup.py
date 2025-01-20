e1bb1b782e32b6f0d56f9cf17e36f444b19420dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:04:31 +0000

    Add package idscheck/2.6.0/idscheck-2.6.0.tar.gz

---
+++ packages/idscheck/idscheck-2.6.0.tar.gz/idscheck-2.6.0/setup.py
@@ -0,0 +1,45 @@
+import setuptools
+from setuptools.command.install_scripts import install_scripts
+
+
+# class InstallScripts(install_scripts):
+
+#     def run(self):
+#         setuptools.command.install_scripts.install_scripts.run(self)
+
+#         # Rename some script files
+#         for script in self.get_outputs():
+#             if basename.endswith(".py") or basename.endswith(".sh"):
+#                 dest = script[:-3]
+#             else:
+#                 continue
+#             print("moving %s to %s" % (script, dest))
+#             shutil.move(script, dest)
+
+
+with open("README.md", "r",  encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="idscheck",
+    version="2.6.0",
+    author="Naibo Wang",
+    author_email="naibowang@foxmail.com",
+    description="A command line tool for IDS student to check GPU allocation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NaiboWang/IDSCheck",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'ids = idscheck.idscheck:cmd',
+            'idsgpu = idscheck.idscheck:gpu',
+            'idsnotify = idscheck.idscheck:notify',
+            'idstop = idscheck.idscheck:top',
+            'idstopall = idscheck.idscheck:topall',
+            'idsquery = idscheck.idscheck:query',
+            'idsrestart = idscheck.idscheck:restart',
+        ],
+    },
+)
