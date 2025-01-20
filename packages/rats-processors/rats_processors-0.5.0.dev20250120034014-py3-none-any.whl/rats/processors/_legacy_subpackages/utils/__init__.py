92cc02636fa78d82d442e5dd472f78d8885dc816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 14:03:58 +0000

    Add package rats-processors/0.5.0.dev20250120034014/rats_processors-0.5.0.dev20250120034014-py3-none-any.whl

---
+++ packages/rats-processors/rats_processors-0.5.0.dev20250120034014-py3-none-any.whl/rats/processors/_legacy_subpackages/utils/__init__.py
@@ -0,0 +1,16 @@
+from ._frozendict import fdict, frozendict
+from ._immutable_no_overlap_chainmap import ImmutableNoOverlapChainMap
+from ._namedcollection import NamedCollection, SupportsAsDict, namedcollection, ncoll
+from ._orderedset import orderedset, oset
+
+__all__ = [
+    "ImmutableNoOverlapChainMap",
+    "NamedCollection",
+    "SupportsAsDict",
+    "fdict",
+    "frozendict",
+    "namedcollection",
+    "ncoll",
+    "orderedset",
+    "oset",
+]
