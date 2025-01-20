716696e512b15ad242df395626b2113475e8c590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 14:04:19 +0000

    Add package parametric-ttl-cache/1.0.1/parametric-ttl-cache-1.0.1.tar.gz

---
+++ packages/parametric-ttl-cache/parametric-ttl-cache-1.0.1.tar.gz/parametric-ttl-cache-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='parametric-ttl-cache',
+    version='1.0.1',
+    author='Yongho Hwang',
+    author_email='jogakdal@gmail.com',
+    description='A function-level memory cache that supports Time To Live (TTL)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jogakdal/python_ttl_cache',
+    install_requires=['inspect', 'expiringdict'],
+    packages=find_packages(exclude=[]),
+    keywords=['cache', 'memory cache', 'ttl cache', 'function cache', 'cache decorator', 'parametric cache', 'jogakdal'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+)
