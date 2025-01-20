1b8e2aa23cb2c4d27eb47b9162e0cba6e0200f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 14:04:18 +0000

    Add package pytest-time/0.4.0/pytest_time-0.4.0-py3-none-any.whl

---
+++ packages/pytest-time/pytest_time-0.4.0-py3-none-any.whl/pytest_time/__init__.py
@@ -0,0 +1,45 @@
+"""Pytest plugin for manipulating the time."""
+
+import time
+
+import pytest
+
+from pytest_time.instant_sleep import instant_sleep, InstantSleep
+
+__all__ = [
+    "InstantSleep",
+    "instant_sleep",
+    "MockWrapper",
+    "mock_instant_sleep",
+    "__version__",
+]
+
+from pytest_time.wrap_time import MockWrapper
+
+try:
+    from ._version import __version__
+except ImportError:  # pragma: no cover
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version("pytest_time")
+    except PackageNotFoundError:
+        __version__ = "dev"
+
+
+@pytest.fixture
+def mock_time(monkeypatch: pytest.MonkeyPatch) -> MockWrapper:
+    wrapper = MockWrapper(time)  # type: ignore[arg-type]
+    wrapper.install(monkeypatch)
+    return wrapper
+
+
+@pytest.fixture
+def mock_instant_sleep(
+    monkeypatch: pytest.MonkeyPatch,
+    instant_sleep: InstantSleep,
+) -> MockWrapper:
+    """Get a mock wrapper that makes sleep occur instantly."""
+    wrapper = MockWrapper(instant_sleep)
+    wrapper.install(monkeypatch)
+    return wrapper
