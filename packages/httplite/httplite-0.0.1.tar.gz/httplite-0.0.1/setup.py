5730020e77c21e76bf12c7e6e3f41502a512e395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:53 +0000

    Add package httplite/0.0.1/httplite-0.0.1.tar.gz

---
+++ packages/httplite/httplite-0.0.1.tar.gz/httplite-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="httplite",
+    version="0.0.1",
+    author="Jun Ke",
+    author_email="kejun91@gmail.com",
+    description="A lightweight http client based on urllib3",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kejun91/httplite",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "urllib3",
+        "certifi"
+    ],
+    python_requires='>=3.9',
+)
