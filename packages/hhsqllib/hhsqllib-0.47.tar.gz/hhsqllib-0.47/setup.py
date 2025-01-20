038450355e7ef62abf9d93062aa86335dfb0d643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:04:06 +0000

    Add package hhsqllib/0.47/hhsqllib-0.47.tar.gz

---
+++ packages/hhsqllib/hhsqllib-0.47.tar.gz/hhsqllib-0.47/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name="hhsqllib",
+	  version="0.47",
+	  packages=find_packages(),
+	  install_requires=[
+'SQLAlchemy==2.0.30',
+	  ],
+	  author="hh",
+	  author_email="hehuang0717@outlook.com",
+	  description="sqllib",
+	  long_description=open('README.md').read(),
+	  long_description_content_type="text/markdown",
+	  url="https://your.project.url",
+	  classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",
+				   "Operating System :: OS Independent", ],
+	  python_requires='>=3.9', )
