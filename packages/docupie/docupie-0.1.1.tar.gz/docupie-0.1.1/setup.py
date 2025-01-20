aeb984b1457a60ce3c31c55772c2f8083ebdd746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:04:31 +0000

    Add package docupie/0.1.1/docupie-0.1.1.tar.gz

---
+++ packages/docupie/docupie-0.1.1.tar.gz/docupie-0.1.1/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="docupie",
+    version="0.1.1",
+    description="An advanced document processing tool that leverages AI to extract structured data from PDFs",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Husam Gibreel",
+    author_email="husamgibreel@gmail.com",
+    url="https://github.com/h-amg/docupie",
+    packages=find_packages(),
+    install_requires=[
+        "pytesseract==0.3.13",
+        "Spire.Doc==13.1.0",
+        "plum-dispatch==1.7.4",
+        "openai==1.59.8",
+        "requests==2.32.3",
+        "pdf2image==1.17.0",
+    ],
+    python_requires=">=3.10.4",
+)
