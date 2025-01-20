936f9eca5f35c19416957ae4b32fedad72e805fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:05:03 +0000

    Add package yta-multimedia/0.1.118/yta_multimedia-0.1.118.tar.gz

---
+++ packages/yta-multimedia/yta_multimedia-0.1.118.tar.gz/yta_multimedia-0.1.118/yta_multimedia/experimental/image/generation/ai/openai.py
@@ -0,0 +1,22 @@
+from yta_general_utils.downloader import Downloader
+from yta_general_utils.image.size import resize_without_scaling
+from openai import OpenAI
+
+
+# TODO: Is this actually useful? I think it could be removed...
+
+def generate_image(prompt, output_filename):
+    client = OpenAI()
+
+    response = client.images.generate(
+        model = "dall-e-3",
+        prompt = prompt,
+        size = "1792x1024",
+        quality = "standard",
+        n = 1,
+    )
+
+    image_url = response.data[0].url
+
+    Downloader.download_image(image_url, output_filename)
+    resize_without_scaling(output_filename)
