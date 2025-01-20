84660b00d76c68aa06ff0c368bd345f1ed58c54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:31 +0000

    Add package fond-utils/0.1.1/fond_utils-0.1.1-py3-none-any.whl

---
+++ packages/fond-utils/fond_utils-0.1.1-py3-none-any.whl/fondutils/__version__.py
@@ -0,0 +1,24 @@
+#
+# Copyright 2024 Christian Muise & Sebastian Sardina
+#
+# ------------------------------
+#
+# This file is part of fond-utils.
+#
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+#
+
+"""Package metadata declaration."""
+
+__title__ = "fond-utils"
+__description__ = "FOND Utilities"
+__url__ = "https://github.com/AI-Planning/fond-utils/"
+__version__ = "0.1.1"
+__author__ = "Christian Muise, Sebastian Sardina"
+__author_email__ = (
+    "christian.muise@queensu.ca, ssardina@gmail.com"
+)
+__license__ = "MIT License"
+__copyright__ = "2024 Christian Muise & Sebastian Sardina"
