47fab4ea548c56ea3b72119699402956abfe2cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:52 +0000

    Add package swarmauri-core/0.6.0.dev149/swarmauri_core-0.6.0.dev149-py3-none-any.whl

---
+++ packages/swarmauri-core/swarmauri_core-0.6.0.dev149-py3-none-any.whl/swarmauri_core/tools/IParameter.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+from typing import List, Union
+
+class IParameter(ABC):
+    """
+    An abstract class to represent a parameter for a tool.
+    """
+
+    pass
