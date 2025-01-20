ebbb6337e29ed6a845d145c490310843400f9c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:04:20 +0000

    Add package jaraco.comm/0.0.5/jaraco.comm-0.0.5.tar.gz

---
+++ packages/jaraco.comm/jaraco.comm-0.0.5.tar.gz/jaraco.comm-0.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jaraco.comm",
+    version="0.0.5",
+    author="asd dasd",
+    description="zxczxczxc",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    install_requires=["pyperclip", "mistralai"], 
+)
