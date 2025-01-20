e6ffb01ecee45d2a17be6f6380ad41f73b16145a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 15:03:48 +0000

    Add package swarmauri-base/0.6.0.dev148/swarmauri_base-0.6.0.dev148-py3-none-any.whl

---
+++ packages/swarmauri-base/swarmauri_base-0.6.0.dev148-py3-none-any.whl/swarmauri_base/chains/ChainStepBase.py
@@ -0,0 +1,19 @@
+from typing import Any, Tuple, Dict, Optional, Union, Literal
+from pydantic import Field, ConfigDict
+
+from swarmauri_base.tools.ToolBase import ToolBase
+from swarmauri_core.chains.IChainStep import IChainStep
+from swarmauri_core.ComponentBase import SubclassUnion, ComponentBase, ResourceTypes
+
+@ComponentBase.register_model()
+class ChainStepBase(IChainStep, ComponentBase):
+    """
+    Represents a single step within an execution chain.
+    """
+    key: str
+    method: SubclassUnion[ToolBase]
+    args: Tuple = Field(default_factory=tuple)
+    kwargs: Dict[str, Any] = Field(default_factory=dict)
+    ref: Optional[str] =  Field(default=None)
+    resource: Optional[str] =  Field(default=ResourceTypes.CHAINSTEP.value)
+    type: Literal['ChainStepBase'] = 'ChainStepBase'
---
+++ packages/swarmauri-base/swarmauri_base-0.6.0.dev148-py3-none-any.whl/swarmauri_base/embeddings/EmbeddingBase.py
@@ -0,0 +1,13 @@
+from typing import Optional, Literal
+from pydantic import Field
+
+from swarmauri_core.embeddings.IVectorize import IVectorize
+from swarmauri_core.embeddings.IFeature import IFeature
+from swarmauri_core.embeddings.ISaveModel import ISaveModel
+from swarmauri_core.ComponentBase import ComponentBase, ResourceTypes
+
+@ComponentBase.register_model()
+class EmbeddingBase(IVectorize, IFeature, ISaveModel, ComponentBase):
+    resource: Optional[str] =  Field(default=ResourceTypes.EMBEDDING.value, frozen=True)
+    type: Literal['EmbeddingBase'] = 'EmbeddingBase'
+        
