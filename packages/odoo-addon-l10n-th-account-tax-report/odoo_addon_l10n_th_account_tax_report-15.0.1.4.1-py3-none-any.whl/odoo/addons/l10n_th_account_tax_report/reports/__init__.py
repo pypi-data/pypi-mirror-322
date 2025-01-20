1ceedc7b33d4b6d4d9de9a2aada9c0bfbf4cd3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:04:39 +0000

    Add package odoo-addon-l10n-th-account-tax-report/15.0.1.4.1/odoo_addon_l10n_th_account_tax_report-15.0.1.4.1-py3-none-any.whl

---
+++ packages/odoo-addon-l10n-th-account-tax-report/odoo_addon_l10n_th_account_tax_report-15.0.1.4.1-py3-none-any.whl/odoo/addons/l10n_th_account_tax_report/reports/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from . import tax_report
+from . import tax_report_xlsx
+from . import report_wht_xlsx
+from . import report_withholding_tax
