b27ba676e5bd9ed08491179def9330bc406ea5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-20 16:05:07 +0000

    Add package eda-nijin/0.3.1/eda_nijin-0.3.1.tar.gz

---
+++ packages/eda-nijin/eda_nijin-0.3.1.tar.gz/eda_nijin-0.3.1/README.md
@@ -0,0 +1,24 @@
+# eda-nijin
+ ![LGTM](https://i.lgtm.fun/2vtu.png)
+
+### USE
+```bash
+$ pip install eda-nijin
+$ python
+>>> from eda-nijin.cli import group_by_count
+>>> group_by_count()
+```
+### DEV
+```bash
+$ source .venv/bin/activate
+$ pdm add pandas
+$ pdm add -dG eda jupyter
+```
+### EDA
+- run jupyterlab
+```
+$ jupyter lab
+```
+### REF
+- [install jupyterlab](https://jupyter.org/install)
+- https://pypi.org/project/president-speech/
---
+++ packages/eda-nijin/eda_nijin-0.3.1.tar.gz/eda_nijin-0.3.1/src/eda_nijin/cli.py
@@ -0,0 +1,11 @@
+from president_speech.db.parquet_interpreter import get_parquet_full_path
+import pandas as pd
+
+def group_by_count():
+    kw = "올림픽"
+    data_path = get_parquet_full_path()
+    df=pd.read_parquet(data_path)
+    f_df = df[df['speech_text'].str.contains('올림픽', case=False)]
+    rdf=f_df.groupby("president").size().reset_index(name="count")
+    sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
+    print(sdf.to_string(index=False))
