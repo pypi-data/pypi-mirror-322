887c376875210813445803cea8b15699309eebdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:00 +0000

    Add package petstore-test-9-api-client/1.0.0/petstore_test_9_api_client-1.0.0-py3-none-any.whl

---
+++ packages/petstore-test-9-api-client/petstore_test_9_api_client-1.0.0-py3-none-any.whl/petstore_test_9_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from petstore_test_9_api_client.api.pet_api import PetApi
+from petstore_test_9_api_client.api.store_api import StoreApi
+from petstore_test_9_api_client.api.user_api import UserApi
+
