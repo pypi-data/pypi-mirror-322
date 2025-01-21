7fb1a61a94cd00bfce0f26bccf583b968259342c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:31 +0000

    Add package ansys-api-platform-instancemanagement/1.1.2/ansys_api_platform_instancemanagement-1.1.2.tar.gz

---
+++ packages/ansys-api-platform-instancemanagement/ansys_api_platform_instancemanagement-1.1.2.tar.gz/ansys_api_platform_instancemanagement-1.1.2/src/ansys/api/platform/instancemanagement/_version.py
@@ -0,0 +1,7 @@
+"""Version of ansys-api-platform-instancemanagement module."""
+
+# Version of the ansys-api-platform-instancemanagement module
+__version__ = "1.1.2"
+
+# Defining a version alias for compatibility with other modules
+version = __version__
