baacb344f96c218c488a3750e77635d42c8f2ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:08 +0000

    Add package edc-pharmacy/1.0.0/edc_pharmacy-1.0.0.tar.gz

---
+++ packages/edc-pharmacy/edc_pharmacy-1.0.0.tar.gz/edc_pharmacy-1.0.0/edc_pharmacy/forms/prescription/rx_form.py
@@ -0,0 +1,28 @@
+from django import forms
+from edc_randomization.site_randomizers import site_randomizers
+
+from ...models import Rx
+
+
+def get_last_weight():
+    return 100
+
+
+class RxForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["randomizer_name"] = forms.ChoiceField(
+            label="Randomizer name",
+            choices=site_randomizers.get_as_choices(),
+            required=False,
+        )
+
+    subject_identifier = forms.CharField(
+        label="Subject Identifier",
+        required=False,
+        widget=forms.TextInput(attrs={"readonly": "readonly"}),
+    )
+
+    class Meta:
+        model = Rx
+        fields = "__all__"
