51b1bb9c76b414439b286ce4a586ef02693d9e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:27 +0000

    Add package AIScratch/1.0.0/AIScratch-1.0.0.tar.gz

---
+++ packages/AIScratch/AIScratch-1.0.0.tar.gz/AIScratch-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AIScratch',
+    version='1.0.0',
+    description='Sets of different AI Classes',
+    author='tomboisse',
+    author_email='tmboisse@gmail.com',
+    url='https://github.com/votre-repo',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
