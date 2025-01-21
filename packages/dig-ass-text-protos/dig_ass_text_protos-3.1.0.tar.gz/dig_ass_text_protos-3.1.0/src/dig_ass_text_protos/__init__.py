8b22137ea69bad229d8b4f53ba074af1b55c62d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:26 +0000

    Add package dig-ass-text-protos/3.1.0/dig_ass_text_protos-3.1.0.tar.gz

---
+++ packages/dig-ass-text-protos/dig_ass_text_protos-3.1.0.tar.gz/dig_ass_text_protos-3.1.0/src/dig_ass_text_protos/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "3.1.0"
+
+from .DigitalAssistantText_pb2 import DigitalAssistantTextRequest, DigitalAssistantTextResponse
+from .DigitalAssistantText_pb2_grpc import DigitalAssistantTextStub, DigitalAssistantText, DigitalAssistantTextServicer
+from .dto import TextRequest, TextHeaders
+from .client import TextClient
