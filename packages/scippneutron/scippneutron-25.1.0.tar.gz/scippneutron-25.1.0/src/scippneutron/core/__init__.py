87eff9dd64d0131df5df881090f0a3da2b357dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:15 +0000

    Add package scippneutron/25.1.0/scippneutron-25.1.0.tar.gz

---
+++ packages/scippneutron/scippneutron-25.1.0.tar.gz/scippneutron-25.1.0/src/scippneutron/core/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2023 Scipp contributors (https://github.com/scipp)
+# @author Simon Heybrock
+
+# flake8: noqa
+
+import os
+
+from .conversions import conversion_graph, convert, deduce_conversion_graph
