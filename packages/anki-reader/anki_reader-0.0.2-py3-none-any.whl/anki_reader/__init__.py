b4b49c37bd5d7823e6b9a08a2141f72564fa13e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 06:07:04 +0000

    Add package anki-reader/0.0.2/anki_reader-0.0.2-py3-none-any.whl

---
+++ packages/anki-reader/anki_reader-0.0.2-py3-none-any.whl/anki_reader/__init__.py
@@ -0,0 +1,6 @@
+"""This module serves as the entry point for the package. It initializes the necessary components and provides a convenient interface for accessing the functionality of the package.
+
+For more details on how to use this package, please refer to the `anki_api.py` file.
+"""
+
+from .anki_api import AnkiDB
