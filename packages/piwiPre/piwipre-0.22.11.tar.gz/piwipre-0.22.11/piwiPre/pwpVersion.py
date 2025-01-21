03b1ae78026ae41b373cd0f532920c5e27ffdc9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 06:06:57 +0000

    Add package piwiPre/0.22.11/piwipre-0.22.11.tar.gz

---
+++ packages/piwiPre/piwipre-0.22.11.tar.gz/piwipre-0.22.11/piwiPre/pwpVersion.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------------------------------------------------------------
+# piwiPre project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini(at)gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+
+
+class PwpVersion:
+    help = '0.22.11 at 01/20/2025 21:13:40'
+    spec = '0.22.11'
+    release = 0
+    version = 22
+    patch = 11
+    date = '01/20/2025 21:13:40'
+    url = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.22.11/piwiPre-0.22.11.exe'
+    installer = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.22.11/pwpInstaller-0.22.11.exe'
+    # noinspection PyPep8
+    configurator = 'https://gitlab.com/api/v4/projects/48915444/packages/generic/piwiPre/0.22.11/pwpConfigurator-0.22.11.exe'
+
+    def __init__(self):
+        pass
