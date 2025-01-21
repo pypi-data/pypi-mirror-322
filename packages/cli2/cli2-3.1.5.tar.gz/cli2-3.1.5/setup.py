ab7936520f9ee24d98199a3d14b9a9cc6b98b7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 16:05:07 +0000

    Add package cli2/3.1.5/cli2-3.1.5.tar.gz

---
+++ packages/cli2/cli2-3.1.5.tar.gz/cli2-3.1.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+setup(
+    name='cli2',
+    version='3.1.5',
+    setup_requires='setupmeta',
+    install_requires=[
+        'docstring_parser',
+        'pyyaml',
+        'pygments',
+    ],
+    extras_require=dict(
+        test=[
+            'freezegun',
+            'pytest',
+            'pytest-cov',
+            'pytest-mock',
+        ],
+    ),
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'cli2 = cli2.cli:main.entry_point',
+            'cli2-example = cli2.example_obj:cli.entry_point',
+        ],
+    },
+)
