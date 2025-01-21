4f30a1fd4f5ea2ebdab0cccea056836e3e891eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:37 +0000

    Add package visvis/1.15.0/visvis-1.15.0.tar.gz

---
+++ packages/visvis/visvis-1.15.0.tar.gz/visvis-1.15.0/tests/test_import.py
@@ -0,0 +1,11 @@
+def test_visvis_import():
+    import visvis as vv
+    import visvis.vvio
+
+    assert vv
+    assert vv.vvio
+    assert vv.imread
+    assert vv.imshow
+    assert vv.plot
+    assert vv.Slider
+    assert vv.Axes
---
+++ packages/visvis/visvis-1.15.0.tar.gz/visvis-1.15.0/visvis/utils/ssdf/tests/test_simple.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+if __name__ == "__main__":
+    sys.path.insert(0, os.path.abspath(os.path.join(__file__, "..", "..", "..")))
+
+    import ssdf
+
+    # Load from file
+    s1 = ssdf.load("test1.ssdf")
+
+    # Write to string and read back
+    tmp = ssdf.saves(s1)
+    s2 = ssdf.loads(tmp)
+
+    # Print, manually inspection required to see whether it matches the test file
+    print(s2)
