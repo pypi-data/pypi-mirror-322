f7ae3e400071ce20fb1fcfba012974369641a949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:18 +0000

    Add package zepben.protobuf/0.34.0b2/zepben.protobuf-0.34.0b2-py3-none-any.whl

---
+++ packages/zepben.protobuf/zepben.protobuf-0.34.0b2-py3-none-any.whl/zepben/protobuf/dc/dc_pb2.pyi
@@ -0,0 +1,8 @@
+from zepben.protobuf.dc import dc_requests_pb2 as _dc_requests_pb2
+from zepben.protobuf.dc import dc_responses_pb2 as _dc_responses_pb2
+from zepben.protobuf.metadata import metadata_requests_pb2 as _metadata_requests_pb2
+from zepben.protobuf.metadata import metadata_responses_pb2 as _metadata_responses_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
---
+++ packages/zepben.protobuf/zepben.protobuf-0.34.0b2-py3-none-any.whl/zepben/protobuf/ns/network_state_pb2.pyi
@@ -0,0 +1,7 @@
+from google.protobuf import empty_pb2 as _empty_pb2
+from zepben.protobuf.ns import network_state_requests_pb2 as _network_state_requests_pb2
+from zepben.protobuf.ns import network_state_responses_pb2 as _network_state_responses_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
