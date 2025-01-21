ce52115d1ab916bceb951c061c646c930e06fc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:05 +0000

    Add package spotpython/0.24.2/spotpython-0.24.2.tar.gz

---
+++ packages/spotpython/spotpython-0.24.2.tar.gz/spotpython-0.24.2/test/test_fit_surrogate.py
@@ -0,0 +1,28 @@
+import numpy as np
+from spotpython.fun.objectivefunctions import Analytical
+from spotpython.spot import spot
+from spotpython.utils.init import fun_control_init, design_control_init
+
+
+def test_fit_surrogate():
+    # number of initial points:
+    ni = 0
+    X_start = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [1, 1]])
+    fun = Analytical().fun_sphere
+    fun_control = fun_control_init(
+        noise=False,
+        lower=np.array([-1, -1]),
+        upper=np.array([1, 1]),
+        show_progress=True,
+    )
+    design_control = design_control_init(init_size=ni)
+    S = spot.Spot(
+        fun=fun,
+        fun_control=fun_control,
+        design_control=design_control,
+    )
+    S.initialize_design(X_start=X_start)
+    S.update_stats()
+    S.fit_surrogate()
+    # correlation matrix should be square and the same size as the number of points
+    assert S.surrogate.Psi.shape[0] == S.X.shape[0]
