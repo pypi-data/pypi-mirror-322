a5d5245db42fedc26ee6860c4e84f0b42bd680ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:05:09 +0000

    Add package multiversum/0.2.11/multiversum-0.2.11-py3-none-any.whl

---
+++ packages/multiversum/multiversum-0.2.11-py3-none-any.whl/multiversum/__init__.py
@@ -0,0 +1,13 @@
+from .multiverse import (
+    generate_multiverse_grid,
+    generate_universe_id,
+    MultiverseAnalysis,
+)
+from .universe import Universe
+
+__all__ = [
+    "generate_multiverse_grid",
+    "generate_universe_id",
+    "MultiverseAnalysis",
+    "Universe",
+]
