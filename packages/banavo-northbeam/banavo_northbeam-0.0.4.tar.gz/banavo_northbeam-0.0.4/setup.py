7c2ffca13ff5d8049cb1086ec39c4e7221bdf9ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:12 +0000

    Add package banavo-northbeam/0.0.4/banavo_northbeam-0.0.4.tar.gz

---
+++ packages/banavo-northbeam/banavo_northbeam-0.0.4.tar.gz/banavo_northbeam-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='banavo_northbeam',
+    version='0.0.4',
+    packages=find_packages(),
+    description='Contains codes for the banavo-northbeam pipeline',
+    author='Naila Fatima',
+    author_email='naila.fatima@andromeda360.ai',
+    install_requires=[],
+)
