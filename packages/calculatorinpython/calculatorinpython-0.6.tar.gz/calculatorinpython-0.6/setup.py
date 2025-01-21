b7b35138ca537654ba1c12604e7f027ddb22562a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:17 +0000

    Add package calculatorinpython/0.6/calculatorinpython-0.6.tar.gz

---
+++ packages/calculatorinpython/calculatorinpython-0.6.tar.gz/calculatorinpython-0.6/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calculatorinpython", 
+    version="0.6",
+    author="Vlad",
+    description="Module calculator in python.",
+    packages=find_packages(),  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=1.0'
+)
