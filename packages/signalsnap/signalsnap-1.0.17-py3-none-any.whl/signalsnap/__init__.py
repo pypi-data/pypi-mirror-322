7ad9e4a5808f7b110c7ec98423c83678b2cd731f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:28 +0000

    Add package signalsnap/1.0.17/signalsnap-1.0.17-py3-none-any.whl

---
+++ packages/signalsnap/signalsnap-1.0.17-py3-none-any.whl/signalsnap/__init__.py
@@ -0,0 +1,12 @@
+# This file is part of signalsnap: Signal Analysis In Python Made Easy
+# Copyright (c) 2020 and later, Markus Sifft and Daniel Hägele.
+#
+# This software is provided under the terms of the 3-Clause BSD License.
+# For details, see the LICENSE file in the root of this repository or
+# https://opensource.org/licenses/BSD-3-Clause
+
+from .spectrum_calculator import SpectrumCalculator, load_spec
+from .spectrum_config import SpectrumConfig
+from .plot_config import PlotConfig
+from .spectrum_plotter import SpectrumPlotter
+__version__ = "1.0.11"
