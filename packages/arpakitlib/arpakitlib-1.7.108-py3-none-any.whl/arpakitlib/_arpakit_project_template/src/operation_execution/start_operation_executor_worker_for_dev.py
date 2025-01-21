1f61435bc0f83d8ef1b37fa55d4bbffb97b10943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:36 +0000

    Add package arpakitlib/1.7.108/arpakitlib-1.7.108-py3-none-any.whl

---
+++ packages/arpakitlib/arpakitlib-1.7.108-py3-none-any.whl/arpakitlib/_arpakit_project_template/src/operation_execution/start_operation_executor_worker_for_dev.py
@@ -0,0 +1,18 @@
+from arpakitlib.ar_operation_execution_util import OperationExecutorWorker
+from src.core.util import setup_logging
+from src.db.util import get_cached_sqlalchemy_db
+from src.operation_execution.operation_executor import OperationExecutor
+
+
+def start_operation_executor_worker_for_dev():
+    setup_logging()
+    worker = OperationExecutorWorker(
+        sqlalchemy_db=get_cached_sqlalchemy_db(),
+        operation_executor=OperationExecutor(sqlalchemy_db=get_cached_sqlalchemy_db()),
+        filter_operation_types=None
+    )
+    worker.sync_safe_run()
+
+
+if __name__ == '__main__':
+    start_operation_executor_worker_for_dev()
