495492c7ee09526747ce1eca948749ae7cb0d8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:03:47 +0000

    Add package luminescent/0.4.14/luminescent-0.4.14.tar.gz

---
+++ packages/luminescent/luminescent-0.4.14.tar.gz/luminescent-0.4.14/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='luminescent',  # Your package name
+    version='0.4.14',  # Your package version
+    description='A description of your package',
+    author='Paul Shen',
+    author_email='pxshen@alumni.stanford.edu',
+    packages=find_packages(),  # Automatically find your package(s)
+    install_requires=[
+        "gdsfactory",
+        "pillow",
+        "pymeshfix",
+        "electromagneticpython",
+        "sortedcontainers",
+    ],
+)
+# python -m build
