ee6fa25784b51983f267750c424b05789522863d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:53 +0000

    Add package ingestify/0.2.0/ingestify-0.2.0.tar.gz

---
+++ packages/ingestify/ingestify-0.2.0.tar.gz/ingestify-0.2.0/ingestify/domain/models/dataset/file_repository.py
@@ -0,0 +1,50 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import BinaryIO
+
+from .dataset import Dataset
+
+
+class FileRepository(ABC):
+    def __init__(self, url: str):
+        self.base_dir = Path(url.split("://")[1])
+
+    @abstractmethod
+    def save_content(
+        self,
+        bucket: str,
+        dataset: Dataset,
+        revision_id: int,
+        filename: str,
+        stream: BinaryIO,
+    ) -> Path:
+        pass
+
+    @abstractmethod
+    def load_content(
+        self, bucket: str, dataset: Dataset, revision_id: int, filename: str
+    ) -> BinaryIO:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def supports(cls, url: str) -> bool:
+        pass
+
+    def get_path(
+        self, bucket: str, dataset: Dataset, revision_id: int, filename: str
+    ) -> Path:
+        path = (
+            self.base_dir
+            / bucket
+            / f"provider={dataset.provider}"
+            / f"dataset_type={dataset.dataset_type}"
+            / str(dataset.identifier)
+            / str(revision_id)
+            / filename
+        )
+        return path
+
+    def get_relative_path(self, path: Path) -> Path:
+        """Return the relative path to the base of the repository"""
+        return path.relative_to(self.base_dir)
