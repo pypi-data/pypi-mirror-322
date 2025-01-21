56d39e62d2ddfb06c9ffaaf969a340ab310c6507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 16:04:43 +0000

    Add package ddunigma/0.0.1/ddunigma-0.0.1.tar.gz

---
+++ packages/ddunigma/ddunigma-0.0.1.tar.gz/ddunigma-0.0.1/README.md
@@ -0,0 +1,4 @@
+## DDUNIGMA
+뜌땨어 고수가 될거야!
+
+by [i3l3](https://github.com/i3l3) & [Rnsn](https://github.com/gunu3371)
---
+++ packages/ddunigma/ddunigma-0.0.1.tar.gz/ddunigma-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="ddunigma",
+    version="0.0.1",
+    author="i3l3, rnsn3371",
+    author_email="gdhan2296@gmail.com",
+    description="An encoding algorithm based on Base64",
+    url="https://github.com/i3l3/ddunigma",
+    package_data={"": ['LICENSE', "README.md"]},
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+)
