dea355802cdd3be24f6a5fe6662673d8d4e76a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:27 +0000

    Add package mf-market-makers/0.1.2/mf_market_makers-0.1.2-cp39-abi3-win_amd64.whl

---
+++ packages/mf-market-makers/mf_market_makers-0.1.2-cp39-abi3-win_amd64.whl/quant/b3xml.pyi
@@ -0,0 +1,15 @@
+###############################################################################
+#
+# (C) Copyright 2020 Maikon Araujo
+#
+# This is an unpublished work containing confidential and proprietary
+# information of Maikon Araujo. Disclosure, use, or reproduction
+# without authorization of Maikon Araujo is prohibited.
+#
+###############################################################################
+
+def instruments_to_csv(fname: str, ftype: str, dest_file: str): ...
+def prices_to_csv(fname: str, ftype: str, dest_file: str): ...
+def prices_to_mssql(
+    fname: str, user: str, pwd: str, host: str, port: int, table: str
+): ...
