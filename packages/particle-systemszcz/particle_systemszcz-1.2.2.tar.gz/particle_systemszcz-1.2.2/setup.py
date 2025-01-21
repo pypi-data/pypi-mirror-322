f9b632d410700f95f3f26c6aaf4967e885e17930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:11 +0000

    Add package particle-systemszcz/1.2.2/particle_systemszcz-1.2.2.tar.gz

---
+++ packages/particle-systemszcz/particle_systemszcz-1.2.2.tar.gz/particle_systemszcz-1.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='particle_systemszcz',
+    version='1.2.2',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pygame-ce',
+    ],
+    author='rutra',
+    author_email='ruterszcz@gmail.com',
+    description='A particle system library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rutra8002/particle_system',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
