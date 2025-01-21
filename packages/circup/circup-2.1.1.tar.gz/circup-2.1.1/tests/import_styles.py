a0bc65c16b48677434e77356f4079c98998082aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:07 +0000

    Add package circup/2.1.1/circup-2.1.1.tar.gz

---
+++ packages/circup/circup-2.1.1.tar.gz/circup-2.1.1/tests/import_styles.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2021 Jeff Epler for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+# pylint: disable=all
+import adafruit_bus_device
+from adafruit_button import Button
+from adafruit_esp32spi import adafruit_esp32spi_socketpool
+import adafruit_hid.consumer_control
