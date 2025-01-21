8f558b19d55317c11dc8daf5f6dd7bcc60ae9023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:20 +0000

    Add package tensorg/0.1.4/tensorg-0.1.4-py3-none-any.whl

---
+++ packages/tensorg/tensorg-0.1.4-py3-none-any.whl/tensorgrad/__init__.py
@@ -0,0 +1,14 @@
+from .tensor import (
+    Tensor,
+    Function,
+    Zero,
+    Product,
+    Sum,
+    Variable,
+    Delta,
+    Ones,
+    Derivative,
+    _make_distinct,
+    function,  # noqa: F401
+)  # noqa: F401
+from .functions import frobenius2, kronecker, diag, sum, log, pow, trace  # noqa: F401
