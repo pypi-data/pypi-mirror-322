80eddf7a293cf8344f32e84f8359ff0435d89490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:04:47 +0000

    Add package zepben.protobuf/0.34.0/zepben.protobuf-0.34.0-py3-none-any.whl

---
+++ packages/zepben.protobuf/zepben.protobuf-0.34.0-py3-none-any.whl/zepben/protobuf/nc/nc_pb2.pyi
@@ -0,0 +1,8 @@
+from zepben.protobuf.nc import nc_requests_pb2 as _nc_requests_pb2
+from zepben.protobuf.nc import nc_responses_pb2 as _nc_responses_pb2
+from zepben.protobuf.metadata import metadata_requests_pb2 as _metadata_requests_pb2
+from zepben.protobuf.metadata import metadata_responses_pb2 as _metadata_responses_pb2
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
