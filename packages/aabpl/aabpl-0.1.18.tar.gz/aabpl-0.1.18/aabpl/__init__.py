5f3432dca3ff1928ead614bb4df29e8b8970a126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:03:57 +0000

    Add package aabpl/0.1.18/aabpl-0.1.18.tar.gz

---
+++ packages/aabpl/aabpl-0.1.18.tar.gz/aabpl-0.1.18/aabpl/__init__.py
@@ -0,0 +1,13 @@
+from importlib.metadata import version
+# TODO replace * with more expliced exports for the final version of the package 
+# # from .radius_search import *
+# import utils
+# import testing
+# import radius_search
+from . import main
+# from .main import *
+# from .radius_search.optimal_grid_spacing import *
+# from .radius_search.offset_regions import *
+# from .radius_search.radius_search_class import *
+
+__version__ = version('aabpl')
