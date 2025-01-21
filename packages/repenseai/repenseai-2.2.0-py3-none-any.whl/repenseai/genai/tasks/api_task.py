cdc7814100397b58239aacced8a75b5afbdfb247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:13 +0000

    Add package repenseai/2.2.0/repenseai-2.2.0-py3-none-any.whl

---
+++ packages/repenseai/repenseai-2.2.0-py3-none-any.whl/repenseai/genai/tasks/api_task.py
@@ -0,0 +1,140 @@
+from copy import deepcopy
+from typing import Any
+from repenseai.genai.tasks.base_task import BaseTask
+
+
+class Task(BaseTask):
+
+    def __init__(
+        self,
+        selector: Any,
+        simple_response: bool = False,
+        instruction: str = "",
+        prompt_template: str = "",
+        history: list | None = None,
+        vision_key: str = "image",
+        audio_key: str = "audio",
+        base_image_key: str = "base_image"
+    ) -> None:
+
+        self.instruction = instruction
+        self.prompt_template = prompt_template
+        self.history = history
+
+        self.selector = selector
+        self.simple_response = simple_response
+
+        self.vision_key = vision_key
+        self.audio_key = audio_key
+        self.base_image_key = base_image_key
+
+        self.prompt = None
+
+    def __build_prompt(self, **kwargs):
+
+        if self.prompt_template != "":
+            content = self.prompt_template.format(
+                instruction=self.instruction, **kwargs
+            )
+        else:
+            content = self.instruction
+
+        self.prompt = [
+            {
+                "role": "user", 
+                "content": [
+                    {
+                        "type": "text", 
+                        "text": content
+                    }
+                ]
+            }
+        ]
+        
+        if self.history:
+            self.prompt = self.history + self.prompt
+        
+        return self.prompt
+    
+    def __process_chat_or_search(self) -> dict:
+        api = self.selector.get_api()
+        prompt = deepcopy(self.prompt)
+
+        response = api.call_api(prompt)
+
+        final_response = {
+            "response": response,
+            "tokens": api.tokens,
+            "cost": self.selector.calculate_cost(api.tokens),
+        }
+
+        if self.selector.model_type == "search":
+            final_response["citations"] = api.response.json().get("citations", [])   
+
+        return final_response       
+
+    def __process_vision(self, context: dict) -> dict:
+        api = self.selector.get_api()
+        prompt = deepcopy(self.prompt)
+
+        image = context.get(self.vision_key)
+
+        response = api.call_api(prompt, image)
+
+        return {
+            "response": response,
+            "tokens": api.tokens,
+            "cost": self.selector.calculate_cost(api.tokens),
+        }
+    
+    def __process_audio(self, context: dict) -> dict:
+        api = self.selector.get_api()
+        audio = context.get(self.audio_key)
+
+        response = api.call_api(audio)
+
+        return {
+            "response": response,
+            "tokens": api.tokens,
+            "cost": self.selector.calculate_cost(api.tokens),
+        }
+    
+    def __process_image(self, context: dict) -> dict:
+        api = self.selector.get_api()
+        image = context.get(self.base_image_key)
+
+        instruction = self.prompt[-1]["content"][0]["text"]
+
+        response = api.call_api(instruction, image)
+
+        return {
+            "response": response,
+            "tokens": api.tokens,
+            "cost": self.selector.calculate_cost(api.tokens),
+        }
+
+    def _process_api_call(self, context: dict) -> dict:
+        match self.selector.model_type:
+            case "chat" | "search":
+                return self.__process_chat_or_search()
+            case "vision":
+                return self.__process_vision(context)
+            case "audio":
+                return self.__process_audio(context)
+            case "image":
+                return self.__process_image(context)                             
+    
+    def predict(self, context: dict) -> str:
+        try:
+            self.__build_prompt(**context)
+            response = self._process_api_call(context)
+
+            if self.simple_response:
+                return response["response"]
+            
+            return response
+
+        except Exception as e:
+            raise e
+        
+    
---
+++ packages/repenseai/repenseai-2.2.0-py3-none-any.whl/repenseai/secrets/aws.py
@@ -0,0 +1,60 @@
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+from repenseai.secrets.base import BaseSecrets
+
+from repenseai.utils.logs import logger
+
+
+class AWSSecrets(BaseSecrets):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(AWSSecrets, cls).__new__(cls)
+            cls._instance._initialized = False
+        return cls._instance
+
+    def __init__(self, secret_name: str, region_name: str, profile_name: str = None):
+        if not self._initialized:
+            self._secrets = {}
+
+            self.secret_name = secret_name
+            self.region_name = region_name
+
+            if profile_name:
+                session = boto3.Session(profile_name=profile_name)
+                self.client = session.client(
+                    service_name="secretsmanager",
+                    region_name=self.region_name
+                )
+            else:
+                self.client = boto3.client(
+                    service_name="secretsmanager",
+                    region_name=self.region_name
+                )
+
+            self._initialized = True
+
+    def get_secret(self, secret_key: str) -> str:
+        if self._secrets.get(secret_key):
+            return self._secrets.get(secret_key)
+
+        try:
+            response = self.client.get_secret_value(
+                SecretId=self.secret_name
+            )
+
+            secrets = json.loads(response["SecretString"])
+        except ClientError as e:
+            logger(f"Error getting secret: {e}")
+            return None
+
+        secret = secrets.get(secret_key)
+
+        if secret_key not in self._secrets:
+            self._secrets[secret_key] = secret
+
+        return secret
