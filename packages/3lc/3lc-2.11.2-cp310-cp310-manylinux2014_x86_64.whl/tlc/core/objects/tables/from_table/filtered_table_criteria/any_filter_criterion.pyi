ddf3036ab29fcf811f230b98247467fcd4c9516b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 06:06:42 +0000

    Add package 3lc/2.11.2/3lc-2.11.2-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/3lc/3lc-2.11.2-cp310-cp310-manylinux2014_x86_64.whl/tlc/core/objects/tables/from_table/filtered_table_criteria/any_filter_criterion.pyi
@@ -0,0 +1,15 @@
+from tlc.core.objects.tables.from_table.filtered_table_criteria.all_filter_criterion import CriteriaListFilterCriterion as CriteriaListFilterCriterion
+from tlc.core.objects.tables.from_table.filtered_table_criteria.filter_criterion import FilterCriterion as FilterCriterion
+from tlc.core.schema import Schema as Schema
+from typing import Any, Mapping
+
+class AnyFilterCriterion(CriteriaListFilterCriterion):
+    def __init__(self, filter_criteria: list[FilterCriterion] | None = None, init_parameters: Any = None) -> None:
+        """A predicate that is the logical or-combination of a list of filter criteria.
+
+        Equivalent to the python any() function.
+
+        :param filter_criteria: The list of input filter criteria.
+        """
+    @staticmethod
+    def from_any(any_value: FilterCriterion | Mapping) -> FilterCriterion: ...
