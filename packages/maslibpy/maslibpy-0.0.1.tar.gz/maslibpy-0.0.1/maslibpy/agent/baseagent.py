66b1a6ac0a7c2c87285cd2f5a3eed9c86c703a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:05:03 +0000

    Add package maslibpy/0.0.1/maslibpy-0.0.1.tar.gz

---
+++ packages/maslibpy/maslibpy-0.0.1.tar.gz/maslibpy-0.0.1/maslibpy/agent/baseagent.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from uuid import uuid4
+from maslibpy.llm.llm import LLM
+class BaseAgent(BaseModel):
+    id:str=str(uuid4())
+    name:str=""
+    role:str=""
+    goal:str=""
+    backstory:str=""
+    llm: LLM = None
+    class Config:
+        arbitrary_types_allowed = True
+        arbitrary_types_allowed = True
