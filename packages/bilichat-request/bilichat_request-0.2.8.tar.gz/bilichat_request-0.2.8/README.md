01ebd62d4fcd8c9f19b34d6cea94d118b37ea2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:04:56 +0000

    Add package bilichat-request/0.2.8/bilichat_request-0.2.8.tar.gz

---
+++ packages/bilichat-request/bilichat_request-0.2.8.tar.gz/bilichat_request-0.2.8/README.md
@@ -0,0 +1,23 @@
+# bilichat-request
+
+> api docs: https://apifox.com/apidoc/shared-4c1ba1cb-aa98-4a24-9986-193ab8f1519e/246937366e0
+
+> config: https://github.com/Well2333/bilichat-request/blob/main/src/bilichat_request/config.py
+
+> cookiecloud: https://github.com/easychen/CookieCloud/blob/master/README_cn.md
+>
+
+## 安装并运行
+
+直接使用 pip 或 pipx 安装即可，推荐使用 pipx 或类似的工具，以避免污染系统环境。
+
+```shell
+pip install pipx
+pipx install bilichat-request
+```
+
+安装完成后，可以直接使用 `bilirq` 命令启动。
+
+```shell
+bilirq
+```
---
+++ packages/bilichat-request/bilichat_request-0.2.8.tar.gz/bilichat_request-0.2.8/src/bilichat_request/model/config.py
@@ -0,0 +1,53 @@
+from typing import Literal
+
+from pydantic import BaseModel
+
+
+class CookieCloud(BaseModel):
+    url: str
+    """CookieCloud URL"""
+    uuid: str
+    """CookieCloud UUID"""
+    password: str
+    """CookieCloud 密码"""
+
+
+class Config(BaseModel):
+    log_level: Literal["TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "DEBUG"
+    """日志等级, nonebot2 运行时此设置无效"""
+    retry: int = 3
+    """请求重试次数"""
+    timeout: int = 20
+    """请求超时时间"""
+    data_path: str = "data"
+    """数据存储路径, nonebot2 运行时此设置无效"""
+    sentry_dsn: str = ""
+    """Sentry DSN"""
+    playwright_download_host: str = ""
+    """playwright 下载地址"""
+    playwright_headless: bool = True
+    """playwright 是否启用无头模式, 生产环境请勿关闭, 使用 nonebot_plugin_htmlrender 运行时此设置无效"""
+
+    # FastAPI config
+    api_host: str = "127.0.0.1"
+    """API 监听地址, nonebot2 运行时此设置无效"""
+    api_port: int = 40432
+    """API 监听端口, nonebot2 运行时此设置无效"""
+    api_path: str = "bilichatapi"
+    """API 路由前缀"""
+    api_access_token: str = ""
+    """API 访问令牌"""
+    api_sub_dynamic_limit: str = "720/hour"
+    """API 订阅动态限制, 参数可参考 https://limits.readthedocs.io/en/stable/quickstart.html#rate-limit-string-notation"""
+    api_sub_live_limit: str = "1800/hour"
+    """API 订阅直播限制, 参数可参考 https://limits.readthedocs.io/en/stable/quickstart.html#rate-limit-string-notation"""
+
+    # cookie cloud config
+    cookie_clouds: list[CookieCloud] = []
+    """CookieCloud 配置, 用于自动获取 cookie"""
+
+    # bilibili config
+    pc_user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:135.0) Gecko/20100101 Firefox/135.0"
+    """PC 端 User-Agent"""
+    mobile_user_agent: str = "Mozilla/5.0 (Android 15; Mobile; rv:135.0) Gecko/135.0 Firefox/135.0"
+    """移动端 User-Agent"""
