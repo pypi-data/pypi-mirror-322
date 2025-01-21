5fe58c0d0db16d7d5b15a53e3d8a91b443cb132e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:25 +0000

    Add package my-petstore-hive-api-client/1.0.0/my_petstore_hive_api_client-1.0.0.tar.gz

---
+++ packages/my-petstore-hive-api-client/my_petstore_hive_api_client-1.0.0.tar.gz/my_petstore_hive_api_client-1.0.0/my-petstore-hive-api-client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from my-petstore-hive-api-client.api.pet_api import PetApi
+from my-petstore-hive-api-client.api.store_api import StoreApi
+from my-petstore-hive-api-client.api.user_api import UserApi
+
