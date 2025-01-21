f3d21746af88342b84d117eefec849380dbb60ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:05:02 +0000

    Add package check-execution-time-of-program/1.0.1/check-execution-time-of-program-1.0.1.tar.gz

---
+++ packages/check-execution-time-of-program/check-execution-time-of-program-1.0.1.tar.gz/check-execution-time-of-program-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import path
+
+working_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_dir,'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+    
+setup(
+    name='check-execution-time-of-program',
+    version='1.0.1',
+    author='white_hat25',
+    author_email='devmarcel30@gmail.com',
+    description='A simple package to calculate the execution time of a program',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir = {"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[] 
+)
