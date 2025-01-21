01b6bda8aecf13afe25245ecfca2950e838c2525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:31 +0000

    Add package maib-ecommerce-sdk/1.0.0/maib_ecommerce_sdk-1.0.0-py3-none-any.whl

---
+++ packages/maib-ecommerce-sdk/maib_ecommerce_sdk-1.0.0-py3-none-any.whl/maib_ecommerce_sdk/__init__.py
@@ -0,0 +1,5 @@
+"""Python SDK for maib ecommerce API"""
+
+from .maibsdk import MaibSdk, MaibTokenException, MaibPaymentException
+from .maibauth import MaibAuthRequest, MaibAuth
+from .maibapi import MaibApiRequest, MaibApi
