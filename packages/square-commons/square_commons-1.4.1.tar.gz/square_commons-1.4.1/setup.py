c53c35bca8d4bb9a6b87b9fc2753cda155c0fe5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:21 +0000

    Add package square-commons/1.4.1/square_commons-1.4.1.tar.gz

---
+++ packages/square-commons/square_commons-1.4.1.tar.gz/square_commons-1.4.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = "square_commons"
+
+setup(
+    name=package_name,
+    version="1.4.1",
+    packages=find_packages(),
+    install_requires=[
+        "configparser>=6.0.0",
+        "requests>=2.32.3",
+    ],
+    extras_require={},
+    author="thePmSquare",
+    author_email="thepmsquare@gmail.com",
+    description="helper module containing common functions for all my python modules.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/thepmsquare/{package_name}",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
