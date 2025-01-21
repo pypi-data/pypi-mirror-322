e0132da73f22d0b75d866cf4391b8ded19215de3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:24 +0000

    Add package hassan_in_matrix/0.0.5/hassan_in_matrix-0.0.5-py3-none-any.whl

---
+++ packages/hassan_in_matrix/hassan_in_matrix-0.0.5-py3-none-any.whl/hassan_in_matrix/utils.py
@@ -0,0 +1,29 @@
+from .matrix import Matrix
+
+
+def input_matrix(square: bool = False) -> Matrix:
+    m = []
+    longestRow = 0
+
+    print(
+        "Enter a binary matrix using 0 and 1 without any delimiters, and press Enter for each row. Press another Enter when it's done."
+    )
+
+    while True:
+        rowInput = input()
+        if rowInput == "":
+            break
+        row = [bool(int(s)) for s in rowInput]
+        if len(row) > longestRow:
+            longestRow = len(row)
+        m.append(row)
+
+    if square:
+        # Make the matrix square by filling rows with zero
+        for row in m:
+            if len(row) < longestRow:
+                row += [False] * (longestRow - len(row))
+        while len(m) < longestRow:
+            m.append([False] * longestRow)
+
+    return Matrix(m)
