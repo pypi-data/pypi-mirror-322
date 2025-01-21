81a5a156fd6a80b78999eec0eadfb5100383a32a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:03:41 +0000

    Add package coziepy/0.0.18/coziepy-0.0.18.tar.gz

---
+++ packages/coziepy/coziepy-0.0.18.tar.gz/coziepy-0.0.18/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(packages=find_packages())
+
+# Build package:
+#   Increase version number in pyproject.toml
+#   delete dist folder, coziepy.egg-info folder
+#   python -m build
+
+# Upload package to pypi:
+#   python -m twine upload --repository testpypi dist/*
+#   python -m twine upload --repository pypi dist/*
+
+# username: __token__
+# password: [actual token from pypi website]
