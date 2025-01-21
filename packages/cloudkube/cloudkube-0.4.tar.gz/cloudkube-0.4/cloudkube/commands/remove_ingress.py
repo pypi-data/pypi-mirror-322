9c9c5092b3ccf585b12a0610dcf5b15d473b3935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:03 +0000

    Add package cloudkube/0.4/cloudkube-0.4.tar.gz

---
+++ packages/cloudkube/cloudkube-0.4.tar.gz/cloudkube-0.4/cloudkube/commands/remove_ingress.py
@@ -0,0 +1,17 @@
+from utils import delete_aws_nginx_controller
+import json
+import os
+
+
+def main():
+
+    with open("config.json", "r") as f:
+        config = json.load(f)
+
+    if config["ingressController"] == "aws-nginx-ingress-controller":
+        delete_aws_nginx_controller()
+
+    else:
+        print(
+            f'The ingress controller specified is {config["ingressController"]}. This is not yet supported by cloudkube.'
+        )
