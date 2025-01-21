8b555f977c006d1f4df2bd65e576dcb549fbfefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 06:06:39 +0000

    Add package petstore-client-test/1.0.0/petstore_client_test-1.0.0.tar.gz

---
+++ packages/petstore-client-test/petstore_client_test-1.0.0.tar.gz/petstore_client_test-1.0.0/petstore_client_test/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from petstore_client_test.api.pet_api import PetApi
+from petstore_client_test.api.store_api import StoreApi
+from petstore_client_test.api.user_api import UserApi
+
