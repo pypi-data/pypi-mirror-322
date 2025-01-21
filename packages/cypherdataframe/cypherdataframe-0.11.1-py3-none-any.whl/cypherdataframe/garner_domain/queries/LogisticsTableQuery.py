796744911208c75fab6798d5017e2ab588571758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:26 +0000

    Add package cypherdataframe/0.11.1/cypherdataframe-0.11.1-py3-none-any.whl

---
+++ packages/cypherdataframe/cypherdataframe-0.11.1-py3-none-any.whl/cypherdataframe/garner_domain/queries/LogisticsTableQuery.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass, field
+
+from cypherdataframe.garner_domain.BranchMaker import BranchMaker
+from cypherdataframe.garner_domain.properties_defaults import ID_PROP
+from cypherdataframe.model.LabelNode import LabelNode
+from cypherdataframe.model.Property import Property
+from cypherdataframe.model.Query import Query
+
+@dataclass
+class LogisticsTableQuery:
+    branchMakers: list[BranchMaker]
+    label: str
+    post_label: str | None = None
+    return_id: str = "l"
+    props: list[Property] = field(default_factory=lambda: [ID_PROP])
+    disable_scan: bool = False
+    filter_string: str | None = None
+    def to_query(self, skip: int | None = None, limit: int | None = None):
+        return Query(
+            LabelNode(
+                return_id=self.return_id,
+                label=self.label,
+                properties=self.props,
+                collect=False,
+                post_label_str=self.post_label
+            ),
+            branches=[b.to_branch() for b in self.branchMakers],
+            disable_scan=self.disable_scan,
+            skip=skip,
+            limit=limit,
+            filter_string=self.filter_string
+        )
+
+
+
