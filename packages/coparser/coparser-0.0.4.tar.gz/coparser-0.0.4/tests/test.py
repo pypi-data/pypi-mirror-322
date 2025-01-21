b3f66800e4e6afaef2519a4ae74fe1655fe94ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:04:55 +0000

    Add package coparser/0.0.4/coparser-0.0.4.tar.gz

---
+++ packages/coparser/coparser-0.0.4.tar.gz/coparser-0.0.4/tests/test.py
@@ -0,0 +1,32 @@
+from coparser.amazon_com import *
+def get_html(url):
+    import time
+    from playwright.sync_api import sync_playwright
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(url,wait_until='domcontentloaded')       
+        time.sleep(10)
+        page_source = page.content()
+        browser.close()
+        with open("debug.html", "w", encoding="utf-8") as file:
+            file.write(page_source)
+        return page_source
+
+if __name__ == '__main__':
+        import lxml.html
+        url='https://www.amazon.com/dp/B0D8GB6VRD/ref=sspa_dk_detail_2'
+        html=get_html(url)
+        tree = lxml.html.fromstring(html)
+        result={}
+        
+        result['SellPrice']=extract_SellPrice(tree)
+        result['ProductName']=extract_ProductName(tree)
+        result['TotalReview']=extract_TotalReview(tree)
+        result['Availability']=extract_Availability(tree)
+        result['ProductImage']=extract_ProductImage(tree)
+        result['AverageReview']=extract_AverageReview(tree)
+
+        print(result)
+
+
