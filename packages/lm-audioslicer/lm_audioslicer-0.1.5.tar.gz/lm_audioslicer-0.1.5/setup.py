cc27dc6364ec5730778b3ff000a0213831e74ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:05:45 +0000

    Add package lm-audioslicer/0.1.5/lm_audioslicer-0.1.5.tar.gz

---
+++ packages/lm-audioslicer/lm_audioslicer-0.1.5.tar.gz/lm_audioslicer-0.1.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+setup(
+    name="lm_audioslicer",
+    version="0.1.5",
+    packages=find_packages(),
+    install_requires=[
+        'librosa==0.9.1',
+        'soundfile>=0.12.1',
+        'numpy==1.23.5',
+    ],
+    author="Lars Monstad",
+    author_email="Lars.Monstad@gmail.com",
+    description="A tool for slicing audio files based on silence detection",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/LarsMonstad/audioslicer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'lm_audioslicer=audioslicer.slicer:main',
+        ],
+    }
+)
