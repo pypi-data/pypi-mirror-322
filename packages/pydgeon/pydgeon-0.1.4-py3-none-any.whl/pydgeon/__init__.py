2e1a5477cbe2dc7aa4311962d4dc916e930e096b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:07 +0000

    Add package pydgeon/0.1.4/pydgeon-0.1.4-py3-none-any.whl

---
+++ packages/pydgeon/pydgeon-0.1.4-py3-none-any.whl/pydgeon/__init__.py
@@ -0,0 +1,8 @@
+# pydgeon/__init__.py
+
+# Importing module1 so it's accessible at the package level
+from .main import coo, dovecot, hoot  # This imports everything from module1 (you can change this to more specific imports)
+
+# Package metadata
+__version__ = "v0.1.4"
+__author__ = "GeeseCanHonk"
