d94b7a48ecb18404a3d333667d37e8557e2e5fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:18 +0000

    Add package hal9/2.8.4/hal9-2.8.4.tar.gz

---
+++ packages/hal9/hal9-2.8.4.tar.gz/hal9-2.8.4/hal9/run.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+
+from hal9.runtimes.image import run as run_image
+from hal9.runtimes.python import run as run_python
+from hal9.runtimes.chainlit import run as run_chainlit
+from hal9.runtimes.r import run as run_r
+from hal9.runtimes.plumber import run as run_plumber
+
+runtime_types = {
+  "python": run_python,
+  "image": run_image,
+  "jpg": run_image,
+  "png": run_image,
+  "chainlit": run_chainlit,
+  "r": run_r,
+  "shiny": run_r,
+  "plumber": run_plumber,
+}
+
+def run(path :str, source :str = "app.py", runtime :str = "python", port :str = "8080") -> str:
+  """Run an application
+
+  Parameters
+  ----------
+  path : str 
+          Path to the application.
+  source : str 
+          The main file to run. Defaults to 'app.py'.
+  runtime : str 
+          The runtime to use to run the source. Defaults to 'python'.
+  """
+
+  source_path = Path(path) / source
+
+  if not source_path.is_file():
+    print(f"Failed to run {source_path}")
+    return
+
+  try:
+    if runtime in runtime_types:
+      runtime_types[runtime](source_path, port)
+    else:
+      print(f"Unsupported runtime: {runtime}")
+  except Exception as e:
+    print(f"An error occurred while running {source}: {e}")
+
+def run_describe():
+  keys = list(runtime_types.keys())
+  return {
+    'runtimes': keys
+  }
