ea7a515d9d515ed1e95a27723ca68d22cb909ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 16:04:48 +0000

    Add package eda-lucas/0.3.1/eda_lucas-0.3.1-py3-none-any.whl

---
+++ packages/eda-lucas/eda_lucas-0.3.1-py3-none-any.whl/eda_lucas/cli.py
@@ -0,0 +1,25 @@
+from president_speech.db.parquet_interpreter import read_parquet, get_parquet_full_path
+import pandas as pd
+import typer
+
+def group_by_count(keyword: str, asc: bool=False, rcnt: int=12):
+    # todo:ascending, 출력 rows size
+    # pytest 코드를 만들어보세요
+    # import this <- 해석해보세요
+    data_path = get_parquet_full_path()
+    df = pd.read_parquet(data_path)
+    fdf = df[df['speech_text'].str.contains(keyword, case=False)]
+    gdf = fdf.groupby("president").size().reset_index(name="count")
+    sdf= gdf.sort_values(by='count', ascending=asc).reset_index(drop=True)
+    rdf = sdf.head(rcnt)
+    return rdf
+
+def print_group_by_count(keyword: str, asc: bool=False, rcnt: int=12):
+    rdf = group_by_count(keyword, asc, rcnt)
+    print(rdf.to_string(index=False))
+
+
+def entry_point():
+    typer.run(print_group_by_count)
+
+
