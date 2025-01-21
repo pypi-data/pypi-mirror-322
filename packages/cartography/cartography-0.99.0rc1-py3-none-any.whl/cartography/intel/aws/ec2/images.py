37c974781e3da6d51d547f69fce8d2365cea6b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 06:06:56 +0000

    Add package cartography/0.99.0rc1/cartography-0.99.0rc1-py3-none-any.whl

---
+++ packages/cartography/cartography-0.99.0rc1-py3-none-any.whl/cartography/intel/aws/ec2/images.py
@@ -0,0 +1,121 @@
+import logging
+from typing import Any
+from typing import Dict
+from typing import List
+
+import boto3
+import neo4j
+from botocore.exceptions import ClientError
+
+from cartography.client.core.tx import load
+from cartography.graph.job import GraphJob
+from cartography.intel.aws.ec2 import get_ec2_regions
+from cartography.intel.aws.ec2.util import get_botocore_config
+from cartography.models.aws.ec2.images import EC2ImageSchema
+from cartography.util import aws_handle_regions
+from cartography.util import timeit
+
+logger = logging.getLogger(__name__)
+
+
+@timeit
+def get_images_in_use(neo4j_session: neo4j.Session, region: str, current_aws_account_id: str) -> List[str]:
+    get_images_query = """
+    MATCH (:AWSAccount{id: $AWS_ACCOUNT_ID})-[:RESOURCE]->(i:EC2Instance)
+    WHERE i.region = $Region
+    RETURN DISTINCT(i.imageid) as image
+    UNION
+    MATCH (:AWSAccount{id: $AWS_ACCOUNT_ID})-[:RESOURCE]->(lc:LaunchConfiguration)
+    WHERE lc.region = $Region
+    RETURN DISTINCT(lc.image_id) as image
+    UNION
+    MATCH (:AWSAccount{id: $AWS_ACCOUNT_ID})-[:RESOURCE]->(ltv:LaunchTemplateVersion)
+    WHERE ltv.region = $Region
+    RETURN DISTINCT(ltv.image_id) as image
+    """
+    results = neo4j_session.run(get_images_query, AWS_ACCOUNT_ID=current_aws_account_id, Region=region)
+    images = []
+    for r in results:
+        images.append(r['image'])
+    return images
+
+
+@timeit
+@aws_handle_regions
+def get_images(boto3_session: boto3.session.Session, region: str, image_ids: List[str]) -> List[Dict]:
+    client = boto3_session.client('ec2', region_name=region, config=get_botocore_config())
+    images = []
+    try:
+        self_images = client.describe_images(Owners=['self'])['Images']
+        images.extend(self_images)
+    except ClientError as e:
+        logger.warning(f"Failed to retrieve private images for region - {region}. Error - {e}")
+    try:
+        if image_ids:
+            image_ids = [image_id for image_id in image_ids if image_id is not None]
+            images_in_use = client.describe_images(ImageIds=image_ids)['Images']
+            # Ensure we're not adding duplicates
+            _ids = [image["ImageId"] for image in images]
+            for image in images_in_use:
+                if image["ImageId"] not in _ids:
+                    images.append(image)
+                    _ids.append(image["ImageId"])
+            # Handle cross region image ids
+            if len(_ids) != len(image_ids):
+                logger.info("Attempting to retrieve images from other regions")
+                pending_ids = [image_id for image_id in image_ids if image_id not in _ids]
+                all_regions = get_ec2_regions(boto3_session)
+                clients = {
+                    other_region: boto3_session.client('ec2', region_name=other_region, config=get_botocore_config())
+                    for other_region in all_regions if other_region != region
+                }
+                for other_region, client in clients.items():
+                    for _id in pending_ids:
+                        try:
+                            pending_image = client.describe_images(ImageIds=[_id])['Images']
+                            images.extend(pending_image)
+                        except ClientError as e:
+                            logger.warning(f"Image {id} could not be found at region - {other_region}. Error - {e}")
+    except ClientError as e:
+        logger.warning(f"Failed to retrieve public images for region - {region}. Error - {e}")
+    return images
+
+
+@timeit
+def load_images(
+        neo4j_session: neo4j.Session,
+        data: List[Dict[str, Any]],
+        region: str,
+        current_aws_account_id: str,
+        update_tag: int,
+) -> None:
+    # AMI IDs are unique to each AWS Region. Hence we make an 'ID' string that is a combo of ImageId and region
+    for image in data:
+        image['ID'] = image['ImageId'] + '|' + region
+    load(
+        neo4j_session,
+        EC2ImageSchema(),
+        data,
+        lastupdated=update_tag,
+        Region=region,
+        AWS_ID=current_aws_account_id,
+    )
+
+
+@timeit
+def cleanup_images(neo4j_session: neo4j.Session, common_job_parameters: Dict[str, Any]) -> None:
+    cleanup_job = GraphJob.from_node_schema(EC2ImageSchema(), common_job_parameters)
+    cleanup_job.run(neo4j_session)
+
+
+@timeit
+def sync_ec2_images(
+        neo4j_session: neo4j.Session, boto3_session: boto3.session.Session, regions: List[str],
+        current_aws_account_id: str, update_tag: int, common_job_parameters: Dict,
+) -> None:
+    for region in regions:
+        logger.info("Syncing images for region '%s' in account '%s'.", region, current_aws_account_id)
+        images_in_use = get_images_in_use(neo4j_session, region, current_aws_account_id)
+        data = get_images(boto3_session, region, images_in_use)
+        load_images(neo4j_session, data, region, current_aws_account_id, update_tag)
+    cleanup_images(neo4j_session, common_job_parameters)
