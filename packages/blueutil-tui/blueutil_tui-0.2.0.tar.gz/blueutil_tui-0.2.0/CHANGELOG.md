a2ef3f727d84dfb2fe425db98ab630e792dc6337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:04:39 +0000

    Add package blueutil-tui/0.2.0/blueutil_tui-0.2.0.tar.gz

---
+++ packages/blueutil-tui/blueutil_tui-0.2.0.tar.gz/blueutil_tui-0.2.0/CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## v0.2.0
+- Add search timer, continuous searching resets the timer
+- Fix bug that crashed app, when pressing `s` while already searching
+
+## v0.1.1
+- Fix thread error from search
+
+## v0.1.0
+- Initial published version
---
+++ packages/blueutil-tui/blueutil_tui-0.2.0.tar.gz/blueutil_tui-0.2.0/tests/test_utils.py
@@ -0,0 +1,13 @@
+from blueutil_tui.utils import check_blueutil_installation
+
+
+def test_check_blueutil_installation(capsys):
+    check_blueutil_installation()
+    captured = capsys.readouterr()
+    err_str = (
+        '"blueutil" was not found, please install with e.g. "brew install blueutil"\n'
+        + "or use another installation method from:\n"
+        + "https://github.com/toy/blueutil?tab=readme-ov-file#installupdateuninstall\n"
+    )
+
+    assert captured.err == err_str
