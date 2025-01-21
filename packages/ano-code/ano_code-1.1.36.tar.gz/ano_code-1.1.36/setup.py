7f76874e51e8145f5da49f1cd390dba054a4ce73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:03:55 +0000

    Add package ano-code/1.1.36/ano_code-1.1.36.tar.gz

---
+++ packages/ano-code/ano_code-1.1.36.tar.gz/ano_code-1.1.36/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+
+deps = '''
+yaspin==3.1.0
+click==8.1.7
+groq==0.11.0
+openai==1.52.2
+'''
+
+setup(
+    name="ano-code",
+    version="1.1.36",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=deps.split("\n"),
+    entry_points='''
+    [console_scripts]
+    ano-code=auto_code.cli:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+)
