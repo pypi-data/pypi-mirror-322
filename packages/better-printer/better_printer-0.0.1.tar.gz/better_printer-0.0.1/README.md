2431bb202c2585af214e6608482a3c02859042e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:45 +0000

    Add package better-printer/0.0.1/better_printer-0.0.1.tar.gz

---
+++ packages/better-printer/better_printer-0.0.1.tar.gz/better_printer-0.0.1/README.md
@@ -0,0 +1,26 @@
+# better_print
+
+you can use this package to print esaily.
+
+## Usage
+
+setup:
+```bash
+pip install better_print
+```
+
+```python
+better_print.__init__()
+```
+
+clear the console:
+
+```python
+better_print.clear_console()
+```
+
+clear the console and print the message:
+
+```python
+better_print.print_message("hello world")
+```
---
+++ packages/better-printer/better_printer-0.0.1.tar.gz/better_printer-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="better_printer",
+    version="0.0.1",
+    author="student_v",
+    author_email="13974164156@163.com",
+    description="you can use this module to print esasily",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://vtongxue.github.io",
+    project_urls={'Bug tracker': 'https://vtongxue.github.io'},
+    classifiers=['Development Status :: 3 - Alpha', 'Programming Language :: Python :: 3', 'License :: OSI Approved :: MIT License', 'Operating System :: OS Independent'],
+    packages=['better_print'],
+    python_requires=">=3.6",
+    install_requires = ['os'],
+    entry_points=""
+) 
