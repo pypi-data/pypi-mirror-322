f148009f44ccd95fb7e570156d39e12ea296e620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:34 +0000

    Add package python-killbill-client/0.3.4/python_killbill_client-0.3.4.tar.gz

---
+++ packages/python-killbill-client/python_killbill_client-0.3.4.tar.gz/python_killbill_client-0.3.4/src/killbill/exceptions.py
@@ -0,0 +1,25 @@
+class KillBillError(Exception):
+    """Raised when the Kill Bill API returns an error"""
+
+    def __init__(self, msg: object) -> None:
+        super().__init__(msg)
+
+
+class UnknownError(KillBillError):
+    def __init__(self, msg: object = "Unknown Error") -> None:
+        super().__init__(msg)
+
+
+class AuthError(Exception):
+    def __init__(self, msg: object = "Unauthorized") -> None:
+        super().__init__(msg)
+
+
+class NotFoundError(Exception):
+    def __init__(self, msg: object = "Source Not Found") -> None:
+        super().__init__(msg)
+
+
+class BadRequestError(Exception):
+    def __init__(self, msg: object = "Bad Request") -> None:
+        super().__init__(msg)
