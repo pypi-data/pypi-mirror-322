a2e4b72aef3da943999e5208b103618221cb1ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:04:18 +0000

    Add package unitway/1.0.0/unitway-1.0.0.tar.gz

---
+++ packages/unitway/unitway-1.0.0.tar.gz/unitway-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="unitway",                         
+    version="1.0.0",                         
+    packages=find_packages(),     
+    author="Azzam Alghamdi",
+    author_email="azzam.mo.alghamdi@gmail.com",
+    url="https://github.com/Azzam-Alghamdi/unitway",
+    python_requires=">=3.6",
+)
