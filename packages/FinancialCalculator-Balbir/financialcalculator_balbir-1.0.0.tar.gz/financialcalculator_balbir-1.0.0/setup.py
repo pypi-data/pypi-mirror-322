658e1a6442c7a24f22923a0729a829db8500d01a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:25 +0000

    Add package FinancialCalculator-Balbir/1.0.0/financialcalculator_balbir-1.0.0.tar.gz

---
+++ packages/FinancialCalculator-Balbir/financialcalculator_balbir-1.0.0.tar.gz/financialcalculator_balbir-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FinancialCalculator-Balbir',
+    version='1.0.0',
+    author='Balbir Singh Bhatia',
+    author_email='balbirs2204@gmail.com',
+    description='A simple financial calculator package for various financial calculations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'numpy', 
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.7',
+    keywords='finance calculator investing loans',
+)
