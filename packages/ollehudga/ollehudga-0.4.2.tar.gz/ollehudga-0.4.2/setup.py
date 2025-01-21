7facbf0111b5d90bcf739c8d4ead54ae62326e18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 06:06:52 +0000

    Add package ollehudga/0.4.2/ollehudga-0.4.2.tar.gz

---
+++ packages/ollehudga/ollehudga-0.4.2.tar.gz/ollehudga-0.4.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="ollehudga",
+    version="0.4.2",
+    packages=find_packages(),
+    install_requires=[
+        # add your dependencies
+    ],
+    entry_points={
+        "console_scripts":[
+            "ollehudga = ollehudga_hello:hello",
+        ],
+    },
+
+    long_description=description,
+    long_description_content_type="text/markdown"
+)
