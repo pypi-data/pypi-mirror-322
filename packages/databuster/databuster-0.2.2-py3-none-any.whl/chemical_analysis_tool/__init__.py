fab5689b98149da89677f62c873991ce027f2fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:35 +0000

    Add package databuster/0.2.2/databuster-0.2.2-py3-none-any.whl

---
+++ packages/databuster/databuster-0.2.2-py3-none-any.whl/chemical_analysis_tool/__init__.py
@@ -0,0 +1,9 @@
+"""
+DataBuster
+=========
+A sophisticated chemical compound analysis platform powered by Streamlit,
+designed to revolutionize drug discovery research and molecular data processing
+through advanced computational chemistry tools.
+"""
+
+__version__ = "0.2.2"
