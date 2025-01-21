b4a89a43d10e334c2da7aa9d33b6ee935307a0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:55 +0000

    Add package FiinQuant/0.10.6/FiinQuant-0.10.6-py3-none-any.whl

---
+++ packages/FiinQuant/FiinQuant-0.10.6-py3-none-any.whl/FiinQuant/SimilarChart.pyi
@@ -0,0 +1,8 @@
+
+class SimilarChart(object):
+    def __init__(self, access_token)->None:
+        ...
+    def plot(self, Ticker:str, t1:str, t2:str)->None:
+        ...
+        """Plot the similar chart of a Ticker."""
+
