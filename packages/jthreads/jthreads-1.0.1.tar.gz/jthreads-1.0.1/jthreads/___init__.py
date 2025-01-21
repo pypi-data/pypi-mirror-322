2a531603df35755296c06643f7feae915afe3081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:38 +0000

    Add package jthreads/1.0.1/jthreads-1.0.1.tar.gz

---
+++ packages/jthreads/jthreads-1.0.1.tar.gz/jthreads-1.0.1/jthreads/___init__.py
@@ -0,0 +1,11 @@
+from jthreads.timer import Timer, Interval
+from jthreads.loopingThread import LoopingThread, CallbackLoopingThread
+from jthreads.countdownTimer import CountdownTimer
+
+__all__ = [
+    "Timer",
+    "Interval",
+    "LoopingThread",
+    "CallbackLoopingThread",
+    "CountdownTimer",
+]
---
+++ packages/jthreads/jthreads-1.0.1.tar.gz/jthreads-1.0.1/jthreads/___init__.pyi
@@ -0,0 +1,5 @@
+from jthreads.countdownTimer import CountdownTimer as CountdownTimer
+from jthreads.loopingThread import CallbackLoopingThread as CallbackLoopingThread, LoopingThread as LoopingThread
+from jthreads.timer import Interval as Interval, Timer as Timer
+
+__all__ = ['Timer', 'Interval', 'LoopingThread', 'CallbackLoopingThread', 'CountdownTimer']
