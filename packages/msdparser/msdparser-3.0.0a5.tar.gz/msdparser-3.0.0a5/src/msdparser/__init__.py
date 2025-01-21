f1405bd8f70d0e3441dff6d7c66a621549b12fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:04:46 +0000

    Add package msdparser/3.0.0a5/msdparser-3.0.0a5.tar.gz

---
+++ packages/msdparser/msdparser-3.0.0a5.tar.gz/msdparser-3.0.0a5/src/msdparser/__init__.py
@@ -0,0 +1,10 @@
+"""
+This top-level module re-exports :func:`.parse_msd`, :class:`.MSDParameter`,
+and :class:`.MSDParserError` for convenience.
+"""
+
+__version__ = "3.0.0a5"
+__all__ = ["MSDParserError", "MSDParameter", "parse_msd"]
+
+from .parser import MSDParserError, parse_msd
+from .parameter import MSDParameter
