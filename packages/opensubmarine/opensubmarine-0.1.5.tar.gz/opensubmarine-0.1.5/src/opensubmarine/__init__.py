9649a4f2819cbb19514e83f987d88be1f58191c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:02 +0000

    Add package opensubmarine/0.1.5/opensubmarine-0.1.5.tar.gz

---
+++ packages/opensubmarine/opensubmarine-0.1.5.tar.gz/opensubmarine-0.1.5/src/opensubmarine/__init__.py
@@ -0,0 +1,12 @@
+from opensubmarine.contracts.access.Ownable.contract import Ownable
+#from opensubmarine.contracts.token.ARC200.src.contract import ARC200Token
+#from opensubmarine.contracts.token.ARC200.src.utils import (
+#    require_payment,
+#    close_offline_on_delete,
+#)
+
+__version__ = "0.1.5"
+
+__all__ = ["Ownable"]
+
+OpenSubmarine_version = __version__
