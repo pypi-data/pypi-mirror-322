4e04237f0f558b6054984130ae3ba327882701d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:02 +0000

    Add package cloudkube/0.3/cloudkube-0.3.tar.gz

---
+++ packages/cloudkube/cloudkube-0.3.tar.gz/cloudkube-0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudkube",
+    version="0.3",
+    author="Chan Ming Han",
+    email="chanminghan00@gmail.com",
+    description="minikube for the cloud",
+    python_requires=">=3.11",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "cloudkube=cloudkube:main",
+        ],
+    },
+    install_requires=[
+      "boto3",
+      "python_terraform",
+      "kubernetes",
+      "requests",
+      "GitPython"
+    ],
+)
