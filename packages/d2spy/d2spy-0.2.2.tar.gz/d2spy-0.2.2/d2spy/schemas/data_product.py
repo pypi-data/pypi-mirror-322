f92dce48afcb74aa24cf0e6469c5997dfe59e13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:07 +0000

    Add package d2spy/0.2.2/d2spy-0.2.2.tar.gz

---
+++ packages/d2spy/d2spy-0.2.2.tar.gz/d2spy-0.2.2/d2spy/schemas/data_product.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from dataclasses import dataclass
+from typing import Dict, Optional
+from uuid import UUID
+
+from d2spy.schemas.stac_properties import STACProperties
+
+
+@dataclass
+class DataProduct:
+    id: UUID
+    data_type: str
+    filepath: str
+    original_filename: str
+    is_active: bool
+    flight_id: UUID
+    deactivated_at: Optional[datetime]
+    public: bool
+    stac_properties: STACProperties
+    status: str
+    url: str
+
+    @classmethod
+    def from_dict(cls, data: Dict) -> "DataProduct":
+        return cls(
+            id=data["id"],
+            data_type=data["data_type"],
+            filepath=data["filepath"],
+            original_filename=data["original_filename"],
+            is_active=data["is_active"],
+            flight_id=data["flight_id"],
+            deactivated_at=data["deactivated_at"],
+            public=data["public"],
+            stac_properties=data["stac_properties"],
+            status=data["status"],
+            url=data["url"],
+        )
