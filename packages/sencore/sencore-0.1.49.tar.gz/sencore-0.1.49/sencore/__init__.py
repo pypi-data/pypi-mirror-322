19270b8c665e2f73fbdb9025117cd2a58ca9d3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:04 +0000

    Add package sencore/0.1.49/sencore-0.1.49.tar.gz

---
+++ packages/sencore/sencore-0.1.49.tar.gz/sencore-0.1.49/sencore/__init__.py
@@ -0,0 +1,35 @@
+"""``sencore`` Package
+
+.. topic:: Parse sentence into vocabularies
+
+  .. code:: python
+
+    from sencore import VocabParser
+
+    def vocab(lang, sen):
+      vp = VocabParser(lang)
+      vocabs = vp.digest(sen)
+      print(vocabs)
+
+.. topic:: Parse sentence into phrases
+
+  .. code:: python
+
+    from sencore import PhraseParser
+
+    def phrase(lang, sen):
+      pp = PhraseParser(lang)
+      phrases = pp.digest(sen)
+      print(phrases)
+
+"""
+
+__version__ = '0.1.49'
+
+#from .parser import Parser
+from .vocab_parser import VocabParser
+from .phrase_parser import PhraseParser
+from .phrase_model_parser import PhraseModelParser
+from .structure_parser import StructureParser
+from .kg_parser import KGParser
+from .deeplapi import DeepLAPI
