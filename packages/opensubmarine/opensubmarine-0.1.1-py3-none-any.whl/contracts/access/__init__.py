2ac5a13b8a08497d9ee928e1913407a145ae956c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 14:04:25 +0000

    Add package opensubmarine/0.1.1/opensubmarine-0.1.1-py3-none-any.whl

---
+++ packages/opensubmarine/opensubmarine-0.1.1-py3-none-any.whl/contracts/access/__init__.py
@@ -0,0 +1,11 @@
+"""
+OpenSubmarine - Access control contracts
+"""
+
+__version__ = "0.1.0"
+
+from .Ownable import Ownable, Ownable_version
+
+__all__ = ["Ownable", "Ownable_version"]
+
+Access_version = __version__
