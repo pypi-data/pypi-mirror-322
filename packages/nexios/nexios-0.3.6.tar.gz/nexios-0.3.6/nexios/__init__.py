aad19f9f3e89ac41b9b66ca45afb44412ae78edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:36 +0000

    Add package nexios/0.3.6/nexios-0.3.6.tar.gz

---
+++ packages/nexios/nexios-0.3.6.tar.gz/nexios-0.3.6/nexios/__init__.py
@@ -0,0 +1,28 @@
+from .application import NexioApp 
+from .sessions.middleware import SessionMiddleware
+from .middlewares.logging import ErrorHandlerMiddleware
+from .middlewares.common import CommonMiddleware
+from .config.base import MakeConfig
+from .config import set_config
+from .routing import Router
+from .middlewares.cors import CORSMiddleware
+import os
+
+def get_application(config = None) -> NexioApp:
+    set_config(config)
+
+    app = NexioApp(
+        middlewares= [
+            
+            ErrorHandlerMiddleware(),
+            CommonMiddleware(),           
+            CORSMiddleware(),
+            SessionMiddleware(),
+
+        ],
+        config=config
+    )
+    
+    
+    return app
+
