9496d085c17ae8b9dc0fba1457ba44e09d145bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:07 +0000

    Add package ezfit/0.2.8/ezfit-0.2.8-py3-none-any.whl

---
+++ packages/ezfit/ezfit-0.2.8-py3-none-any.whl/ezfit/__init__.py
@@ -0,0 +1,23 @@
+from ezfit.fit import FitAccessor, Model, Parameter
+from ezfit.functions import (
+    power_law,
+    exponential,
+    gaussian,
+    lorentzian,
+    pseudo_voigt,
+    linear,
+)
+
+__all__ = [
+    "Parameter",
+    "Model",
+    "FitAccessor",
+    "MultiPeakModel",
+    "PeakAccessor",
+    "power_law",
+    "exponential",
+    "gaussian",
+    "lorentzian",
+    "pseudo_voigt",
+    "linear",
+]
