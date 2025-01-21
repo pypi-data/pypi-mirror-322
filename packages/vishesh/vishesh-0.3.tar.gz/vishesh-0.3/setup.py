01cd32bb290d059ea664a68b284cec61992360c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:04:31 +0000

    Add package vishesh/0.3/vishesh-0.3.tar.gz

---
+++ packages/vishesh/vishesh-0.3.tar.gz/vishesh-0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vishesh',  # Package name you want to use on PyPI
+    version='0.3',  # Version number
+    description='A package to fetch and download programs from GitHub repository',
+    author='Vishesh Jain',
+    author_email='visheshj2005@gmail.com',  # Your email
+    url='https://github.com/visheshj2005/leetcode',  # Your GitHub repository URL
+    packages=find_packages(),  # This will automatically find the packages
+    entry_points={
+        'console_scripts': [
+            'vishesh = vishesh.main:main',  # Adjusted to the correct function path
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',  # Specifies Python version compatibility
+        'License :: OSI Approved :: MIT License',  # MIT License
+        'Operating System :: OS Independent',  # OS compatibility
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
+)
