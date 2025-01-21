47835fe3aac80499b1f434552e27c003c85d0927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 03:04:45 +0000

    Add package rb-commons/0.1.5/rb_commons-0.1.5-py3-none-any.whl

---
+++ packages/rb-commons/rb_commons-0.1.5-py3-none-any.whl/rb_commons/schemes/jwt.py
@@ -0,0 +1,14 @@
+import uuid
+from enum import Enum
+from pydantic import BaseModel, Field
+
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    CUSTOMER = "customer"
+    GUEST = "guest"
+
+class Claims(BaseModel):
+    user_id: str = Field(None, alias="x-user-id")
+    user_role: UserRole = Field(UserRole.GUEST, alias="x-user-role")
+    shop_id: uuid.UUID = Field(None, alias="x-shop-id")
