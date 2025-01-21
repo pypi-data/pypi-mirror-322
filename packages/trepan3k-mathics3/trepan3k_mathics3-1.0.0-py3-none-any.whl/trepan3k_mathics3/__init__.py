5eb0a7a40ae342be8c3a63a218fc7f1ee618555b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:48 +0000

    Add package trepan3k-mathics3/1.0.0/trepan3k_mathics3-1.0.0-py3-none-any.whl

---
+++ packages/trepan3k-mathics3/trepan3k_mathics3-1.0.0-py3-none-any.whl/trepan3k_mathics3/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2024 Rocky Bernstein <rocky@gnu.org>
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+trepan3k plugin which adds command `mathics3`.
+This adds Mathics3 debugging support from inside trepan3k.
+
+Authors: Rocky Bernstein rb@dustyfeet.com
+
+License: GPL3
+
+"""
+__docformat__ = "restructuredtext"
+from trepan3k_mathics3 import mathics3, mbacktrace, mdown, mup, printelement
+from trepan3k_mathics3.version import __version__
+
+mdown.DownCommand.__doc__ = mdown.DownCommand.__doc__.replace("down", "mdown")
+mup.UpCommand.__doc__ = mup.UpCommand.__doc__.replace("up", "mup")
+
+__all__ = ["__version__", "mathics3", "mbacktrace", "mdown", "mup", "printelement"]
