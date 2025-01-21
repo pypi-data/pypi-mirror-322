778eceba1fc474e1a8ea386aaf6a11d7982158fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:03:42 +0000

    Add package mltrainer/0.2b1/mltrainer-0.2b1-py3-none-any.whl

---
+++ packages/mltrainer/mltrainer-0.2b1-py3-none-any.whl/mltrainer/imagemodels.py
@@ -0,0 +1,135 @@
+from dataclasses import dataclass
+
+import gin
+import torch
+from torch import nn
+
+
+@gin.configurable
+class NeuralNetwork(nn.Module):
+    def __init__(self, num_classes: int, units1: int, units2: int) -> None:
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28 * 28, units1),
+            nn.ReLU(),
+            nn.Linear(units1, units2),
+            nn.ReLU(),
+            nn.Linear(units2, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+
+@gin.configurable
+class CNN(nn.Module):
+    def __init__(
+        self, num_classes: int, kernel_size: int, filter1: int, filter2: int
+    ) -> None:
+        super().__init__()
+
+        self.convolutions = nn.Sequential(
+            nn.Conv2d(1, filter1, kernel_size=kernel_size, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(filter1, filter2, kernel_size=kernel_size, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(filter2, 32, kernel_size=kernel_size, stride=1, padding=0),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.dense = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.convolutions(x)
+        logits = self.dense(x)
+        return logits
+
+
+@dataclass
+class CNNConfig:
+    matrixshape: tuple
+    batchsize: int
+    input_channels: int
+    hidden: int
+    kernel_size: int
+    maxpool: int
+    num_layers: int
+    num_classes: int
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels, out_channels, kernel_size=kernel_size, stride=1, padding=1
+            ),
+            nn.ReLU(),
+            nn.Conv2d(
+                out_channels, out_channels, kernel_size=kernel_size, stride=1, padding=1
+            ),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class CNNblocks(nn.Module):
+    def __init__(self, config: CNNConfig) -> None:
+        super().__init__()
+        input_channels = config.input_channels
+        kernel_size = config.kernel_size
+        hidden = config.hidden
+        # first convolution
+        self.convolutions = nn.ModuleList(
+            [
+                ConvBlock(input_channels, hidden, kernel_size),
+            ]
+        )
+
+        # additional convolutions
+        pool = config.maxpool
+        num_maxpools = 0
+        for i in range(config.num_layers):
+            self.convolutions.extend(
+                [ConvBlock(hidden, hidden, kernel_size), nn.ReLU()]
+            )
+            # every two layers, add a maxpool
+            if i % 2 == 0:
+                num_maxpools += 1
+                self.convolutions.append(nn.MaxPool2d(pool, pool))
+
+        # let's try to calculate the size of the linear layer
+        # please note that changing stride/padding will change the logic
+        matrix_size = (config.matrixshape[0] // (pool**num_maxpools)) * (
+            config.matrixshape[1] // (pool**num_maxpools)
+        )
+        print(f"Calculated matrix size: {matrix_size}")
+        print(f"Caluclated flatten size: {matrix_size * hidden}")
+
+        self.dense = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(matrix_size * hidden, hidden),
+            nn.ReLU(),
+            nn.Linear(hidden, config.num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for conv in self.convolutions:
+            x = conv(x)
+        x = self.dense(x)
+        return x
---
+++ packages/mltrainer/mltrainer-0.2b1-py3-none-any.whl/mltrainer/settings.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from enum import Enum
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional
+
+from loguru import logger
+from pydantic import BaseModel, ConfigDict, field_validator
+
+
+class FileTypes(Enum):
+    JPG = ".jpg"
+    PNG = ".png"
+    TXT = ".txt"
+    ZIP = ".zip"
+    TGZ = ".tgz"
+    TAR = ".tar.gz"
+    GZ = ".gz"
+
+
+class ReportTypes(Enum):
+    GIN = 1
+    TENSORBOARD = 2
+    MLFLOW = 3
+    RAY = 4
+
+
+class FormattedBase(BaseModel):
+    def __str__(self) -> str:
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
+
+    def __repr__(self) -> str:
+        return "\n".join(f"{k}: {v}" for k, v in self.__dict__.items())
+
+
+class TrainerSettings(FormattedBase):
+    epochs: int
+    metrics: List[Callable]
+    logdir: Path
+    train_steps: int
+    valid_steps: int
+    reporttypes: List[ReportTypes]
+    optimizer_kwargs: Dict[str, Any] = {"lr": 1e-3, "weight_decay": 1e-5}
+    scheduler_kwargs: Optional[Dict[str, Any]] = {"factor": 0.1, "patience": 10}
+    earlystop_kwargs: Optional[Dict[str, Any]] = {
+        "save": False,
+        "verbose": True,
+        "patience": 10,
+    }
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    @field_validator("logdir")
+    @classmethod
+    def check_path(cls, logdir: Path) -> Path:  # noqa: N805
+        if isinstance(logdir, str):  # type: ignore
+            logdir = Path(logdir)  # type: ignore
+        if not logdir.resolve().exists():  # type: ignore
+            logdir.mkdir(parents=True)
+            logger.info(f"Created logdir {logdir.absolute()}")
+        return logdir
