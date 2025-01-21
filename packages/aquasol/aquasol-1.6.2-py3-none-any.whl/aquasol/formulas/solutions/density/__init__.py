e7ade9484ed605b13adfe2fb53f4307b4b7c87bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:04:01 +0000

    Add package aquasol/1.6.2/aquasol-1.6.2-py3-none-any.whl

---
+++ packages/aquasol/aquasol-1.6.2-py3-none-any.whl/aquasol/formulas/solutions/density/__init__.py
@@ -0,0 +1,19 @@
+"""Density of solutions"""
+
+from .CaCl2 import Density_CaCl2_Formulas
+from .KCl import Density_KCl_Formulas
+from .KI import Density_KI_Formulas
+from .LiCl import Density_LiCl_Formulas
+from .MgCl2 import Density_MgCl2_Formulas
+from .Na2SO4 import Density_Na2SO4_Formulas
+from .NaCl import Density_NaCl_Formulas
+
+DensityFormulas = (
+    Density_CaCl2_Formulas +
+    Density_KCl_Formulas +
+    Density_KI_Formulas +
+    Density_LiCl_Formulas +
+    Density_MgCl2_Formulas +
+    Density_Na2SO4_Formulas +
+    Density_NaCl_Formulas
+)
