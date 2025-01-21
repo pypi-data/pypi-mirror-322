4899ece7aa80e815bb9b334af48b63aa4b93aa3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:44 +0000

    Add package package-creation-tutorial-borghel/0.1.0/package_creation_tutorial_borghel-0.1.0-py3-none-any.whl

---
+++ packages/package-creation-tutorial-borghel/package_creation_tutorial_borghel-0.1.0-py3-none-any.whl/package_creation_tutorial/__init__.py
@@ -0,0 +1,8 @@
+from .string_ops import reverse_string, count_vowels, capitalize_words
+<<<<<<< HEAD
+
+
+def hello():
+    return "Bonjour depuis le package_creation_tutorial !"
+=======
+>>>>>>> ca3c15f98b008f99e541177f49e21fe79e9ba3c5
