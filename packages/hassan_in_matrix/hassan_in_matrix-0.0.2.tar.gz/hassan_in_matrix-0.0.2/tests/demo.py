eef30b566acd596fb78283e3af0a3045e87b0787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 07:04:10 +0000

    Add package hassan_in_matrix/0.0.2/hassan_in_matrix-0.0.2.tar.gz

---
+++ packages/hassan_in_matrix/hassan_in_matrix-0.0.2.tar.gz/hassan_in_matrix-0.0.2/tests/demo.py
@@ -0,0 +1,90 @@
+from src.hassan_in_matrix.utils import input_matrix
+
+print("DM v1.0.0 by Hassan Heydari Nasab")
+print("License: WTFPL")
+print("Matrix is a class in matrix.py file and contains useful methods.")
+print("You can find the test cases in test.py or choose a demo:")
+
+while True:
+    print("1. multiply matrices")
+    print("2. Rn matrix")
+    print("3. join matrices")
+    print("4. meet matrices")
+    print("5. is reflexive")
+    print("6. is symmetric")
+    print("7. is antisymmetric")
+    print("8. is transitive")
+    print("9. is equivalent")
+    print("10. reflexive closure")
+    print("11. symmetric closure")
+    print("12. transitive closure")
+    print("13. transitive closure using Floyd-Warshall's algorithm")
+
+    demo = input("Enter a number [1-13]: ")
+
+    print("\n" * 200)
+
+    if demo == "1":
+        m1 = input_matrix()
+        m2 = input_matrix()
+        print("\nResult:")
+        print(m1 * m2)
+    elif demo == "2":
+        m = input_matrix()
+        p = int(input("Enter the power: "))
+        print("\nResult:")
+        print(m**p)
+    elif demo == "3":
+        m1 = input_matrix()
+        m2 = input_matrix()
+        print(m1)
+        print(m2)
+        print("\nResult:")
+        print(m1 | m2)
+    elif demo == "4":
+        m1 = input_matrix()
+        m2 = input_matrix()
+        print("\nResult:")
+        print(m1 & m2)
+    elif demo == "5":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.is_reflexive())
+    elif demo == "6":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.is_symmetric())
+    elif demo == "7":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.is_antisymmetric())
+    elif demo == "8":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.is_transitive())
+    elif demo == "9":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.is_equivalent())
+    elif demo == "10":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.reflexive_closure())
+    elif demo == "11":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.symmetric_closure())
+    elif demo == "12":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.transitive_closure())
+    elif demo == "13":
+        m = input_matrix()
+        print("\nResult:")
+        print(m.transitive_closure_using_warshall_alg())
+    else:
+        print("Are we done? Bye!")
+        exit(0)
+
+    input("\nDone! Press Enter to continue...")
+    print("\n" * 200)
---
+++ packages/hassan_in_matrix/hassan_in_matrix-0.0.2.tar.gz/hassan_in_matrix-0.0.2/tests/test.py
@@ -0,0 +1,135 @@
+from src.hassan_in_matrix.matrix import Matrix
+
+m1 = Matrix(
+    [
+        [False, False, False, False],
+        [False, True, False, False],
+        [False, False, True, False],
+        [False, False, False, True],
+    ]
+)
+m2 = Matrix(
+    [
+        [True, False, False, False],
+        [False, True, False, False],
+        [False, False, True, False],
+        [False, False, False, True],
+    ]
+)
+m3 = Matrix(
+    [
+        [False, True, False, False],
+        [True, True, False, False],
+        [False, False, True, False],
+        [False, False, False, True],
+    ]
+)
+m4 = Matrix(
+    [
+        [True, True, False, False],
+        [False, True, False, False],
+        [False, False, True, False],
+        [False, False, False, True],
+    ]
+)
+m5 = Matrix(
+    [
+        [False, True, False, False],
+        [False, False, True, False],
+        [True, False, False, False],
+        [False, False, False, True],
+    ]
+)
+m6 = Matrix(
+    [
+        [False, True, False, False],
+        [False, False, True, False],
+        [True, False, False, False],
+        [False, False, False, True],
+    ]
+)
+m7 = Matrix(
+    [
+        [False, False, True, False],
+        [True, False, False, False],
+        [False, False, False, True],
+    ]
+)
+
+assert not m1.is_reflexive()
+assert m1.reflexive_closure().is_reflexive()
+assert m1.is_symmetric()
+assert m1.is_antisymmetric()
+assert m1.is_transitive()
+assert m1 == m1
+assert m5 == m6
+assert m6 != m7
+assert m1 | m5 == Matrix(
+    [
+        [False, True, False, False],
+        [False, True, True, False],
+        [True, False, True, False],
+        [False, False, False, True],
+    ]
+)
+assert m1 & m5 == Matrix(
+    [
+        [False, False, False, False],
+        [False, False, False, False],
+        [False, False, False, False],
+        [False, False, False, True],
+    ]
+)
+assert (m1 * True) == m1
+assert m1 * m1 == m1
+assert m2 * m2 == m2
+assert m3 * m3 == Matrix(
+    [
+        [True, True, False, False],
+        [True, True, False, False],
+        [False, False, True, False],
+        [False, False, False, True],
+    ]
+)
+try:
+    result = m6 * m7
+    assert False
+except ArithmeticError:
+    assert True
+except Exception:
+    assert False
+
+assert m6**4 == m6 * m6 * m6 * m6
+
+assert m2.is_reflexive()
+assert m2.reflexive_closure() == m2
+assert m2.is_symmetric()
+assert m2.is_antisymmetric()
+assert m2.is_transitive()
+assert m2.is_equivalent()
+
+assert not m3.is_reflexive()
+assert m3.reflexive_closure().is_reflexive()
+assert m3.is_symmetric()
+assert not m3.is_antisymmetric()
+assert not m3.is_transitive()
+assert m3.transitive_closure().is_transitive()
+assert m3.transitive_closure_using_warshall_alg().is_transitive()
+
+assert m4.is_reflexive()
+assert m4.reflexive_closure() == m4
+assert not m4.is_symmetric()
+assert m4.symmetric_closure().is_symmetric()
+assert m4.is_antisymmetric()
+assert m4.is_transitive()
+
+assert not m5.is_reflexive()
+assert m5.reflexive_closure().is_reflexive()
+assert not m5.is_symmetric()
+assert m5.symmetric_closure().is_symmetric()
+assert m5.is_antisymmetric()
+assert not m5.is_transitive()
+assert m5.transitive_closure().is_transitive()
+assert m5.transitive_closure_using_warshall_alg().is_transitive()
+
+print("All tests passed!")
