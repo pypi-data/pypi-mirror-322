59e87df26592a0aa0f51e57cebde4a1ddfc49f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:46 +0000

    Add package metpx-sr3/3.0.57/metpx_sr3-3.0.57-py3-none-any.whl

---
+++ packages/metpx-sr3/metpx_sr3-3.0.57-py3-none-any.whl/sarracenia/flowcb/nodupe/name.py
@@ -0,0 +1,21 @@
+import logging
+from sarracenia.flowcb import FlowCB
+
+logger = logging.getLogger(__name__)
+
+
+class Name(FlowCB):
+    """
+      Override the the comparison so that files with the same name,
+      regardless of what directory they are in, are considered the same.
+      This is useful when receiving data from two different sources (two different trees)
+      and winnowing between them.
+    """
+    def after_accept(self, worklist):
+        for m in worklist.incoming:
+            if not 'nodupe_override' in m:
+                m['_deleteOnPost'] |= set(['nodupe_override'])
+                m['nodupe_override'] = {}
+
+            m['nodupe_override']['path'] = m['relPath'].split('/')[-1]
+            m['nodupe_override']['key'] = m['relPath'].split('/')[-1]
