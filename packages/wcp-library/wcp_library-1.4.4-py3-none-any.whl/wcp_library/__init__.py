339a2b55f54f02de331ad32d1cfa6da2103bcbf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:04:14 +0000

    Add package wcp-library/1.4.4/wcp_library-1.4.4-py3-none-any.whl

---
+++ packages/wcp-library/wcp_library-1.4.4-py3-none-any.whl/wcp_library/__init__.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from pathlib import Path
+
+# PyInstaller import
+import pip_system_certs.wrapt_requests
+import cryptography.hazmat.primitives.kdf.pbkdf2
+
+
+# Application Path
+if getattr(sys, 'frozen', False):
+    application_path = sys.executable
+    application_path = Path(application_path).parent
+else:
+    application_path = Path().absolute()
+
+
+def divide_chunks(list_obj: list, size: int) -> list:
+    """
+    Divide a list into chunks of size
+
+    :param list_obj:
+    :param size:
+    :return:
+    """
+
+    for i in range(0, len(list_obj), size):
+        yield list_obj[i:i + size]
