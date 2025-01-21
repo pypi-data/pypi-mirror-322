b1283cde8bcf5cb41eaeb7433607989c8e9a9118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 05:03:36 +0000

    Add package visvis/1.15.0/visvis-1.15.0-py2.py3-none-any.whl

---
+++ packages/visvis/visvis-1.15.0-py2.py3-none-any.whl/visvis/functions/movieWrite.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012, Almar Klein
+#
+# Visvis is distributed under the terms of the (new) BSD License.
+# The full license can be found in 'license.txt'.
+
+import visvis as vv
+
+# Try importing imageio
+iio = None
+
+try:
+    import imageio
+
+    if hasattr(imageio, "v2"):
+        iio = imageio.v2
+    else:
+        iio = imageio
+except ImportError:
+    pass
+
+
+def movieWrite(filename, images, *args, **kwargs):
+    """Proxy for imageio.mimwrite()"""
+
+    if iio is None:
+        raise RuntimeError("visvis.movieWrite requires the imageio package.")
+
+    return iio.mimwrite(filename, images, *args, **kwargs)
+
+
+if __name__ == "__main__":
+    ims = vv.movieRead("newtonscradle.gif")
+    vv.movieWrite("newtonscradle.swf", ims)
---
+++ packages/visvis/visvis-1.15.0-py2.py3-none-any.whl/visvis/vvio/__init__.py
@@ -0,0 +1,4 @@
+# ruff: noqa: F401
+
+import visvis.vvio.stl
+import visvis.vvio.wavefront
