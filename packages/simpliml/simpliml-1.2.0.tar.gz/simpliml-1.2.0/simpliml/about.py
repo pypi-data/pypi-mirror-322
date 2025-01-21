a5e07996942438d8d2a884738d01e406f1743ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 15:05:38 +0000

    Add package simpliml/1.2.0/simpliml-1.2.0.tar.gz

---
+++ packages/simpliml/simpliml-1.2.0.tar.gz/simpliml-1.2.0/simpliml/about.py
@@ -0,0 +1,17 @@
+import json
+
+version_json = '''{
+    "__date__": "2025-01-21 12:45:07.683490",
+    "__author__": "Dharmaraj D",
+    "__version__": "1.2.0",
+    "__email__": "rajaddr@gmail.com",
+    "__description__": "Machine Learning, Artificial Intelligence, Mathematics",
+    "__keywords__": "Supervised Learning, Neural Networks, Reinforcement Learning, Gradient Descent, TensorFlow, Probability Distributions, PCA, Convex Optimization, Transformers, Bayesian Inference, Markov Chains, GANs, SVD, Entropy, Attention Mechanism",
+    "__url__": "https://github.com/rajaddr/simpliml",    
+    "__license__": "MIT",
+    "__status__": "Planning",
+    "__doc__": "https://simpliml.readthedocs.io/"
+}'''
+
+def get_about():
+    return json.loads(version_json)
