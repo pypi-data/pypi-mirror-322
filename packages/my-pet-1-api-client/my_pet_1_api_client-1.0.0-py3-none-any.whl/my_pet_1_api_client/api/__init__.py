88eaea067be1380692891a1e8fe880322b168ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 16:05:05 +0000

    Add package my-pet-1-api-client/1.0.0/my_pet_1_api_client-1.0.0-py3-none-any.whl

---
+++ packages/my-pet-1-api-client/my_pet_1_api_client-1.0.0-py3-none-any.whl/my_pet_1_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from my_pet_1_api_client.api.pet_api import PetApi
+from my_pet_1_api_client.api.store_api import StoreApi
+from my_pet_1_api_client.api.user_api import UserApi
+
