09598f290917d7e9d615ac4cba48ee45ebff9523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 04:03:58 +0000

    Add package pywayne/1.0.1.1.4/pywayne-1.0.1.1.4-py3-none-any.whl

---
+++ packages/pywayne/pywayne-1.0.1.1.4-py3-none-any.whl/pywayne/adb/__init__.py
@@ -0,0 +1,15 @@
+# author: wangye(Wayne)
+# license: Apache Licence
+# file: __init__.py
+# time: 2023-11-07-11:00:41
+# contact: wang121ye@hotmail.com
+# site:  wangyendt@github.com
+# software: PyCharm
+# code is far away from bugs.
+
+
+from . import logcat_reader
+
+__all_ = [
+    'logcat_reader.py'
+]
