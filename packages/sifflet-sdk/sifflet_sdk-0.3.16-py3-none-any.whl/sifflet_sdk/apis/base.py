fe8f44d2d48a9d835349cd16c75fa408588cf8b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-21 16:05:06 +0000

    Add package sifflet-sdk/0.3.16/sifflet_sdk-0.3.16-py3-none-any.whl

---
+++ packages/sifflet-sdk/sifflet_sdk-0.3.16-py3-none-any.whl/sifflet_sdk/apis/base.py
@@ -0,0 +1,15 @@
+from sifflet_sdk import client
+from sifflet_sdk.config import SiffletConfig
+from sifflet_sdk.errors import config_needed_handler
+
+
+class BaseApi:
+    @config_needed_handler
+    def __init__(self, sifflet_config: SiffletConfig):
+        self.sifflet_config = sifflet_config
+        if sifflet_config.backend_url:
+            self.host = sifflet_config.backend_url.rstrip("/")
+        else:
+            self.host = f"https://{self.sifflet_config.tenant}api.siffletdata.com"
+        configuration = client.Configuration(host=self.host, access_token=self.sifflet_config.token)
+        self.api = client.ApiClient(configuration)
