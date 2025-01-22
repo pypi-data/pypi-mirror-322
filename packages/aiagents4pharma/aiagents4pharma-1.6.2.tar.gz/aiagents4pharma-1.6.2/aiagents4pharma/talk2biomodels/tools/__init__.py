40e17eba2831c2be2382dafcfe122f11dab80970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:07 +0000

    Add package aiagents4pharma/1.6.2/aiagents4pharma-1.6.2.tar.gz

---
+++ packages/aiagents4pharma/aiagents4pharma-1.6.2.tar.gz/aiagents4pharma-1.6.2/aiagents4pharma/talk2biomodels/tools/__init__.py
@@ -0,0 +1,10 @@
+'''
+This file is used to import all the modules in the package.
+'''
+# import everything from the module
+from . import ask_question
+from . import simulate_model
+from . import custom_plotter
+from . import fetch_parameters
+from . import model_description
+from . import search_models
