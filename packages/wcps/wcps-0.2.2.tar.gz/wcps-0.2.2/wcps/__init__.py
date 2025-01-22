e2fd30d69ae20de05c4e448b11c3c8ed1ef6bd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 03:10:13 +0000

    Add package wcps/0.2.2/wcps-0.2.2.tar.gz

---
+++ packages/wcps/wcps-0.2.2.tar.gz/wcps-0.2.2/wcps/__init__.py
@@ -0,0 +1,7 @@
+"""
+- :mod:`wcps.model` contains classes for dynamically building WCPS queries.
+- :module:`wcps.service` allows to execute queries on a WCPS server.
+"""
+__title__ = 'wcps'
+__author__ = 'rasdaman team'
+__version__ = "0.2.2"
