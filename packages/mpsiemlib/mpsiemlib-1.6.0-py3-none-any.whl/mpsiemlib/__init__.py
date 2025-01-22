bdad29e8610c732bb046992c6e5c00c3558a8b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 04:06:06 +0000

    Add package mpsiemlib/1.6.0/mpsiemlib-1.6.0-py3-none-any.whl

---
+++ packages/mpsiemlib/mpsiemlib-1.6.0-py3-none-any.whl/mpsiemlib/__init__.py
@@ -0,0 +1,11 @@
+from mpsiemlib.modules import MPSIEMWorker
+
+__title__ = 'mpsiemlib'
+__description__ = 'MaxPatrol SIEM API SDK'
+__url__ = 'https://github.com/GenRockeR/mpsiemlib'
+__version__ = '1.6.0'
+__author__ = 'nikolaiav'
+__maintainer__ = 'GenRockeR'
+__license__ = 'GPLv3'
+__copyright__ = '2024, Nikolai Arefiev, Gennadiy Mukhamedzyanov'
+
