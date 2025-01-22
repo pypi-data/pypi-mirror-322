9c7f586af98d1360fcb401d15c9f380f685b7a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 14:05:42 +0000

    Add package ai-marketplace-monitor/0.3.1/ai_marketplace_monitor-0.3.1-py3-none-any.whl

---
+++ packages/ai-marketplace-monitor/ai_marketplace_monitor-0.3.1-py3-none-any.whl/ai_marketplace_monitor/utils.py
@@ -0,0 +1,51 @@
+import hashlib
+import os
+from typing import Any, Dict, List
+
+from joblib import Memory  # type: ignore
+
+# home directory for all settings and caches
+amm_home = os.path.join(os.path.expanduser("~"), ".ai-marketplace-monitor")
+os.makedirs(amm_home, exist_ok=True)
+
+memory = Memory(location=amm_home, verbose=0)
+
+
+def calculate_file_hash(file_paths: List[str]) -> str:
+    """Calculate the SHA-256 hash of the file content."""
+    hasher = hashlib.sha256()
+    # they should exist, just to make sure
+    for file_path in file_paths:
+        if not os.path.exists(file_path):
+            raise FileNotFoundError(f"File not found: {file_path}")
+        #
+        with open(file_path, "rb") as file:
+            while chunk := file.read(8192):
+                hasher.update(chunk)
+    return hasher.hexdigest()
+
+
+def merge_dicts(dicts: list) -> dict:
+    """Merge a list of dictionaries into a single dictionary, including nested dictionaries.
+
+    :param dicts: A list of dictionaries to merge.
+    :return: A single merged dictionary.
+    """
+
+    def merge(d1: dict, d2: dict) -> dict:
+        for key, value in d2.items():
+            if key in d1:
+                if isinstance(d1[key], dict) and isinstance(value, dict):
+                    d1[key] = merge(d1[key], value)
+                elif isinstance(d1[key], list) and isinstance(value, list):
+                    d1[key].extend(value)
+                else:
+                    d1[key] = value
+            else:
+                d1[key] = value
+        return d1
+
+    result: Dict[str, Any] = {}
+    for dictionary in dicts:
+        result = merge(result, dictionary)
+    return result
