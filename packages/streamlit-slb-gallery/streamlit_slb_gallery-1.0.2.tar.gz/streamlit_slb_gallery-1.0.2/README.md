6e6780a7b5c317deb97016a0bedfa4c3acb24fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:07:01 +0000

    Add package streamlit-slb-gallery/1.0.2/streamlit_slb_gallery-1.0.2.tar.gz

---
+++ packages/streamlit-slb-gallery/streamlit_slb_gallery-1.0.2.tar.gz/streamlit_slb_gallery-1.0.2/README.md
@@ -0,0 +1,23 @@
+# streamlit_slb_gallery
+
+Streamlit component that allows user display images and video gallery
+
+## Installation instructions
+
+```sh
+pip install streamlit_slb_gallery
+```
+
+## Release Notes
+
+1.0.0
+
+- Initial Release
+
+  1.0.1
+
+- Fix Release Issue
+
+  1.0.2
+
+- Fix load card issue
---
+++ packages/streamlit-slb-gallery/streamlit_slb_gallery-1.0.2.tar.gz/streamlit_slb_gallery-1.0.2/streamlit_slb_gallery/example.py
@@ -0,0 +1,124 @@
+import os
+import streamlit as st
+
+from cognite.client import CogniteClient, ClientConfig
+from cognite.client.credentials import OAuthInteractive, OAuthClientCredentials
+from streamlit_slb_gallery import streamlit_slb_gallery
+
+def assign_auth(project_name):
+        
+    if project_name == "slb-test":        
+        tenant_id = os.environ.get("CDF_SLBTEST_TENANT_ID") 
+        client_id = os.environ.get("CDF_SLBTEST_CLIENT_ID") 
+        client_secret = os.environ.get("CDF_SLBTEST_CLIENT_SECRET")
+        cluster = os.environ.get("CDF_SLBTEST_CLUSTER")     
+    elif project_name == "petronas-pma-dev" or project_name == "petronas-pma-playground":
+        tenant_id = os.environ.get("CDF_PETRONASPMA_TENANT_ID") 
+        cluster = os.environ.get("CDF_PETRONASPMA_CLUSTER") 
+        client_id = os.environ.get("CDF_PETRONASPMA_CLIENT_ID") 
+        client_secret = ""
+    elif project_name == "hess-malaysia-dev":
+        tenant_id = os.environ.get("CDF_HESSDEV_TENANT_ID") 
+        client_id = os.environ.get("CDF_HESSDEV_CLIENT_ID") 
+        client_secret = os.environ.get("CDF_HESSDEV_CLIENT_SECRET") 
+        cluster = os.environ.get("CDF_HESSDEV_CLUSTER") 
+    elif project_name == "hess-malaysia-prod":
+        tenant_id = os.environ.get("CDF_HESSPROD_TENANT_ID") 
+        client_id = os.environ.get("CDF_HESSPROD_CLIENT_ID") 
+        client_secret = os.environ.get("CDF_HESSPROD_CLIENT_SECRET") 
+        cluster = os.environ.get("CDF_HESSPROD_CLUSTER")     
+    elif project_name == "mubadala-dev":
+        tenant_id = os.environ.get("CDF_MUBADALADEV_TENANT_ID") 
+        cluster = os.environ.get("CDF_MUBADALADEV_CLUSTER")
+        client_id = os.environ.get("CDF_MUBADALADEV_CLIENT_ID") 
+        client_secret = os.environ.get("CDF_MUBADALADEV_CLIENT_SECRET") 
+           
+    base_url = f"https://{cluster}.cognitedata.com"
+    scopes = [f"{base_url}/.default"]
+    
+    return {
+        "tenant_id": tenant_id, 
+        "client_id": client_id, 
+        "client_secret": client_secret, 
+        "cluster": cluster,
+        "base_url": base_url,
+        "project_name": project_name,
+        "scopes": scopes
+    }
+
+def interactive_client(project_name):
+    
+    auth_data: any = assign_auth(project_name)
+    
+    """Function to instantiate the CogniteClient, using the interactive auth flow"""
+    return CogniteClient(
+        ClientConfig(
+            client_name=auth_data['project_name'],
+            project=auth_data['project_name'],
+            base_url=auth_data['base_url'],
+            credentials=OAuthInteractive(
+                authority_url=f"https://login.microsoftonline.com/{auth_data['tenant_id']}",
+                client_id=auth_data['client_id'],
+                scopes=auth_data['scopes'],
+            ),
+        )
+    )
+
+def client_credentials(project_name):
+    
+    auth_data = assign_auth(project_name)
+
+    credentials = OAuthClientCredentials(
+        token_url=f"https://login.microsoftonline.com/{auth_data['tenant_id']}/oauth2/v2.0/token", 
+        client_id=auth_data['client_id'], 
+        client_secret= auth_data['client_secret'],
+        scopes=auth_data['scopes']
+    )
+
+    config = ClientConfig(
+        client_name=auth_data['project_name'],
+        project=auth_data['project_name'],
+        base_url=auth_data['base_url'],
+        credentials=credentials,
+    )
+    client = CogniteClient(config)
+
+    return client
+
+def connect(project_name):
+    auth = assign_auth(project_name=project_name)  
+    if auth["client_secret"] == "":
+        return interactive_client(project_name)
+    else:
+        return client_credentials(project_name)
+
+st.set_page_config(page_title="Streamlit Slb Gallery", layout='wide')
+st.subheader("Streamlit Slb Gallery")
+
+client: CogniteClient = connect("mubadala-dev")
+
+cognite_token = client.iam.token
+     
+def show_streamlit_slb_gallery():      
+    st.session_state.selected_data = streamlit_slb_gallery(
+        data={"height": 600, "items_per_page": 15, "event_start_time": 1719784800000, "event_end_time": 1719924007000, "event_type": "PPE_VIOLATION"},
+        token="eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IllUY2VPNUlKeXlxUjZqekRTNWlBYnBlNDJKdyIsImtpZCI6IllUY2VPNUlKeXlxUjZqekRTNWlBYnBlNDJKdyJ9.eyJhdWQiOiJodHRwczovL2F6LXNpbi1zcC0wMDEuY29nbml0ZWRhdGEuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvNmUzMDJmZTktMTE4Ni00MjgxLTlmYjMtOTQ0ZDdiYjgyOGNjLyIsImlhdCI6MTczNzQ3MTYwNiwibmJmIjoxNzM3NDcxNjA2LCJleHAiOjE3Mzc0NzU1MDYsImFpbyI6ImsyUmdZTGpLK1RDamVzMGl0U3NTUDc1TSsybGlBUUE9IiwiYXBwaWQiOiIzM2ZiY2NjYS0xZjEzLTQzMzktOWQ0Ni02NDE4MjJiYWRiZmUiLCJhcHBpZGFjciI6IjEiLCJncm91cHMiOlsiNDc5YTM2M2QtZGQ5Ny00ZTNjLTk5MjktMWQyOTljODk0ZmIxIiwiNGZhYzhhNWMtNjQzNC00MzQwLTgzMTQtNWRiOWQ0ZjdjNzBiIl0sImlkcCI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzZlMzAyZmU5LTExODYtNDI4MS05ZmIzLTk0NGQ3YmI4MjhjYy8iLCJvaWQiOiI1OGYzZjk5ZS1kZWUxLTQ4YmEtODYyMS00ZThkNzMzZmU4NzUiLCJyaCI6IjEuQWNZQTZTOHdib1lSZ1VLZnM1Uk5lN2dvekVMc1hQNlk0cWhPcWZkVmZLbTFVYTdHQUFER0FBLiIsInN1YiI6IjU4ZjNmOTllLWRlZTEtNDhiYS04NjIxLTRlOGQ3MzNmZTg3NSIsInRpZCI6IjZlMzAyZmU5LTExODYtNDI4MS05ZmIzLTk0NGQ3YmI4MjhjYyIsInV0aSI6ImdzVUtPTjRqSWtXRTl5MGpaN0oxQUEiLCJ2ZXIiOiIxLjAifQ.YrZJJk8nW0p6ze4cCl1e0OgPdHTXvhEcW-OKkTSBMP_eDrRblqrNvxgiuFfSm6LUR-9zjUbHj5Zmb03-Q6Owpti4BkCLRPgN6peOgEKWLr7zH8bs6t8D5m1QYLQvklWpYCRMrj-JufQR1F-IqFCGrNe2V-UWAHHd5pSnI1Di67FU6CYbWpozPxrh3APhJc7gdmo8QTh6kaUFXEl_Ve1wkYxJGJzb41jSFdjw96KgDP9HqyMksFwQm4We8ilIDd1I3YkmPMsXqs3Fkjgk1C7vBDEyHLuGQi9UyjonDBVF7PfJ9Cf1g7443p_vOnEyjTcTSY1w3CuNSrfq_AZijMN6yQ",
+        key="streamlit_slb_gallery"
+    )
+
+def show_main_content():
+    st.session_state.selected_deck = st.selectbox(label="Deck", options=["Main Deck", "Upper Deck"])
+    
+    if "selected_data" in st.session_state:
+        st.write(st.session_state.selected_data)
+
+main_content, gallery = st.columns([1,1])
+
+with main_content:
+    show_main_content()
+    
+with gallery:
+    show_streamlit_slb_gallery()
+
+
+
