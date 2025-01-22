c21bda5eb2a9cacc974120a6713b6c9b8b769b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:29 +0000

    Add package orc-package/0.0.0.4/orc_package-0.0.0.4-py3-none-any.whl

---
+++ packages/orc-package/orc_package-0.0.0.4-py3-none-any.whl/build/lib/astro_package/__init__.py
@@ -0,0 +1,16 @@
+"""
+This function calculates the factorial of a given number.
+
+Parameters:
+number (int): The number for which the factorial is to be calculated. Must be a non-negative integer.
+
+Returns:
+int: The factorial of the given number.
+
+Raises:
+ValueError: If the input number is negative.
+
+Example:
+>>> factorial(5)
+120
+"""
