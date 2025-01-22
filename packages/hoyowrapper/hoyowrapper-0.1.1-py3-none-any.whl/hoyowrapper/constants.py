486a2abf7d7eab9ada3fddd0561e81e8c941ce32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:29 +0000

    Add package hoyowrapper/0.1.1/hoyowrapper-0.1.1-py3-none-any.whl

---
+++ packages/hoyowrapper/hoyowrapper-0.1.1-py3-none-any.whl/hoyowrapper/constants.py
@@ -0,0 +1,22 @@
+_constant_info = {
+    "genshin" : {
+        "title" : "Genshin Impact",
+        "api_base" : "https://sg-hk4e-api.hoyolab.com/event/sol",
+        "act_id" : "e202102251931481",
+        "url" : "https://act.hoyolab.com/ys/event/signin-sea-v3/index.html?act_id=e202102251931481",
+        "chronicle" : "https://sg-public-api.hoyolab.com/event/game_record/genshin/api"
+    },
+    "starrail" : {
+        "title" : "Honkai Star Rail",
+        "api_base" : "https://sg-public-api.hoyolab.com/event/luna/os",
+        "act_id" : "e202303301540311",
+        "url" : "https://act.hoyolab.com/bbs/event/signin/hkrpg/e202303301540311.html?act_id=e202303301540311",
+        "chronicle" : "https://sg-public-api.hoyolab.com/event/game_record/hkrpg/api"
+    },
+    "hi3" : {
+        "title" : "Honkai Impact 3rd",
+        "api_base" : "https://sg-public-api.hoyolab.com/event/mani",
+        "act_id" : "e202110291205111",
+        "url" : "https://act.hoyolab.com/bbs/event/signin-bh3/index.html?act_id=e202110291205111"
+    }
+}
