e9c4db7528ecbbea0265f58f7c9353f32a1adbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:04:55 +0000

    Add package pdf-table-processor/0.1.1/pdf_table_processor-0.1.1.tar.gz

---
+++ packages/pdf-table-processor/pdf_table_processor-0.1.1.tar.gz/pdf_table_processor-0.1.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+des = """
+PDF-to-CSV-JSON AI Web Application Effortlessly extract structured complex data tables from your PDF documents and convert it into clean, ready-to-use CSV or JSON files. Powered by cutting-edge AI and optimized for real-world use cases, this application simplifies data extraction like never before.
+
+🚀 Features AI-Driven Extraction: Intelligent algorithms ensure accurate data parsing from PDFs, even for complex layouts. User-Friendly Interface: Upload your PDF, specify the target pages, and receive a downloadable CSV-JSON in seconds. Advanced Error Handling: Ensures clean outputs with minimal data loss. Powerful Integrations: Built with support for Hugging Face models, Camelot library, and vector databases. 🔧 Tech Stack Backend: Python (FastAPI), Hugging Face Transformers Frontend: React.js PDF Processing: gmft, PyPDF2 Database: PostgreSQL AI Features: Custom NER and summarization models
+
+Use Cases This application is tailored to handle real-world scenarios where accurate and efficient PDF data extraction is critical. Here are some specific use cases:
+
+Complex Table Extraction Extract intricate and multi-layered tables from PDFs with precision. Handle nested headers, merged cells, and irregular column structures seamlessly. Ideal for financial reports, research papers, and government data with complex tabular layouts.
+RAG (Retrieval-Augmented Generation) Integration Transform extracted data into structured formats for use in AI workflows. Use the generated CSVs to power retrieval-based systems like chatbots, summarization tools, or decision-support applications. Streamline document-based knowledge retrieval pipelines.
+Large-Scale Data Processing Process bulk PDF uploads for large-scale data extraction needs. Generate clean CSV outputs for integration into analytics platforms or machine learning pipelines.
+Business Intelligence Enable data teams to quickly extract actionable insights from complex PDF reports. Use the structured CSV outputs for dashboards, reports, and predictive modeling.
+Automation in Operations Automate repetitive data extraction tasks to save time and reduce manual errors. Useful for industries such as logistics (invoice processing), healthcare (patient reports), and legal (contract analysis).
+Compliance and Audit Extract key financial or operational metrics for regulatory compliance and audits. Ensure data integrity by minimizing human intervention during the extraction process.
+Document Summarization and Analysis Leverage the extracted data to build summaries or generate insights using advanced AI models. Suitable for simplifying decision-making processes in research or business contexts."""
+
+
+setup(
+    name="pdf_table_processor",  # Paket adı
+    author='Çağatay',
+    description= des,
+    author_email='cgtyklnc@gmail.com',
+    url='https://github.com/klncgty/Pdf_Table_Extractor',
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "pandas",
+        "pandasai",
+        "gmft",
+        "matplotlib",
+        "pillow",
+        "PyPDF2",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pdf-table-processor = api.main:main',  # Komut satırı aracı
+        ],
+    },
+)
