e4566e8f7c8ca517bd6f17d01809841903f92e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:14 +0000

    Add package pyphot/1.6.0/pyphot-1.6.0.tar.gz

---
+++ packages/pyphot/pyphot-1.6.0.tar.gz/pyphot-1.6.0/pyphot/__init__.py
@@ -0,0 +1,14 @@
+from .phot import (Library, Ascii_Library, HDF_Library, Filter)
+from .licks import (LickLibrary, LickIndex, reduce_resolution)
+
+from .sandbox import (UnitLibrary, UnitAscii_Library, UnitHDF_Library,
+                      UnitFilter, UnitLickIndex, UnitLickLibrary)
+
+from .sandbox import get_library
+from .ezunits import unit
+from .helpers import (STmag_from_flux, STmag_to_flux, extractPhotometry,
+                      extractSEDs, fluxErrTomag, fluxToMag, magErrToFlux,
+                      magToFlux)
+from .vega import Vega
+from .sun import Sun
+from .version import __VERSION__
