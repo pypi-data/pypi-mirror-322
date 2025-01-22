6cee8619cf481e0841731c6e8f5a3637f81e704b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:31 +0000

    Add package pyjzq/0.1/pyjzq-0.1.tar.gz

---
+++ packages/pyjzq/pyjzq-0.1.tar.gz/pyjzq-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyjzq',
+    version='0.1',
+    author='tianhukj',
+    author_email='tianhukj@outlook.com',
+    description='A command-line tic-tac-toe game',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tianhukj/pyjzq',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # 添加依赖包，如有需要
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyjzq=pyjzq.game:main',
+        ],
+    },
+)
