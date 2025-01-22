3f836baacb49e632100edf010db9c78a19fc2ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:03 +0000

    Add package sztpd/0.0.29/sztpd-0.0.29.tar.gz

---
+++ packages/sztpd/sztpd-0.0.29.tar.gz/sztpd-0.0.29/src/sztpd/yl.py
@@ -0,0 +1,8 @@
+import json,importlib.resources as importlib_resources
+def sztpd_native_yang_library():
+	A=importlib_resources.files('sztpd')/'yang'/'yang-library-nbi.json'
+	with open(A,'r',encoding='utf-8')as B:return json.load(B)
+def sztpd_rfc8572_yang_library():
+	A=importlib_resources.files('sztpd')/'yang'/'yang-library-sbi.json'
+	with open(A,'r',encoding='utf-8')as B:return json.load(B)# Copyright (c) 2020-2025 Watsen Networks. All Rights Reserved.
+
