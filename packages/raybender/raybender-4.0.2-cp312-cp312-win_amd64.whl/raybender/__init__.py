e30fbca9a7a658add5bc0b3853e5f79731a796e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 14:05:11 +0000

    Add package raybender/4.0.2/raybender-4.0.2-cp312-cp312-win_amd64.whl

---
+++ packages/raybender/raybender-4.0.2-cp312-cp312-win_amd64.whl/raybender/__init__.py
@@ -0,0 +1,15 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_10_0():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'raybender.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_10_0()
+del _delvewheel_patch_1_10_0
+# end delvewheel patch
+
+from .scene import EmbreeScene
+from . import _raybender as raw
+
+__all__ = ["EmbreeScene", "raw"]
