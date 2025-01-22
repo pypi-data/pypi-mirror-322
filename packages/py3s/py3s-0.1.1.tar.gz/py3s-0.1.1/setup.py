07fb3008c00a093686068001348c9870397f239c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:16 +0000

    Add package py3s/0.1.1/py3s-0.1.1.tar.gz

---
+++ packages/py3s/py3s-0.1.1.tar.gz/py3s-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py3s',
+    version='0.1.1',
+    packages=['py3s'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+    long_description=open('py3s/README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dwdwow/py3s',
+)
