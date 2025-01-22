8ed618f22204f9f9a12e550dacd371fd1b63f7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:23 +0000

    Add package pylwr/1.0.20/pylwr-1.0.20.tar.gz

---
+++ packages/pylwr/pylwr-1.0.20.tar.gz/pylwr-1.0.20/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pylwr",
+    version="1.0.20",
+    author="linwr",
+    author_email="953297255@qq.com",
+    description="各种包使用的二次封装",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/linwanrui/pylwr",
+    license="GPL-3.0-only",
+    license_files=['LICENSE'],  # 使用正确的字段
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.10",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
