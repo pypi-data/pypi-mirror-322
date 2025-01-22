c0d3d98ff82a7b97e72820a18219ca1fd309e1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:05:41 +0000

    Add package edc-notification/1.0.0/edc_notification-1.0.0-py3-none-any.whl

---
+++ packages/edc-notification/edc_notification-1.0.0-py3-none-any.whl/edc_notification/post_migrate_signals.py
@@ -0,0 +1,10 @@
+from django.core.management.color import color_style
+
+style = color_style()
+
+
+def post_migrate_update_notifications(sender=None, **kwargs):
+    from .site_notifications import site_notifications
+
+    site_notifications.update_notification_list(verbose=True)
+    site_notifications.create_mailing_lists(verbose=True)
