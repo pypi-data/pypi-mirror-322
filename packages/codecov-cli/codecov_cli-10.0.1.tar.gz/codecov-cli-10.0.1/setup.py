e66dc9470852cbfd0df30c35c82dfa512630ffb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:30 +0000

    Add package codecov-cli/10.0.1/codecov_cli-10.0.1.tar.gz

---
+++ packages/codecov-cli/codecov_cli-10.0.1.tar.gz/codecov-cli-10.0.1/setup.py
@@ -0,0 +1,59 @@
+from os import path
+from platform import system
+
+from setuptools import Extension, find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="codecov-cli",
+    version="10.0.1",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    description="Codecov Command Line Interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Codecov",
+    author_email="support@codecov.io",
+    install_requires=[
+        "click==8.*",
+        "httpx==0.27.*",
+        "ijson==3.*",
+        "pyyaml==6.*",
+        "responses==0.21.*",
+        "tree-sitter==0.20.*",
+        "test-results-parser==0.5.*",
+        "regex",
+        "sentry-sdk>=2.20.0",
+        "wrapt>=1.17.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "codecovcli = codecov_cli.main:run",
+        ],
+    },
+    python_requires=">=3.9",
+    ext_modules=[
+        Extension(
+            "staticcodecov_languages",
+            [
+                "languages/languages.c",
+                "languages/treesitterpython/src/parser.c",
+                "languages/treesitterjavascript/src/parser.c",
+                "languages/treesitterpython/src/scanner.cc",
+                "languages/treesitterjavascript/src/scanner.c",
+            ],
+            include_dirs=[
+                "languages/treesitterpython/src",
+                "languages/treesitterjavascript/src",
+                "languages/treesitterjavascript/src/tree_sitter",
+                "languages/treesitterpython/src/tree_sitter",
+            ],
+            extra_compile_args=(
+                ["-Wno-unused-variable"] if system() != "Windows" else None
+            ),
+        )
+    ],
+)
