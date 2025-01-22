daca3a2a725bd555160b99a898ddc8936045ee3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 03:07:14 +0000

    Add package pet-alpha-api-client/1.0.0/pet_alpha_api_client-1.0.0-py3-none-any.whl

---
+++ packages/pet-alpha-api-client/pet_alpha_api_client-1.0.0-py3-none-any.whl/pet_alpha_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from pet_alpha_api_client.api.pet_api import PetApi
+from pet_alpha_api_client.api.store_api import StoreApi
+from pet_alpha_api_client.api.user_api import UserApi
+
