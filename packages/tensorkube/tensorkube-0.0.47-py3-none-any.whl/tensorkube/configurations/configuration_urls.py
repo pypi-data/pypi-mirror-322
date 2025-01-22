eabad35f41eeabf1c705812ba65ded664540037f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 04:07:04 +0000

    Add package tensorkube/0.0.47/tensorkube-0.0.47-py3-none-any.whl

---
+++ packages/tensorkube/tensorkube-0.0.47-py3-none-any.whl/tensorkube/configurations/configuration_urls.py
@@ -0,0 +1,5 @@
+DOMAIN_SERVER_URL = 'https://github.com/knative/serving/releases/download/knative-v1.14.0/serving-default-domain.yaml'
+KNATIVE_ISTIO_CONTROLLER_URL = 'https://github.com/knative/net-istio/releases/latest/download/release.yaml'
+KNATIVE_CRD_URL = 'https://github.com/knative/serving/releases/latest/download/serving-crds.yaml'
+KNATIVE_CORE_URL = 'https://github.com/knative/serving/releases/latest/download/serving-core.yaml'
+
