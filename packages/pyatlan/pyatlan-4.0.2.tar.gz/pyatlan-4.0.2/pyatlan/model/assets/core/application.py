741e6f5db7dc2018d2a060b5ba105e92ccb9e571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:19 +0000

    Add package pyatlan/4.0.2/pyatlan-4.0.2.tar.gz

---
+++ packages/pyatlan/pyatlan-4.0.2.tar.gz/pyatlan-4.0.2/pyatlan/model/assets/core/application.py
@@ -0,0 +1,111 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2022 Atlan Pte. Ltd.
+
+
+from __future__ import annotations
+
+from typing import ClassVar, List, Optional
+
+from pydantic.v1 import Field, validator
+
+from pyatlan.model.enums import AtlanConnectorType
+from pyatlan.model.fields.atlan_fields import RelationField
+from pyatlan.utils import init_guid, validate_required_fields
+
+from .app import App
+
+
+class Application(App):
+    """Description"""
+
+    @classmethod
+    @init_guid
+    def creator(
+        cls,
+        *,
+        name: str,
+        connection_qualified_name: str,
+    ) -> Application:
+        validate_required_fields(
+            ["name", "connection_qualified_name"], [name, connection_qualified_name]
+        )
+        attributes = Application.Attributes.creator(
+            name=name,
+            connection_qualified_name=connection_qualified_name,
+        )
+        return cls(attributes=attributes)
+
+    type_name: str = Field(default="Application", allow_mutation=False)
+
+    @validator("type_name")
+    def validate_type_name(cls, v):
+        if v != "Application":
+            raise ValueError("must be Application")
+        return v
+
+    def __setattr__(self, name, value):
+        if name in Application._convenience_properties:
+            return object.__setattr__(self, name, value)
+        super().__setattr__(name, value)
+
+    APPLICATION_OWNED_ASSETS: ClassVar[RelationField] = RelationField(
+        "applicationOwnedAssets"
+    )
+    """
+    TBC
+    """
+
+    _convenience_properties: ClassVar[List[str]] = [
+        "application_owned_assets",
+    ]
+
+    @property
+    def application_owned_assets(self) -> Optional[List[Asset]]:
+        return (
+            None
+            if self.attributes is None
+            else self.attributes.application_owned_assets
+        )
+
+    @application_owned_assets.setter
+    def application_owned_assets(self, application_owned_assets: Optional[List[Asset]]):
+        if self.attributes is None:
+            self.attributes = self.Attributes()
+        self.attributes.application_owned_assets = application_owned_assets
+
+    class Attributes(App.Attributes):
+        application_owned_assets: Optional[List[Asset]] = Field(
+            default=None, description=""
+        )  # relationship
+
+        @classmethod
+        @init_guid
+        def creator(
+            cls,
+            *,
+            name: str,
+            connection_qualified_name: str,
+        ) -> Application.Attributes:
+            validate_required_fields(
+                ["name", "connection_qualified_name"], [name, connection_qualified_name]
+            )
+            return Application.Attributes(
+                name=name,
+                qualified_name=f"{connection_qualified_name}/{name}",
+                connection_qualified_name=connection_qualified_name,
+                connector_name=AtlanConnectorType.get_connector_name(
+                    connection_qualified_name
+                ),
+            )
+
+    attributes: Application.Attributes = Field(
+        default_factory=lambda: Application.Attributes(),
+        description=(
+            "Map of attributes in the instance and their values. "
+            "The specific keys of this map will vary by type, "
+            "so are described in the sub-types of this schema."
+        ),
+    )
+
+
+from .asset import Asset  # noqa
---
+++ packages/pyatlan/pyatlan-4.0.2.tar.gz/pyatlan-4.0.2/pyatlan/model/packages/__init__.py
@@ -0,0 +1,42 @@
+# flake8: noqa
+from .asset_export_basic import AssetExportBasic
+from .asset_import import AssetImport
+from .big_query_crawler import BigQueryCrawler
+from .confluent_kafka_crawler import ConfluentKafkaCrawler
+from .connection_delete import ConnectionDelete
+from .databricks_crawler import DatabricksCrawler
+from .databricks_miner import DatabricksMiner
+from .dbt_crawler import DbtCrawler
+from .dynamo_d_b_crawler import DynamoDBCrawler
+from .glue_crawler import GlueCrawler
+from .mongodb_crawler import MongoDBCrawler
+from .postgres_crawler import PostgresCrawler
+from .powerbi_crawler import PowerBICrawler
+from .relational_assets_builder import RelationalAssetsBuilder
+from .s_q_l_server_crawler import SQLServerCrawler
+from .sigma_crawler import SigmaCrawler
+from .snowflake_crawler import SnowflakeCrawler
+from .snowflake_miner import SnowflakeMiner
+from .tableau_crawler import TableauCrawler
+
+__all__ = [
+    "BigQueryCrawler",
+    "ConfluentKafkaCrawler",
+    "ConnectionDelete",
+    "DbtCrawler",
+    "DynamoDBCrawler",
+    "DatabricksCrawler",
+    "DatabricksMiner",
+    "GlueCrawler",
+    "PostgresCrawler",
+    "PowerBICrawler",
+    "SQLServerCrawler",
+    "SigmaCrawler",
+    "SnowflakeCrawler",
+    "MongoDBCrawler",
+    "TableauCrawler",
+    "SnowflakeMiner",
+    "AssetImport",
+    "AssetExportBasic",
+    "RelationalAssetsBuilder",
+]
