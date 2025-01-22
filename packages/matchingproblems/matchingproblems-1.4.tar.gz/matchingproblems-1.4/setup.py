8ed4d2a4c3e5035b2adf360aefdb64db4f2cd667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:05:36 +0000

    Add package matchingproblems/1.4/matchingproblems-1.4.tar.gz

---
+++ packages/matchingproblems/matchingproblems-1.4.tar.gz/matchingproblems-1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+	name='matchingproblems',
+    version='1.4',
+    packages=find_packages(),
+    license='MIT',
+
+    description='A matching problem generator and solver.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    
+    url='http://github.com/fmcooper/matchingproblems',
+    author='Frances Cooper',
+    author_email='fmcooper234@gmail.com',
+
+    python_requires='>=3.12',
+    install_requires=[
+        'numpy==1.26.4',
+        'pulp==2.9.0',
+    ]
+)
