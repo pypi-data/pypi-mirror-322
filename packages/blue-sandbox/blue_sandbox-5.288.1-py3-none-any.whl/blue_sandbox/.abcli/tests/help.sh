782f7968fe50e6f62fede36c649297cdedb4db3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 09:04:41 +0000

    Add package blue-sandbox/5.288.1/blue_sandbox-5.288.1-py3-none-any.whl

---
+++ packages/blue-sandbox/blue_sandbox-5.288.1-py3-none-any.whl/blue_sandbox/.abcli/tests/help.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+function test_blue_sandbox_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@sandbox" \
+        \
+        "@damages" \
+        "@damages ingest" \
+        "@damages ingest list" \
+        "@damages install" \
+        "@damages label" \
+        "@damages tensorboard" \
+        "@damages train" \
+        \
+        "palisades" \
+        "palisades ingest" \
+        "palisades label" \
+        "palisades predict" \
+        "palisades train" \
+        \
+        "blue_sandbox"; do
+        abcli_eval ,$options \
+            abcli_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
