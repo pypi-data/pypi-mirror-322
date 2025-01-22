61a455ccceeb249292da8e06cea80919f96d591d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:06:04 +0000

    Add package srtranslator-bariskeser/0.0.5/srtranslator_bariskeser-0.0.5.tar.gz

---
+++ packages/srtranslator-bariskeser/srtranslator_bariskeser-0.0.5.tar.gz/srtranslator_bariskeser-0.0.5/setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="srtranslator-bariskeser",
+    description="Özel bir çevirmen kullanarak bir .SRT dosyasını çevirin",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/barkeser2002/srtranslator-bariskeser",
+    version="0.0.5",
+    author="bariskeser2002",
+    author_email="info@bariskeser.com",
+    license="FREE",
+    python_requires=">=3.6",
+    install_requires=requirements,
+    packages=find_packages(),
+    keywords=["python", "srt", "diller", "çevirmen", "altyazılar"],
+)
