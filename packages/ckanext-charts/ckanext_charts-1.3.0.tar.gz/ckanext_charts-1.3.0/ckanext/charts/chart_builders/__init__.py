cddb23928876a85a2417ecb6d382a5fb9e497581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 04:06:51 +0000

    Add package ckanext-charts/1.3.0/ckanext_charts-1.3.0.tar.gz

---
+++ packages/ckanext-charts/ckanext_charts-1.3.0.tar.gz/ckanext_charts-1.3.0/ckanext/charts/chart_builders/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from .base import BaseChartBuilder
+from .chartjs import ChartJSBarBuilder
+from .observable import ObservableBuilder
+from .plotly import PlotlyBuilder
+from .plotly.bar import PlotlyBarForm
+
+DEFAULT_CHART_FORM = PlotlyBarForm
+
+
+def get_chart_engines() -> dict[str, type[BaseChartBuilder]]:
+    return {
+        "plotly": PlotlyBuilder,
+        "observable": ObservableBuilder,
+        "chartjs": ChartJSBarBuilder,
+    }
