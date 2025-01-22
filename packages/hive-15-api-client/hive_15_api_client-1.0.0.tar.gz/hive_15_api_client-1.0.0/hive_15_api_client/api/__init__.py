02e954abec8fb01b2af26b53df83594dfcedbdb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:43 +0000

    Add package hive-15-api-client/1.0.0/hive_15_api_client-1.0.0.tar.gz

---
+++ packages/hive-15-api-client/hive_15_api_client-1.0.0.tar.gz/hive_15_api_client-1.0.0/hive_15_api_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from hive_15_api_client.api.pet_api import PetApi
+from hive_15_api_client.api.store_api import StoreApi
+from hive_15_api_client.api.user_api import UserApi
+
