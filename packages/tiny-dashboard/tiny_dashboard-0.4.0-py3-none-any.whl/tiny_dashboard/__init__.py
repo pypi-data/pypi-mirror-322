4c7779789f401fb58b630a99b7f77dbd3e14551e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:19 +0000

    Add package tiny-dashboard/0.4.0/tiny_dashboard-0.4.0-py3-none-any.whl

---
+++ packages/tiny-dashboard/tiny_dashboard-0.4.0-py3-none-any.whl/tiny_dashboard/__init__.py
@@ -0,0 +1,11 @@
+from .feature_centric_dashboards import (
+    OfflineFeatureCentricDashboard,
+    OnlineFeatureCentricDashboard,
+    AbstractOnlineFeatureCentricDashboard,
+)
+
+__all__ = [
+    "OfflineFeatureCentricDashboard",
+    "OnlineFeatureCentricDashboard",
+    "AbstractOnlineFeatureCentricDashboard",
+]
