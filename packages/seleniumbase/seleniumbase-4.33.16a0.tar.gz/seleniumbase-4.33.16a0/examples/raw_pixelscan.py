9885bcbd6a430b5336317ee2d208a2f106fb276b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 09:04:41 +0000

    Add package seleniumbase/4.33.16a0/seleniumbase-4.33.16a0.tar.gz

---
+++ packages/seleniumbase/seleniumbase-4.33.16a0.tar.gz/seleniumbase-4.33.16a0/examples/raw_pixelscan.py
@@ -0,0 +1,17 @@
+from seleniumbase import SB
+
+with SB(uc=True, incognito=True, test=True) as sb:
+    sb.driver.uc_open_with_reconnect("https://pixelscan.net/", 7)
+    sb.remove_elements("div.banner")  # Remove the banner
+    sb.remove_elements("jdiv")  # Remove chat widgets
+    no_automation_detected = "No automation framework detected"
+    sb.assert_text(no_automation_detected, "pxlscn-bot-detection")
+    not_masking_text = "You are not masking your fingerprint"
+    sb.assert_text(not_masking_text, "pxlscn-fingerprint-masking")
+    sb.highlight("span.text-success", loops=8)
+    sb.sleep(1)
+    fingerprint_masking_div = "pxlscn-fingerprint-masking div"
+    sb.highlight(fingerprint_masking_div, loops=9, scroll=False)
+    sb.sleep(1)
+    sb.highlight(".bot-detection-context", loops=10, scroll=False)
+    sb.sleep(2)
