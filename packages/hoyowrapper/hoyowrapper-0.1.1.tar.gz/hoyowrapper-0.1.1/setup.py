236a69f11a3056e62f8384881fa6f5fe17ac441c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:29 +0000

    Add package hoyowrapper/0.1.1/hoyowrapper-0.1.1.tar.gz

---
+++ packages/hoyowrapper/hoyowrapper-0.1.1.tar.gz/hoyowrapper-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+long_description = Path('README.md').read_text()
+
+setup(
+    name='hoyowrapper',
+    version='0.1.1',
+    packages=find_packages(include=['hoyowrapper']),
+    author='Guitarband',
+    author_email='preetish_choudhary@outlook.com',
+    description='A library for handling interactions with Hoyolab via Python.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['requests', 'asyncio', 'pyppeteer'],
+    setup_requires=[],
+    tests_require=[],
+    test_suite='tests'
+)
