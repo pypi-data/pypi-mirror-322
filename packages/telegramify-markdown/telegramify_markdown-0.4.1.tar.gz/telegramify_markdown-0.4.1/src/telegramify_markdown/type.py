e75674c750c69d2cdb58cf687f518755e2113377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:17 +0000

    Add package telegramify-markdown/0.4.1/telegramify_markdown-0.4.1.tar.gz

---
+++ packages/telegramify-markdown/telegramify_markdown-0.4.1.tar.gz/telegramify_markdown-0.4.1/src/telegramify_markdown/type.py
@@ -0,0 +1,47 @@
+import dataclasses
+from enum import Enum
+from typing import Tuple, List, Any, Union
+
+TaskType = Tuple[str, List[Tuple[Any, Any]]]
+SentType = List[Union["Text", "File", "Photo"]]
+
+
+class ContentTypes(Enum):
+    TEXT = "text"
+    FILE = "file"
+    PHOTO = "photo"
+
+
+@dataclasses.dataclass
+class ContentTrace:
+    source_type: str
+    extra: dict = dataclasses.field(default_factory=dict, kw_only=True)
+
+
+@dataclasses.dataclass
+class Text:
+    content: str
+    content_trace: ContentTrace
+    content_type: ContentTypes = ContentTypes.TEXT
+
+
+@dataclasses.dataclass
+class File:
+    file_name: str
+    file_data: bytes
+
+    content_trace: ContentTrace
+    caption: str = ""
+    """Please use render_lines_func to render the content."""
+    content_type: ContentTypes = ContentTypes.FILE
+
+
+@dataclasses.dataclass
+class Photo:
+    file_name: str
+    file_data: bytes
+
+    content_trace: ContentTrace
+    caption: str = ""
+    """Please use render_lines_func to render the content."""
+    content_type: ContentTypes = ContentTypes.PHOTO
