6062522494fb000a1d4c94e15caae51f6dbc7363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:40 +0000

    Add package isee/0.2.45/isee-0.2.45-py3-none-any.whl

---
+++ packages/isee/isee-0.2.45-py3-none-any.whl/isee/generation_utils.py
@@ -0,0 +1,132 @@
+"""Generation utils for the isee package.
+
+Has two main functions:
+- gen_semver: Generate a new semantic version based on git commit messages and tags.
+- generate_documentation: Generate documentation for the project.
+
+"""
+
+import os
+import re
+from functools import partial
+from warnings import warn
+
+import semver
+from epythet.autogen import make_autodocs
+from epythet.setup_docsrc import make_docsrc
+
+from wads.pack import versions_from_different_sources, validate_versions
+from isee.common import get_env_var, git as _git
+
+DFLT_NEW_VERSION = "0.1.0"  # Default version if no tags are found
+
+
+def get_new_version(
+    *,
+    work_tree=".",
+    version_patch_prefix: str = "",
+    action_when_versions_not_valid=warn,
+):
+    """
+    Get the latest version from git tags and determine the new version based on
+    the commit message.
+    """
+
+    work_tree = os.path.expanduser(os.path.abspath(work_tree))
+
+    git = partial(_git, work_tree=work_tree)
+
+    def bump(latest):
+        """
+        Bump the version based on the commit message.
+
+        Args:
+        - latest (str): The latest version.
+
+        Returns:
+        - str: The new bumped version.
+        """
+        # Get the latest commit message
+        commit_message = git("show-branch", "--no-name", "HEAD")
+        # Check if the commit message contains bump directives
+        if re.search(r"\[bump major\]", commit_message):
+            return semver.bump_major(latest)
+        if re.search(r"\[bump minor\]", commit_message):
+            return semver.bump_minor(latest)
+        # Default to bumping the patch version
+        return semver.bump_patch(latest)
+
+    def format_version(version):
+        """
+        Format the version by adding the patch prefix.
+
+        Args:
+        - version (str): The version to format.
+
+        Returns:
+        - str: The formatted version.
+        """
+        version_parts = version.split(".")
+        version_parts[2] = f"{version_patch_prefix}{version_parts[2]}"
+        return ".".join(version_parts)
+
+    versions = versions_from_different_sources(work_tree)
+
+    validate_versions(versions, action_when_not_valid=action_when_versions_not_valid)
+
+    # Take the highest version from the different sources to be the latest version
+    latest_version = max(filter(None, versions.values()), key=semver.VersionInfo.parse)
+
+    if latest_version:
+        # If there are existing versions, bump the latest version
+        new_version = bump(latest_version)
+    else:
+        # No versions found so, use the default version
+        new_version = DFLT_NEW_VERSION
+    return format_version(new_version)
+
+
+def gen_semver(
+    *,
+    dir_path: str = None,
+    version_patch_prefix: str = "",
+    verbose=True,
+):
+    """
+    Generate a new semantic version based on git commit messages and tags.
+
+    Args:
+    - dir_path (str): The directory path where the git repository is located.
+        If None, uses the current directory.
+    - version_patch_prefix (str): A prefix to be added to the patch version.
+
+    Returns:
+    - None: Prints the new version.
+    """
+
+    # if dir_path:
+    #     os.chdir(dir_path)  # Change to the specified directory
+    work_tree = dir_path or os.path.abspath(".")
+    # Generate the new version:
+    version = get_new_version(
+        work_tree=work_tree, version_patch_prefix=version_patch_prefix
+    )
+    if version is None:
+        raise ValueError("No version found")
+    print(version)  # WARNING!! Don't edit!!! This is effectively the return value
+    # --> This is a hack because returning the version is not working in github actions
+    #     CI, so printing the version is a workaround for now.
+    #     if you condition this print, or enhance it (e.g. f"{version=}") you will break
+    #     the CI. So, please don't edit this line.
+
+    # if verbose:
+    #     print(f"New version: {version}")  # Print the new version
+    # return version
+
+
+def generate_documentation(*, project_dir=None):
+    if not project_dir:
+        project_dir = get_env_var("GITHUB_WORKSPACE")
+    if not os.path.exists(project_dir + "/docsrc"):
+        make_docsrc(project_dir)
+    make_autodocs(project_dir)
