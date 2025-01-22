f047d00d313dce2b6b9212e8a623668178778dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 04:06:36 +0000

    Add package edc-vitals/1.0.0/edc_vitals-1.0.0-py3-none-any.whl

---
+++ packages/edc-vitals/edc_vitals-1.0.0-py3-none-any.whl/edc_vitals/form_validators/weight_height_bmi_form_validator_mixin.py
@@ -0,0 +1,12 @@
+from django import forms
+from edc_reportable import CalculatorError, calculate_bmi
+
+
+class WeightHeightBmiFormValidatorMixin:
+    @staticmethod
+    def validate_weight_height_with_bmi(weight_kg=None, height_cm=None, **kwargs):
+        try:
+            bmi = calculate_bmi(weight_kg=weight_kg, height_cm=height_cm, **kwargs)
+        except CalculatorError as e:
+            raise forms.ValidationError(f"Please check weight and height. {e}")
+        return bmi
