bd49b315271c035daffcaa76f0229ec8ceda1c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 14:05:34 +0000

    Add package aind-behavior-experiment-launcher/0.3.2/aind_behavior_experiment_launcher-0.3.2-py3-none-any.whl

---
+++ packages/aind-behavior-experiment-launcher/aind_behavior_experiment_launcher-0.3.2-py3-none-any.whl/aind_behavior_experiment_launcher/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.2"
+
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
+
+fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+logging.basicConfig(level=logging.INFO, format=fmt, datefmt="%Y-%m-%dT%H%M%S%z")
