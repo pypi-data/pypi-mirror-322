77eb0f54e365db33f70c1462e0577f63f2ac86b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 09:04:30 +0000

    Add package rapidtide/3.0a4/rapidtide-3.0a4-py3-none-any.whl

---
+++ packages/rapidtide/rapidtide-3.0a4-py3-none-any.whl/rapidtide/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
