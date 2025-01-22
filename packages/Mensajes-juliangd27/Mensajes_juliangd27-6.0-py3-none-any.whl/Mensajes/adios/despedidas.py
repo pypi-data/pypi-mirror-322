5445d3eb8b4583c8fd6eac7ed3aa214d27e4c3c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:24 +0000

    Add package Mensajes-juliangd27/6.0/Mensajes_juliangd27-6.0-py3-none-any.whl

---
+++ packages/Mensajes-juliangd27/Mensajes_juliangd27-6.0-py3-none-any.whl/Mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adiós. Me despido desde despedidas.despedir()")
+
+class Despedida:
+    def __init__(self):
+        print("Adiós. Me despido desde Despedida.__init__()")
