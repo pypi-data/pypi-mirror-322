5a974b1bc2450093fe2f39ebc43f44ab757c3474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 14:04:57 +0000

    Add package palisades/4.15.1/palisades-4.15.1-py3-none-any.whl

---
+++ packages/palisades/palisades-4.15.1-py3-none-any.whl/palisades/README.py
@@ -0,0 +1,55 @@
+import os
+
+from blue_objects import file, README
+from blue_geo import ICON as blue_geo_ICON
+from roofai import ICON as roofai_ICON
+
+from palisades import NAME, VERSION, ICON, REPO_NAME, MARQUEE
+
+# refactor
+
+list_of_menu_item = {
+    "STAC Catalog: Maxar Open Data": {
+        "ICON": blue_geo_ICON,
+        "url": "https://github.com/kamangir/blue-geo/tree/main/blue_geo/catalog/maxar_open_data",
+        "marquee": "https://github.com/kamangir/assets/blob/main/blue-geo/Maxar-Open-Datacube.png?raw=true",
+        "title": 'Integration with ["Satellite imagery for select sudden onset major crisis events."](https://www.maxar.com/open-data/)',
+    },
+    "Algo: Semantic Segmentation": {
+        "ICON": roofai_ICON,
+        "url": "https://github.com/kamangir/palisades/blob/main/palisades/docs/step-by-step.md",
+        "marquee": "https://github.com/kamangir/assets/raw/main/palisades/prediction.png?raw=true",
+        "title": "Labelling -> Training -> Inference.",
+    },
+    "template": {
+        "ICON": ICON,
+        "url": "#",
+        "marquee": "",
+        "title": "",
+    },
+}
+
+
+items = [
+    "{}[`{}`]({}) [![image]({})]({}) {}".format(
+        menu_item["ICON"],
+        menu_item_name,
+        menu_item["url"],
+        menu_item["marquee"],
+        menu_item["url"],
+        menu_item["title"],
+    )
+    for menu_item_name, menu_item in list_of_menu_item.items()
+    if menu_item_name != "template"
+]
+
+
+def build():
+    return README.build(
+        items=items,
+        path=os.path.join(file.path(__file__), ".."),
+        ICON=ICON,
+        NAME=NAME,
+        VERSION=VERSION,
+        REPO_NAME=REPO_NAME,
+    )
