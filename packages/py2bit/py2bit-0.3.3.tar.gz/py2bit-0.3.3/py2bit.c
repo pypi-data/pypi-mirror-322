e82190af360f707a4563418ddcc88e51152275e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:05:45 +0000

    Add package py2bit/0.3.3/py2bit-0.3.3.tar.gz

---
+++ packages/py2bit/py2bit-0.3.3.tar.gz/py2bit-0.3.3/py2bit.c
@@ -0,0 +1,477 @@
+#include <Python.h>
+#include <inttypes.h>
+#include "py2bit.h"
+
+static PyObject *py2bitOpen(PyObject *self, PyObject *args, PyObject *kwds) {
+    char *fname = NULL;
+    PyObject *storeMaskedO = Py_False;
+    pyTwoBit_t *pytb;
+    int storeMasked = 0;
+    TwoBit *tb = NULL;
+    static char *kwd_list[] = {"fname", "storeMasked", NULL};
+
+    if(!PyArg_ParseTupleAndKeywords(args, kwds, "s|O", kwd_list, &fname, &storeMaskedO)) goto error;
+
+    if(storeMaskedO == Py_True) storeMasked = 1;
+
+    //Open the file
+    tb = twobitOpen(fname, storeMasked);
+    if(!tb) goto error;
+
+    pytb = PyObject_New(pyTwoBit_t, &pyTwoBit);
+    if(!pytb) goto error;
+    pytb->storeMasked = storeMasked;
+    pytb->tb = tb;
+
+    return (PyObject*) pytb;
+
+error:
+    if(tb) twobitClose(tb);
+    PyErr_SetString(PyExc_RuntimeError, "Received an error during file opening!");
+    return NULL;
+}
+
+PyObject *py2bitEnter(pyTwoBit_t *self, PyObject *args) {
+    TwoBit *tb = self->tb;
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    Py_INCREF(self);
+
+    return (PyObject*) self;
+}
+
+static void py2bitDealloc(pyTwoBit_t *self) {
+    if(self->tb) twobitClose(self->tb);
+    PyObject_DEL(self);
+}
+
+static PyObject *py2bitClose(pyTwoBit_t *self, PyObject *args) {
+    if(self->tb) twobitClose(self->tb);
+    self->tb = NULL;
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+//Returns the file size, number of chromosomes/contigs, total sequence length and total masked length
+static PyObject *py2bitInfo(pyTwoBit_t *self, PyObject *args) {
+    TwoBit *tb = self->tb;
+    PyObject *ret = NULL, *val = NULL;
+    uint32_t i, j, foo;
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    ret = PyDict_New();
+
+    //file size
+    val = PyLong_FromUnsignedLongLong(tb->sz);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "file size", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //nContigs
+    val = PyLong_FromUnsignedLong(tb->hdr->nChroms);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "nChroms", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //sequence length
+    foo = 0;
+    for(i=0; i<tb->hdr->nChroms; i++) foo += tb->idx->size[i];
+    val = PyLong_FromUnsignedLong(foo);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "sequence length", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //hard-masked length
+    foo = 0;
+    for(i=0; i<tb->hdr->nChroms; i++) {
+        for(j=0; j<tb->idx->nBlockCount[i]; j++) {
+            foo += tb->idx->nBlockSizes[i][j];
+        }
+    }
+    val = PyLong_FromUnsignedLong(foo);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "hard-masked length", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //soft-masked length
+    if(tb->idx->maskBlockStart) {
+        foo = 0;
+        for(i=0; i<tb->hdr->nChroms; i++) {
+            for(j=0; j<tb->idx->maskBlockCount[i]; j++) {
+                foo += tb->idx->maskBlockSizes[i][j];
+            }
+        }
+
+        val = PyLong_FromUnsignedLong(foo);
+        if(!val) goto error;
+        if(PyDict_SetItemString(ret, "soft-masked length", val) == -1) goto error;
+        Py_DECREF(val);
+    }
+
+    return ret;
+
+error:
+    Py_XDECREF(val);
+    Py_XDECREF(ret);
+    PyErr_SetString(PyExc_RuntimeError, "Received an error while gathering information on the 2bit file!");
+    return NULL;
+}
+
+static PyObject *py2bitChroms(pyTwoBit_t *self, PyObject *args) {
+    PyObject *ret = NULL, *val = NULL;
+    TwoBit *tb = self->tb;
+    char *chrom = NULL;
+    uint32_t i;
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    if(!(PyArg_ParseTuple(args, "|s", &chrom)) || !chrom) {
+        ret = PyDict_New();
+        if(!ret) goto error;
+        for(i=0; i<tb->hdr->nChroms; i++) {
+            val = PyLong_FromUnsignedLong(tb->idx->size[i]);
+            if(!val) goto error;
+            if(PyDict_SetItemString(ret, tb->cl->chrom[i], val) == -1) goto error;
+            Py_DECREF(val);
+        }
+    } else {
+        for(i=0; i<tb->hdr->nChroms; i++) {
+            if(strcmp(tb->cl->chrom[i], chrom) == 0) {
+                ret = PyLong_FromUnsignedLong(tb->idx->size[i]);
+                if(!ret) goto error;
+                break;
+            }
+        }
+    }
+
+    if(!ret) {
+        Py_INCREF(Py_None);
+        ret = Py_None;
+    }
+
+    return ret;
+
+error :
+    Py_XDECREF(val);
+    Py_XDECREF(ret);
+    PyErr_SetString(PyExc_RuntimeError, "Received an error while adding an item to the output dictionary!");
+    return NULL;
+}
+
+#if PY_MAJOR_VERSION >= 3
+PyObject *PyString_FromString(char *seq) {
+    return PyUnicode_FromString(seq);
+}
+#endif
+
+static PyObject *py2bitSequence(pyTwoBit_t *self, PyObject *args, PyObject *kwds) {
+    PyObject *ret = NULL;
+    TwoBit *tb = self->tb;
+    char *seq, *chrom;
+    unsigned long startl = 0, endl = 0;
+    uint32_t start, end, len;
+    static char *kwd_list[] = {"chrom", "start", "end", NULL};
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    if(!PyArg_ParseTupleAndKeywords(args, kwds, "s|kk", kwd_list, &chrom, &startl, &endl)) {
+        PyErr_SetString(PyExc_RuntimeError, "You must supply at least a chromosome!");
+        return NULL;
+    }
+
+    len = twobitChromLen(tb, chrom);
+    if(len == 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The specified chromosome doesn't exist in the 2bit file!");
+        return NULL;
+    }
+    if(endl > len) endl = len;
+    end = (uint32_t) endl;
+    if(startl >= endl && startl > 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The start value must be less then the end value (and the end of the chromosome");
+        return NULL;
+    }
+    start = (uint32_t) startl;
+    seq = twobitSequence(tb, chrom, start, end);
+    if(!seq) {
+        PyErr_SetString(PyExc_RuntimeError, "There was an error while fetching the sequence!");
+        return NULL;
+    }
+
+    ret = PyString_FromString(seq);
+    free(seq);
+    if(!ret) {
+        PyErr_SetString(PyExc_RuntimeError, "Received an error while converting the C-level char array to a python string!");
+        return NULL;
+    }
+
+    return ret;
+}
+
+static PyObject *py2bitBases(pyTwoBit_t *self, PyObject *args, PyObject *kwds) {
+    PyObject *ret = NULL, *val = NULL;
+    PyObject *fractionO = Py_True;
+    TwoBit *tb = self->tb;
+    char *chrom;
+    void *o = NULL;
+    unsigned long startl = 0, endl = 0;
+    uint32_t start, end, len;
+    static char *kwd_list[] = {"chrom", "start", "end", "fraction", NULL};
+    int fraction = 1;
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    if(!PyArg_ParseTupleAndKeywords(args, kwds, "s|kkO", kwd_list, &chrom, &startl, &endl, &fractionO)) {
+        PyErr_SetString(PyExc_RuntimeError, "You must supply at least a chromosome!");
+        return NULL;
+    }
+
+    len = twobitChromLen(tb, chrom);
+    if(len == 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The specified chromosome doesn't exist in the 2bit file!");
+        return NULL;
+    }
+    if(endl > len) endl = len;
+    end = (uint32_t) endl;
+    if(startl >= endl && startl > 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The start value must be less then the end value (and the end of the chromosome");
+        return NULL;
+    }
+    start = (uint32_t) startl;
+
+    if(fractionO == Py_False) fraction = 0;
+
+    o = twobitBases(tb, chrom, start, end, fraction);
+    if(!o) {
+        PyErr_SetString(PyExc_RuntimeError, "Received an error while determining the per-base metrics.");
+        return NULL;
+    }
+
+    ret = PyDict_New();
+    if(!ret) goto error;
+
+    //A
+    if(fraction) val = PyFloat_FromDouble(((double*)o)[0]);
+    else val = PyLong_FromUnsignedLong(((uint32_t*)o)[0]);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "A", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //C
+    if(fraction) val = PyFloat_FromDouble(((double*)o)[1]);
+    else val = PyLong_FromUnsignedLong(((uint32_t*)o)[1]);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "C", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //T
+    if(fraction) val = PyFloat_FromDouble(((double*)o)[2]);
+    else val = PyLong_FromUnsignedLong(((uint32_t*)o)[2]);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "T", val) == -1) goto error;
+    Py_DECREF(val);
+
+    //G
+    if(fraction) val = PyFloat_FromDouble(((double*)o)[3]);
+    else val = PyLong_FromUnsignedLong(((uint32_t*)o)[3]);
+    if(!val) goto error;
+    if(PyDict_SetItemString(ret, "G", val) == -1) goto error;
+    Py_DECREF(val);
+
+    free(o);
+
+    return ret;
+
+error:
+    if(o) free(o);
+    Py_XDECREF(ret);
+    Py_XDECREF(val);
+    PyErr_SetString(PyExc_RuntimeError, "Received an error while constructing the output dictionary!");
+    return NULL;
+}
+
+static PyObject *py2bitHardMaskedBlocks(pyTwoBit_t *self, PyObject *args, PyObject *kwds) {
+    PyObject *ret = NULL, *tup = NULL;
+    TwoBit *tb = self->tb;
+    char *chrom;
+    unsigned long startl = 0, endl = 0, totalBlocks = 0, tid;
+    uint32_t start, end, len, blockStart, blockEnd, i, j;
+    static char *kwd_list[] = {"chrom", "start", "end", NULL};
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    if(!PyArg_ParseTupleAndKeywords(args, kwds, "s|kk", kwd_list, &chrom, &startl, &endl)) {
+        PyErr_SetString(PyExc_RuntimeError, "You must supply at least a chromosome!");
+        return NULL;
+    }
+
+    //Get the chromosome ID
+    for(i=0; i<tb->hdr->nChroms; i++) {
+        if(strcmp(tb->cl->chrom[i], chrom) == 0) {
+            tid = i;
+            break;
+        }
+    }
+
+    len = twobitChromLen(tb, chrom);
+    if(len == 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The specified chromosome doesn't exist in the 2bit file!");
+        return NULL;
+    }
+    if(endl == 0) endl = len;
+    if(endl > len) endl = len;
+    end = (uint32_t) endl;
+    if(startl >= endl && startl > 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The start value must be less then the end value (and the end of the chromosome");
+        return NULL;
+    }
+    start = (uint32_t) startl;
+
+    // Count the total number of overlapping N-masked blocks
+    for(i=0; i<tb->idx->nBlockCount[tid]; i++) {
+        blockStart = tb->idx->nBlockStart[tid][i];
+        blockEnd = blockStart + tb->idx->nBlockSizes[tid][i];
+        if(blockStart < end && blockEnd > start) totalBlocks++;
+    }
+
+    // Form the output
+    ret = PyList_New(totalBlocks);
+    if(!ret) goto error;
+    if(totalBlocks == 0) return ret;
+    for(i=0, j=0; i<tb->idx->nBlockCount[tid]; i++) {
+        blockStart = tb->idx->nBlockStart[tid][i];
+        blockEnd = blockStart + tb->idx->nBlockSizes[tid][i];
+        if(blockStart < end && blockEnd > start) {
+            tup = Py_BuildValue("(kk)", (unsigned long) blockStart, (unsigned long) blockEnd);
+            if(!tup) goto error;
+            if(PyList_SetItem(ret, j++, tup)) goto error;
+        }
+    }
+
+    return ret;
+
+error:
+    if(ret) Py_XDECREF(ret);
+    if(tup) Py_XDECREF(tup);
+    PyErr_SetString(PyExc_RuntimeError, "Received an error while constructing the output list and tuples!");
+    return NULL;
+}
+
+static PyObject *py2bitSoftMaskedBlocks(pyTwoBit_t *self, PyObject *args, PyObject *kwds) {
+    PyObject *ret = NULL, *tup = NULL;
+    TwoBit *tb = self->tb;
+    char *chrom;
+    unsigned long startl = 0, endl = 0, totalBlocks = 0, tid;
+    uint32_t start, end, len, blockStart, blockEnd, i, j;
+    static char *kwd_list[] = {"chrom", "start", "end", NULL};
+
+    if(!tb) {
+        PyErr_SetString(PyExc_RuntimeError, "The 2bit file handle is not open!");
+        return NULL;
+    }
+
+    if(!PyArg_ParseTupleAndKeywords(args, kwds, "s|kk", kwd_list, &chrom, &startl, &endl)) {
+        PyErr_SetString(PyExc_RuntimeError, "You must supply at least a chromosome!");
+        return NULL;
+    }
+
+    //Get the chromosome ID
+    for(i=0; i<tb->hdr->nChroms; i++) {
+        if(strcmp(tb->cl->chrom[i], chrom) == 0) {
+            tid = i;
+            break;
+        }
+    }
+
+    len = twobitChromLen(tb, chrom);
+    if(len == 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The specified chromosome doesn't exist in the 2bit file!");
+        return NULL;
+    }
+    if(endl == 0) endl = len;
+    if(endl > len) endl = len;
+    end = (uint32_t) endl;
+    if(startl >= endl && startl > 0) {
+        PyErr_SetString(PyExc_RuntimeError, "The start value must be less then the end value (and the end of the chromosome");
+        return NULL;
+    }
+    start = (uint32_t) startl;
+
+    if(!tb->idx->maskBlockStart) {
+        PyErr_SetString(PyExc_RuntimeError, "The file was not opened with storeMasked=True! Consequently, there are no stored soft-masked regions.");
+        return NULL;
+    }
+    
+    // Count the total number of overlapping soft-masked blocks
+    for(i=0; i<tb->idx->maskBlockCount[tid]; i++) {
+        blockStart = tb->idx->maskBlockStart[tid][i];
+        blockEnd = blockStart + tb->idx->maskBlockSizes[tid][i];
+        if(blockStart < end && blockEnd > start) totalBlocks++;
+    }
+
+    // Form the output
+    ret = PyList_New(totalBlocks);
+    if(!ret) goto error;
+    if(totalBlocks == 0) return ret;
+    for(i=0, j=0; i<tb->idx->maskBlockCount[tid]; i++) {
+        blockStart = tb->idx->maskBlockStart[tid][i];
+        blockEnd = blockStart + tb->idx->maskBlockSizes[tid][i];
+        if(blockStart < end && blockEnd > start) {
+            tup = Py_BuildValue("(kk)", (unsigned long) blockStart, (unsigned long) blockEnd);
+            if(!tup) goto error;
+            if(PyList_SetItem(ret, j++, tup)) goto error;
+        }
+    }
+
+    return ret;
+error:
+    if(ret) Py_XDECREF(ret);
+    if(tup) Py_XDECREF(tup);
+    PyErr_SetString(PyExc_RuntimeError, "Received an error while constructing the output list and tuples!");
+    return NULL;
+}
+
+#if PY_MAJOR_VERSION >= 3
+PyMODINIT_FUNC PyInit_py2bit(void) {
+    PyObject *res;
+
+    if(PyType_Ready(&pyTwoBit) < 0) return NULL;
+    res = PyModule_Create(&py2bitmodule);
+    if(!res) return NULL;
+
+    Py_INCREF(&pyTwoBit);
+    PyModule_AddObject(res, "py2bit", (PyObject *) &pyTwoBit);
+    PyModule_AddStringConstant(res, "__version__", pyTwoBitVersion);
+
+    return res;
+}
+#else
+//Python2 initialization
+PyMODINIT_FUNC initpy2bit(void) {
+    PyObject *res;
+    if(PyType_Ready(&pyTwoBit) < 0) return;
+    res = Py_InitModule3("py2bit", tbMethods, "A module for handling 2bit files");
+    Py_INCREF(&pyTwoBit);
+    PyModule_AddObject(res, "py2bit", (PyObject *) &pyTwoBit);
+    PyModule_AddStringConstant(res, "__version__", pyTwoBitVersion);
+}
+#endif
---
+++ packages/py2bit/py2bit-0.3.3.tar.gz/py2bit-0.3.3/py2bit.h
@@ -0,0 +1,264 @@
+#include <Python.h>
+#include "2bit.h"
+
+#define pyTwoBitVersion "0.3.3"
+
+typedef struct {
+    PyObject_HEAD
+    TwoBit *tb;
+    int storeMasked; //Whether storeMasked was set. 0 = False, 1 = True
+} pyTwoBit_t;
+
+static PyObject* py2bitOpen(PyObject *self, PyObject *args, PyObject *kwds);
+static PyObject *py2bitEnter(pyTwoBit_t *pybw, PyObject *args);
+static PyObject *py2bitInfo(pyTwoBit_t *pybw, PyObject *args);
+static PyObject* py2bitClose(pyTwoBit_t *pybw, PyObject *args);
+static PyObject* py2bitChroms(pyTwoBit_t *pybw, PyObject *args);
+static PyObject *py2bitSequence(pyTwoBit_t *pybw, PyObject *args, PyObject *kwds);
+static PyObject *py2bitBases(pyTwoBit_t *pybw, PyObject *args, PyObject *kwds);
+static PyObject *py2bitHardMaskedBlocks(pyTwoBit_t *pybw, PyObject *args, PyObject *kwds);
+static PyObject *py2bitSoftMaskedBlocks(pyTwoBit_t *pybw, PyObject *args, PyObject *kwds);
+static void py2bitDealloc(pyTwoBit_t *pybw);
+
+static PyMethodDef tbMethods[] = {
+    {"open", (PyCFunction)py2bitOpen, METH_VARARGS|METH_KEYWORDS,
+"Open a 2bit file.\n\
+\n\
+Returns:\n\
+   A TwoBit object on success, otherwise None.\n\
+\n\
+Arguments:\n\
+    file: The name of a 2bit file.\n\
+\n\
+Optional arguments:\n\
+    storeMasked: Whether to store information about soft-masking (default False).\n\
+\n\
+Note that storing soft-masking information can be memory intensive and doing so\n\
+will result in soft-masked bases being lower case if the sequence is fetched\n\
+(see the sequence() function)\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"some_file.2bit\")\n\
+\n\
+To store soft-masking information:\n\
+>>> tb = py2bit.open(\"some_file.2bit\", True)"},
+    {NULL, NULL, 0, NULL}
+};
+
+static PyMethodDef tbObjMethods[] = {
+    {"info", (PyCFunction)py2bitInfo, METH_VARARGS,
+"Returns a dictionary containing the following key:value pairs: \n\
+\n\
+  * The file size, in bytes ('file size').\n\
+  * The number of chromosomes/contigs ('nChroms').\n\
+  * The total sequence length ('sequence length').\n\
+  * The total hard-masked length ('hard-masked length').\n\
+  * The total soft-masked length, if available ('soft-masked length').\n\
+\n\
+A base is hard-masked if it is an N and soft-masked if it's lower case. Note that soft-masking is ignored by default (you must specify 'storeMasked=True' when you open the file.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"some_file.2bit\")\n\
+>>> tb.info()\n\
+{'file size': 160L, 'nChroms': 2L, 'sequence length': 250L, 'hard-masked length': 150L, 'soft-masked length': 8L}\n\
+>>> tb.close()\n"},
+    {"close", (PyCFunction)py2bitClose, METH_VARARGS,
+"Close a 2bit file.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"some_file.2bit\")\n\
+>>> tb.close()\n"},
+    {"chroms", (PyCFunction)py2bitChroms, METH_VARARGS,
+"Return a chromosome: length dictionary. The order is typically not\n\
+alphabetical and the lengths are long (thus the 'L' suffix).\n\
+\n\
+Optional arguments:\n\
+    chrom: An optional chromosome name\n\
+\n\
+Returns:\n\
+    A list of chromosome lengths or a dictionary of them.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"test/test.2bit\")\n\
+>>> tb.chroms()\n\
+{'chr1': 150L, 'chr2': 100L}\n\
+\n\
+Note that you may optionally supply a specific chromosome:\n\
+\n\
+>>> tb.chroms(\"chr1\")\n\
+150L\n\
+\n\
+If you specify a non-existant chromosome then no output is produced:\n\
+\n\
+>>> tb.chroms(\"foo\")\n\
+>>>\n"},
+    {"sequence", (PyCFunction)py2bitSequence, METH_VARARGS|METH_KEYWORDS,
+"Retrieve the sequence of a chromosome, or subset of it. On error, a runtime\n\
+exception is thrown.\n\
+\n\
+Positional arguments:\n\
+    chr:   Chromosome name\n\
+\n\
+Keyword arguments:\n\
+    start: Starting position (0-based)\n\
+    end:   Ending position (1-based)\n\
+\n\
+Returns:\n\
+    A string containing the sequence.\n\
+\n\
+If start and end aren't specified, the entire chromosome is returned. If the\n\
+end value is beyond the end of the chromosome then it is adjusted accordingly.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"test/test.2bit\")\n\
+>>> tb.sequence(\"chr1\")\n\
+NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNACGTACGTACGTagctagctGATCGATCGTAGCTAGCTAGCTAGCTGATCNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN\n\
+>>> tb.sequence(\"chr1\", 24, 74)\n\
+NNNNNNNNNNNNNNNNNNNNNNNNNNACGTACGTACGTagctagctGATC\n\
+>>> tb.close()"},
+    {"bases", (PyCFunction)py2bitBases, METH_VARARGS|METH_KEYWORDS,
+"Retrieve the percentage or number of A, C, T, and Gs in a chromosome or subset\n\
+thereof. On error, a runtime exception is thrown.\n\
+\n\
+Positional arguments:\n\
+    chr:   Chromosome name\n\
+\n\
+Optional keyword arguments:\n\
+    start: Starting position (0-based)\n\
+    end:   Ending position (1-based)\n\
+    fraction: Whether to return fractional or integer values (default 'True',\n\
+              so fractional values are returned)\n\
+\n\
+Returns:\n\
+    A dictionary with nucleotide as the key and fraction (or count) as the\n\
+    value.\n\
+\n\
+If start and end aren't specified, the entire chromosome is returned. If the\n\
+end value is beyond the end of the chromosome then it is adjusted accordingly.\n\
+\n\
+Note that the fractions will sum to much less than 1 if there are hard-masked\n\
+bases. Counts may sum to less than the length of the region for the same reason.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"test/test.2bit\")\n\
+>>> tb.bases(tb, \"chr1\")\n\
+{'A': 0.08, 'C': 0.08, 'T': 0.08666666666666667, 'G': 0.08666666666666667}\n\
+>>> tb.bases(tb, \"chr1\", 24, 74)\n\
+{'A': 0.12, 'C': 0.12, 'T': 0.12, 'G': 0.12}\n\
+>>> tb.bases(tb, \"chr1\", 24, 74, True)\n\
+{'A': 6, 'C': 6, 'T': 6, 'G': 6}\n\
+>>> tb.close()"},
+    {"hardMaskedBlocks", (PyCFunction)py2bitHardMaskedBlocks, METH_VARARGS|METH_KEYWORDS,
+"Retrieve a list of hard-masked blocks on a single-chromosome (or range on it).\n\
+\n\
+Positional arguments:\n\
+    chr:   Chromosome name\n\
+\n\
+Optional keyword arguments:\n\
+    start: Starting position (0-based)\n\
+    end:   Ending position (1-based)\n\
+\n\
+Returns:\n\
+    A list of tuples, with items start and end.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"test/test.2bit\")\n\
+>>> print(tb.hardMaskedBlocks(\"chr1\")\n\
+[(0, 50), (100, 150)]\n\
+>>> print(tb.hardMaskedBlocks(\"chr1\", 75, 100)\n\
+[]\n\
+>>> print(tb.hardMaskedBlocks(\"chr1\", 75, 101)\n\
+[(100, 150)]\n\
+>>> tb.close()"},
+    {"softMaskedBlocks", (PyCFunction)py2bitSoftMaskedBlocks, METH_VARARGS|METH_KEYWORDS,
+"Retrieve a list of soft-masked blocks on a single-chromosome (or range on it).\n\
+\n\
+Positional arguments:\n\
+    chr:   Chromosome name\n\
+\n\
+Optional keyword arguments:\n\
+    start: Starting position (0-based)\n\
+    end:   Ending position (1-based)\n\
+\n\
+Returns:\n\
+    A list of tuples, with items start and end.\n\
+\n\
+>>> import py2bit\n\
+>>> tb = py2bit.open(\"test/test.2bit\", storeMasked=True)\n\
+>>> print(tb.softMaskedBlocks(\"chr1\")\n\
+[(62, 70)]\n\
+>>> print(tb.softMaskedBlocks(\"chr1\", 0, 50)\n\
+[]\n\
+>>> tb.close()"},
+    {"__enter__", (PyCFunction) py2bitEnter, METH_NOARGS, NULL},
+    {"__exit__", (PyCFunction) py2bitClose, METH_VARARGS, NULL},
+    {NULL, NULL, 0, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+struct py2bitmodule_state {
+    PyObject *error;
+};
+
+#define GETSTATE(m) ((struct py2bitmodule_state*)PyModule_GetState(m))
+
+static PyModuleDef py2bitmodule = {
+    PyModuleDef_HEAD_INIT,
+    "py2bit",
+    "A python module for accessing 2bit files",
+    -1,
+    tbMethods,
+    NULL, NULL, NULL, NULL
+};
+#endif
+
+static PyTypeObject pyTwoBit = {
+#if PY_MAJOR_VERSION >= 3
+    PyVarObject_HEAD_INIT(NULL, 0)
+#else
+    PyObject_HEAD_INIT(NULL)
+    0,              /*ob_size*/
+#endif
+    "py2bit.pyTwoBit",         /*tp_name*/
+    sizeof(pyTwoBit),          /*tp_basicsize*/
+    0,                         /*tp_itemsize*/
+    (destructor)py2bitDealloc,     /*tp_dealloc*/
+    0,                         /*tp_print*/
+    0,                         /*tp_getattr*/
+    0,                         /*tp_setattr*/
+    0,                         /*tp_compare*/
+    0,                         /*tp_repr*/
+    0,                         /*tp_as_number*/
+    0,                         /*tp_as_sequence*/
+    0,                         /*tp_as_mapping*/
+    0,                         /*tp_hash*/
+    0,                         /*tp_call*/
+    0,                         /*tp_str*/
+    PyObject_GenericGetAttr, /*tp_getattro*/
+    PyObject_GenericSetAttr, /*tp_setattro*/
+    0,                         /*tp_as_buffer*/
+#if PY_MAJOR_VERSION >= 3
+    Py_TPFLAGS_DEFAULT,        /*tp_flags*/
+#else
+    Py_TPFLAGS_HAVE_CLASS,     /*tp_flags*/
+#endif
+    "bigWig File",             /*tp_doc*/
+    0,                         /*tp_traverse*/
+    0,                         /*tp_clear*/
+    0,                         /*tp_richcompare*/
+    0,                         /*tp_weaklistoffset*/
+    0,                         /*tp_iter*/
+    0,                         /*tp_iternext*/
+    tbObjMethods,                 /*tp_methods*/
+    0,                         /*tp_members*/
+    0,                         /*tp_getset*/
+    0,                         /*tp_base*/
+    0,                         /*tp_dict*/
+    0,                         /*tp_descr_get*/
+    0,                         /*tp_descr_set*/
+    0,                         /*tp_dictoffset*/
+    0,                         /*tp_init*/
+    0,                         /*tp_alloc*/
+    0,                         /*tp_new*/
+    0,0,0,0,0,0
+};
