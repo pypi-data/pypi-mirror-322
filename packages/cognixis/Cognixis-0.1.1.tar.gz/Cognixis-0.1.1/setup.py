b45fef20a590259aa923555e4d180a1f3c63dce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 04:06:53 +0000

    Add package cognixis/0.1.1/Cognixis-0.1.1.tar.gz

---
+++ packages/cognixis/Cognixis-0.1.1.tar.gz/Cognixis-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Cognixis",  # Package name
+    version="0.1.1",  # Initial version
+    description="A PyTorch training loop utility",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sidh2690",
+    author_email="sidhu2690@gmail.com",
+    url="https://github.com/sidhu2690/CogniXis",  # Your GitHub repository URL
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.0",  # PyTorch dependency
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
