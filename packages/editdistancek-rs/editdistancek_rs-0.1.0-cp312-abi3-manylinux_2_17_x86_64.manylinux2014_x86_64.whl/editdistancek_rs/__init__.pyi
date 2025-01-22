ac45b306efb9ae672e2cdda54291240251840a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 08:05:12 +0000

    Add package editdistancek-rs/0.1.0/editdistancek_rs-0.1.0-cp312-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/editdistancek-rs/editdistancek_rs-0.1.0-cp312-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/editdistancek_rs/__init__.pyi
@@ -0,0 +1,10 @@
+__all__ = (
+    "distance",
+    "distance_unbounded",
+)
+
+def distance(s1: str, s2: str, /, k: int) -> int:
+    ...
+
+def distance_unbounded(s1: str, s2: str, /) -> int:
+    ...
