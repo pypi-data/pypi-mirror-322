d7b5673da1e272f496c5eef01c236f7ee3432892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 03:11:32 +0000

    Add package biobb-amber/5.0.4/biobb_amber-5.0.4-py3-none-any.whl

---
+++ packages/biobb-amber/biobb_amber-5.0.4-py3-none-any.whl/biobb_amber/__init__.py
@@ -0,0 +1,27 @@
+from . import (
+    ambpdb,
+    cphstats,
+    cpptraj,
+    leap,
+    nab,
+    parmed,
+    pdb4amber,
+    pmemd,
+    process,
+    sander,
+)
+
+name = "biobb_amber"
+__all__ = [
+    "ambpdb",
+    "cpptraj",
+    "leap",
+    "nab",
+    "parmed",
+    "pdb4amber",
+    "process",
+    "sander",
+    "pmemd",
+    "cphstats",
+]
+__version__ = "5.0.4"
