8bea3a86297e9b0fc108987bc5e41de30999442a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:04:45 +0000

    Add package pyrocko/2025.1.21/pyrocko-2025.1.21-cp313-cp313-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2025.1.21-cp313-cp313-win_amd64.whl/pyrocko/info.py
@@ -0,0 +1,10 @@
+# This module is automatically created from setup.py
+"""
+Version information (generated from setup.py).
+"""
+git_sha1 = '53b9a254d49afdaaa7eba106219a93e9e93aa796'
+local_modifications = False
+version = '2025.01.21'
+long_version = 'pyrocko-2025.01.21-53b9a254d49afdaaa7eba106219a93e9e93aa796-2025-01-21_08:19:56'  # noqa
+installed_date = '2025-01-21_08:19:56'
+src_path = 'C:\\Windows\\TEMP\\drone-lUmNj4vLSEVPagEs\\drone\\src'
