5826199903aea48e6ead626d8086da42742cd737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:25 +0000

    Add package ascii-monet/0.0.1/ascii_monet-0.0.1.tar.gz

---
+++ packages/ascii-monet/ascii_monet-0.0.1.tar.gz/ascii_monet-0.0.1/CHANGELOG.md
@@ -0,0 +1,9 @@
+
+## 0.0.4
+- Made git repo and change urls
+
+## 0.0.3
+- Changed dependency to opencv
+
+## 0.0.2
+- Changed pillow version
---
+++ packages/ascii-monet/ascii_monet-0.0.1.tar.gz/ascii_monet-0.0.1/src/ascii_monet/ascii_monet.py
@@ -0,0 +1,14 @@
+
+
+print()
+print('Imported ascii monet!')
+
+class ascii_monet:
+    
+    def __init__(self):
+        ...
+    
+    @classmethod
+    def generate(self, path):
+        print('in asciimonet: ', path)
+
