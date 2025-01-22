901ff7f3d2df4b95c46fb700e3874b84a224d252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:06:41 +0000

    Add package nemos/0.2.0/nemos-0.2.0.tar.gz

---
+++ packages/nemos/nemos-0.2.0.tar.gz/nemos-0.2.0/src/nemos/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+__version__ = "0.2.0"
+
+from . import (
+    basis,
+    convolve,
+    exceptions,
+    fetch,
+    glm,
+    identifiability_constraints,
+    observation_models,
+    pytrees,
+    regularizer,
+    simulation,
+    styles,
+    tree_utils,
+    type_casting,
+    utils,
+)
