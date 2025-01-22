854cfb2234d3923645477729e08d698b95813e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:04:49 +0000

    Add package robotframework-keyta/1.2.1/robotframework_keyta-1.2.1.tar.gz

---
+++ packages/robotframework-keyta/robotframework_keyta-1.2.1.tar.gz/robotframework_keyta-1.2.1/src/keyta/apps/keywords/admin/steps_inline.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.utils.translation import gettext as _
+
+from apps.common.admin import SortableTabularInlineWithDelete
+
+from apps.common.widgets import open_link_in_modal
+
+from ..forms import StepsForm
+from ..models import KeywordCall
+
+
+class StepsInline(SortableTabularInlineWithDelete):
+    model = KeywordCall
+    fields = ['to_keyword', 'first_arg', 'args']
+    form = StepsForm
+    readonly_fields = ['first_arg', 'args']
+    extra = 1  # Must be > 0 in order for SequenceSteps to work
+
+    @admin.display(description=_('Werte'))
+    def args(self, obj):
+        kw_call: KeywordCall = obj
+
+        if not kw_call.pk:
+            return '-'
+
+        if kw_call.has_empty_arg():
+            return open_link_in_modal(
+                kw_call.get_admin_url(),
+                '<i class=" error-duotone fa-solid fa-list" style="font-size: 36px;"></i>'
+            )
+        else:
+            return open_link_in_modal(
+                kw_call.get_admin_url(),
+                '<i class=" fa-solid fa-list" style="font-size: 36px"></i>'
+            )
+
+    @admin.display(description=_('1. Parameter'))
+    def first_arg(self, obj: KeywordCall):
+        return obj.parameters.first().current_value or "-"
