de7ea93ec231464ad9c188beea4bcdfca087ce3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:43 +0000

    Add package django-opfield/0.2.0/django_opfield-0.2.0-py3-none-any.whl

---
+++ packages/django-opfield/django_opfield-0.2.0-py3-none-any.whl/django_opfield/conf.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import os
+import shutil
+import sys
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import (
+        override,  # pyright: ignore[reportUnreachable]  # pragma: no cover
+    )
+
+OPFIELD_SETTINGS_NAME = "DJANGO_OPFIELD"
+
+
+def _get_user_setting(setting: str, fallback: Any = None) -> Any:
+    user_settings = getattr(settings, OPFIELD_SETTINGS_NAME, {})
+
+    if user_setting := user_settings.get(setting, fallback):
+        ret = user_setting
+    else:
+        ret = os.environ.get(setting, None)
+
+    return ret
+
+
+@dataclass(frozen=True)
+class AppSettings:
+    OP_COMMAND_TIMEOUT: int = 5  # in seconds
+    OP_CLI_PATH: str = ""
+    OP_SERVICE_ACCOUNT_TOKEN: str = ""
+
+    @override
+    def __getattribute__(self, __name: str) -> Any:
+        user_setting = _get_user_setting(__name)
+        return user_setting or super().__getattribute__(__name)
+
+    def get_op_cli_path(self) -> Path:
+        path: str | None = None
+
+        if user_cli_path := self.OP_CLI_PATH:
+            path = user_cli_path
+        else:
+            path = shutil.which("op")
+
+        if not path:
+            raise RuntimeError("Could not find the 'op' CLI command")
+
+        return Path(path).resolve()
+
+    def get_op_service_account_token(self) -> str:
+        token = self.OP_SERVICE_ACCOUNT_TOKEN
+
+        if not token:
+            raise ImproperlyConfigured("OP_SERVICE_ACCOUNT_TOKEN is not set")
+
+        return token
+
+
+app_settings = AppSettings()
---
+++ packages/django-opfield/django_opfield-0.2.0-py3-none-any.whl/django_opfield/fields.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import subprocess
+import sys
+from typing import Any
+
+from django.db import models
+
+from django_opfield.conf import app_settings
+from django_opfield.validators import OPURIValidator
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import (
+        override,  # pyright: ignore[reportUnreachable]  # pragma: no cover
+    )
+
+
+class OPField(models.CharField):
+    description = "1Password secret"
+
+    def __init__(
+        self,
+        vaults: list[str] | None = None,
+        secret_name: str | None = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> None:
+        self.vaults = vaults
+        self.secret_name = secret_name
+        kwargs.setdefault("max_length", 255)
+        super().__init__(*args, **kwargs)
+        self.validators.append(OPURIValidator(vaults=self.vaults))
+
+    @override
+    def contribute_to_class(
+        self, cls: type[models.Model], name: str, private_only: bool = False
+    ):
+        super().contribute_to_class(cls, name, private_only)
+
+        def get_secret(self: models.Model) -> str | None:
+            op = app_settings.get_op_cli_path()
+            op_uri = getattr(self, name)
+            # call to check that the token is configured correctly
+            _ = app_settings.get_op_service_account_token()
+            op_timeout = app_settings.OP_COMMAND_TIMEOUT
+            result = subprocess.run(
+                [op, "read", op_uri],
+                capture_output=True,
+                timeout=op_timeout,
+            )
+            if result.returncode != 0:
+                raise ValueError(
+                    f"Could not read secret from 1Password: {result.stderr.decode('utf-8')}"
+                )
+            return result.stdout.decode("utf-8").strip()
+
+        def set_secret(self: models.Model, value: str) -> None:
+            raise NotImplementedError("OPField does not support setting secret value")
+
+        property_name = (
+            f"{name}_secret" if self.secret_name is None else self.secret_name
+        )
+
+        setattr(cls, property_name, property(get_secret, set_secret))
+
+    @override
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        if self.vaults is not None:
+            kwargs["vaults"] = self.vaults
+        return name, path, args, kwargs
