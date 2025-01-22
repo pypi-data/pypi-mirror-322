bb1ffdd31b4e8975affe77233ee841a482215249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:06:02 +0000

    Add package xxe/0.1.2/xxe-0.1.2.tar.gz

---
+++ packages/xxe/xxe-0.1.2.tar.gz/xxe-0.1.2/xxe/__init__.py
@@ -0,0 +1,27 @@
+"""
+XXE (XML External Entity) Analysis Package
+Provides utilities for detecting and preventing XXE vulnerabilities.
+"""
+
+from .core import *
+
+__version__ = '0.1.1'
+
+
+if True:
+    def cli():
+        """Command line interface for XXE scanning"""
+        
+        parser = argparse.ArgumentParser(description='XXE vulnerability scanner')
+        parser.add_argument('file', help='XML file to scan')
+        parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+        args = parser.parse_args()
+        
+        analyzer = XXEAnalyzer(verbose=args.verbose)
+        analyzer.analyze_file(args.file)
+        report = analyzer.get_report()
+        print(json.dumps(report, indent=2))
+
+    def main():
+        """Entry point for console script"""
+        cli()
