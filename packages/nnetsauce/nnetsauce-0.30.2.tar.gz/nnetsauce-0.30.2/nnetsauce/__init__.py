a1a997c58e221b8a2ff0e209d1fd20a0b22efcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:23 +0000

    Add package nnetsauce/0.30.2/nnetsauce-0.30.2.tar.gz

---
+++ packages/nnetsauce/nnetsauce-0.30.2.tar.gz/nnetsauce-0.30.2/nnetsauce/__init__.py
@@ -0,0 +1,77 @@
+from .base.base import Base
+from .base.baseRegressor import BaseRegressor
+from .boosting.adaBoostClassifier import AdaBoostClassifier
+from .custom.customClassifier import CustomClassifier
+from .custom.customRegressor import CustomRegressor
+from .datasets import Downloader
+from .deep.deepClassifier import DeepClassifier
+from .deep.deepRegressor import DeepRegressor
+from .deep.deepMTS import DeepMTS
+from .glm.glmClassifier import GLMClassifier
+from .glm.glmRegressor import GLMRegressor
+from .lazypredict.lazydeepClassifier import LazyDeepClassifier, LazyClassifier
+from .lazypredict.lazydeepRegressor import LazyDeepRegressor, LazyRegressor
+from .lazypredict.lazydeepClassifier import LazyDeepClassifier
+from .lazypredict.lazydeepRegressor import LazyDeepRegressor
+from .lazypredict.lazydeepMTS import LazyDeepMTS, LazyMTS
+from .mts.mts import MTS
+from .mts.classical import ClassicalMTS
+from .multitask.multitaskClassifier import MultitaskClassifier
+from .multitask.simplemultitaskClassifier import SimpleMultitaskClassifier
+from .neuralnet.neuralnetregression import NeuralNetRegressor
+from .neuralnet.neuralnetclassification import NeuralNetClassifier
+from .optimizers.optimizer import Optimizer
+from .predictioninterval import PredictionInterval
+from .quantile.quantileregression import QuantileRegressor
+from .quantile.quantileclassification import QuantileClassifier
+from .randombag.randomBagClassifier import RandomBagClassifier
+from .randombag.randomBagRegressor import RandomBagRegressor
+from .ridge2.ridge2Classifier import Ridge2Classifier
+from .ridge2.ridge2Regressor import Ridge2Regressor
+from .ridge2.ridge2MultitaskClassifier import Ridge2MultitaskClassifier
+from .rvfl.bayesianrvflRegressor import BayesianRVFLRegressor
+from .rvfl.bayesianrvfl2Regressor import BayesianRVFL2Regressor
+from .sampling import SubSampler
+from .updater import RegressorUpdater, ClassifierUpdater
+from .votingregressor import MedianVotingRegressor
+
+__all__ = [
+    "AdaBoostClassifier",
+    "Base",
+    "BaseRegressor",
+    "BayesianRVFLRegressor",
+    "BayesianRVFL2Regressor",
+    "ClassicalMTS",
+    "CustomClassifier",
+    "CustomRegressor",
+    "DeepClassifier",
+    "DeepRegressor",
+    "DeepMTS",
+    "Downloader",
+    "GLMClassifier",
+    "GLMRegressor",
+    "LazyClassifier",
+    "LazyRegressor",
+    "LazyDeepClassifier",
+    "LazyDeepRegressor",
+    "LazyMTS",
+    "LazyDeepMTS",
+    "MedianVotingRegressor",
+    "MTS",
+    "MultitaskClassifier",
+    "NeuralNetRegressor",
+    "NeuralNetClassifier",
+    "PredictionInterval",
+    "SimpleMultitaskClassifier",
+    "Optimizer",
+    "QuantileRegressor",
+    "QuantileClassifier",
+    "RandomBagRegressor",
+    "RandomBagClassifier",
+    "RegressorUpdater",
+    "ClassifierUpdater",
+    "Ridge2Regressor",
+    "Ridge2Classifier",
+    "Ridge2MultitaskClassifier",
+    "SubSampler",
+]
