cf742875b445809408d601edfbfca396c7882910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:54 +0000

    Add package foliantcontrib.swaggerdoc/1.2.6/foliantcontrib_swaggerdoc-1.2.6.tar.gz

---
+++ packages/foliantcontrib.swaggerdoc/foliantcontrib_swaggerdoc-1.2.6.tar.gz/foliantcontrib_swaggerdoc-1.2.6/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = 'Documentation generator for Swagger API'
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.swaggerdoc',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='1.2.6',
+    author='Daniil Minukhin',
+    author_email='ddddsa@gmail.com',
+    packages=['foliant.preprocessors.swaggerdoc'],
+    package_data={'foliant.preprocessors.swaggerdoc': ['template/*.j2']},
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.5',
+        'jinja2',
+        'ruamel.yaml==0.17.40',
+        'foliantcontrib.utils>=1.0.2',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
