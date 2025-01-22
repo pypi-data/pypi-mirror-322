ddeccf49812687098fc14b5775d7cf6076e30b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 09:04:31 +0000

    Add package edc-microscopy/1.0.0/edc_microscopy-1.0.0.tar.gz

---
+++ packages/edc-microscopy/edc_microscopy-1.0.0.tar.gz/edc_microscopy-1.0.0/runtests.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+
+from edc_test_utils import DefaultTestSettings, func_main
+
+app_name = "edc_microscopy"
+base_dir = Path(__file__).absolute().parent
+
+project_settings = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
+    SILENCED_SYSTEM_CHECKS=[
+        "sites.E101",
+        "edc_navbar.E002",
+        "edc_navbar.E003",
+        "edc_consent.E001",
+        "edc_sites.E001",
+        "edc_sites.E002",
+    ],
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    SUBJECT_VISIT_MISSED_MODEL="edc_appointment.subjectvisitmissed",
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "multisite",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_adverse_event.apps.AppConfig",
+        "adverse_event_app.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
+        "edc_auth.apps.AppConfig",
+        "edc_dashboard.apps.AppConfig",
+        "edc_data_manager.apps.AppConfig",
+        "edc_facility.apps.AppConfig",
+        "edc_form_runners.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_list_data.apps.AppConfig",
+        "edc_listboard.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_navbar.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_review_dashboard.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        "edc_subject_dashboard.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_microscopy.apps.AppConfig",
+        "edc_appconfig.apps.AppConfig",
+    ],
+    add_dashboard_middleware=True,
+).settings
+
+
+def main():
+    func_main(project_settings, *[f"{app_name}.tests"])
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
