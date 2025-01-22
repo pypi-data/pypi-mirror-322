9fb411571f4d98c60e41c28136fe8d4b8ebdba5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:32 +0000

    Add package torchic/0.5.3/torchic-0.5.3.tar.gz

---
+++ packages/torchic/torchic-0.5.3.tar.gz/torchic-0.5.3/torchic/__init__.py
@@ -0,0 +1,20 @@
+from torchic.core.api import (
+    Dataset,
+    AxisSpec,
+    HistLoadInfo,
+    histogram,
+    Roofitter,
+)
+
+from torchic import physics
+from torchic import utils
+
+__all__ = [
+    'Dataset',
+    'AxisSpec',
+    'HistLoadInfo',
+    'histogram',
+    'Roofitter',
+    'physics',
+    'utils',
+]
