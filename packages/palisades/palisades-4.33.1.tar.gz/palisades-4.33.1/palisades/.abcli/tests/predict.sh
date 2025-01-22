6b5c63116007cdc06175b8eb8014b37aa48a76a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:50 +0000

    Add package palisades/4.33.1/palisades-4.33.1.tar.gz

---
+++ packages/palisades/palisades-4.33.1.tar.gz/palisades-4.33.1/palisades/.abcli/tests/predict.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+function test_palisades_predict() {
+    local options=$1
+
+    palisades_predict \
+        ingest,$options \
+        - \
+        - \
+        datacube-maxar_open_data-WildFires-LosAngeles-Jan-2025-11-031311102213-103001010B9A1B00
+}
