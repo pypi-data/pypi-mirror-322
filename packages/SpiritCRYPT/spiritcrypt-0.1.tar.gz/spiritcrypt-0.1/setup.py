85a5933b0bdc7577cce6dcfb655e714ca4614bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:29 +0000

    Add package SpiritCRYPT/0.1/spiritcrypt-0.1.tar.gz

---
+++ packages/SpiritCRYPT/spiritcrypt-0.1.tar.gz/spiritcrypt-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SpiritCRYPT',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+    ],
+    description='A custom encryption algorithm combining Fernet, AES 256 GCM, RSA, and Vigenère Cipher.',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
