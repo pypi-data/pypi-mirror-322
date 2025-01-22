09a00bcc9af15bb8135c7062f890f7d7862a6d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 09:04:36 +0000

    Add package seqikpy/1.0.3/seqikpy-1.0.3.tar.gz

---
+++ packages/seqikpy/seqikpy-1.0.3.tar.gz/seqikpy-1.0.3/README.md
@@ -0,0 +1,83 @@
+<div align="center">
+
+
+<p align="center">
+<img src="https://github.com/NeLy-EPFL/sequential-inverse-kinematics/blob/main/docs/images/logo.png?raw=true" width="95%">
+</p>
+
+[![License](https://img.shields.io/badge/License-Apache_2.0-red)](https://opensource.org/license/apache-2-0)
+[![python](https://img.shields.io/badge/python-3.9%2C%203.10-blue)]()
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.12601317.svg)](https://doi.org/10.5281/zenodo.12601317)
+</div>
+
+
+# 🪰 Overview
+
+`SeqIKPy` is a Python package that provides an implementation of inverse kinematics (IK) that is based on the open-source Python package [IKPy](https://github.com/Phylliade/ikpy). In constrast to the current IK approaches that aims to match only the end-effector, `SeqIKPy` is designed to calculate the joint angles of the fly body parts to align the 3D pose of the entire kinematic chain to a desired 3D pose. In particular, you can use `SeqIKPy` in the pipeline shown below.
+
+<p align="center">
+<img src="https://github.com/NeLy-EPFL/sequential-inverse-kinematics/blob/main/docs/images/pipeline.png?raw=true" width="95%">
+</p>
+
+
+# 📐 Features
+
+* **Pose alignment:** Align of 3D pose data to a fly biomechanical model, e.g., [NeuroMechFly](https://github.com/NeLy-EPFL/NeuroMechFly).
+* **Leg inverse kinematics:** Calculate leg joint angles using sequential inverse kinematics.
+* **Head inverse kinematics:** Calculate head and antenna joint angles using the vector dot product method.
+* **Visualization and animation:** Visualize and animate the results in 3D.
+
+# 🗂️ Summary of directories
+
+```
+.
+├── data: Folder containing the sample data.
+├── docs: Documentation for the website.
+├── examples: Examples and tutorials on how to use the package.
+├── seqikpy: Main package.
+└── tests: Tests for the package.
+```
+
+
+# 📚 Documentation
+
+Documentation can be found [here](https://nely-epfl.github.io/sequential-inverse-kinematics/).
+
+# 🛠️ Installation
+
+You can install the newest version of the package manually by running the following line in the terminal:
+```bash
+$ pip install https://github.com/NeLy-EPFL/sequential-inverse-kinematics.git
+```
+
+# 🏁 Quick Start
+
+Please see the quick start guide [here](https://nely-epfl.github.io/sequential-inverse-kinematics/).
+
+
+# 💻 Contributing
+
+We welcome contributions from the community. If you would like to contribute to the project, please refer to the [contribution guidelines](https://github.com/NeLy-EPFL/sequential-inverse-kinematics/blob/e10f700ef9dd925b49cb98858763225e3d64bc7b/CONTRIBUTING.md). Also, read our [code of conduct](https://github.com/NeLy-EPFL/sequential-inverse-kinematics/blob/e10f700ef9dd925b49cb98858763225e3d64bc7b/CONDUCT.md). If you have any questions, please feel free to open an issue or contact the developers.
+
+# 📖 License
+
+This project is licensed under the [Apache 2.0 License](https://github.com/NeLy-EPFL/sequential-inverse-kinematics/blob/e10f700ef9dd925b49cb98858763225e3d64bc7b/LICENSE.md).
+
+# 🐞 Issues
+If you encounter any bugs or request a new feature, please open an issue in our [issues page](https://github.com/NeLy-EPFL/sequential-inverse-kinematics/issues).
+
+# 💬 Citing
+If you find this package useful in your research, please consider citing it using the following BibTeX entry:
+
+```bibtex
+@software{ozdil2024seqikpy,
+  author       = {Ozdil, Pembe Gizem and Ijspeert, Auke and Ramdya, Pavan},
+  title        = {sequential-inverse-kinematics: v1.0.0},
+  month        = jun,
+  year         = 2024,
+  publisher    = {Zenodo},
+  version      = {v1.0.0},
+  doi          = {10.5281/zenodo.12601316},
+  url          = {https://doi.org/10.5281/zenodo.12601316}
+}
+```
---
+++ packages/seqikpy/seqikpy-1.0.3.tar.gz/seqikpy-1.0.3/tests/test_plot_grid.py
@@ -0,0 +1,150 @@
+""" Test visualization tools """
+
+import pytest
+
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+
+from PIL import Image
+import imagehash
+
+import seqikpy
+from seqikpy.visualization import (
+    load_grid_plot_data,
+    get_plot_config,
+    plot_grid,
+    video_frames_generator,
+)
+
+PKG_PATH = Path(seqikpy.__path__[0])
+
+
+def test_plot_config():
+    # Load the data - right left legs are amptuated until coxa
+    data_path = Path("./Fly001/001_RLF_coxa/behData/pose-3d")
+    # plot config determines which body parts to plot
+    exp_type, plot_config = get_plot_config(data_path)
+    assert exp_type == "coxa"
+    assert plot_config["plot_head"] == True
+    assert plot_config["plot_right_leg"] == True
+    assert plot_config["plot_left_leg"] == True
+    # Load the data - right left legs are completely amputated
+    data_path = Path("./Fly001/001_RLF/behData/pose-3d")
+    exp_type, plot_config = get_plot_config(data_path)
+    assert exp_type == "RLF"
+    assert plot_config["plot_head"] == True
+    assert plot_config["plot_right_leg"] == False
+    assert plot_config["plot_left_leg"] == False
+    # Load the data - right leg amputated
+    data_path = Path("./Fly001/001_RF/behData/pose-3d")
+    exp_type, plot_config = get_plot_config(data_path)
+    assert exp_type == "RF"
+    assert plot_config["plot_head"] == True
+    assert plot_config["plot_right_leg"] == False
+    assert plot_config["plot_left_leg"] == True
+    # Load the data - intact
+    data_path = Path("./Fly001/001_Beh/behData/pose-3d")
+    exp_type, plot_config = get_plot_config(data_path)
+    assert exp_type == "Beh"
+    assert plot_config["plot_head"] == True
+    assert plot_config["plot_right_leg"] == True
+    assert plot_config["plot_left_leg"] == True
+    # Check error with a wrong path name
+    data_path = Path("./Fly001/001_Beh/pose-3d")
+    with pytest.raises(Exception):
+        exp_type, plot_config = get_plot_config(data_path)
+
+
+def test_grid_plot():
+    # Set up the constant variables
+    leg_joint_angle_names = [
+        "ThC_yaw",
+        "ThC_pitch",
+        "ThC_roll",
+        "CTr_pitch",
+        "CTr_roll",
+        "FTi_pitch",
+        "TiTa_pitch",
+    ]
+
+    head_angles_to_plot = [
+        "Angle_head_roll",
+        "Angle_head_pitch",
+        "Angle_head_yaw",
+        "Angle_antenna_pitch_L",
+        "Angle_antenna_pitch_R",
+    ]
+
+    # Load the data
+    data_path = PKG_PATH / "../data/anipose_220807_Fly002_002"
+    video_path_front = data_path / "camera_3.mp4"
+    video_path_side = data_path / "camera_5.mp4"
+
+    # plot config determines which body parts to plot
+    # as our path is not standard, we need to specify plot config
+    plot_config = {
+        "plot_head": True,
+        "plot_right_leg": True,
+        "plot_left_leg": True,
+    }
+
+    # loads the joint angles and the aligned pos
+    joint_angles, aligned_pose = load_grid_plot_data(data_path)
+
+    # as neck pose is one dimensional, make it consistent with the other key points
+    aligned_pose["Neck"] = np.tile(
+        aligned_pose["Neck"], (aligned_pose["RF_leg"].shape[0], 1)
+    ).reshape(-1, 1, 3)
+
+    # Start, end of the plotting data
+    t_start = 0
+    t_end = 200
+
+    # t: snapshot to show
+    t = t_start + 100
+    fps = 100
+
+    # Stimulation applied? If so, when
+    stim_lines = [50]
+
+    # Ignore this, we put it because we cropped the data in the previous tutorial
+    crop_time = 400
+
+    fly_frames_front = video_frames_generator(
+        video_path_front, t_start + crop_time, t_end + crop_time, stim_lines
+    )
+    fly_frames_side = video_frames_generator(
+        video_path_side, t_start + crop_time, t_end + crop_time, stim_lines
+    )
+
+    fig = plot_grid(
+        img_front=list(fly_frames_front)[t - t_start],
+        img_side=list(fly_frames_side)[t - t_start],
+        aligned_pose=aligned_pose,
+        joint_angles=joint_angles,
+        leg_angles_to_plot=leg_joint_angle_names,
+        head_angles_to_plot=head_angles_to_plot,
+        key_points_to_trail={"LF_leg": [3], "RF_leg": [3]},
+        marker_trail="x",
+        t=t,
+        t_start=t_start,
+        t_end=t_end,
+        fps=fps,
+        trail=30,
+        t_interval=100,
+        stim_lines=stim_lines,
+        export_path=PKG_PATH / "../tests" / f"generate_frame_{t}.png",
+        **plot_config,
+    )
+
+    true_hash = imagehash.average_hash(
+        Image.open(PKG_PATH / "../tests" / "test_frame_100.png")
+    )
+    generated_hash = imagehash.average_hash(
+        Image.open(PKG_PATH / "../tests" / f"generate_frame_{t}.png")
+    )
+
+    assert (
+        true_hash - generated_hash
+    ) <= 5, "The generated image is not similar to the true image"
