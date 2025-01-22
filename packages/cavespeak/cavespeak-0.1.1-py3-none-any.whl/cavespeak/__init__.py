cfdf72c3cf9cda7b2776127cfd126381f3487885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:03 +0000

    Add package cavespeak/0.1.1/cavespeak-0.1.1-py3-none-any.whl

---
+++ packages/cavespeak/cavespeak-0.1.1-py3-none-any.whl/cavespeak/__init__.py
@@ -0,0 +1,8 @@
+# cavespeak/__init__.py
+
+# Importing module1 so it's accessible at the package level
+from .main import cspeak  # This imports everything from main.py
+
+# Package metadata
+__version__ = "v0.1.1"
+__author__ = "GeeseCanHonk"
---
+++ packages/cavespeak/cavespeak-0.1.1-py3-none-any.whl/cavespeak/main.py
@@ -0,0 +1,1 @@
+# cavespeak/main.py
