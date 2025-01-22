58c9a4ade2bbfa2c9a13e2114c1040aabb852b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 14:05:02 +0000

    Add package dicom-anon/1.0.13.post2/dicom-anon-1.0.13.post2.tar.gz

---
+++ packages/dicom-anon/dicom-anon-1.0.13.post2.tar.gz/dicom-anon-1.0.13.post2/tests/test_anon.py
@@ -0,0 +1,133 @@
+import copy
+import pytest
+import warnings
+
+from pathlib import Path
+from functools import lru_cache
+from pydicom import dcmread
+from pydicom.config import settings, IGNORE
+from pydicom.data import get_testdata_files
+
+from dicomanonymizer.simpledicomanonymizer import anonymize_dataset
+from dicomanonymizer.dicom_anonymization_databases import dicomfields_2023
+
+# Ignore warnings from pydicom validation
+settings.writing_validation_mode = IGNORE
+settings.reading_validation_mode = IGNORE
+
+warnings.filterwarnings("ignore")
+
+
+def get_all_failed():  # sourcery skip: inline-immediately-returned-variable
+    # The following files are intended to fail dcmread
+    # No point including them for anonymization testing
+    dcmread_failed = [
+        "ExplVR_BigEndNoMeta.dcm",
+        "ExplVR_LitEndNoMeta.dcm",
+        "no_meta.dcm",
+        "rtstruct.dcm",
+        "OT-PAL-8-face.dcm",
+    ]
+
+    return dcmread_failed
+
+
+@lru_cache(maxsize=None)
+def get_passing_files():
+    all_files = get_testdata_files("*.dcm")
+    all_failed = get_all_failed()
+    return [x for x in all_files if Path(x).name not in all_failed]
+
+
+@pytest.fixture(scope="module", params=get_passing_files())
+def orig_anon_dataset(request):
+    orig_ds = dcmread(request.param, force=True)
+    orig_ds.filename = (
+        None  # Non-None value causes warnings in copy(). Not needed for this testing
+    )
+    anon_ds = copy.deepcopy(orig_ds)
+    anonymize_dataset(anon_ds)
+    return (orig_ds, anon_ds)
+
+
+def test_deleted_tags_are_removed(orig_anon_dataset):
+    orig_ds, anon_ds = orig_anon_dataset
+    deleted_tags = dicomfields_2023.X_TAGS
+
+    for tt in deleted_tags:  # sourcery skip: no-loop-in-tests
+        if (
+            len(tt) == 2 and tt in orig_ds
+        ):  # sourcery skip: merge-nested-ifs, no-conditionals-in-tests
+            # TODO: Investigate why Date type are replaced instead of deleted
+            # See issue https://github.com/KitwareMedical/dicom-anonymizer/issues/56
+            if orig_ds[tt].VR != "DA":  # sourcery skip: no-conditionals-in-tests
+                assert tt not in anon_ds, (
+                    f"({tt[0]:04X},{tt[1]:04x}):{orig_ds[tt].value}->{anon_ds[tt].value}"
+                )
+
+
+changed_tags = (
+    dicomfields_2023.U_TAGS
+    + dicomfields_2023.D_TAGS
+    + dicomfields_2023.Z_D_TAGS
+    + dicomfields_2023.X_D_TAGS
+    + dicomfields_2023.X_Z_D_TAGS
+    + dicomfields_2023.X_Z_U_STAR_TAGS
+)
+
+empty_values = (0, "", "00010101", "000000.00", "ANONYMIZED")
+
+
+def is_elem_replaced(orig, anon) -> bool:
+    if orig.VR == "SQ":
+        for x, y in zip(orig.value, anon.value):
+            for tt in changed_tags:
+                if tt in x and len(x[tt].value) > 0:
+                    assert tt in y, f"({tt[0]:04X},{tt[1]:04x}):{x[tt].value}->missing!"
+                    assert is_elem_replaced(x[tt], y[tt]), (
+                        f"({tt[0]:04X},{tt[1]:04x}):{x[tt].value} not replaced"
+                    )
+        return True
+
+    return orig.value != anon.value if orig.value not in empty_values else True
+
+
+def test_changed_tags_are_replaced(orig_anon_dataset):
+    orig_ds, anon_ds = orig_anon_dataset
+
+    for tt in changed_tags:  # sourcery skip: no-loop-in-tests
+        if tt in orig_ds:  # sourcery skip: no-conditionals-in-tests
+            assert tt in anon_ds, (
+                f"({tt[0]:04X},{tt[1]:04x}):{orig_ds[tt].value}->missing!"
+            )
+            assert is_elem_replaced(orig_ds[tt], anon_ds[tt]), (
+                f"({tt[0]:04X},{tt[1]:04x}):{orig_ds[tt].value} not replaced"
+            )
+
+
+empty_tags = dicomfields_2023.Z_TAGS + dicomfields_2023.X_Z_TAGS
+
+
+def is_elem_empty(elem) -> bool:
+    if elem.VR == "SQ":
+        for x in elem.value:
+            for tt in empty_tags:
+                if tt in x and len(x[tt].value) > 0:
+                    assert is_elem_empty(x[tt]), (
+                        f"({tt[0]:04X},{tt[1]:04x}):{x[tt].value} is not empty"
+                    )
+        return True
+
+    return elem.value in empty_values
+
+
+def test_empty_tags_are_emptied(orig_anon_dataset):
+    orig_ds, anon_ds = orig_anon_dataset
+
+    for tt in empty_tags:  # sourcery skip: no-loop-in-tests
+        if (
+            tt in orig_ds and len(orig_ds[tt].value) > 0
+        ):  # sourcery skip: no-conditionals-in-tests
+            assert is_elem_empty(anon_ds[tt]), (
+                f"({tt[0]:04X},{tt[1]:04x}):{anon_ds[tt].value} is not empty"
+            )
---
+++ packages/dicom-anon/dicom-anon-1.0.13.post2.tar.gz/dicom-anon-1.0.13.post2/tests/test_anonymization_without_dicom.py
@@ -0,0 +1,113 @@
+import pydicom
+
+from dicomanonymizer import anonymize_dataset
+from dicomanonymizer.simpledicomanonymizer import (
+    empty,
+    initialize_actions,
+    initialize_actions_2024b,
+)
+
+
+def test_anonymization_without_dicom_file():
+    # Create a list of tags object that should contains id, type and value
+    fields = [
+        {  # Replaced by Anonymized
+            "id": (0x0040, 0xA123),
+            "type": "LO",
+            "value": "Annie de la Fontaine",
+        },
+        {  # Replaced with empty value
+            "id": (0x0008, 0x0050),
+            "type": "TM",
+            "value": "bar",
+        },
+        {  # Deleted
+            "id": (0x0018, 0x4000),
+            "type": "VR",
+            "value": "foo",
+        },
+        {  # Relaced with empty value via extra anonymization rules since no tags with IS value type are anonymized by default.
+            "id": (0x0020, 0x0012),
+            "type": "IS",
+            "value": "123",
+        },
+    ]
+
+    # Create a readable dataset for pydicom
+    data = pydicom.Dataset()
+
+    # Add each field into the dataset
+    for field in fields:  # sourcery skip: no-loop-in-tests
+        data.add_new(field["id"], field["type"], field["value"])
+
+    anonymize_dataset(data, extra_anonymization_rules={(0x0020, 0x0012): empty})
+
+    assert data[(0x0040, 0xA123)].value == "ANONYMIZED"
+    assert data[(0x0008, 0x0050)].value == "000000.00"
+    assert (0x0018, 0x4000) not in data
+    assert int(data[(0x0020, 0x0012)].value) == 0
+
+
+def test_anonymization_of_ranged_tags_without_dicom_file():
+    """Test the anonymization of ranged tags in a dataset without a DICOM file."""
+    # Create Curve Data (50xx, xxxx) which must be deleted
+    # per (0x5000, 0x0000, 0xFF00, 0x0000) rule.
+    fields = [
+        {  # to be deleted
+            "id": (0x5011, 0x0110),  # Curve Data Descriptor
+            "type": "US",
+            "value": "dummy curve data descriptor 1",
+        },
+        {  # to be deleted
+            "id": (0x5012, 0x0112),  # Coordinate Start Value
+            "type": "US",
+            "value": "dummy curve data descriptor 2",
+        },
+    ]
+
+    # Create a readable dataset for pydicom
+    data = pydicom.Dataset()
+
+    # Add each field into the dataset
+    for field in fields:  # sourcery skip: no-loop-in-tests
+        data.add_new(field["id"], field["type"], field["value"])
+
+    anon_ds = data.copy()
+    anonymize_dataset(anon_ds)
+
+    # Check that the dataset has been anonymized
+    assert (0x5011, 0x0110) not in anon_ds
+    assert (0x5012, 0x0112) not in anon_ds
+
+
+def test_switching_dicom_versions():
+    """To confirm the different behavior of annonymization beteen dicom versions of 2023 and 2024b."""
+    fields = [
+        {  # Replaced by Anonymized
+            "id": (0x0010, 0x0020),
+            "type": "LO",
+            "value": "Test Patient ID",
+        },
+    ]
+
+    # Create a readable dataset for pydicom
+    data = pydicom.Dataset()
+    data_2023 = pydicom.Dataset()
+    data_2024b = pydicom.Dataset()
+
+    for field in fields:  # sourcery skip: no-loop-in-tests
+        data.add_new(field["id"], field["type"], field["value"])
+        data_2023.add_new(field["id"], field["type"], field["value"])
+        data_2024b.add_new(field["id"], field["type"], field["value"])
+
+    anonymize_dataset(data, base_rules_gen=initialize_actions)
+    anonymize_dataset(
+        data_2023, base_rules_gen=lambda: initialize_actions("dicomfields_2023")
+    )
+    anonymize_dataset(data_2024b, base_rules_gen=initialize_actions_2024b)
+
+    assert data[(0x0010, 0x0020)].value == ""  # default behavior which is DICOM 2023.
+    assert data_2023[(0x0010, 0x0020)].value == ""  # same as the default.
+    assert (
+        data_2024b[(0x0010, 0x0020)].value == "ANONYMIZED"
+    )  # 2024b differs from the default
