e10c86d9044654772dfbb389f366a5498e72c6db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:29 +0000

    Add package envvault/0.1.2/envvault-0.1.2-py3-none-any.whl

---
+++ packages/envvault/envvault-0.1.2-py3-none-any.whl/envvault/settings.py
@@ -0,0 +1,52 @@
+import os
+from functools import lru_cache
+
+from pydantic import create_model
+from pydantic_settings import BaseSettings
+
+from .core import CredentialsManager
+
+
+class Settings(BaseSettings):
+    """
+    Manages environment variables using pydantic.
+    """
+
+    @classmethod
+    def from_credentials(cls, env_name="development"):
+        """
+        Dynamically loads environment variables from an encrypted .env.enc file.
+
+        :param env_name: Environment name (e.g., development, production).
+        :return: Settings instance.
+        """
+        # Decrypt the .env.enc file
+        credentials = CredentialsManager(env_name=env_name).decrypt_env()
+
+        # Dynamically create a model
+        fields = {}
+        for key, value in credentials.items():
+            # Infer the type based on the value's content
+            if value.isdigit():
+                fields[key] = (int, ...)  # Integer type
+            elif value.lower() in ("true", "false"):
+                fields[key] = (bool, ...)  # Boolean type
+            else:
+                fields[key] = (str, ...)  # Default string type
+
+        DynamicSettings = create_model(
+            "DynamicSettings", **fields, __base__=BaseSettings
+        )
+
+        # Return the dynamically created model instance
+        return DynamicSettings(**credentials)
+
+
+@lru_cache
+def get_settings():
+    return Settings.from_credentials(
+        env_name=os.getenv("ENV") or os.getenv("NODE_ENV") or "development"
+    )
+
+
+# envvault = get_settings()
