e9343f2d45fbee2fb4b9c2dab902fbf7f5da5a0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:12 +0000

    Add package chain_harvester/1.5.0/chain_harvester-1.5.0-py3-none-any.whl

---
+++ packages/chain_harvester/chain_harvester-1.5.0-py3-none-any.whl/chain_harvester/helpers.py
@@ -0,0 +1,33 @@
+from chain_harvester.networks.arbitrum.mainnet import ArbitrumMainnetChain
+from chain_harvester.networks.base.mainnet import BaseMainnetChain
+from chain_harvester.networks.ethereum.mainnet import EthereumMainnetChain
+from chain_harvester.networks.filecoin.mainnet import FilecoinMainnetChain
+from chain_harvester.networks.optimism.mainnet import OptimismMainnetChain
+from chain_harvester.networks.polygon.mainnet import PolygonMainnetChain
+from chain_harvester.networks.rari.mainnet import RariMainnetChain
+from chain_harvester.networks.scroll.mainnet import ScrollMainnetChain
+from chain_harvester.networks.avalanche.mainnet import AvalancheMainnetChain
+
+
+def get_chain(network, *args, **kwargs):
+    match network:
+        case "ethereum":
+            return EthereumMainnetChain(*args, **kwargs)
+        case "polygon":
+            return PolygonMainnetChain(*args, **kwargs)
+        case "optimism":
+            return OptimismMainnetChain(*args, **kwargs)
+        case "arbitrum":
+            return ArbitrumMainnetChain(*args, **kwargs)
+        case "base":
+            return BaseMainnetChain(*args, **kwargs)
+        case "scroll":
+            return ScrollMainnetChain(*args, **kwargs)
+        case "filecoin":
+            return FilecoinMainnetChain(*args, **kwargs)
+        case "rari":
+            return RariMainnetChain(*args, **kwargs)
+        case "avalanche":
+            return AvalancheMainnetChain(*args, **kwargs)
+        case _:
+            raise ValueError(f"Unknown network: {network}")
