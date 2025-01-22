cc611fd405d0e1f867a587bbb3948f6a4f043e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:05:15 +0000

    Add package pyrocko/2025.1.21/pyrocko-2025.1.21-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/pyrocko/pyrocko-2025.1.21-cp310-cp310-macosx_10_9_universal2.whl/pyrocko/info.py
@@ -0,0 +1,10 @@
+# This module is automatically created from setup.py
+"""
+Version information (generated from setup.py).
+"""
+git_sha1 = '53b9a254d49afdaaa7eba106219a93e9e93aa796'
+local_modifications = False
+version = '2025.01.21'
+long_version = 'pyrocko-2025.01.21-53b9a254d49afdaaa7eba106219a93e9e93aa796-2025-01-21_17:09:18'  # noqa
+installed_date = '2025-01-21_17:09:18'
+src_path = '/private/var/folders/d4/j4dz67n12fb9w265kz3h8dbw0000gp/T/drone-9gjFHwO9gy1jfipv/drone/src'
