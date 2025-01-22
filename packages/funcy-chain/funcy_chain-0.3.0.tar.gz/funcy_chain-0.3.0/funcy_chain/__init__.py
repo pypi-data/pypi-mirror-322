3852d497c583b20db086e509d9e30ba23b80fc2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:06:12 +0000

    Add package funcy-chain/0.3.0/funcy_chain-0.3.0.tar.gz

---
+++ packages/funcy-chain/funcy_chain-0.3.0.tar.gz/funcy_chain-0.3.0/funcy_chain/__init__.py
@@ -0,0 +1,12 @@
+"""Method chaining with funcy functions."""
+
+__version__ = "0.3.0"
+
+
+from .chain import Chain
+from .iterchain import IterChain
+
+__all__ = [
+    "Chain",
+    "IterChain",
+]
