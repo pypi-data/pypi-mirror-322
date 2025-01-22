054d6405f580a5f30dbfa3904e7f0c2cc6b5450b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:05:53 +0000

    Add package celery-redis-cluster/0.1.6/celery_redis_cluster-0.1.6.tar.gz

---
+++ packages/celery-redis-cluster/celery_redis_cluster-0.1.6.tar.gz/celery_redis_cluster-0.1.6/src/celery_redis_cluster_backend/__init__.py
@@ -0,0 +1,17 @@
+"""
+Celery Redis Cluster Backend
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Redis Cluster backend implementation for Celery.
+"""
+
+from .backend import RedisClusterBackend
+
+__version__ = '0.1.6'
+
+# Register the backend
+def install_redis_cluster_backend():
+    from celery.app.backends import BACKEND_ALIASES
+    BACKEND_ALIASES['rediscluster'] = 'celery_redis_cluster_backend.backend:RedisClusterBackend'
+
+__all__ = ['RedisClusterBackend', "install_redis_cluster_backend"]
