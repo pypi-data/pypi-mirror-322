9addf41cf1dab562ea8d59a884a893f4b72cfb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 03:08:23 +0000

    Add package ndevsecuritychecker/0.1.1/ndevsecuritychecker-0.1.1-py3-none-any.whl

---
+++ packages/ndevsecuritychecker/ndevsecuritychecker-0.1.1-py3-none-any.whl/tests/test_sec_names.py
@@ -0,0 +1,9 @@
+from dev_utils import sec_names
+
+def test_add_variables():
+    assert sec_names.add_new_sensible_var_name("test_new_var_name_snake_case", "snake_case") == True 
+    assert sec_names.add_new_sensible_var_name("test_new_var_name_uppercase", "uppercase") == False
+    assert sec_names.add_new_sensible_var_name("TEST_NEW_VAR_NAME_UPPERCASE", "uppercase") == True
+    assert sec_names.add_new_sensible_var_name("TestNewVarNameCamelCase", "camel_case") == True
+    assert sec_names.add_new_sensible_var_name("TestNewVarNameCamelCase", "snake_case") == False
+    assert sec_names.add_new_sensible_var_name("TestNewVarNameCamelCase", "uppercase") == False
