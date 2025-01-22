8feb3c423d3c61e8ecede12be31a5964fe942934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 03:09:26 +0000

    Add package maniik/0.0.0/maniik-0.0.0.tar.gz

---
+++ packages/maniik/maniik-0.0.0.tar.gz/maniik-0.0.0/src/maniik/maniik.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def info():
+    print('Drarga jat f Agadiiir hhhh, Dir lez ecotor')
+
+def chkon_ana():
+    print("Wa Smitni l9iiirch 🦈🦈")    
