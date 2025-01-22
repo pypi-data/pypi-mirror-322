08078866ef8193c2c68d291ff4a6ccef8b1b8bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:22 +0000

    Add package bomiot/0.4.75/bomiot-0.4.75.tar.gz

---
+++ packages/bomiot/bomiot-0.4.75.tar.gz/bomiot-0.4.75/bomiot/templates/src/components/toc/DocsToc.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="q-pa-md">
+      <div v-html="tocRouter" style="margin-top: 25px; width: 99%; max-width: 99%"></div>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useMDDataStore } from "stores/mdDocs"
+
+
+const mdStore = useMDDataStore()
+
+const tocRouter = computed(() => mdStore.tocRouter)
+
+</script>
+
