1d8b5e7382af4cdc5b49c17ea5c060360ebed80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:31 +0000

    Add package pyoctopus/0.1.7/pyoctopus-0.1.7-py3-none-any.whl

---
+++ packages/pyoctopus/pyoctopus-0.1.7-py3-none-any.whl/pyoctopus/store/store.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+
+from ..request import Request, State
+
+
+class Store:
+
+    @abstractmethod
+    def put(self, r: Request) -> bool:
+        pass
+
+    @abstractmethod
+    def get(self) -> Request | None:
+        pass
+
+    @abstractmethod
+    def exists(self, id: str) -> bool:
+        pass
+
+    @abstractmethod
+    def update_state(self, r: Request, state: State, msg: str = None):
+        pass
+
+    @abstractmethod
+    def reply_failed(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_statistics(self) -> (int, int, int, int, int):
+        pass
---
+++ packages/pyoctopus/pyoctopus-0.1.7-py3-none-any.whl/pyoctopus/types.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from typing import TypeVar, Callable, Any
+
+from .request import Request
+from .response import Response
+
+R = TypeVar('R', bound=[type])
+
+Converter = TypeVar('Converter', bound=[Callable[[str], Any]])
+
+Matcher = TypeVar('Matcher', bound=[Callable[[Response], bool]])
+
+Processor = TypeVar('Processor', bound=[Callable[[Response], list[Request]]])
+
+Collector = TypeVar('Collector', bound=[Callable[[R], None]])
+
+Terminable = TypeVar('Terminable', bound=[Callable[[R, str, Response], bool]])
+
+
+class Downloader(Enum):
+    REQUESTS = 'requests',
+    CURL_CFFI = 'curl_cffi'
