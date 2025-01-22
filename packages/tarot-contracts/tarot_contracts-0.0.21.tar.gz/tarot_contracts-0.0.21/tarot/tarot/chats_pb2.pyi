4a645ef31bf240ef436fc2c2e37819f4db3a75b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 03:10:38 +0000

    Add package tarot-contracts/0.0.21/tarot_contracts-0.0.21.tar.gz

---
+++ packages/tarot-contracts/tarot_contracts-0.0.21.tar.gz/tarot_contracts-0.0.21/tarot/tarot/chats_pb2.pyi
@@ -0,0 +1,19 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class CreateChatRequest(_message.Message):
+    __slots__ = ["name"]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    def __init__(self, name: _Optional[str] = ...) -> None: ...
+
+class CreateChatResponse(_message.Message):
+    __slots__ = ["id", "name"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    name: str
+    def __init__(self, id: _Optional[str] = ..., name: _Optional[str] = ...) -> None: ...
