14e877c4df05952aaa3854c4d65df2016dae270b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:06:01 +0000

    Add package mediacatch/0.4.0/mediacatch-0.4.0-py3-none-any.whl

---
+++ packages/mediacatch/mediacatch-0.4.0-py3-none-any.whl/mediacatch/__init__.py
@@ -0,0 +1,16 @@
+import logging
+import sys
+import os
+
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    format='[%(asctime)s][%(levelname)s][%(name)s] %(message)s',
+    datefmt='%d/%m/%Y-%H:%M:%S',
+    handlers=[logging.StreamHandler(sys.stdout)],
+    level=logging.INFO,
+)
+
+load_dotenv()
+
+__version__ = '0.4.0'
