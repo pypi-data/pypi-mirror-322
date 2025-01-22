208897b93763dcab9ec3b87ab2fa4fd9ff8fd49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:06:19 +0000

    Add package edc-ltfu/1.0.0/edc_ltfu-1.0.0-py3-none-any.whl

---
+++ packages/edc-ltfu/edc_ltfu-1.0.0-py3-none-any.whl/edc_ltfu/tests/models.py
@@ -0,0 +1,19 @@
+from edc_action_item.models import ActionModelMixin
+from edc_model.models import BaseUuidModel
+from edc_offstudy.constants import END_OF_STUDY_ACTION
+from edc_sites.model_mixins import SiteModelMixin
+from edc_visit_schedule.model_mixins import OffScheduleModelMixin
+
+
+class OffSchedule(
+    OffScheduleModelMixin,
+    SiteModelMixin,
+    ActionModelMixin,
+    BaseUuidModel,
+):
+    action_name = END_OF_STUDY_ACTION
+
+    tracking_identifier_prefix = "ST"
+
+    class Meta(OffScheduleModelMixin.Meta, BaseUuidModel.Meta):
+        pass
