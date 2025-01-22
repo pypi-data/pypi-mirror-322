ed86e9afc12935324db6b131d4dc67c516fa0d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 07:04:48 +0000

    Add package boutpp-nightly/5.2.0.dev726/boutpp_nightly-v5.2.0.dev726.tar.gz

---
+++ packages/boutpp-nightly/boutpp_nightly-v5.2.0.dev726.tar.gz/boutpp_nightly-v5.2.0.dev726/tests/MMS/spatial/d2dz2/test_d2dz2.cxx
@@ -0,0 +1,29 @@
+/*
+ * Test D2DZ2 operator without time integration
+ */
+
+#include <bout/bout.hxx>
+#include <bout/derivs.hxx>
+#include <bout/field_factory.hxx>
+
+using bout::globals::mesh;
+
+int main(int argc, char** argv) {
+
+  BoutInitialise(argc, argv);
+
+  Field3D input = FieldFactory::get()->create3D("input", Options::getRoot(), mesh);
+  Field3D solution = FieldFactory::get()->create3D("solution", Options::getRoot(), mesh);
+
+  Field3D result = D2DZ2(input);
+
+  Options dump;
+  dump["input"] = input;
+  dump["solution"] = solution;
+  dump["result"] = result;
+  bout::writeDefaultOutputFile(dump);
+
+  BoutFinalise();
+
+  return 0;
+}
