094c22f98af472967100a86b0b5b6734c898fa3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:57 +0000

    Add package vidyut/0.4.0/vidyut-0.4.0-cp313-cp313t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/vidyut/vidyut-0.4.0-cp313-cp313t-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/vidyut/prakriya.py
@@ -0,0 +1,35 @@
+"""`vidyut.prakriya` generates Sanskrit words by applying Paninian rules."""
+
+from vidyut.vidyut import prakriya as __mod
+
+Vyakarana = __mod.Vyakarana
+
+# Data types
+Data = __mod.Data
+DhatupathaEntry = __mod.DhatupathaEntry
+Sutra = __mod.Sutra
+Source = __mod.Source
+
+# Deprecated data type
+Entry = __mod.DhatupathaEntry
+
+# Input types
+Dhatu = __mod.Dhatu
+DhatuPada = __mod.DhatuPada
+Pratipadika = __mod.Pratipadika
+Antargana = __mod.Antargana
+Gana = __mod.Gana
+Krt = __mod.Krt
+Lakara = __mod.Lakara
+Linga = __mod.Linga
+Pada = __mod.Pada
+Prayoga = __mod.Prayoga
+Purusha = __mod.Purusha
+Sanadi = __mod.Sanadi
+Taddhita = __mod.Taddhita
+Vacana = __mod.Vacana
+Vibhakti = __mod.Vibhakti
+
+# Output types
+Prakriya = __mod.Prakriya
+Step = __mod.Step
