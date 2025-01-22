e66c793bd3c6e35aaa8fda82d8b209b1ca616cf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:54 +0000

    Add package vxparser/1.4.3.post3/vxparser-1.4.3.post3-py3-none-any.whl

---
+++ packages/vxparser/vxparser-1.4.3.post3-py3-none-any.whl/vxparser/helper/sites/__init__.py
@@ -0,0 +1,3 @@
+from . import dokus4, einschalten, hdfilme, kinokiste, kkiste, megakino, movie2k, serienstream, xcine
+# hdfilme_1, moflix-stream, topstreamfilm, filmpalast, movie4k, streamcloud,
+sites = [dokus4, einschalten, hdfilme, kinokiste, kkiste, megakino, movie2k, serienstream, xcine]
