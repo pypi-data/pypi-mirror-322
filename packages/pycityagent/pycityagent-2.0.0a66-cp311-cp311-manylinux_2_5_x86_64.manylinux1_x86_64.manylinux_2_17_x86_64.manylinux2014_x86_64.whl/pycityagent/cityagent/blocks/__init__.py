12d35b13d6b164f020701754443a562849f844ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:06:57 +0000

    Add package pycityagent/2.0.0a66/pycityagent-2.0.0a66-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pycityagent/pycityagent-2.0.0a66-cp311-cp311-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pycityagent/cityagent/blocks/__init__.py
@@ -0,0 +1,18 @@
+from .mobility_block import MobilityBlock
+from .cognition_block import CognitionBlock
+from .plan_block import PlanBlock
+from .needs_block import NeedsBlock
+from .social_block import SocialBlock
+from .economy_block import EconomyBlock
+from .other_block import OtherBlock
+
+__all__ = [
+    "MobilityBlock",
+    "CognitionBlock",
+    "PlanBlock",
+    "NeedsBlock",
+    "SocialBlock",
+    "EconomyBlock",
+    "OtherBlock",
+    "LongTermDecisionBlock",    
+]
