f1b5e9a92fcfa5c5d1b07e8b6e5f831459b7c50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 08:05:01 +0000

    Add package yta-general-utils/0.0.173/yta_general_utils-0.0.173-py3-none-any.whl

---
+++ packages/yta-general-utils/yta_general_utils-0.0.173-py3-none-any.whl/yta_general_utils/math/rate_functions/rate_function_argument.py
@@ -0,0 +1,31 @@
+from yta_general_utils.programming.parameter_validator import PythonValidator
+from yta_general_utils.math.rate_functions.rate_function import RateFunction
+from yta_general_utils.math.rate_functions.rate_function_graphic import RateFunctionGraphic
+from typing import Union
+
+
+class RateFunctionArgument:
+    """
+    An argument that can act as a rate function
+    modifying the calculations for the animations
+    we need. Anything that is able to recalculate
+    an 'n' normalized value according and return
+    it is accepted here.
+
+    Check the accepted types in the '__init__'
+    method.
+    """
+    default = RateFunction.LINEAR
+
+    def __init__(self, argument: Union[RateFunction, RateFunctionGraphic]):
+        if not PythonValidator.is_instance(argument, [RateFunction, RateFunctionGraphic]):
+            raise Exception('The provided "argument" parameter is not an instance of RateFunction nor RateFunctionGraphic class.')
+        
+        self.argument = argument
+    
+    def get_n_value(self, n: float):
+        """
+        Get the corresponding value for the given 'n' normalized
+        value that must be between 0 and 1.
+        """
+        return self.argument.get_n_value(n)
