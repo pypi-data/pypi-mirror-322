dd0a6da77d4bdb9306fda69bbe36f10c66c1a018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:22 +0000

    Add package digipigi/0.2.0/digipigi-0.2.0.tar.gz

---
+++ packages/digipigi/digipigi-0.2.0.tar.gz/digipigi-0.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="digipigi",  # Package name
+    version="0.2.0",  # Initial version
+    description="API to use the digipigi",    author="Sebastian Huber",
+    author_email="huberse@phys.ethz.ch",
+    url="https://gitlab.phys.ethz.ch/cmtqo-projects/delicate/digipigi.git",
+    packages=[],
+    py_modules=["digipigi.digipigi"], 
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.18.0"
+    ]
+)
