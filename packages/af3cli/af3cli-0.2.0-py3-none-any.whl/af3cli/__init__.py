a06d11de8fb1523ed549392e299ac604d362c4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:46 +0000

    Add package af3cli/0.2.0/af3cli-0.2.0-py3-none-any.whl

---
+++ packages/af3cli/af3cli-0.2.0-py3-none-any.whl/af3cli/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.2.0"
+
+from .ligand import Ligand, LigandType
+from .sequence import Sequence, SequenceType
+from .sequence import Template, TemplateType, MSA
+from .sequence import ResidueModification, NucleotideModification
+from .bond import Atom, Bond
+from .input import InputFile
+from .builder import InputBuilder
