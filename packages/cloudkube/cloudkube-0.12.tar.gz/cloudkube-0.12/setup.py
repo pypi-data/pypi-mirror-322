fa2edb7c08f0ac6d6630e43e5784ad719f87de47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 04:07:19 +0000

    Add package cloudkube/0.12/cloudkube-0.12.tar.gz

---
+++ packages/cloudkube/cloudkube-0.12.tar.gz/cloudkube-0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cloudkube",
+    version="0.12",
+    author="Chan Ming Han",
+    email="chanminghan00@gmail.com",
+    description="minikube for the cloud",
+    python_requires=">=3.11",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "cloudkube=cloudkube:main",
+        ],
+    },
+    install_requires=[
+        "boto3",
+        "python_terraform",
+        "kubernetes",
+        "requests",
+        "GitPython",
+    ],
+    package_data={"cloudkube": ["terraform/*"]},
+    include_package_data=True,
+)
