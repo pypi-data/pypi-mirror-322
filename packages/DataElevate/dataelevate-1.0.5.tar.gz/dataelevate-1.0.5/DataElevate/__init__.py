9f440271b66cbf7d0d00201cc1574aacf6cd4827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 05:05:48 +0000

    Add package DataElevate/1.0.5/dataelevate-1.0.5.tar.gz

---
+++ packages/DataElevate/dataelevate-1.0.5.tar.gz/dataelevate-1.0.5/DataElevate/__init__.py
@@ -0,0 +1,21 @@
+# Import required modules and classes for public access
+from .download import Download_data
+from .load_data import Load_data
+from .extractor import Extractor
+from . import GoogleDrive_api
+from . import Kaggle_api
+from . import EDAerror
+from . import load_data_api
+__all__ = [
+    'Download_data',
+    'Load_data',
+    'Extractor',
+    'GoogleDrive_api',
+    'Kaggle_api',
+    'EDAerror',
+    'load_data_api'
+]
+
+__version__ = "1.0.5" 
+__author__ = "Monal Bhiwgade"
+__email__ = "3051monal@gmail.com"
