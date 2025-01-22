5862c77b852f4f18a07c9e5318474b74affcf8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:06:12 +0000

    Add package hrenpack/1.1.2/hrenpack-1.1.2.tar.gz

---
+++ packages/hrenpack/hrenpack-1.1.2.tar.gz/hrenpack-1.1.2/hrenpack/framework/pygame.py
@@ -0,0 +1,35 @@
+import pygame
+from dataclasses import dataclass
+
+
+@dataclass
+class Keyboard:
+    A = pygame.K_a
+    B = pygame.K_b
+    C = pygame.K_c
+    D = pygame.K_d
+    E = pygame.K_e
+    F = pygame.K_f
+    G = pygame.K_g
+    H = pygame.K_h
+    I = pygame.K_i
+    J = pygame.K_j
+    K = pygame.K_k
+    L = pygame.K_l
+    M = pygame.K_m
+    N = pygame.K_n
+    O = pygame.K_o
+    P = pygame.K_p
+    Q = pygame.K_q
+    R = pygame.K_r
+    S = pygame.K_s
+    T = pygame.K_t
+    U = pygame.K_u
+    V = pygame.K_v
+    W = pygame.K_w
+    X = pygame.K_x
+    Y = pygame.K_y
+    Z = pygame.K_z
+
+    SPACE = pygame.K_SPACE
+
