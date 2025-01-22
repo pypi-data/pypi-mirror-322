a095bbac07e0034fc3a9935933e6c74cbe96f337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:31 +0000

    Add package low-index/1.2.1/low_index-1.2.1-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/low-index/low_index-1.2.1-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/low_index/__init__.py
@@ -0,0 +1,8 @@
+from ._low_index import *
+
+# Version 1.1 was used for the cython implemention
+# Version 1.2 (and later) is used for the C++/pybind11 implementation
+__version__ = '1.2.1'
+
+def version():
+    return __version__
