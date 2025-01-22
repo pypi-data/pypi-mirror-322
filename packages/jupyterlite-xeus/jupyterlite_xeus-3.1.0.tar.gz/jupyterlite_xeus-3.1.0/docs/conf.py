904c97fb07d5c7e7ef8104505d5b3bedc83874b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:06:09 +0000

    Add package jupyterlite-xeus/3.1.0/jupyterlite_xeus-3.1.0.tar.gz

---
+++ packages/jupyterlite-xeus/jupyterlite_xeus-3.1.0.tar.gz/jupyterlite_xeus-3.1.0/docs/conf.py
@@ -0,0 +1,35 @@
+extensions = [
+    "jupyterlite_sphinx",
+    "myst_parser",
+]
+
+myst_enable_extensions = [
+    "linkify",
+]
+
+master_doc = "index"
+source_suffix = ".rst"
+
+project = "jupyterlite-xeus"
+copyright = "JupyterLite Team"
+author = "JupyterLite Team"
+
+exclude_patterns = []
+
+html_theme = "pydata_sphinx_theme"
+
+html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
+
+jupyterlite_dir = "."
+jupyterlite_silence = False
+
+html_theme_options = {
+    "logo": {
+        "image_light": "jupyterlite.svg",
+        "image_dark": "jupyterlite.svg",
+    }
+}
