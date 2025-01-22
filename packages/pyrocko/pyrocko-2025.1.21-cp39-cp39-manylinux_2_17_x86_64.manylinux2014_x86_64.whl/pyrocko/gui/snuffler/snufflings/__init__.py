2658dbd630b506617c56b8ed878aedc5a72aaa2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 06:05:25 +0000

    Add package pyrocko/2025.1.21/pyrocko-2025.1.21-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyrocko/pyrocko-2025.1.21-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyrocko/gui/snuffler/snufflings/__init__.py
@@ -0,0 +1,24 @@
+# https://pyrocko.org - GPLv3
+#
+# The Pyrocko Developers, 21st Century
+# ---|P------/S----------~Lg----------
+
+from . import (
+    minmax, rms, stalta, geofon, ampspec, catalogs, download, cake_phase,
+    seismosizer, map, polarization, spectrogram, deep_picker)
+
+modules = [
+    minmax, rms, download, stalta, geofon, ampspec, catalogs, map, cake_phase,
+    seismosizer, polarization, spectrogram, deep_picker]
+
+
+def __snufflings__():
+    snufflings = []
+    for mod in modules:
+        snufflings.extend(mod.__snufflings__())
+
+    for snuffling in snufflings:
+        snuffling.setup()
+        snuffling.set_name(snuffling.get_name() + ' (builtin)')
+
+    return snufflings
