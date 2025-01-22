1bf7104a8e723bd6fd9086fa135e4034fcf7d466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 15:06:23 +0000

    Add package py3s/0.1.3/py3s-0.1.3.tar.gz

---
+++ packages/py3s/py3s-0.1.3.tar.gz/py3s-0.1.3/README.md
@@ -0,0 +1,2 @@
+# py3s
+python solscan.io sdk -> pysss -> py3s
---
+++ packages/py3s/py3s-0.1.3.tar.gz/py3s-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='py3s',
+    version='0.1.3',
+    packages=['py3s'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dwdwow/py3s',
+)
