749ea540e9d7fbeede6c02d67f41ca2f3b72a040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-22 16:05:30 +0000

    Add package earth-data-fetcher/0.1.1/earth_data_fetcher-0.1.1-py3-none-any.whl

---
+++ packages/earth-data-fetcher/earth_data_fetcher-0.1.1-py3-none-any.whl/src/earth_data_fetcher/__init__.py
@@ -0,0 +1,4 @@
+from .utils import make_downloader as make_downloader
+from .utils import read_sources as read_data_sources_yaml
+from .download_dataset import DownloaderLazyXarray
+from .download_https import DownloaderFsspec
