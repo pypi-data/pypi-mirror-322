e0a3f8d4431843ef6b1517381361176850fc8922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:05:52 +0000

    Add package torchCompactRadius/0.2.4/torchcompactradius-0.2.4.tar.gz

---
+++ packages/torchCompactRadius/torchcompactradius-0.2.4.tar.gz/torchcompactradius-0.2.4/src/torchCompactRadius/compactHashing/cppWrapper.py
@@ -0,0 +1,52 @@
+from torchCompactRadius.compiler import compileSourceFiles
+from typing import Optional, List
+import torch
+import warnings
+# from torch.profiler import record_function
+
+hasPrecompiledCPU = False
+hasPrecompiledGPU = False
+
+try:
+    import torchCompactRadius_cuda
+    neighborSearch_cpp = torchCompactRadius_cuda
+    if not torch.cuda.is_available():
+        warnings.warn('Precompiled GPU version of the neighbor search is being used even though CUDA is not available!')
+    hasPrecompiledGPU = True
+except ImportError:
+    # warnings.warn('No cuda version of the neighbor search is available.')
+    pass
+if not hasPrecompiledGPU:
+    try:
+        import torchCompactRadius_cpu
+        neighborSearch_cpp = torchCompactRadius_cpu
+        if torch.cuda.is_available():
+            warnings.warn('Precompiled CPU version of the neighbor search is being used even though CUDA is available!')
+        hasPrecompiledCPU = True
+    except ImportError:
+        # warnings.warn('No cpu version of the neighbor search is available.')
+        pass
+
+if not hasPrecompiledCPU and not hasPrecompiledGPU:
+    warnings.warn('No precompiled version of the neighbor search is available.')
+    neighborSearch_cpp = compileSourceFiles(
+        ['cppSrc/neighborhoodDynamic_cpu.cpp',  'cppSrc/neighborhoodDynamic_cuda.cu', 
+        'cppSrc/neighborhoodFixed_cpu.cpp',     'cppSrc/neighborhoodFixed_cuda.cu',
+        'cppSrc/hashing_cpu.cpp',               'cppSrc/hashing_cuda.cu',
+        'cppSrc/neighborhoodSmall_cpu.cpp',     'cppSrc/neighborhoodSmall_cuda.cu',
+        'cppSrc/cppWrapper.cpp'], module_name = 'torchCompactRadius_jit', verbose = False, openMP = True, verboseCuda = False, cuda_arch = None)
+
+countNeighbors_cpp = neighborSearch_cpp.countNeighbors
+buildNeighborList_cpp = neighborSearch_cpp.buildNeighborList
+countNeighborsFixed_cpp = neighborSearch_cpp.countNeighborsFixed
+buildNeighborListFixed_cpp = neighborSearch_cpp.buildNeighborListFixed
+hashCells_cpp = neighborSearch_cpp.computeHashIndices
+neighborSearchSmall = neighborSearch_cpp.neighborSearchSmall
+neighborSearchSmallFixed = neighborSearch_cpp.neighborSearchSmallFixed
+
+# countNeighbors_cpp = None
+# buildNeighborList_cpp = None
+# countNeighborsFixed_cpp = None
+# buildNeighborListFixed_cpp = None
+# neighborSearchSmall = None
+# neighborSearchSmallFixed = None
---
+++ packages/torchCompactRadius/torchcompactradius-0.2.4.tar.gz/torchcompactradius-0.2.4/src/torchCompactRadius/cppSrc/cppWrapper.cpp
@@ -0,0 +1,18 @@
+#include "neighborhood.h"
+#include "hashing.h"
+#include "neighborhoodSmall.h"
+
+
+// Create the python bindings for the C++ functions
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("countNeighbors", &countNeighbors, "Count the Number of Neighbors (C++) using a precomputed hash table and cell map");
+  m.def("buildNeighborList", &buildNeighborList, "Build the Neighborlist (C++) using a precomputed hash table and cell map as well as neighbor counts");
+    
+  m.def("countNeighborsFixed", &countNeighborsFixed, "Count the Number of Neighbors (C++) using a precomputed hash table and cell map (fixed support radius)");
+  m.def("buildNeighborListFixed", &buildNeighborListFixed, "Build the Neighborlist (C++) using a precomputed hash table and cell map as well as neighbor counts (fixed support radius)");
+
+  m.def("computeHashIndices", &computeHashIndices, "Compute the Hash Indices (C++)");
+
+  m.def("neighborSearchSmall", &neighborSearchSmall, "Neighbor Search (C++)");
+  m.def("neighborSearchSmallFixed", &neighborSearchSmallFixed, "Neighbor Search (C++) (fixed support radius)");
+}
