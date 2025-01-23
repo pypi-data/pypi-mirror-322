ca58fb6371d62cad893289eaabf0feafd88e05b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:02 +0000

    Add package owl-eye/0.1.6/owl_eye-0.1.6-py3-none-any.whl

---
+++ packages/owl-eye/owl_eye-0.1.6-py3-none-any.whl/owl_eye/__init__.py
@@ -0,0 +1,11 @@
+from .banner import display_banner
+from .utils import check_network
+from .whois_lookup import get_data
+
+__all__ = [
+    "display_banner",
+    "check_network",
+    "get_data"
+]
+
+__version__ = "0.2"
