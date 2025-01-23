677fd4e279f60c21800d33ff27fd04808d80ff24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 15:06:03 +0000

    Add package maisaedu-poormans-dms/1.2.7/maisaedu-poormans-dms-1.2.7.tar.gz

---
+++ packages/maisaedu-poormans-dms/maisaedu-poormans-dms-1.2.7.tar.gz/maisaedu-poormans-dms-1.2.7/maisaedu_poormans_dms/redshift_dynamo_migration/models/Struct.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class Struct:
+    s3_url: str # S3 URL for the full load
+    database: str # Database name, used to build the table names in the queries
+    source_schema: str # Schema where the MVW is
+    source_table: str # DynamoDB table name
+
+    target_schema: str
+    target_table: str
+
+    incremental_interval_delta: int # Interval in seconds to consider for incremental loads
