8eeffb1a5e87d910bf4a4fe551eeee3a497bdc60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:17 +0000

    Add package antares-results/0.0.2/antares_results-0.0.2-py3-none-any.whl

---
+++ packages/antares-results/antares_results-0.0.2-py3-none-any.whl/amplitudes/jjjj/test_coefficients.py
@@ -0,0 +1,30 @@
+import numpy
+import pytest
+
+from pathlib import Path
+from termcolor import colored
+
+from antares.terms.terms import LoadResults
+
+from .momenta import oPs
+from .target_values import target_values
+
+
+this_script_path = Path(__file__).resolve().parent
+
+
+@pytest.mark.parametrize(
+    "helicity, coeff_targets", target_values.items()
+)
+def test_independent_coefficients(helicity, coeff_targets):
+    for coeff, target in coeff_targets.items():
+        print(helicity, coeff)
+        oTerms = LoadResults(f"{this_script_path}/gggggg/{helicity}/{coeff}")[0]
+        actual = 0 if oTerms == 0 else complex(oTerms[0](oPs))
+        print("actual: ", actual)
+        print("target: ", target)
+        if numpy.isclose(actual, target):
+            print(colored("PASSED", "green"))
+        else:
+            print(colored("FAILED", "red"))
+        print()
