053061b40f182cdb83360ed7db5ce0899ae08ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:29 +0000

    Add package types-aiobotocore-frauddetector/2.19.0/types_aiobotocore_frauddetector-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-frauddetector/types_aiobotocore_frauddetector-2.19.0.tar.gz/types_aiobotocore_frauddetector-2.19.0/types_aiobotocore_frauddetector/__init__.py
@@ -0,0 +1,28 @@
+"""
+Main interface for frauddetector service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_frauddetector import (
+        Client,
+        FraudDetectorClient,
+    )
+
+    session = get_session()
+    async with session.create_client("frauddetector") as client:
+        client: FraudDetectorClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import FraudDetectorClient
+
+Client = FraudDetectorClient
+
+
+__all__ = ("Client", "FraudDetectorClient")
