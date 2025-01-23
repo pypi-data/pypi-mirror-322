91364f0ba8ab0f37eae6b7033d018ccbd8aa9e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:11 +0000

    Add package multiversx-sdk/1.0.0b2/multiversx_sdk-1.0.0b2-py3-none-any.whl

---
+++ packages/multiversx-sdk/multiversx_sdk-1.0.0b2-py3-none-any.whl/multiversx_sdk/abi/address_value_test.py
@@ -0,0 +1,57 @@
+import re
+from types import SimpleNamespace
+
+import pytest
+
+from multiversx_sdk.abi.address_value import AddressValue
+from multiversx_sdk.core.address import Address
+
+
+def test_set_payload_and_get_payload():
+    # Simple
+    pubkey = bytes.fromhex("0139472eff6886771a982f3083da5d421f24c29181e63888228dc81ca60d69e1")
+    value = AddressValue()
+    value.set_payload(pubkey)
+    assert value.get_payload() == pubkey
+
+    # Simple (from Address)
+    address = Address.new_from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
+    value = AddressValue()
+    value.set_payload(address)
+    assert value.get_payload() == address.get_public_key()
+
+    # Simple (from bech32)
+    address = Address.new_from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
+    value = AddressValue()
+    value.set_payload(address.to_bech32())
+    assert value.get_payload() == address.get_public_key()
+
+    # From dict using a bech32 address
+    address = Address.new_from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
+    value = AddressValue()
+    value.set_payload({"bech32": address.to_bech32()})
+    assert value.get_payload() == address.get_public_key()
+
+    # From dict using a hex address
+    address = Address.new_from_bech32("erd1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssycr6th")
+    value = AddressValue()
+    value.set_payload({"hex": address.to_hex()})
+    assert value.get_payload() == address.get_public_key()
+
+    value = AddressValue.new_from_address(address)
+    assert value.get_payload() == address.get_public_key()
+
+    # With errors
+    with pytest.raises(ValueError, match=re.escape("public key (address) has invalid length: 3")):
+        AddressValue().set_payload(bytes([1, 2, 3]))
+
+    # With errors
+    with pytest.raises(TypeError, match="cannot convert 'types.SimpleNamespace' object to bytes"):
+        AddressValue().set_payload(SimpleNamespace(a=1, b=2, c=3))
+
+    # With errors
+    with pytest.raises(
+        ValueError,
+        match="cannot extract pubkey from dictionary: missing 'bech32' or 'hex' keys",
+    ):
+        AddressValue().set_payload({})
---
+++ packages/multiversx-sdk/multiversx_sdk-1.0.0b2-py3-none-any.whl/multiversx_sdk/core/code_metadata_test.py
@@ -0,0 +1,46 @@
+import pytest
+
+from multiversx_sdk.core.code_metadata import CodeMetadata
+
+
+def test_code_metadata_new_from_bytes():
+    metadata = CodeMetadata.new_from_bytes(bytes([0x05, 0x00]))
+    assert metadata.upgradeable
+    assert metadata.readable
+    assert metadata.payable is False
+    assert metadata.payable_by_contract is False
+
+    metadata = CodeMetadata.new_from_bytes(bytes([0x05, 0x06]))
+    assert metadata.upgradeable
+    assert metadata.readable
+    assert metadata.payable
+    assert metadata.payable_by_contract
+
+    metadata = CodeMetadata.new_from_bytes(bytes([0x05, 0x04]))
+    assert metadata.upgradeable
+    assert metadata.readable
+    assert metadata.payable is False
+    assert metadata.payable_by_contract
+
+    metadata = CodeMetadata.new_from_bytes(bytes([0x00, 0x00]))
+    assert metadata.upgradeable is False
+    assert metadata.readable is False
+    assert metadata.payable is False
+    assert metadata.payable_by_contract is False
+
+    with pytest.raises(ValueError, match="code metadata buffer has length 4, expected 2"):
+        CodeMetadata.new_from_bytes(bytes([0x00, 0x01, 0x02, 0x03]))
+
+
+def test_code_metadata_serialize():
+    assert CodeMetadata().serialize() == bytes([0x05, 0x00])
+    assert CodeMetadata(upgradeable=True, readable=True).serialize() == bytes([0x05, 0x00])
+    assert CodeMetadata(upgradeable=True, readable=True, payable=True, payable_by_contract=True).serialize() == bytes(
+        [0x05, 0x06]
+    )
+    assert CodeMetadata(upgradeable=True, readable=True, payable=False, payable_by_contract=True).serialize() == bytes(
+        [0x05, 0x04]
+    )
+    assert CodeMetadata(
+        upgradeable=False, readable=False, payable=False, payable_by_contract=False
+    ).serialize() == bytes([0x00, 0x00])
