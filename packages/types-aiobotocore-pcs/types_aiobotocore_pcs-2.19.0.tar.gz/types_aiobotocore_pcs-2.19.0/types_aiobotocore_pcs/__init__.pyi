c414131c555d12350e2e8eb1b8353dd13e0fa4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:27 +0000

    Add package types-aiobotocore-pcs/2.19.0/types_aiobotocore_pcs-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-pcs/types_aiobotocore_pcs-2.19.0.tar.gz/types_aiobotocore_pcs-2.19.0/types_aiobotocore_pcs/__init__.pyi
@@ -0,0 +1,41 @@
+"""
+Main interface for pcs service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_pcs import (
+        Client,
+        ListClustersPaginator,
+        ListComputeNodeGroupsPaginator,
+        ListQueuesPaginator,
+        ParallelComputingServiceClient,
+    )
+
+    session = get_session()
+    async with session.create_client("pcs") as client:
+        client: ParallelComputingServiceClient
+        ...
+
+
+    list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+    list_compute_node_groups_paginator: ListComputeNodeGroupsPaginator = client.get_paginator("list_compute_node_groups")
+    list_queues_paginator: ListQueuesPaginator = client.get_paginator("list_queues")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import ParallelComputingServiceClient
+from .paginator import ListClustersPaginator, ListComputeNodeGroupsPaginator, ListQueuesPaginator
+
+Client = ParallelComputingServiceClient
+
+__all__ = (
+    "Client",
+    "ListClustersPaginator",
+    "ListComputeNodeGroupsPaginator",
+    "ListQueuesPaginator",
+    "ParallelComputingServiceClient",
+)
