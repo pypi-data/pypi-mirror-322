18e4fbbb64fa145859b013cc9ed15137beac2b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:05:36 +0000

    Add package lazy-action/0.0.2/lazy_action-0.0.2.tar.gz

---
+++ packages/lazy-action/lazy_action-0.0.2.tar.gz/lazy_action-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lazy_action",
+    version="0.0.2",
+    author="Irid",
+    author_email="irid.zzy@gmail.com",
+    description="lazy_action make your func lazy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iridesc/lazy_action",
+    packages=setuptools.find_packages(),
+    # include_package_data=True,
+    package_data={
+    },
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "diskcache"
+    ],
+)
