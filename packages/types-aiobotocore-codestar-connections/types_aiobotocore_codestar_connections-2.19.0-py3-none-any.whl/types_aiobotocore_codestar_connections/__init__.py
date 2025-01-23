3065ae1b50c0db6a42015b8673f65bc0f6b384dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:30 +0000

    Add package types-aiobotocore-codestar-connections/2.19.0/types_aiobotocore_codestar_connections-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-codestar-connections/types_aiobotocore_codestar_connections-2.19.0-py3-none-any.whl/types_aiobotocore_codestar_connections/__init__.py
@@ -0,0 +1,28 @@
+"""
+Main interface for codestar-connections service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_codestar_connections import (
+        Client,
+        CodeStarconnectionsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("codestar-connections") as client:
+        client: CodeStarconnectionsClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import CodeStarconnectionsClient
+
+Client = CodeStarconnectionsClient
+
+
+__all__ = ("Client", "CodeStarconnectionsClient")
