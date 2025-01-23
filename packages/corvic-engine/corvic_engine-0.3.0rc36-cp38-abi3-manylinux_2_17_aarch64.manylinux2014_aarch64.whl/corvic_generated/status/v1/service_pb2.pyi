e774d5dbf221330d7b17e8bd34d4118bd36231b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:05:43 +0000

    Add package corvic-engine/0.3.0rc36/corvic_engine-0.3.0rc36-cp38-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/corvic-engine/corvic_engine-0.3.0rc36-cp38-abi3-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/corvic_generated/status/v1/service_pb2.pyi
@@ -0,0 +1,15 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class GetShaResponse(_message.Message):
+    __slots__ = ("sha",)
+    SHA_FIELD_NUMBER: _ClassVar[int]
+    sha: str
+    def __init__(self, sha: _Optional[str] = ...) -> None: ...
+
+class GetShaRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
