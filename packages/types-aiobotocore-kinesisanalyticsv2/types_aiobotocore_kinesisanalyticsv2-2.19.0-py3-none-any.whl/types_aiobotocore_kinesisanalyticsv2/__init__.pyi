e0fd3916b7244aa5a5d74a130612036be847643c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:22 +0000

    Add package types-aiobotocore-kinesisanalyticsv2/2.19.0/types_aiobotocore_kinesisanalyticsv2-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-kinesisanalyticsv2/types_aiobotocore_kinesisanalyticsv2-2.19.0-py3-none-any.whl/types_aiobotocore_kinesisanalyticsv2/__init__.pyi
@@ -0,0 +1,49 @@
+"""
+Main interface for kinesisanalyticsv2 service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_kinesisanalyticsv2 import (
+        Client,
+        KinesisAnalyticsV2Client,
+        ListApplicationOperationsPaginator,
+        ListApplicationSnapshotsPaginator,
+        ListApplicationVersionsPaginator,
+        ListApplicationsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("kinesisanalyticsv2") as client:
+        client: KinesisAnalyticsV2Client
+        ...
+
+
+    list_application_operations_paginator: ListApplicationOperationsPaginator = client.get_paginator("list_application_operations")
+    list_application_snapshots_paginator: ListApplicationSnapshotsPaginator = client.get_paginator("list_application_snapshots")
+    list_application_versions_paginator: ListApplicationVersionsPaginator = client.get_paginator("list_application_versions")
+    list_applications_paginator: ListApplicationsPaginator = client.get_paginator("list_applications")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import KinesisAnalyticsV2Client
+from .paginator import (
+    ListApplicationOperationsPaginator,
+    ListApplicationSnapshotsPaginator,
+    ListApplicationsPaginator,
+    ListApplicationVersionsPaginator,
+)
+
+Client = KinesisAnalyticsV2Client
+
+__all__ = (
+    "Client",
+    "KinesisAnalyticsV2Client",
+    "ListApplicationOperationsPaginator",
+    "ListApplicationSnapshotsPaginator",
+    "ListApplicationVersionsPaginator",
+    "ListApplicationsPaginator",
+)
