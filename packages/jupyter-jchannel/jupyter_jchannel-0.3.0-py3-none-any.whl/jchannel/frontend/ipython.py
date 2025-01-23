8f3adbe317e7c15b880a8afa926026cb9306de4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:06:52 +0000

    Add package jupyter-jchannel/0.3.0/jupyter_jchannel-0.3.0-py3-none-any.whl

---
+++ packages/jupyter-jchannel/jupyter_jchannel-0.3.0-py3-none-any.whl/jchannel/frontend/ipython.py
@@ -0,0 +1,39 @@
+# Copyright (c) 2024 Marcelo Hashimoto
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+
+
+from IPython.display import HTML, display, clear_output
+from ipywidgets import Output
+from jchannel.frontend.abstract import AbstractFrontend
+
+
+SHEET = '''
+.cell-output-ipywidget-background {
+    background-color: transparent !important;
+}
+'''
+
+
+class IPythonFrontend(AbstractFrontend):
+    def __init__(self):
+        super().__init__()
+        self.output = Output(_view_count=0)
+
+    def _run(self, code):
+        if self.output._view_count == 0:
+            style = HTML(f'<style>{SHEET}</style>')
+            display(style)
+            display(self.output)
+
+        with self.output:
+            # NOTE: Using IPython.display.JavaScript
+            # would be more elegant, but does not seem
+            # to be working in Visual Studio Code.
+            script = HTML(f'<script>{code}</script>')
+            display(script)
+            clear_output()
