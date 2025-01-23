a8148546d2e86e499a273bb6dd911c51b0928ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:13 +0000

    Add package otto64/0.0.1/otto64-0.0.1.tar.gz

---
+++ packages/otto64/otto64-0.0.1.tar.gz/otto64-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='otto64',
+    version='0.0.1',
+    description='otto64 encoding/decoding',
+    url='https://github.com/suizhuchen/otto64-python',
+    author='suizhuchen & hua-zhi-wan',
+)
---
+++ packages/otto64/otto64-0.0.1.tar.gz/otto64-0.0.1/test/test.py
@@ -0,0 +1,3 @@
+import otto64
+
+print(otto64.text_to_otto64('hello world'))
