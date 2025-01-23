531781b12ce28afee50cb7584b2972b8617f045b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:08 +0000

    Add package t-utils-kit/0.1.4/t_utils_kit-0.1.4.tar.gz

---
+++ packages/t-utils-kit/t_utils_kit-0.1.4.tar.gz/t_utils_kit-0.1.4/t_utils/robocloud_utils.py
@@ -0,0 +1,15 @@
+"""Constants Module."""
+import os
+
+RC_ORGANIZATION_ID = os.environ.get("RC_ORGANIZATION_ID")
+RC_WORKSPACE_ID = os.environ.get("RC_WORKSPACE_ID")
+RC_RUNTIME_ID = os.environ.get("RC_RUNTIME_ID")
+RC_PROCESS_ID = os.environ.get("RC_PROCESS_ID")
+RC_PROCESS_NAME = os.environ.get("RC_PROCESS_NAME")
+RC_PROCESS_RUN_ID = os.environ.get("RC_PROCESS_RUN_ID")
+RC_PROCESS_RUN_NUMBER = os.environ.get("RC_PROCESS_RUN_NUMBER")
+RC_ACTIVITY_ID = os.environ.get("RC_ACTIVITY_ID")
+RC_ACTIVITY_NAME = os.environ.get("RC_ACTIVITY_NAME")
+RC_ACTIVITY_RUN_ID = os.environ.get("RC_ACTIVITY_RUN_ID")
+RC_ACTIVITY_RUN_NUMBER = os.environ.get("RC_ACTIVITY_RUN_NUMBER")
+RC_WORKITEM_ID = os.environ.get("RC_WORKITEM_ID")
