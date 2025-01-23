31248f9e1e71f58da82a6a610bbecb2ab05d087b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:05:48 +0000

    Add package fast-bitrix24/1.8.3/fast_bitrix24-1.8.3.tar.gz

---
+++ packages/fast-bitrix24/fast_bitrix24-1.8.3.tar.gz/fast_bitrix24-1.8.3/fast_bitrix24/correct_asyncio.py
@@ -0,0 +1,9 @@
+import asyncio
+import sys
+
+if (
+    sys.version_info[0] == 3
+    and sys.version_info[1] >= 8
+    and sys.platform.startswith("win")
+):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
