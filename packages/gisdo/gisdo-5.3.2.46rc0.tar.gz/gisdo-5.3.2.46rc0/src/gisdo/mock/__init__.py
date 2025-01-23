19153ba6a5c65e79bc09ea45470509b872a4a4a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:05:52 +0000

    Add package gisdo/5.3.2.46rc0/gisdo-5.3.2.46rc0.tar.gz

---
+++ packages/gisdo/gisdo-5.3.2.46rc0.tar.gz/gisdo-5.3.2.46rc0/src/gisdo/mock/__init__.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+
+from distutils.dir_util import copy_tree
+from distutils.dir_util import mkpath
+import os
+
+
+def create_mock_server(path):
+    """Создания Mock-сервера для приёма федеральной отчётности
+    по указанному пути."""
+
+    # Копируем исходники
+    copy_tree(
+        os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)
+            ),
+            'src'
+        ),
+        path
+    )
+
+    # Создаём папку для хранения отчётов
+    mkpath(os.path.join(path, 'xml', '3'))
