c4a1370ee18702a7b90a7833064e8bd80a16f8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:04:59 +0000

    Add package lonelypsp/0.0.28/lonelypsp-0.0.28.tar.gz

---
+++ packages/lonelypsp/lonelypsp-0.0.28.tar.gz/lonelypsp-0.0.28/src/lonelypsp/tracing/impl/simple/stateless/root.py
@@ -0,0 +1,48 @@
+from typing import TYPE_CHECKING, Literal, Type
+
+from lonelypsp.tracing.impl.simple.config import SimpleTracingConfig
+from lonelypsp.tracing.impl.simple.db import SimpleTracingDBSidecar
+from lonelypsp.tracing.impl.simple.stateless.notify import (
+    SimpleStatelessTracingNotifyOnReceived,
+    SimpleStatelessTracingNotifyStart,
+)
+from lonelypsp.tracing.stateless.notify import (
+    StatelessTracingNotifyOnReceived,
+    StatelessTracingNotifyStart,
+)
+from lonelypsp.tracing.stateless.root import (
+    StatelessTracingBroadcasterRoot,
+    StatelessTracingSubscriberRoot,
+)
+
+
+class SimpleStatelessTracingSubscriberRoot:
+    def __init__(self, db: SimpleTracingDBSidecar, config: SimpleTracingConfig) -> None:
+        self.db = db
+        self.config = config
+
+    def notify(self, initializer: Literal[None], /) -> StatelessTracingNotifyStart:
+        return SimpleStatelessTracingNotifyStart(
+            db=self.db,
+            config=self.config,
+        )
+
+
+class SimpleStatelessTracingBroadcasterRoot:
+    def __init__(self, db: SimpleTracingDBSidecar, config: SimpleTracingConfig) -> None:
+        self.db = db
+        self.config = config
+
+    def receive_notify(
+        self, initializer: Literal[None], /
+    ) -> StatelessTracingNotifyOnReceived:
+        return SimpleStatelessTracingNotifyOnReceived(db=self.db, config=self.config)
+
+
+if TYPE_CHECKING:
+    _: Type[StatelessTracingSubscriberRoot[Literal[None]]] = (
+        SimpleStatelessTracingSubscriberRoot
+    )
+    __: Type[StatelessTracingBroadcasterRoot[Literal[None]]] = (
+        SimpleStatelessTracingBroadcasterRoot
+    )
