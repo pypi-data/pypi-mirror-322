38ffefb93029588649ceb17587cf9928fa548dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:05:37 +0000

    Add package freeact/0.3.3/freeact-0.3.3.tar.gz

---
+++ packages/freeact/freeact-0.3.3.tar.gz/freeact-0.3.3/freeact/__init__.py
@@ -0,0 +1,15 @@
+from freeact.agent import CodeActAgent, CodeActAgentResponse, CodeActAgentTurn, CodeExecution, CodeExecutionResult
+from freeact.executor import CodeExecutionContainer, CodeExecutionEnvironment, CodeExecutor, execution_environment
+from freeact.model import (
+    Claude,
+    ClaudeModelName,
+    CodeActModel,
+    CodeActModelResponse,
+    CodeActModelTurn,
+    DeepSeek,
+    Gemini,
+    GeminiLive,
+    GeminiModelName,
+    GenericModel,
+    QwenCoder,
+)
