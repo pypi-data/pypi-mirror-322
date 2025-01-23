02c481c9895687baf69ce27f881d9fd6eaa8edc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:27 +0000

    Add package types-aiobotocore-docdb-elastic/2.19.0/types_aiobotocore_docdb_elastic-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-docdb-elastic/types_aiobotocore_docdb_elastic-2.19.0.tar.gz/types_aiobotocore_docdb_elastic-2.19.0/types_aiobotocore_docdb_elastic/__init__.pyi
@@ -0,0 +1,45 @@
+"""
+Main interface for docdb-elastic service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_docdb_elastic import (
+        Client,
+        DocDBElasticClient,
+        ListClusterSnapshotsPaginator,
+        ListClustersPaginator,
+        ListPendingMaintenanceActionsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("docdb-elastic") as client:
+        client: DocDBElasticClient
+        ...
+
+
+    list_cluster_snapshots_paginator: ListClusterSnapshotsPaginator = client.get_paginator("list_cluster_snapshots")
+    list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+    list_pending_maintenance_actions_paginator: ListPendingMaintenanceActionsPaginator = client.get_paginator("list_pending_maintenance_actions")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import DocDBElasticClient
+from .paginator import (
+    ListClusterSnapshotsPaginator,
+    ListClustersPaginator,
+    ListPendingMaintenanceActionsPaginator,
+)
+
+Client = DocDBElasticClient
+
+__all__ = (
+    "Client",
+    "DocDBElasticClient",
+    "ListClusterSnapshotsPaginator",
+    "ListClustersPaginator",
+    "ListPendingMaintenanceActionsPaginator",
+)
