cbffeb724ea99c8305e2ecbf8c4d1f31d404ccbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:08 +0000

    Add package django-devops/0.2.16/django_devops-0.2.16.tar.gz

---
+++ packages/django-devops/django_devops-0.2.16.tar.gz/django_devops-0.2.16/README.md
@@ -0,0 +1,78 @@
+<div align="center">
+
+<h1> django-DevOps </h1>
+
+[![Code Quality](https://github.com/justinmerrell/django-DevOps/actions/workflows/pylint.yml/badge.svg)](https://github.com/justinmerrell/django-DevOps/actions/workflows/pylint.yml)
+[![Script Check](https://github.com/justinmerrell/django-DevOps/actions/workflows/shellcheck.yml/badge.svg)](https://github.com/justinmerrell/django-DevOps/actions/workflows/shellcheck.yml) &nbsp;
+[![CodeQL](https://github.com/justinmerrell/django-DevOps/actions/workflows/codeql-analysis.yml/badge.svg)](https://github.com/justinmerrell/django-DevOps/actions/workflows/codeql-analysis.yml)
+
+</div>
+
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [What is django-DevOps?](#what-is-django-devops)
+- [Getting Started](#getting-started)
+  - [Configuration Files](#configuration-files)
+- [Manage Commands](#manage-commands)
+- [Directory Structure](#directory-structure)
+- [License](#license)
+
+## What is django-DevOps?
+
+The goal of this repository is to provide a set of programmatic tools to help you build and deploy your Django projects. This is done by defining best practices for the following:
+
+- Service and Config file management
+- Auto deploy from GIT
+- Guided feature implementation
+
+No more returning to the same Stackoverflow pages every time you start a new project just to remind yourself what directory config files should be placed in. All files can now be managed from a project level and quickly deployed/updated.
+
+## Getting Started
+
+To install the package, run the following command:
+
+```bash
+pip install django-DevOps
+```
+
+Then add `django_devoop` to your `INSTALLED_APPS` list in your `settings.py` file.
+
+```python
+INSTALLED_APPS = [
+    ...
+    'django_devops',
+    ...
+]
+```
+
+For additional pip information visit: [https://pypi.org/project/django-DevOps/](https://pypi.org/project/django-DevOps/)
+
+### Configuration Files
+
+Under your project folder create a `config_files` and `service_files` folder to place files to be deployed.
+
+A config file _with the same name_ as the project will be treated as the NGINX config file and copied to site-available.
+
+## Manage Commands
+
+| Command          | Description                                                                                            |
+| ---------------- | ------------------------------------------------------------------------------------------------------ |
+| devops           | Guided project review. (Recommended)                                                                   |
+| do_guide_account | Walks through the guide for user account management.                                                   |
+| prep_gunicorn    | Prepares the gunicorn config file for use with gunicorn.                                               |
+| prep_celery      | Prepares the celery config file for use with celery.                                                   |
+| prep_nginx       | Prepares the nginx config file for use with nginx.                                                     |
+| update_services  | Similar to "collectstatic", this command will deploy config and service files from the project folder. |
+
+## Directory Structure
+
+```default
+.
+├── .github             # CI/CD using GitHub Actions and other functions.
+└── django_devops       # Main package directory.
+```
+
+## License
+
+This project is licensed under the terms of the [MIT license](https://opensource.org/licenses/MIT).
---
+++ packages/django-devops/django_devops-0.2.16.tar.gz/django_devops-0.2.16/django_devops/management/commands/prep_celery.py
@@ -0,0 +1,131 @@
+'''
+A programmatic way to prepare and configure celery.
+'''
+
+import os
+from textwrap import dedent
+from os.path import exists
+
+from django.core.management.base import BaseCommand, CommandError
+
+from django.conf import settings
+
+from django_devops.utils.user_input import query_yes_no
+
+PROJECT_NAME = os.path.basename(os.path.normpath(settings.BASE_DIR))
+
+
+def generate_file(file_path, file_template):
+    '''
+    Generates a file from a template.
+    '''
+    if exists(file_path):
+        if query_yes_no(f'{file_path} exists. Overwrite?'):
+            pass
+        else:
+            raise CommandError(f'{file_path} will not be overwritten.')
+
+    with open(file_path, 'w+', encoding='UTF-8') as file:
+        file.seek(0)
+        file.write(dedent(file_template))
+        file.truncate()
+
+
+def create_and_set_permissions(directory, owner, group):
+    '''
+    Creates a directory and sets the owner and group.
+    '''
+    if not exists(directory):
+        os.makedirs(directory)
+        os.system(f'chown -R {owner}:{group} {directory}')
+
+
+class Command(BaseCommand):
+    '''
+    Programmatically generates the service file for celery.
+    '''
+
+    help = 'PProgrammatically generates the service file for celery.'
+
+    def handle(self, *args, **options):
+        '''
+        Verifies that the service folder exists for use with django_devops
+        '''
+        service_files_path = f'{settings.BASE_DIR}/{PROJECT_NAME}/service_files'
+        config_files_path = f'{settings.BASE_DIR}/{PROJECT_NAME}/config_files'
+
+        if not exists(service_files_path):
+            raise CommandError(f'''
+                {service_files_path} does not exist.
+                First run "python manage.py devops" to configure django_devops.
+            ''')
+
+        # Generate celery.service file.
+        celery_service_template = f'''
+            [Unit]
+            Description = Celery Service
+            After = network.target
+
+            [Service]
+            Type = forking
+            User = {PROJECT_NAME}
+            Group = {PROJECT_NAME}
+
+            EnvironmentFile = /etc/conf.d/celery
+
+            WorkingDirectory = /opt/{PROJECT_NAME}
+
+            ExecStart   =   /bin/sh -c '/opt/{PROJECT_NAME}/.venv/bin/celery multi start ${{CELERYD_NODES}} \
+                            -A ${{CELERY_APP}} --pidfile=${{CELERYD_PID_FILE}} \
+                            --logfile=${{CELERYD_LOG_FILE}} --loglevel=${{CELERYD_LOG_LEVEL}} $CELERYD_OPTS'
+
+            ExecStop    =   /bin/sh -c '/opt/{PROJECT_NAME}/.venv/bin/celery ${{CELERY_BIN}} multi stopwait ${{CELERYD_NODES}} \
+                            --pidfile=${{CELERYD_PID_FILE}}'
+
+            ExecReload  =   /bin/sh -c '/opt/{PROJECT_NAME}/.venv/bin/celery ${{CELERY_BIN}} multi restart ${{CELERYD_NODES}} \
+                            -A ${{CELERY_APP}} --pidfile=${{CELERYD_PID_FILE}} \
+                            --logfile=${{CELERYD_LOG_FILE}} --loglevel=${{CELERYD_LOG_LEVEL}} $CELERYD_OPTS'
+
+            Restart=always
+
+            [Install]
+            WantedBy = multi-user.target
+        '''
+
+        # Generate celery.service file.
+        celery_config_template = f'''
+            # Name of nodes to start.
+            CELERYD_NODES="worker"
+
+            CELERY_BIN="/opt/{PROJECT_NAME}/.venv/bin/celery"
+
+            CELERY_APP="{PROJECT_NAME}"
+
+            CELERYD_CHDIR="/opt/{PROJECT_NAME}/"
+
+            CELERYD_OPTS="--time-limit=300 --concurrency=8"
+
+            CELERYD_LOG_FILE="/var/log/celery/%n%I.log"
+            CELERYD_PID_FILE="/var/run/celery/%n.pid"
+
+            # Workers should run as an unprivileged user.
+            #   You need to create this user manually (or you can choose
+            #   a user/group combination that already exists (e.g., nobody).
+            CELERYD_USER="{PROJECT_NAME}"
+            CELERYD_GROUP="{PROJECT_NAME}"
+            CELERYD_LOG_LEVEL="INFO"
+
+            # If enabled PID and log directories will be created if missing,
+            # and owned by the userid/group configured.
+            CELERY_CREATE_DIRS=1
+
+
+            CELERYBEAT_PID_FILE="/var/run/celery/beat.pid"
+            CELERYBEAT_LOG_FILE="/var/log/celery/beat.log"
+        '''
+
+        generate_file(f'{service_files_path}/celery.service', celery_service_template)
+        generate_file(f'{config_files_path}/celery', celery_config_template)
+
+        create_and_set_permissions('/var/run/celery/', PROJECT_NAME, PROJECT_NAME)
+        create_and_set_permissions('/var/log/celery/', PROJECT_NAME, PROJECT_NAME)
