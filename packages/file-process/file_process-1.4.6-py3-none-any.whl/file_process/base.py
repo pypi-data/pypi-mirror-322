852e533670f84d22eb90d793505677bb82bc3caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:04:57 +0000

    Add package file-process/1.4.6/file_process-1.4.6-py3-none-any.whl

---
+++ packages/file-process/file_process-1.4.6-py3-none-any.whl/file_process/base.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from io import BytesIO
+from typing import Optional, List
+
+
+class FileProcessorBase(ABC):
+    @abstractmethod
+    def validate(self, model_metadata_file: Optional[BytesIO] = None, validation_rules: Optional[dict] = None):
+        pass
+
+    @abstractmethod
+    def get_preview(self) -> (List[str], List[str], List[dict], List[dict], str):
+        pass
+
+    def cleanup_temp_files(self) -> ():
+        pass
