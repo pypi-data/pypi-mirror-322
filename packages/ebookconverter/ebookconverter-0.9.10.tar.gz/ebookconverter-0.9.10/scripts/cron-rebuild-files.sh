19f1f1c5d33dce5ff2c1b37b1161f2f5f22110c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:05:40 +0000

    Add package ebookconverter/0.9.10/ebookconverter-0.9.10.tar.gz

---
+++ packages/ebookconverter/ebookconverter-0.9.10.tar.gz/ebookconverter-0.9.10/scripts/cron-rebuild-files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+D=$(date +"%-d")
+
+# will rebuild up to PG 71299 in a 31 day month
+BOOKS_PER_DAY=2300
+
+START=$((($D-1)*$BOOKS_PER_DAY))
+STOP=$(($D*$BOOKS_PER_DAY-1))
+#STOP=$(($START + 300))
+
+echo "Invoking ebookconverter for range $START to $STOP ..."
+
+cd /export/sunsite/users/gutenbackend/ebookconverter
+~/.local/bin/pipenv run ebookconverter -v -v --range=$START-$STOP --build=all --jobs=20 --pidfile=/tmp/pg-cron-rebuild.pid
+
