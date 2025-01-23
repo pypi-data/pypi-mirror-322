baac308777865d7f9e6e05b9ec56a9426f9bc301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 05:05:40 +0000

    Add package django-hugeicons-stroke/1.0.0/django_hugeicons_stroke-1.0.0.tar.gz

---
+++ packages/django-hugeicons-stroke/django_hugeicons_stroke-1.0.0.tar.gz/django_hugeicons_stroke-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+	name='django_hugeicons_stroke',
+	version='1.0.0',
+	description='A Django templatetag library for using hugeicons free stroke icons',
+	url='',
+	author='Kyvex',
+	author_email='developer.gdean@proton.me',
+	license='GPL',
+	packages=['django_hugeicons_stroke'],
+	zip_safe=False
+)
