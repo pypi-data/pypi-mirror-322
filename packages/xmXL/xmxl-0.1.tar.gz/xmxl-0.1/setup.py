638a892bea15bd66eaf843dda98ad440e08c115f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:06:46 +0000

    Add package xmXL/0.1/xmxl-0.1.tar.gz

---
+++ packages/xmXL/xmxl-0.1.tar.gz/xmxl-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xmXL',  # Package name
+    version='0.1',        # Version number
+    packages=find_packages(),  # This will automatically find the package
+    install_requires=['openpyxl'],  # Dependencies
+    author='Yagna',
+    author_email='yagna781@gmail.com',
+    description='A utility for converting XML files to Excel',
+    #long_description=open('README.md').read(),  # Optional
+    long_description_content_type='text/markdown',  # Optional
+    url='https://github.com/yourusername/xml_to_excel',  # Optional
+    classifiers=[  # Optional but helps with categorizing
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Optional, specify Python versions
+)
+
