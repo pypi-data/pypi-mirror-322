df549e594c90e8b1f3b85133eb9cf944201f9f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:24 +0000

    Add package types-aiobotocore-rds-data/2.19.0/types_aiobotocore_rds_data-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-rds-data/types_aiobotocore_rds_data-2.19.0-py3-none-any.whl/types_aiobotocore_rds_data/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for rds-data service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_rds_data import (
+        Client,
+        RDSDataServiceClient,
+    )
+
+    session = get_session()
+    async with session.create_client("rds-data") as client:
+        client: RDSDataServiceClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import RDSDataServiceClient
+
+Client = RDSDataServiceClient
+
+__all__ = ("Client", "RDSDataServiceClient")
