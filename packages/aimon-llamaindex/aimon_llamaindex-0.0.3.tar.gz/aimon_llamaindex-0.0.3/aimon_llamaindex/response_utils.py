9e18e73c9d5b99261bf0106341dda926f4b89412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:05:08 +0000

    Add package aimon-llamaindex/0.0.3/aimon_llamaindex-0.0.3.tar.gz

---
+++ packages/aimon-llamaindex/aimon_llamaindex-0.0.3.tar.gz/aimon_llamaindex-0.0.3/aimon_llamaindex/response_utils.py
@@ -0,0 +1,35 @@
+# AIMon - LlamaIndex: Response Utilities
+
+import logging
+from llama_index.core.query_engine import RetrieverQueryEngine
+
+# Function to build a query engine and get LLM response
+def get_response(user_query, retriever, llm):
+    query_engine = RetrieverQueryEngine.from_args(retriever, llm)
+    response = query_engine.query(user_query)
+    return response
+
+
+# Function to extract metadata from the response
+def extract_response_metadata(user_query, user_instructions, response):
+
+    def get_source_docs(chat_response):
+        contexts = []
+        relevance_scores = []
+        if hasattr(chat_response, 'source_nodes'):
+            for node in chat_response.source_nodes:
+                if hasattr(node, 'node') and hasattr(node.node, 'text') and hasattr(node, 'score') and node.score is not None:
+                    contexts.append(node.node.text)
+                    relevance_scores.append(node.score)
+                elif hasattr(node, 'text') and hasattr(node, 'score') and node.score is not None:
+                    contexts.append(node.text)
+                    relevance_scores.append(node.score)
+                else:
+                    logging.info("Node does not have required attributes.")
+        else:
+            logging.info("No source_nodes attribute found in the chat response.")
+        return contexts, relevance_scores
+
+    context, relevance_scores = get_source_docs(response)
+
+    return context, user_query, user_instructions, response.response
