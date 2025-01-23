92e09b665968c67894efb105b1c0036aba7bd6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:06:11 +0000

    Add package paddlepaddle/3.0.0rc1/paddlepaddle-3.0.0rc1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/paddlepaddle/paddlepaddle-3.0.0rc1-cp311-cp311-macosx_11_0_arm64.whl/paddle/tensorrt/impls/ops.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import tensorrt as trt
+
+from paddle.tensorrt.register import converter_registry
+
+ops_type_map = {
+    "pd_op.sqrt": trt.UnaryOperation.SQRT,
+    "pd_op.sqrt_": trt.UnaryOperation.SQRT,
+    "pd_op.floor": trt.UnaryOperation.FLOOR,
+}
+
+
+@converter_registry.register("pd_op.sqrt", trt_version="trt_version_ge=8.0")
+@converter_registry.register("pd_op.sqrt_", trt_version="trt_version_ge=8.0")
+@converter_registry.register("pd_op.floor", trt_version="8.x")
+def sqrt_converter(network, paddle_op, inputs):
+    input_tensor = inputs[0]
+    layer = network.add_unary(input_tensor, ops_type_map[paddle_op.name()])
+    return layer.get_output(0)
