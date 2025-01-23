5d66836c561fc21ee3759f7e3c90660c18ae29db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:31 +0000

    Add package types-aiobotocore-application-autoscaling/2.19.0/types_aiobotocore_application_autoscaling-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-application-autoscaling/types_aiobotocore_application_autoscaling-2.19.0.tar.gz/types_aiobotocore_application_autoscaling-2.19.0/types_aiobotocore_application_autoscaling/__init__.pyi
@@ -0,0 +1,49 @@
+"""
+Main interface for application-autoscaling service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_application_autoscaling import (
+        ApplicationAutoScalingClient,
+        Client,
+        DescribeScalableTargetsPaginator,
+        DescribeScalingActivitiesPaginator,
+        DescribeScalingPoliciesPaginator,
+        DescribeScheduledActionsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("application-autoscaling") as client:
+        client: ApplicationAutoScalingClient
+        ...
+
+
+    describe_scalable_targets_paginator: DescribeScalableTargetsPaginator = client.get_paginator("describe_scalable_targets")
+    describe_scaling_activities_paginator: DescribeScalingActivitiesPaginator = client.get_paginator("describe_scaling_activities")
+    describe_scaling_policies_paginator: DescribeScalingPoliciesPaginator = client.get_paginator("describe_scaling_policies")
+    describe_scheduled_actions_paginator: DescribeScheduledActionsPaginator = client.get_paginator("describe_scheduled_actions")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import ApplicationAutoScalingClient
+from .paginator import (
+    DescribeScalableTargetsPaginator,
+    DescribeScalingActivitiesPaginator,
+    DescribeScalingPoliciesPaginator,
+    DescribeScheduledActionsPaginator,
+)
+
+Client = ApplicationAutoScalingClient
+
+__all__ = (
+    "ApplicationAutoScalingClient",
+    "Client",
+    "DescribeScalableTargetsPaginator",
+    "DescribeScalingActivitiesPaginator",
+    "DescribeScalingPoliciesPaginator",
+    "DescribeScheduledActionsPaginator",
+)
