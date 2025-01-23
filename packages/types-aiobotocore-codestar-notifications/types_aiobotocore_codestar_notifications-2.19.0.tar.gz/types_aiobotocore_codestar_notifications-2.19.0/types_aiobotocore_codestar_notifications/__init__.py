577c8bfc69a117ed283d152af7ea1fae08a22cc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:31 +0000

    Add package types-aiobotocore-codestar-notifications/2.19.0/types_aiobotocore_codestar_notifications-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-codestar-notifications/types_aiobotocore_codestar_notifications-2.19.0.tar.gz/types_aiobotocore_codestar_notifications-2.19.0/types_aiobotocore_codestar_notifications/__init__.py
@@ -0,0 +1,42 @@
+"""
+Main interface for codestar-notifications service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_codestar_notifications import (
+        Client,
+        CodeStarNotificationsClient,
+        ListEventTypesPaginator,
+        ListNotificationRulesPaginator,
+        ListTargetsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("codestar-notifications") as client:
+        client: CodeStarNotificationsClient
+        ...
+
+
+    list_event_types_paginator: ListEventTypesPaginator = client.get_paginator("list_event_types")
+    list_notification_rules_paginator: ListNotificationRulesPaginator = client.get_paginator("list_notification_rules")
+    list_targets_paginator: ListTargetsPaginator = client.get_paginator("list_targets")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import CodeStarNotificationsClient
+from .paginator import ListEventTypesPaginator, ListNotificationRulesPaginator, ListTargetsPaginator
+
+Client = CodeStarNotificationsClient
+
+
+__all__ = (
+    "Client",
+    "CodeStarNotificationsClient",
+    "ListEventTypesPaginator",
+    "ListNotificationRulesPaginator",
+    "ListTargetsPaginator",
+)
