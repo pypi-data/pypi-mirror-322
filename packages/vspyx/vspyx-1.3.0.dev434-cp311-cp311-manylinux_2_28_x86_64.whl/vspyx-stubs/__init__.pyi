d35610c560742c36e549df2fb6822abd7cb97da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:06:02 +0000

    Add package vspyx/1.3.0.dev434/vspyx-1.3.0.dev434-cp311-cp311-manylinux_2_28_x86_64.whl

---
+++ packages/vspyx/vspyx-1.3.0.dev434-cp311-cp311-manylinux_2_28_x86_64.whl/vspyx-stubs/__init__.pyi
@@ -0,0 +1,1 @@
+from . import VehicleSpy, Dissector, icsneo, Scripting, RPC, Ford, Frames, Runtime, Exporter, MonitorView, Core, intrepidcs, Diagnostics, TCPIP, IO, SOMEIP, Communication, std, AUTOSAR
