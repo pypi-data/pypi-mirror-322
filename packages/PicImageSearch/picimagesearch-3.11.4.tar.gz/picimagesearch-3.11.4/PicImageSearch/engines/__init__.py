c5efacfbcec42a0db15af549f5d0864b9b1bdd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:04:59 +0000

    Add package PicImageSearch/3.11.4/picimagesearch-3.11.4.tar.gz

---
+++ packages/PicImageSearch/picimagesearch-3.11.4.tar.gz/picimagesearch-3.11.4/PicImageSearch/engines/__init__.py
@@ -0,0 +1,25 @@
+from .ascii2d import Ascii2D
+from .baidu import BaiDu
+from .bing import Bing
+from .copyseeker import Copyseeker
+from .ehentai import EHentai
+from .google import Google
+from .iqdb import Iqdb
+from .saucenao import SauceNAO
+from .tineye import Tineye
+from .tracemoe import TraceMoe
+from .yandex import Yandex
+
+__all__ = [
+    "Ascii2D",
+    "BaiDu",
+    "Bing",
+    "Copyseeker",
+    "EHentai",
+    "Google",
+    "Iqdb",
+    "SauceNAO",
+    "Tineye",
+    "TraceMoe",
+    "Yandex",
+]
