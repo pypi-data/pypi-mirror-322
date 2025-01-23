9408cbae2f90be5dc46a79a6a95fb2d7145c2bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:05:41 +0000

    Add package autogen-openaiext-client/0.0.3/autogen_openaiext_client-0.0.3-py3-none-any.whl

---
+++ packages/autogen-openaiext-client/autogen_openaiext_client-0.0.3-py3-none-any.whl/autogen_openaiext_client/__init__.py
@@ -0,0 +1,8 @@
+from autogen_openaiext_client.client import (
+    GeminiChatCompletionClient,
+    GroqChatCompletionClient,
+    TogetherAIChatCompletionClient,
+)
+from autogen_openaiext_client.info import GeminiInfo, GroqInfo, TogetherAIInfo
+
+__version__ = "0.0.3"
