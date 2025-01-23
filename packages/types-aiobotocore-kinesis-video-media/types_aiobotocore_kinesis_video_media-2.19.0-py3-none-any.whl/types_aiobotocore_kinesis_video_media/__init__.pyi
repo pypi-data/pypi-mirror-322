7b3ae9a159393c840d7bf1da2d77f8d706cb6d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:22 +0000

    Add package types-aiobotocore-kinesis-video-media/2.19.0/types_aiobotocore_kinesis_video_media-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-kinesis-video-media/types_aiobotocore_kinesis_video_media-2.19.0-py3-none-any.whl/types_aiobotocore_kinesis_video_media/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for kinesis-video-media service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_kinesis_video_media import (
+        Client,
+        KinesisVideoMediaClient,
+    )
+
+    session = get_session()
+    async with session.create_client("kinesis-video-media") as client:
+        client: KinesisVideoMediaClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import KinesisVideoMediaClient
+
+Client = KinesisVideoMediaClient
+
+__all__ = ("Client", "KinesisVideoMediaClient")
