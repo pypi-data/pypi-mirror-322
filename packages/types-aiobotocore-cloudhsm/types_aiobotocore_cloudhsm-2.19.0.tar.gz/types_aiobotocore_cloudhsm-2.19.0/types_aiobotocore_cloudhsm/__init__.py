c628a11a12e6093330d49c42372e34dd86182a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:25 +0000

    Add package types-aiobotocore-cloudhsm/2.19.0/types_aiobotocore_cloudhsm-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-cloudhsm/types_aiobotocore_cloudhsm-2.19.0.tar.gz/types_aiobotocore_cloudhsm-2.19.0/types_aiobotocore_cloudhsm/__init__.py
@@ -0,0 +1,42 @@
+"""
+Main interface for cloudhsm service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cloudhsm import (
+        Client,
+        CloudHSMClient,
+        ListHapgsPaginator,
+        ListHsmsPaginator,
+        ListLunaClientsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("cloudhsm") as client:
+        client: CloudHSMClient
+        ...
+
+
+    list_hapgs_paginator: ListHapgsPaginator = client.get_paginator("list_hapgs")
+    list_hsms_paginator: ListHsmsPaginator = client.get_paginator("list_hsms")
+    list_luna_clients_paginator: ListLunaClientsPaginator = client.get_paginator("list_luna_clients")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import CloudHSMClient
+from .paginator import ListHapgsPaginator, ListHsmsPaginator, ListLunaClientsPaginator
+
+Client = CloudHSMClient
+
+
+__all__ = (
+    "Client",
+    "CloudHSMClient",
+    "ListHapgsPaginator",
+    "ListHsmsPaginator",
+    "ListLunaClientsPaginator",
+)
