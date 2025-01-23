8c737e8c03af3ec6ed015e512c2ce75764f55aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:29 +0000

    Add package types-aiobotocore-emr-serverless/2.19.0/types_aiobotocore_emr_serverless-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-emr-serverless/types_aiobotocore_emr_serverless-2.19.0.tar.gz/types_aiobotocore_emr_serverless-2.19.0/types_aiobotocore_emr_serverless/__init__.py
@@ -0,0 +1,42 @@
+"""
+Main interface for emr-serverless service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_emr_serverless import (
+        Client,
+        EMRServerlessClient,
+        ListApplicationsPaginator,
+        ListJobRunAttemptsPaginator,
+        ListJobRunsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("emr-serverless") as client:
+        client: EMRServerlessClient
+        ...
+
+
+    list_applications_paginator: ListApplicationsPaginator = client.get_paginator("list_applications")
+    list_job_run_attempts_paginator: ListJobRunAttemptsPaginator = client.get_paginator("list_job_run_attempts")
+    list_job_runs_paginator: ListJobRunsPaginator = client.get_paginator("list_job_runs")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import EMRServerlessClient
+from .paginator import ListApplicationsPaginator, ListJobRunAttemptsPaginator, ListJobRunsPaginator
+
+Client = EMRServerlessClient
+
+
+__all__ = (
+    "Client",
+    "EMRServerlessClient",
+    "ListApplicationsPaginator",
+    "ListJobRunAttemptsPaginator",
+    "ListJobRunsPaginator",
+)
