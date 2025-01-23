9529cf6ddcee82db39b0920bd1d275c1cbfd6ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:20 +0000

    Add package types-aiobotocore-snow-device-management/2.19.0/types_aiobotocore_snow_device_management-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-snow-device-management/types_aiobotocore_snow_device_management-2.19.0.tar.gz/types_aiobotocore_snow_device_management-2.19.0/types_aiobotocore_snow_device_management/__init__.py
@@ -0,0 +1,50 @@
+"""
+Main interface for snow-device-management service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_snow_device_management import (
+        Client,
+        ListDeviceResourcesPaginator,
+        ListDevicesPaginator,
+        ListExecutionsPaginator,
+        ListTasksPaginator,
+        SnowDeviceManagementClient,
+    )
+
+    session = get_session()
+    async with session.create_client("snow-device-management") as client:
+        client: SnowDeviceManagementClient
+        ...
+
+
+    list_device_resources_paginator: ListDeviceResourcesPaginator = client.get_paginator("list_device_resources")
+    list_devices_paginator: ListDevicesPaginator = client.get_paginator("list_devices")
+    list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+    list_tasks_paginator: ListTasksPaginator = client.get_paginator("list_tasks")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import SnowDeviceManagementClient
+from .paginator import (
+    ListDeviceResourcesPaginator,
+    ListDevicesPaginator,
+    ListExecutionsPaginator,
+    ListTasksPaginator,
+)
+
+Client = SnowDeviceManagementClient
+
+
+__all__ = (
+    "Client",
+    "ListDeviceResourcesPaginator",
+    "ListDevicesPaginator",
+    "ListExecutionsPaginator",
+    "ListTasksPaginator",
+    "SnowDeviceManagementClient",
+)
