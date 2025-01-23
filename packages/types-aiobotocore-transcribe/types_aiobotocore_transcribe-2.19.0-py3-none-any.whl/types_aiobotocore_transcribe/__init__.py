afd6f83154ba450adcb741ff9138dc8298a79f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:22 +0000

    Add package types-aiobotocore-transcribe/2.19.0/types_aiobotocore_transcribe-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-transcribe/types_aiobotocore_transcribe-2.19.0-py3-none-any.whl/types_aiobotocore_transcribe/__init__.py
@@ -0,0 +1,28 @@
+"""
+Main interface for transcribe service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_transcribe import (
+        Client,
+        TranscribeServiceClient,
+    )
+
+    session = get_session()
+    async with session.create_client("transcribe") as client:
+        client: TranscribeServiceClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import TranscribeServiceClient
+
+Client = TranscribeServiceClient
+
+
+__all__ = ("Client", "TranscribeServiceClient")
