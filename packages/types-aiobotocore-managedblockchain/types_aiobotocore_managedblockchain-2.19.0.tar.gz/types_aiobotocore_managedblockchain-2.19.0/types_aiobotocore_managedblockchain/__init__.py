45bc0c50faad21ad9192cc3541a7ee8af62b8f89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:25 +0000

    Add package types-aiobotocore-managedblockchain/2.19.0/types_aiobotocore_managedblockchain-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-managedblockchain/types_aiobotocore_managedblockchain-2.19.0.tar.gz/types_aiobotocore_managedblockchain-2.19.0/types_aiobotocore_managedblockchain/__init__.py
@@ -0,0 +1,32 @@
+"""
+Main interface for managedblockchain service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_managedblockchain import (
+        Client,
+        ListAccessorsPaginator,
+        ManagedBlockchainClient,
+    )
+
+    session = get_session()
+    async with session.create_client("managedblockchain") as client:
+        client: ManagedBlockchainClient
+        ...
+
+
+    list_accessors_paginator: ListAccessorsPaginator = client.get_paginator("list_accessors")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import ManagedBlockchainClient
+from .paginator import ListAccessorsPaginator
+
+Client = ManagedBlockchainClient
+
+
+__all__ = ("Client", "ListAccessorsPaginator", "ManagedBlockchainClient")
