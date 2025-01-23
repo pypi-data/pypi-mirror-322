3c2950bc3ec5b22249e7222c5133c88c9e2e7a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:26 +0000

    Add package types-aiobotocore-detective/2.19.0/types_aiobotocore_detective-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-detective/types_aiobotocore_detective-2.19.0-py3-none-any.whl/types_aiobotocore_detective/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for detective service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_detective import (
+        Client,
+        DetectiveClient,
+    )
+
+    session = get_session()
+    async with session.create_client("detective") as client:
+        client: DetectiveClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import DetectiveClient
+
+Client = DetectiveClient
+
+__all__ = ("Client", "DetectiveClient")
