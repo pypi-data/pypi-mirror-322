96c86262e61dffc42e9e9c3a331dbd8fd1527ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:06:06 +0000

    Add package anipy-api/3.3.5/anipy_api-3.3.5-py3-none-any.whl

---
+++ packages/anipy-api/anipy_api-3.3.5-py3-none-any.whl/anipy_api/provider/providers/__init__.py
@@ -0,0 +1,4 @@
+from anipy_api.provider.providers.gogo_provider import GoGoProvider
+from anipy_api.provider.providers.anivibe_provider import AnivibeProvider
+
+__all__ = ["GoGoProvider", "AnivibeProvider"]
