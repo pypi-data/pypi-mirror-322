9015730b9c5cbea7e2368480a8b18908479a0f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 15:05:45 +0000

    Add package online-svr/0.1.1/online-svr-0.1.1.tar.gz

---
+++ packages/online-svr/online-svr-0.1.1.tar.gz/online-svr-0.1.1/online-svr/__init__.py
@@ -0,0 +1,1 @@
+from .core import Serie_analisys, DataPreparation, PredictionModel
---
+++ packages/online-svr/online-svr-0.1.1.tar.gz/online-svr-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="online-svr",
+    version="0.1.1",
+    description="Uma biblioteca para realizar previsões em SVR com aprendizado online",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ademir Neto",
+    author_email="ademir.bsn@gmail.com",
+    url="https://github.com/ademirNeto/online-svr",
+    packages=find_packages(),
+    install_requires=[
+    "numpy",
+    "pandas",
+    "scikit-learn",
+    "statsmodels",
+    "scipy"
+    ],
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
