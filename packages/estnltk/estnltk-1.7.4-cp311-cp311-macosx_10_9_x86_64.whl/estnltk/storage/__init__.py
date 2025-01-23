7edddc08464a0ed771e02fc111192ac5619f4001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:08:28 +0000

    Add package estnltk/1.7.4/estnltk-1.7.4-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/estnltk/estnltk-1.7.4-cp311-cp311-macosx_10_9_x86_64.whl/estnltk/storage/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module enables to store and search `Text` objects in sqlite and Postgres databases.
+
+For Postgres usage examples, see tutorial `estnltk/tutorials/storing_text_objects_in_postgres.ipynb`
+"""
+
+# DO not import PostgresStorage, avoid ModuleNotFoundError: No module named 'psycopg2'
+# (people should be able to import estnltk.storage.sqlite without running into this error)
+#from estnltk.storage.postgres.storage import PostgresStorage
