40f8f643888e565e3ffee57d80222c6b249df571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:26 +0000

    Add package types-aiobotocore-mediastore/2.19.0/types_aiobotocore_mediastore-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-mediastore/types_aiobotocore_mediastore-2.19.0-py3-none-any.whl/types_aiobotocore_mediastore/__init__.pyi
@@ -0,0 +1,31 @@
+"""
+Main interface for mediastore service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mediastore import (
+        Client,
+        ListContainersPaginator,
+        MediaStoreClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mediastore") as client:
+        client: MediaStoreClient
+        ...
+
+
+    list_containers_paginator: ListContainersPaginator = client.get_paginator("list_containers")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import MediaStoreClient
+from .paginator import ListContainersPaginator
+
+Client = MediaStoreClient
+
+__all__ = ("Client", "ListContainersPaginator", "MediaStoreClient")
