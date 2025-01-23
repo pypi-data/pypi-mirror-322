afdb09e27757f9c20b2b302a248a5ce3271f5651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:06:25 +0000

    Add package py-bEaNs-lib/0.1.1/py_beans_lib-0.1.1.tar.gz

---
+++ packages/py-bEaNs-lib/py_beans_lib-0.1.1.tar.gz/py_beans_lib-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-bEaNs', 
+    version='0.1.0',
+    packages=find_packages(),
+    author='SmartBaby626',  
+    description='A library to encode and decode messages with the power of BEANS',
+    python_requires='>=3.6',
+
+)
