e52e90eafd2aee503dd7122d4e766d1f6d3d475a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:31 +0000

    Add package types-aiobotocore-cognito-identity/2.19.0/types_aiobotocore_cognito_identity-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-cognito-identity/types_aiobotocore_cognito_identity-2.19.0-py3-none-any.whl/types_aiobotocore_cognito_identity/__init__.pyi
@@ -0,0 +1,31 @@
+"""
+Main interface for cognito-identity service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cognito_identity import (
+        Client,
+        CognitoIdentityClient,
+        ListIdentityPoolsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("cognito-identity") as client:
+        client: CognitoIdentityClient
+        ...
+
+
+    list_identity_pools_paginator: ListIdentityPoolsPaginator = client.get_paginator("list_identity_pools")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import CognitoIdentityClient
+from .paginator import ListIdentityPoolsPaginator
+
+Client = CognitoIdentityClient
+
+__all__ = ("Client", "CognitoIdentityClient", "ListIdentityPoolsPaginator")
