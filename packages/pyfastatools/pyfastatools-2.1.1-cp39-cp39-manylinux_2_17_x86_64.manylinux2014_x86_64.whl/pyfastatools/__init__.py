5e03b2b017478c543abab1dd21b02491bb8cbefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:05:41 +0000

    Add package pyfastatools/2.1.1/pyfastatools-2.1.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyfastatools/pyfastatools-2.1.1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyfastatools/__init__.py
@@ -0,0 +1,14 @@
+from pyfastatools._fastatools import Record, Records
+from pyfastatools._parser import Parser
+from pyfastatools._simple import PyParser
+from pyfastatools.file import FastaFile
+from pyfastatools.utils import write_fasta
+
+__all__ = [
+    "Parser",
+    "Record",
+    "Records",
+    "write_fasta",
+    # "FastaFile",
+    # "PyParser",
+]
