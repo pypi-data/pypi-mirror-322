d91180b193d7c8f3bf3962ce87b654d0015639bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:28 +0000

    Add package alist3/1.3.5/alist3-1.3.5.tar.gz

---
+++ packages/alist3/alist3-1.3.5.tar.gz/alist3-1.3.5/alist/__init__.py
@@ -0,0 +1,17 @@
+from .main import AList
+from .main import AListAdmin
+
+from .sync import AListSync
+from .sync import AListAdminSync
+
+from .model import AListFile
+from .model import AListFolder
+
+from .sync import AListFileSync
+
+from .utils import AListUser
+from .error import *
+
+AListAsync = AList
+AListAdminAsync = AListAdmin
+AListFileAsync = AListFile
