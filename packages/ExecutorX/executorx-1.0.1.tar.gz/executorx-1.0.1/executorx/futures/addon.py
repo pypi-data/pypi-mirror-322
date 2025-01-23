9118cef1b6e024b44747f4aa25a95e91d8ffc808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:06:19 +0000

    Add package ExecutorX/1.0.1/executorx-1.0.1.tar.gz

---
+++ packages/ExecutorX/executorx-1.0.1.tar.gz/executorx-1.0.1/executorx/futures/addon.py
@@ -0,0 +1,33 @@
+# Author: Yuting Zhang
+# This file is part of ExecutorX, licensed under the GNU Lesser General Public License V2.1.
+
+__author__ = ['Yuting Zhang']
+
+__all__ = ['PoolExecutorAddon']
+
+from typing import Optional
+from concurrent.futures import Future, Executor
+from weakref import ProxyType
+
+
+class PoolExecutorAddon:
+
+    def __init__(self):
+        from .executors import PoolExecutor
+        self.executor: Optional[ProxyType[PoolExecutor]] = None
+
+    def on_start(self) -> None:
+        pass
+
+    def initializer(self) -> None:
+        pass
+
+    def pre_submit(self) -> None:
+        pass
+
+    def post_submit(self, future: Future) -> None:
+        pass
+
+    def after_join(self) -> None:
+        pass
+
