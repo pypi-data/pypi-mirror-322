c5fc868d4808cc3bdeb1087f03e286cc36f110cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:05:01 +0000

    Add package batchTranslate/1.2/batchTranslate-1.2-py3-none-any.whl

---
+++ packages/batchTranslate/batchTranslate-1.2-py3-none-any.whl/batchTranslate/main.py
@@ -0,0 +1,15 @@
+from deep_translator import GoogleTranslator
+import random
+
+def translate(times,original):
+    languages =list(GoogleTranslator().get_supported_languages(as_dict=True).values())
+    translated=original
+    for i in range(times):
+        random.shuffle(languages)
+        translator=GoogleTranslator(source='auto', target=languages[0])
+        translated=translator.translate(translated)
+        if i==times-1:
+            translator=GoogleTranslator(source='auto', target="en")
+            translated=translator.translate(translated)
+            return translated
+print(translate(5,"Hello World! \n 2+2"))
