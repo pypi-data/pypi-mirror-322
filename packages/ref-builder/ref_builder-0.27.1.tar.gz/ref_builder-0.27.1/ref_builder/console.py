117142fa6f2e5c966a76cd9cf7d75d2855a8ddad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:06:19 +0000

    Add package ref-builder/0.27.1/ref_builder-0.27.1.tar.gz

---
+++ packages/ref-builder/ref_builder-0.27.1.tar.gz/ref_builder-0.27.1/ref_builder/console.py
@@ -0,0 +1,146 @@
+from collections.abc import Iterator
+
+import rich.console
+from rich.table import Table
+
+from ref_builder.models import OTUMinimal
+from ref_builder.plan import Plan, SegmentRule
+from ref_builder.resources import RepoOTU
+
+
+def _render_taxonomy_id_link(taxid: int) -> str:
+    return f"[link=https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?id={taxid}]{taxid}[/link]"
+
+
+def _render_nucleotide_link(accession: str) -> str:
+    return f"[link=https://www.ncbi.nlm.nih.gov/nuccore/{accession}]{accession}[/link]"
+
+
+def print_otu(otu: RepoOTU) -> None:
+    """Print the details for an OTU to the console.
+
+    :param otu: The OTU to print.
+
+    """
+    console.print(f"[bold][underline]{otu.name}[/bold][/underline]")
+    console.line()
+
+    table = Table(
+        box=None,
+        show_header=False,
+    )
+
+    table.add_row("[bold]ACRONYM[/bold]", otu.acronym)
+    table.add_row("[bold]ID[/bold]", str(otu.id))
+
+    if otu.legacy_id:
+        table.add_row("[bold]LEGACY ID[/bold]", otu.legacy_id)
+
+    table.add_row("[bold]TAXID[/bold]", _render_taxonomy_id_link(otu.taxid))
+
+    max_accession_length = max(
+        len(str(sequence.accession))
+        for isolate in otu.isolates
+        for sequence in isolate.sequences
+    )
+
+    max_segment_name_length = max(
+        len(str(segment.name)) for segment in otu.plan.segments
+    )
+
+    console.print(table)
+
+    console.line()
+    console.print("[bold]SCHEMA[/bold]")
+    console.line()
+
+    schema_table = Table(box=None)
+
+    schema_table.add_column("SEGMENT")
+    schema_table.add_column("REQUIRED")
+    schema_table.add_column("LENGTH")
+    schema_table.add_column("TOLERANCE")
+
+    if not otu.plan.monopartite:
+        for segment in otu.plan.segments:
+            schema_table.add_row(
+                str(segment.name),
+                "[red]Yes[/red]"
+                if segment.required == SegmentRule.REQUIRED
+                else "[grey]No[/grey]",
+                str(segment.length),
+                str(segment.length_tolerance),
+            )
+    else:
+        monopartite_segment = otu.plan.segments[0]
+        schema_table.add_row(
+            monopartite_segment.name if monopartite_segment.name is not None else "N/A",
+            "[red]Yes[/red]",
+            str(monopartite_segment.length),
+            str(monopartite_segment.length_tolerance),
+        )
+
+    console.print(schema_table)
+
+    console.line()
+    console.print("[bold]ISOLATES[/bold]")
+
+    for isolate in otu.isolates:
+        console.line()
+        console.print(str(isolate.name)) if isolate.name is not None else console.print(
+            "[UNNAMED]",
+        )
+        console.line()
+
+        isolate_table = Table(
+            box=None,
+        )
+
+        isolate_table.add_column("ACCESSION", width=max_accession_length)
+        isolate_table.add_column("SEGMENT", min_width=max_segment_name_length)
+        isolate_table.add_column("DEFINITION")
+
+        for sequence in sorted(isolate.sequences, key=lambda s: s.accession):
+            isolate_table.add_row(
+                _render_nucleotide_link(str(sequence.accession)),
+                sequence.segment,
+                sequence.definition,
+            )
+
+        console.print(isolate_table)
+
+
+def print_otu_list(otus: Iterator[OTUMinimal]) -> None:
+    """Print a list of OTUs to the console.
+
+    :param otus: The OTUs to print.
+
+    """
+    table = Table(
+        box=None,
+    )
+
+    table.add_column("NAME")
+    table.add_column("ACRONYM")
+    table.add_column("TAXID")
+    table.add_column("ID")
+
+    added_rows = False
+
+    for otu in otus:
+        table.add_row(
+            otu.name,
+            otu.acronym,
+            str(otu.taxid),
+            str(otu.id),
+        )
+
+        added_rows = True
+
+    if added_rows:
+        console.print(table)
+    else:
+        console.print("No OTUs found")
+
+
+console = rich.console.Console()
---
+++ packages/ref-builder/ref_builder-0.27.1.tar.gz/ref_builder-0.27.1/ref_builder/legacy/convert.py
@@ -0,0 +1,116 @@
+import json
+import uuid
+from pathlib import Path
+
+from ref_builder.console import console
+from ref_builder.legacy.utils import iter_legacy_otus
+from ref_builder.logs import configure_logger
+from ref_builder.models import Molecule
+from ref_builder.ncbi.client import NCBIClient
+from ref_builder.repo import Repo
+from ref_builder.plan import Plan
+from ref_builder.utils import DataType, IsolateName, IsolateNameType
+
+
+def convert_legacy_repo(name: str, path: Path, target_path: Path) -> None:
+    """Convert the legacy repo at ``path`` to a new event-sourced repo at
+    ``target_path``.
+
+    A name is required for the new repo as legacy repos do not have names.
+
+    :param name: the name of the new repo
+    :param path: the path to the legacy repo
+    :param target_path: the path to the new repo
+
+    """
+    configure_logger(False)
+
+    with (path / "src" / "meta.json").open() as f:
+        data = json.load(f)
+
+    repo = Repo.new(
+        DataType(data["data_type"]),
+        name,
+        target_path,
+        data["organism"],
+    )
+
+    ncbi_client = NCBIClient(False)
+
+    for otu in iter_legacy_otus(path / "src"):
+        console.print(f"Converting {otu['name']} ({otu['taxid']})")
+
+        first_unversioned_accession = otu["isolates"][0]["sequences"][0][
+            "accession"
+        ].split(".")[0]
+
+        record = ncbi_client.fetch_genbank_records([first_unversioned_accession])[0]
+
+        molecule = Molecule(
+            strandedness=record.strandedness,
+            topology=record.topology,
+            type=record.moltype,
+        )
+
+        original_segments = [
+            {**segment, "id": uuid.uuid4()} for segment in otu["schema"]
+        ]
+        plan = Plan.model_validate(
+            {
+                "id": uuid.uuid4(),
+                "segments": original_segments,
+            }
+        )
+
+        try:
+            try:
+                repo_otu = repo.create_otu(
+                    otu["abbreviation"],
+                    otu["_id"],
+                    molecule=molecule,
+                    name=otu["name"],
+                    plan=plan,
+                    taxid=otu["taxid"],
+                )
+            except ValueError as err:
+                if "OTU already exists" in str(err):
+                    console.print(
+                        f"[red]Error:[/red] Already exists: {otu['name']} ({otu['taxid']})",
+                    )
+                    continue
+
+                raise
+        except ValueError as err:
+            if "OTU already exists" in str(err):
+                console.print(
+                    f"[red]Error:[/red] Already exists: {otu['name']} ({otu['taxid']})",
+                )
+                continue
+
+            raise
+
+        for isolate in otu["isolates"]:
+            if isolate["source_type"] in ("genbank", "refseq"):
+                name = None
+            else:
+                name = IsolateName(
+                    IsolateNameType(isolate["source_type"]),
+                    isolate["source_name"],
+                )
+
+            repo_isolate = repo.create_isolate(
+                repo_otu.id,
+                isolate["id"],
+                name,
+            )
+
+            for sequence in isolate["sequences"]:
+                repo.create_sequence(
+                    repo_otu.id,
+                    repo_isolate.id,
+                    sequence["accession"],
+                    sequence["definition"],
+                    sequence["_id"],
+                    sequence["segment"],
+                    sequence["sequence"],
+                )
