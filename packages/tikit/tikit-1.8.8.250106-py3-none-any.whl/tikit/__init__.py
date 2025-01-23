5d295d3a0e67f5dc4a0ffa70a1901567087c9185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:06:47 +0000

    Add package tikit/1.8.8.250106/tikit-1.8.8.250106-py3-none-any.whl

---
+++ packages/tikit/tikit-1.8.8.250106-py3-none-any.whl/tikit/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__version__ = '1.8.8.250106'
+from .default_client import *
+from .display_optimize import *
+# from .client import Client
+# from .hdfs import *
+# from .hive import *
+# from .models import *
+
