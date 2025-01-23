055a8be703c43ee997b25b03b88303ef20a124af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:21 +0000

    Add package types-aiobotocore-timestream-influxdb/2.19.0/types_aiobotocore_timestream_influxdb-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-timestream-influxdb/types_aiobotocore_timestream_influxdb-2.19.0.tar.gz/types_aiobotocore_timestream_influxdb-2.19.0/types_aiobotocore_timestream_influxdb/client.pyi
@@ -0,0 +1,261 @@
+"""
+Type annotations for timestream-influxdb service Client.
+
+[Documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_timestream_influxdb.client import TimestreamInfluxDBClient
+
+    session = get_session()
+    async with session.create_client("timestream-influxdb") as client:
+        client: TimestreamInfluxDBClient
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from __future__ import annotations
+
+import sys
+from types import TracebackType
+from typing import Any, overload
+
+from aiobotocore.client import AioBaseClient
+from botocore.client import ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import ListDbInstancesPaginator, ListDbParameterGroupsPaginator
+from .type_defs import (
+    CreateDbInstanceInputRequestTypeDef,
+    CreateDbInstanceOutputTypeDef,
+    CreateDbParameterGroupInputRequestTypeDef,
+    CreateDbParameterGroupOutputTypeDef,
+    DeleteDbInstanceInputRequestTypeDef,
+    DeleteDbInstanceOutputTypeDef,
+    EmptyResponseMetadataTypeDef,
+    GetDbInstanceInputRequestTypeDef,
+    GetDbInstanceOutputTypeDef,
+    GetDbParameterGroupInputRequestTypeDef,
+    GetDbParameterGroupOutputTypeDef,
+    ListDbInstancesInputRequestTypeDef,
+    ListDbInstancesOutputTypeDef,
+    ListDbParameterGroupsInputRequestTypeDef,
+    ListDbParameterGroupsOutputTypeDef,
+    ListTagsForResourceRequestRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceRequestRequestTypeDef,
+    UntagResourceRequestRequestTypeDef,
+    UpdateDbInstanceInputRequestTypeDef,
+    UpdateDbInstanceOutputTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from builtins import type as Type
+    from collections.abc import Mapping
+else:
+    from typing import Mapping, Type
+if sys.version_info >= (3, 12):
+    from typing import Literal, Self, Unpack
+else:
+    from typing_extensions import Literal, Self, Unpack
+
+__all__ = ("TimestreamInfluxDBClient",)
+
+class Exceptions(BaseClientExceptions):
+    AccessDeniedException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    InternalServerException: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+class TimestreamInfluxDBClient(AioBaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Client)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        TimestreamInfluxDBClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/can_paginate.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#can_paginate)
+        """
+
+    async def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/generate_presigned_url.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#generate_presigned_url)
+        """
+
+    async def create_db_instance(
+        self, **kwargs: Unpack[CreateDbInstanceInputRequestTypeDef]
+    ) -> CreateDbInstanceOutputTypeDef:
+        """
+        Creates a new Timestream for InfluxDB DB instance.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/create_db_instance.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#create_db_instance)
+        """
+
+    async def create_db_parameter_group(
+        self, **kwargs: Unpack[CreateDbParameterGroupInputRequestTypeDef]
+    ) -> CreateDbParameterGroupOutputTypeDef:
+        """
+        Creates a new Timestream for InfluxDB DB parameter group to associate with DB
+        instances.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/create_db_parameter_group.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#create_db_parameter_group)
+        """
+
+    async def delete_db_instance(
+        self, **kwargs: Unpack[DeleteDbInstanceInputRequestTypeDef]
+    ) -> DeleteDbInstanceOutputTypeDef:
+        """
+        Deletes a Timestream for InfluxDB DB instance.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/delete_db_instance.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#delete_db_instance)
+        """
+
+    async def get_db_instance(
+        self, **kwargs: Unpack[GetDbInstanceInputRequestTypeDef]
+    ) -> GetDbInstanceOutputTypeDef:
+        """
+        Returns a Timestream for InfluxDB DB instance.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/get_db_instance.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#get_db_instance)
+        """
+
+    async def get_db_parameter_group(
+        self, **kwargs: Unpack[GetDbParameterGroupInputRequestTypeDef]
+    ) -> GetDbParameterGroupOutputTypeDef:
+        """
+        Returns a Timestream for InfluxDB DB parameter group.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/get_db_parameter_group.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#get_db_parameter_group)
+        """
+
+    async def list_db_instances(
+        self, **kwargs: Unpack[ListDbInstancesInputRequestTypeDef]
+    ) -> ListDbInstancesOutputTypeDef:
+        """
+        Returns a list of Timestream for InfluxDB DB instances.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/list_db_instances.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#list_db_instances)
+        """
+
+    async def list_db_parameter_groups(
+        self, **kwargs: Unpack[ListDbParameterGroupsInputRequestTypeDef]
+    ) -> ListDbParameterGroupsOutputTypeDef:
+        """
+        Returns a list of Timestream for InfluxDB DB parameter groups.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/list_db_parameter_groups.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#list_db_parameter_groups)
+        """
+
+    async def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        A list of tags applied to the resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/list_tags_for_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#list_tags_for_resource)
+        """
+
+    async def tag_resource(
+        self, **kwargs: Unpack[TagResourceRequestRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Tags are composed of a Key/Value pairs.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/tag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#tag_resource)
+        """
+
+    async def untag_resource(
+        self, **kwargs: Unpack[UntagResourceRequestRequestTypeDef]
+    ) -> EmptyResponseMetadataTypeDef:
+        """
+        Removes the tag from the specified resource.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/untag_resource.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#untag_resource)
+        """
+
+    async def update_db_instance(
+        self, **kwargs: Unpack[UpdateDbInstanceInputRequestTypeDef]
+    ) -> UpdateDbInstanceOutputTypeDef:
+        """
+        Updates a Timestream for InfluxDB DB instance.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/update_db_instance.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#update_db_instance)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_db_instances"]
+    ) -> ListDbInstancesPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_db_parameter_groups"]
+    ) -> ListDbParameterGroupsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb/client/get_paginator.html)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/#get_paginator)
+        """
+
+    async def __aenter__(self) -> Self:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/)
+        """
+
+    async def __aexit__(
+        self,
+        exc_type: Type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/timestream-influxdb.html#TimestreamInfluxDB.Client)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_timestream_influxdb/client/)
+        """
