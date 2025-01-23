e0b1f2e22353f5d709effad936d9aba3c63f1b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:31 +0000

    Add package types-aiobotocore-connectcampaigns/2.19.0/types_aiobotocore_connectcampaigns-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-connectcampaigns/types_aiobotocore_connectcampaigns-2.19.0.tar.gz/types_aiobotocore_connectcampaigns-2.19.0/types_aiobotocore_connectcampaigns/__init__.py
@@ -0,0 +1,32 @@
+"""
+Main interface for connectcampaigns service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_connectcampaigns import (
+        Client,
+        ConnectCampaignServiceClient,
+        ListCampaignsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("connectcampaigns") as client:
+        client: ConnectCampaignServiceClient
+        ...
+
+
+    list_campaigns_paginator: ListCampaignsPaginator = client.get_paginator("list_campaigns")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import ConnectCampaignServiceClient
+from .paginator import ListCampaignsPaginator
+
+Client = ConnectCampaignServiceClient
+
+
+__all__ = ("Client", "ConnectCampaignServiceClient", "ListCampaignsPaginator")
