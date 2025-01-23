db8589597e283f9dd2a78b9101eee8313459b3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:26 +0000

    Add package types-aiobotocore-meteringmarketplace/2.19.0/types_aiobotocore_meteringmarketplace-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-meteringmarketplace/types_aiobotocore_meteringmarketplace-2.19.0.tar.gz/types_aiobotocore_meteringmarketplace-2.19.0/types_aiobotocore_meteringmarketplace/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for meteringmarketplace service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_meteringmarketplace import (
+        Client,
+        MarketplaceMeteringClient,
+    )
+
+    session = get_session()
+    async with session.create_client("meteringmarketplace") as client:
+        client: MarketplaceMeteringClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import MarketplaceMeteringClient
+
+Client = MarketplaceMeteringClient
+
+__all__ = ("Client", "MarketplaceMeteringClient")
