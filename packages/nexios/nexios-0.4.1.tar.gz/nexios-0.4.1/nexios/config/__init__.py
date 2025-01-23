0475e187f3f2454674afc74747cc76f1c531011b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:30 +0000

    Add package nexios/0.4.1/nexios-0.4.1.tar.gz

---
+++ packages/nexios/nexios-0.4.1.tar.gz/nexios-0.4.1/nexios/config/__init__.py
@@ -0,0 +1,15 @@
+_global_config = None
+
+def set_config(config):
+    global _global_config
+    _global_config = config
+
+def get_config(): #support for this will soo be droped
+    if _global_config is None:
+        raise RuntimeError("Configuration has not been initialized.")
+    return _global_config
+
+
+lambda _:_()
+def app_config():
+    return get_config().get("app")
