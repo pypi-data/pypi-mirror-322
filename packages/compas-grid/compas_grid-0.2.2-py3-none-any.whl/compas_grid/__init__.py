ff00e428ec6604f9e9517fcf6c17d7588c3b11fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:16 +0000

    Add package compas-grid/0.2.2/compas_grid-0.2.2-py3-none-any.whl

---
+++ packages/compas-grid/compas_grid-0.2.2-py3-none-any.whl/compas_grid/__init__.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import os
+
+from .models import GridModel
+
+# Temporary gloval property for debugging.
+debug = False
+global_property = []
+
+
+__author__ = ["Petras Vestartas"]
+__copyright__ = "Petras Vestartas"
+__license__ = "MIT License"
+__email__ = "petrasvestartas@gmail.com"
+__version__ = "0.2.2"
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DATA = os.path.abspath(os.path.join(HOME, "data"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+TEMP = os.path.abspath(os.path.join(HOME, "temp"))
+
+__all__ = ["HOME", "DATA", "DOCS", "TEMP", "GridModel"]
