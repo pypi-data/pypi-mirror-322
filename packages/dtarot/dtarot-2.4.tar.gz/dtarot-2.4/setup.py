65f29238fa8a57064c633ec99ede1e6d15838dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 15:06:00 +0000

    Add package dtarot/2.4/dtarot-2.4.tar.gz

---
+++ packages/dtarot/dtarot-2.4.tar.gz/dtarot-2.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='dtarot',  
+     version='2.4',
+     scripts=['dtarot'] ,
+     author="Lord Imbrius the Despondent",
+     author_email="darthferrett@gmail.com",
+     description="Discordian Tarot reading generator",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/lorimbrius/dtarot",
+     packages=setuptools.find_packages(),
+     include_package_data=True,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+	 "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+         "Operating System :: OS Independent",
+     ],
+ )
