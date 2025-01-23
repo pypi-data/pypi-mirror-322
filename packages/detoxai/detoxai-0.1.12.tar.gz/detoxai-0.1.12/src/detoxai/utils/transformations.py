bba9036f5afc14ba25f51279b70544697cf83d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:05:28 +0000

    Add package detoxai/0.1.12/detoxai-0.1.12.tar.gz

---
+++ packages/detoxai/detoxai-0.1.12.tar.gz/detoxai-0.1.12/src/detoxai/utils/transformations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# import torch
+import torchvision.transforms.functional as F
+
+
+class SquarePad:
+    def __call__(self, image):
+        w, h = image.size
+        max_wh = np.max([w, h])
+        hp = int((max_wh - w) / 2)
+        vp = int((max_wh - h) / 2)
+        padding = (hp, vp, hp, vp)
+        return F.pad(image, padding, 0, "constant")
+
+    def __repr__(self):
+        return self.__class__.__name__ + "()"
