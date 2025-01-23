685464aee6ef5fa1c29f70d3d942e8b75cfe76a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:05:09 +0000

    Add package benanalysis/4.0.26/benanalysis-4.0.26-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/benanalysis/benanalysis-4.0.26-cp312-cp312-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/benanalysis/monochromator/__init__.py
@@ -0,0 +1,9 @@
+#
+# @file __init__.py
+# @author Markus Führer
+# @date 5 May 2023
+# @copyright Copyright © 2023 Bentham Instruments Ltd. All Rights Reserved.
+#
+
+from benanalysis._benpy_core.monochromator import *
+from benanalysis._benpy_core.monochromator import slit_function
