87362bd442192ae3b3cef3948fa2f245a274911e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:42 +0000

    Add package nominal/1.28.0/nominal-1.28.0.tar.gz

---
+++ packages/nominal/nominal-1.28.0.tar.gz/nominal-1.28.0/nominal/cli/__init__.py
@@ -0,0 +1,14 @@
+import click
+
+from nominal.cli import attachment, auth, dataset, run
+
+
+@click.group(context_settings={"show_default": True, "help_option_names": ("-h", "--help")})
+def nom() -> None:
+    pass
+
+
+nom.add_command(attachment.attachment_cmd)
+nom.add_command(auth.auth_cmd)
+nom.add_command(dataset.dataset_cmd)
+nom.add_command(run.run_cmd)
