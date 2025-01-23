1ab614f458b6bf4f3a41c0daade2ed2012e77121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:28 +0000

    Add package types-aiobotocore-elastictranscoder/2.19.0/types_aiobotocore_elastictranscoder-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-elastictranscoder/types_aiobotocore_elastictranscoder-2.19.0.tar.gz/types_aiobotocore_elastictranscoder-2.19.0/types_aiobotocore_elastictranscoder/__init__.pyi
@@ -0,0 +1,54 @@
+"""
+Main interface for elastictranscoder service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_elastictranscoder import (
+        Client,
+        ElasticTranscoderClient,
+        JobCompleteWaiter,
+        ListJobsByPipelinePaginator,
+        ListJobsByStatusPaginator,
+        ListPipelinesPaginator,
+        ListPresetsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("elastictranscoder") as client:
+        client: ElasticTranscoderClient
+        ...
+
+
+    job_complete_waiter: JobCompleteWaiter = client.get_waiter("job_complete")
+
+    list_jobs_by_pipeline_paginator: ListJobsByPipelinePaginator = client.get_paginator("list_jobs_by_pipeline")
+    list_jobs_by_status_paginator: ListJobsByStatusPaginator = client.get_paginator("list_jobs_by_status")
+    list_pipelines_paginator: ListPipelinesPaginator = client.get_paginator("list_pipelines")
+    list_presets_paginator: ListPresetsPaginator = client.get_paginator("list_presets")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import ElasticTranscoderClient
+from .paginator import (
+    ListJobsByPipelinePaginator,
+    ListJobsByStatusPaginator,
+    ListPipelinesPaginator,
+    ListPresetsPaginator,
+)
+from .waiter import JobCompleteWaiter
+
+Client = ElasticTranscoderClient
+
+__all__ = (
+    "Client",
+    "ElasticTranscoderClient",
+    "JobCompleteWaiter",
+    "ListJobsByPipelinePaginator",
+    "ListJobsByStatusPaginator",
+    "ListPipelinesPaginator",
+    "ListPresetsPaginator",
+)
