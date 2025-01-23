b2c6f880f4fca355269b9ea198574f43348f8e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:04:56 +0000

    Add package CIBer/0.1.7/ciber-0.1.7.tar.gz

---
+++ packages/CIBer/ciber-0.1.7.tar.gz/ciber-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+  
+with open("README.md", "r") as fh: 
+    description_long = fh.read() 
+  
+setup( 
+    name="CIBer", 
+    version="0.1.7", 
+    author="Kaiser & Ross", 
+    author_email="rosscooper@link.cuhk.edu.hk", 
+    packages=find_packages(),
+    description="A Comonotone-Independence Bayesian Classifier", 
+    long_description=description_long, 
+    long_description_content_type="text/markdown", 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        ],
+    url="https://github.com/ross1456/CIBer-Package.git", 
+    license='MIT', 
+    python_requires='>=3.7', 
+    install_requires=["numpy","pandas","scipy","scikit-learn"] 
+)
