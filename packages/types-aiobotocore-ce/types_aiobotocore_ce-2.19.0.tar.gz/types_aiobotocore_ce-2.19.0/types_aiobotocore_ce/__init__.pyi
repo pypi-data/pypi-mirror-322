c574d3f4b511e5e4d89fadce2bf94dcac0a1cd1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:24 +0000

    Add package types-aiobotocore-ce/2.19.0/types_aiobotocore_ce-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-ce/types_aiobotocore_ce-2.19.0.tar.gz/types_aiobotocore_ce-2.19.0/types_aiobotocore_ce/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for ce service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ce import (
+        Client,
+        CostExplorerClient,
+    )
+
+    session = get_session()
+    async with session.create_client("ce") as client:
+        client: CostExplorerClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import CostExplorerClient
+
+Client = CostExplorerClient
+
+__all__ = ("Client", "CostExplorerClient")
