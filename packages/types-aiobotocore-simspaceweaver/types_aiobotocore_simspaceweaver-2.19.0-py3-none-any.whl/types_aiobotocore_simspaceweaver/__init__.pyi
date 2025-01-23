aa43db859acdf0ff54f6445c8cc261b6495e90fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:31 +0000

    Add package types-aiobotocore-simspaceweaver/2.19.0/types_aiobotocore_simspaceweaver-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-simspaceweaver/types_aiobotocore_simspaceweaver-2.19.0-py3-none-any.whl/types_aiobotocore_simspaceweaver/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for simspaceweaver service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_simspaceweaver import (
+        Client,
+        SimSpaceWeaverClient,
+    )
+
+    session = get_session()
+    async with session.create_client("simspaceweaver") as client:
+        client: SimSpaceWeaverClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import SimSpaceWeaverClient
+
+Client = SimSpaceWeaverClient
+
+__all__ = ("Client", "SimSpaceWeaverClient")
