9e20a63d24c95ae1ce764e5db5de968430559e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:04:58 +0000

    Add package rocraters/0.4.4/rocraters-0.4.4.tar.gz

---
+++ packages/rocraters/rocraters-0.4.4.tar.gz/rocraters-0.4.4/src/ro_crate/mod.rs
@@ -0,0 +1,18 @@
+//! Utility functions and structure for RO-Crate
+//!
+//! # Note
+//! Serialisatoin and deserialisation of RO-Crates to json-ld files heavily leverages
+//! the serde and serde-json library
+
+pub mod constraints;
+pub mod context;
+pub mod contextual_entity;
+pub mod data_entity;
+pub mod graph_vector;
+pub mod metadata_descriptor;
+pub mod modify;
+pub mod read;
+pub mod rocrate;
+pub mod root;
+pub mod schema;
+pub mod write;
