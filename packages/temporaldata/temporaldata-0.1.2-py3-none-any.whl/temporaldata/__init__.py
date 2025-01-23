aaefc7af549bedf67190d84c84e225e4989829f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 15:05:37 +0000

    Add package temporaldata/0.1.2/temporaldata-0.1.2-py3-none-any.whl

---
+++ packages/temporaldata/temporaldata-0.1.2-py3-none-any.whl/temporaldata/__init__.py
@@ -0,0 +1,16 @@
+from .temporaldata import (
+    ArrayDict,
+    Data,
+    Interval,
+    IrregularTimeSeries,
+    RegularTimeSeries,
+)
+from .temporaldata import (
+    LazyArrayDict,
+    LazyIrregularTimeSeries,
+    LazyRegularTimeSeries,
+    LazyInterval,
+)
+from .concat import concat
+
+__version__ = "0.1.1"
