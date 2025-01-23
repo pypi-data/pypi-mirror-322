48b6bc47859e637f99ae842561eb63f6fa92a19e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:24 +0000

    Add package types-aiobotocore-appconfigdata/2.19.0/types_aiobotocore_appconfigdata-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-appconfigdata/types_aiobotocore_appconfigdata-2.19.0-py3-none-any.whl/types_aiobotocore_appconfigdata/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for appconfigdata service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_appconfigdata import (
+        AppConfigDataClient,
+        Client,
+    )
+
+    session = get_session()
+    async with session.create_client("appconfigdata") as client:
+        client: AppConfigDataClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import AppConfigDataClient
+
+Client = AppConfigDataClient
+
+__all__ = ("AppConfigDataClient", "Client")
