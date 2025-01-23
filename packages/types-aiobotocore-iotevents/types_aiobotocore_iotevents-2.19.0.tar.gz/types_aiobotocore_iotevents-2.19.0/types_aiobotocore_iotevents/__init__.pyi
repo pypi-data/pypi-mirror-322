7f72566a95031c8a5170a3418e06ab94c7f99840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:21 +0000

    Add package types-aiobotocore-iotevents/2.19.0/types_aiobotocore_iotevents-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-iotevents/types_aiobotocore_iotevents-2.19.0.tar.gz/types_aiobotocore_iotevents-2.19.0/types_aiobotocore_iotevents/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for iotevents service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iotevents import (
+        Client,
+        IoTEventsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("iotevents") as client:
+        client: IoTEventsClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import IoTEventsClient
+
+Client = IoTEventsClient
+
+__all__ = ("Client", "IoTEventsClient")
