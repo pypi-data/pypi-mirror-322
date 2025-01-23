268a0f38b7cc92eae378cb6e9641a7568c480ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:54 +0000

    Add package uv-iso-env/1.0.20/uv_iso_env-1.0.20.tar.gz

---
+++ packages/uv-iso-env/uv_iso_env-1.0.20.tar.gz/uv_iso_env-1.0.20/src/iso_env/__init__.py
@@ -0,0 +1,15 @@
+import subprocess
+
+from .types import IsoEnv, IsoEnvArgs, PyProjectToml, Requirements
+
+
+def iso_run(
+    args: IsoEnvArgs, cmd_list: list[str] | str, **process_args
+) -> subprocess.CompletedProcess:
+    """Runs the command using the isolated environment."""
+    from .run import run
+
+    return run(args, cmd_list, **process_args)
+
+
+__all__ = ["IsoEnv", "IsoEnvArgs", "Requirements", "PyProjectToml", "IsoEnv"]
