f31a4f3a82330893921b36e3c5ce2f13eafa990a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:05:58 +0000

    Add package aduneoclientfedid/2.0.0/aduneoclientfedid-2.0.0.tar.gz

---
+++ packages/aduneoclientfedid/aduneoclientfedid-2.0.0.tar.gz/aduneoclientfedid-2.0.0/src/aduneoclientfedid/test/TestMain.py
@@ -0,0 +1,13 @@
+from .configuration import configuration_test
+from .proposition import proposition_test
+from .lightmarkdown import lightmarkdown_test
+from .jwt import jwt_test
+
+class TestMain():
+  
+  def test():
+    
+    #configuration_test.test()
+    #jwt_test.test()
+    #proposition_test.test()
+    lightmarkdown_test.test()
