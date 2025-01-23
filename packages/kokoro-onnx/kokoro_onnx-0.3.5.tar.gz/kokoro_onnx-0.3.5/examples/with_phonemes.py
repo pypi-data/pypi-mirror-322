eb3f35b2cbf6679c0f4ea8d9041a744f135b2b79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:04:57 +0000

    Add package kokoro-onnx/0.3.5/kokoro_onnx-0.3.5.tar.gz

---
+++ packages/kokoro-onnx/kokoro_onnx-0.3.5.tar.gz/kokoro_onnx-0.3.5/examples/with_phonemes.py
@@ -0,0 +1,19 @@
+"""
+pip install kokoro-onnx sounddevice
+
+wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files/kokoro-v0_19.onnx
+wget https://github.com/thewh1teagle/kokoro-onnx/releases/download/model-files/voices.bin
+python examples/with_phonemes.py
+"""
+
+import sounddevice as sd
+from kokoro_onnx import Kokoro
+
+kokoro = Kokoro("kokoro-v0_19.onnx", "voices.bin")
+phonemes = "mˈʌsk sˈɛd ɪnðɪ ɑːktˈoʊbɚ twˈɛnti twˈɛnti θɹˈiː kˈɔːl."  # Musk said in the October 2023 call
+samples, sample_rate = kokoro.create(
+    "", phonemes=phonemes, voice="af_sarah", speed=1.0, lang="en-us"
+)
+print("Playing audio...")
+sd.play(samples, sample_rate)
+sd.wait()
