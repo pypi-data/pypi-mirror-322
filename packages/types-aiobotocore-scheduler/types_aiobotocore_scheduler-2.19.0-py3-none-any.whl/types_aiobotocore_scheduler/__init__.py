7cd21429c44810b351b847e3669b07797edf12a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:30 +0000

    Add package types-aiobotocore-scheduler/2.19.0/types_aiobotocore_scheduler-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-scheduler/types_aiobotocore_scheduler-2.19.0-py3-none-any.whl/types_aiobotocore_scheduler/__init__.py
@@ -0,0 +1,39 @@
+"""
+Main interface for scheduler service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_scheduler import (
+        Client,
+        EventBridgeSchedulerClient,
+        ListScheduleGroupsPaginator,
+        ListSchedulesPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("scheduler") as client:
+        client: EventBridgeSchedulerClient
+        ...
+
+
+    list_schedule_groups_paginator: ListScheduleGroupsPaginator = client.get_paginator("list_schedule_groups")
+    list_schedules_paginator: ListSchedulesPaginator = client.get_paginator("list_schedules")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import EventBridgeSchedulerClient
+from .paginator import ListScheduleGroupsPaginator, ListSchedulesPaginator
+
+Client = EventBridgeSchedulerClient
+
+
+__all__ = (
+    "Client",
+    "EventBridgeSchedulerClient",
+    "ListScheduleGroupsPaginator",
+    "ListSchedulesPaginator",
+)
