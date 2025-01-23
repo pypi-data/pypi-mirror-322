70b1f7942e41b821c7adec9505a68d1e09112a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:05:41 +0000

    Add package test-hive-60-ti-client/1.0.0/test_hive_60_ti_client-1.0.0.tar.gz

---
+++ packages/test-hive-60-ti-client/test_hive_60_ti_client-1.0.0.tar.gz/test_hive_60_ti_client-1.0.0/test_hive_60_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from test_hive_60_ti_client.api.pet_api import PetApi
+from test_hive_60_ti_client.api.store_api import StoreApi
+from test_hive_60_ti_client.api.user_api import UserApi
+
