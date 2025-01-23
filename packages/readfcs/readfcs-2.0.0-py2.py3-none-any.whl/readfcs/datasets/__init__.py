903c2c924217964657bd495008bf146d54809c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:06:46 +0000

    Add package readfcs/2.0.0/readfcs-2.0.0-py2.py3-none-any.whl

---
+++ packages/readfcs/readfcs-2.0.0-py2.py3-none-any.whl/readfcs/datasets/__init__.py
@@ -0,0 +1,12 @@
+"""Datasets.
+
+.. autosummary::
+   :toctree: .
+
+   Oetjen18_t1
+   Oetjen18_t2
+   Oetjen18_dc
+   example
+"""
+
+from ._datasets import example, Oetjen18_t1, Oetjen18_t2, Oetjen18_dc
