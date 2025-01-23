5fa41177fc47c6df435d587f8bd4fde24cef19e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:05:01 +0000

    Add package shppy/0.2.0a0/shppy-0.2.0a0-cp310-cp310-win_amd64.whl

---
+++ packages/shppy/shppy-0.2.0a0-cp310-cp310-win_amd64.whl/shppy/_core/hello.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+
+extern "C" {
+    float add_floats(float a, float b){
+        return a + b;
+    }
+
+    void square_array(float* array, long size){
+        for(size_t i = 0; i < size; i++) {
+            array[i] = array[i] * array[i];
+        }
+    }
+}
