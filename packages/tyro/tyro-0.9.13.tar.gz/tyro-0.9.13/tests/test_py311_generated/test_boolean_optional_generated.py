1367119d10cae484780fbacfcf00ee1764c7f55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:06:08 +0000

    Add package tyro/0.9.13/tyro-0.9.13.tar.gz

---
+++ packages/tyro/tyro-0.9.13.tar.gz/tyro-0.9.13/tests/test_py311_generated/test_boolean_optional_generated.py
@@ -0,0 +1,85 @@
+import dataclasses
+
+from helptext_utils import get_helptext_with_checks
+
+import tyro
+
+
+def test_flag_default_false() -> None:
+    """Test for argparse.BooleanOptionalAction-style usage."""
+
+    @dataclasses.dataclass
+    class A:
+        x: bool
+
+    assert tyro.cli(
+        A,
+        args=["--x"],
+        default=A(False),
+    ) == A(True)
+
+    assert tyro.cli(
+        A,
+        args=["--no-x"],
+        default=A(False),
+    ) == A(False)
+
+    assert tyro.cli(
+        A,
+        args=[],
+        default=A(False),
+    ) == A(False)
+
+    # Type ignore can be removed once TypeForm lands.
+    # https://discuss.python.org/t/typeform-spelling-for-a-type-annotation-object-at-runtime/51435
+    assert tyro.cli(
+        tyro.conf.FlagConversionOff[A],
+        args=["--x", "True"],
+        default=A(False),  # type: ignore
+    ) == A(True)
+
+
+def test_flag_default_true() -> None:
+    """Test for argparse.BooleanOptionalAction-style usage."""
+
+    @dataclasses.dataclass
+    class A:
+        x: bool
+
+    assert tyro.cli(
+        A,
+        args=["--x"],
+        default=A(True),
+    ) == A(True)
+
+    assert tyro.cli(
+        A,
+        args=["--no-x"],
+        default=A(True),
+    ) == A(False)
+
+    assert tyro.cli(
+        A,
+        args=[],
+        default=A(True),
+    ) == A(True)
+
+    # Type ignore can be removed once TypeForm lands.
+    # https://discuss.python.org/t/typeform-spelling-for-a-type-annotation-object-at-runtime/51435
+    assert tyro.cli(
+        tyro.conf.FlagConversionOff[A],
+        args=["--x", "True"],
+        default=A(False),  # type: ignore
+    ) == A(True)
+
+
+def test_flag_default_true_helptext() -> None:
+    """Test for argparse.BooleanOptionalAction-style usage."""
+
+    @dataclasses.dataclass
+    class A:
+        x: bool = True
+
+    assert "(default: True)" in get_helptext_with_checks(A)
+    assert "(default: False)" not in get_helptext_with_checks(A)
+    assert "(default: None)" not in get_helptext_with_checks(A)
