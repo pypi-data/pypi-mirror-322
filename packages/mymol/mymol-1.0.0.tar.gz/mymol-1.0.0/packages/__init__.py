74429290c80753b2ed8dda28e685a0e12c07672f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 03:07:28 +0000

    Add package mymol/1.0.0/mymol-1.0.0.tar.gz

---
+++ packages/mymol/mymol-1.0.0.tar.gz/mymol-1.0.0/packages/__init__.py
@@ -0,0 +1,16 @@
+"""
+mymol - A main Python module for Arthur Kwak.
+"""
+from . import data_utils
+from . import date_utils
+from . import db_utils
+from . import file_utils
+from . import general_utils
+from . import lists_dicts_utils
+from . import math_utils
+from . import networking_utils
+from . import string_utils
+from . import text_utils
+
+__version__ = '1.0.0'
+__author__ = 'Arthur Kwak'
