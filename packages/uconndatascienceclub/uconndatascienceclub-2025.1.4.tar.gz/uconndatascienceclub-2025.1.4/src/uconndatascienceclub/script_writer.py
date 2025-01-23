b5cd91e5f3c9023836ddd4bfbf2ace968f02a947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 15:05:31 +0000

    Add package uconndatascienceclub/2025.1.4/uconndatascienceclub-2025.1.4.tar.gz

---
+++ packages/uconndatascienceclub/uconndatascienceclub-2025.1.4.tar.gz/uconndatascienceclub-2025.1.4/src/uconndatascienceclub/script_writer.py
@@ -0,0 +1,9 @@
+from available_scripts import all_scripts
+
+def write(date):
+    '''Writes the scripts that were used in the given meeting date'''
+    pass 
+
+def available_dates():
+    for key, value in all_scripts:
+        print(key)
