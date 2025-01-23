9da9a3f43ecbed4cfea73b1cec03cc9a2569334a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:06:03 +0000

    Add package lv-ui-testing/1.1.0/lv_ui_testing-1.1.0.tar.gz

---
+++ packages/lv-ui-testing/lv_ui_testing-1.1.0.tar.gz/lv_ui_testing-1.1.0/tests/test_plots.py
@@ -0,0 +1,26 @@
+# Start the VI "example 1 - Plot" first then run that script
+import importlib.util
+import sys
+spec = importlib.util.spec_from_file_location("lv_ui_testing", "../src/lv_ui_testing/ui_testing.py")
+ui_testing = importlib.util.module_from_spec(spec)
+sys.modules["lv_ui_testing"] = ui_testing
+spec.loader.exec_module(ui_testing)
+
+def test_front_most_vi():
+    # Ask the tester daemon what is the front most VI
+    front_most_vi = ui_testing.FMV_get_vi_name()
+    assert front_most_vi == "example 1 - plot.vi"
+
+def test_random_plot():
+    # Generate a random plot and query the data insisde it
+    ui_testing.FMV_click_on_button("Random")
+    data = ui_testing.FMV_get_value("Plot")
+    print(f"data from plot : {data}")
+
+    # Repeate the previous step
+    ui_testing.FMV_click_on_button("Random")
+    data2 = ui_testing.FMV_get_value("Plot")
+    print(f"data from plot : {data2}")
+
+    # Compare the results, they should be differnet
+    assert data != data2
