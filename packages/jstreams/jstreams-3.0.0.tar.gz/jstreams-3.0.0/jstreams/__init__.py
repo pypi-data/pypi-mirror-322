6653c8d2cf256552048582ccb531c9598dbf8d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 15:05:31 +0000

    Add package jstreams/3.0.0/jstreams-3.0.0.tar.gz

---
+++ packages/jstreams/jstreams-3.0.0.tar.gz/jstreams-3.0.0/jstreams/__init__.py
@@ -0,0 +1,121 @@
+from jstreams.stream import (
+    each,
+    dictUpdate,
+    Stream,
+    findFirst,
+    mapIt,
+    matching,
+    flatMap,
+    reduce,
+    takeWhile,
+    dropWhile,
+    isNotNone,
+    sort,
+    Opt,
+    ClassOps,
+    stream,
+    optional,
+)
+
+from jstreams.tryOpt import (
+    Try,
+    ErrorLog,
+)
+
+from jstreams.rx import (
+    ObservableSubscription,
+    Observable,
+    Flowable,
+    Single,
+    BehaviorSubject,
+    PublishSubject,
+    ReplaySubject,
+)
+
+from jstreams.rxops import (
+    Pipe,
+    Reduce,
+    Filter,
+    Map,
+    rxReduce,
+    rxFilter,
+    rxMap,
+    RxOperator,
+    BaseFilteringOperator,
+    BaseMappingOperator,
+)
+
+from jstreams.thread import (
+    LoopingThread,
+    CallbackLoopingThread,
+    cancelThread,
+)
+
+from jstreams.timer import (
+    Timer,
+    Interval,
+    CountdownTimer,
+    setTimer,
+    setInterval,
+    clear,
+)
+
+from jstreams.ioc import (
+    Injector,
+    injector,
+)
+
+from jstreams.noop import (
+    NoOp,
+    NoOpCls,
+)
+__all__ = [
+    "each",
+    "dictUpdate",
+    "Stream",
+    "findFirst",
+    "mapIt",
+    "matching",
+    "flatMap",
+    "reduce",
+    "takeWhile",
+    "dropWhile",
+    "isNotNone",
+    "sort",
+    "Opt",
+    "ClassOps",
+    "stream",
+    "optional",
+    "Try",
+    "ErrorLog",
+    "ObservableSubscription",
+    "Observable",
+    "Flowable",
+    "Single",
+    "BehaviorSubject",
+    "PublishSubject",
+    "ReplaySubject",
+    "Pipe",
+    "Reduce",
+    "Filter",
+    "Map",
+    "rxReduce",
+    "rxFilter",
+    "rxMap",
+    "RxOperator",
+    "BaseFilteringOperator",
+    "BaseMappingOperator",
+    "LoopingThread",
+    "CallbackLoopingThread",
+    "Timer",
+    "Interval",
+    "CountdownTimer",
+    "cancelThread",
+    "setTimer",
+    "setInterval",
+    "clear",
+    "Injector",
+    "injector",
+    "NoOp",
+    "NoOpCls",
+]
