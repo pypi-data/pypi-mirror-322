a4d5e3f13f9a75b33bfd480282d6284eaf4b4774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:06:18 +0000

    Add package simpleunit/1.0.30/simpleunit-1.0.30-py3-none-any.whl

---
+++ packages/simpleunit/simpleunit-1.0.30-py3-none-any.whl/simpleunit/__init__.py
@@ -0,0 +1,7 @@
+"""
+simpleunit module initializer in order to import simpleunit instead of simpleunit.simpleunit
+"""
+from simpleunit.unit_simple import Factor, Unit, FundamentalUnit, DerivedUnit, TransformedUnit, UnitConverter, \
+                                     UnitConverters, Metric, Time, Length, Surface, Temperature, Intensity, Current, \
+                                     Mass, Angle, SolidAngle, Speed, Volume, Frequency, Substance, Force, Energy, \
+                                     Charge, Voltage, Si
