e218a493387c0d92c05268d8f21982d7a9d00512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:24 +0000

    Add package WIOpy/1.1.1/wiopy-1.1.1.tar.gz

---
+++ packages/WIOpy/wiopy-1.1.1.tar.gz/wiopy-1.1.1/wiopy/__init__.py
@@ -0,0 +1,13 @@
+"""
+Walmart IO Wrapper.
+
+A basic wrapper for the WalmartIO API.
+
+:copyright: (c) 2022-present CoderJoshDK
+:license: MIT, see LICENSE for more details.
+"""
+# ruff: noqa: F403
+
+from .AsyncWIO import *
+from .WalmartIO import *
+from .WalmartResponse import *
---
+++ packages/WIOpy/wiopy-1.1.1.tar.gz/wiopy-1.1.1/wiopy/utils/errors.py
@@ -0,0 +1,37 @@
+"""Module for errors that the API might encounter."""
+
+__all__ = ("InvalidRequestException",)
+
+
+class WalmartException(Exception):
+    """Base Class for Walmart Api Exceptions."""
+
+
+class InvalidRequestException(WalmartException):
+    """Exception thrown if an invalid request response is thrown by Walmart."""
+
+    def __init__(self, status_code, **kwargs):  # noqa: D107
+        error_message = "Error"
+        if status_code == 400:
+            error_message = "Bad Request"
+            if kwargs.get("detail"):
+                error_message = error_message + " - " + str(kwargs["detail"])
+        elif status_code == 403:
+            error_message = "Forbidden"
+        elif status_code == 404:
+            error_message = "Wrong endpoint"
+        elif status_code == 414:
+            error_message = "Request URI too long"
+        elif status_code == 500:
+            error_message = "Internal Server Error"
+        elif status_code == 502:
+            error_message = "Bad Gateway"
+        elif status_code == 503:
+            error_message = "Service Unavailable/ API maintenance"
+        elif status_code == 504:
+            error_message = "Gateway Timeout"
+        message = (
+            "[Request failed] Walmart server answered with the following error: "
+            f"{error_message:s}. Status code: {status_code:d}"
+        )
+        super(self.__class__, self).__init__(message)
