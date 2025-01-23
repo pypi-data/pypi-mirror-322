778c9af799e6f97a20ce511fa0549e911357e4e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:05:56 +0000

    Add package a9x-webstatistics/1.4.22/a9x_webstatistics-1.4.22-py3-none-any.whl

---
+++ packages/a9x-webstatistics/a9x_webstatistics-1.4.22-py3-none-any.whl/a9x_webstatistics/gencockpitsubV0001/__init__.py
@@ -0,0 +1,5 @@
+from .cockpitlanguages import cockpitLanguages
+from .externalfriends import externalFriends
+from .genheader import genHeader
+from .performance import performance
+from .navchart import navchart
