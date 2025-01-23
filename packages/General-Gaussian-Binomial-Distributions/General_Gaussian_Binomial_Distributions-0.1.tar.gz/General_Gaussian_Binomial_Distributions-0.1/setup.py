f436836697e38fc9b1f6acf1adb6f9f0cc3c85f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 19:06:18 +0000

    Add package General-Gaussian-Binomial-Distributions/0.1/General_Gaussian_Binomial_Distributions-0.1.tar.gz

---
+++ packages/General-Gaussian-Binomial-Distributions/General_Gaussian_Binomial_Distributions-0.1.tar.gz/General_Gaussian_Binomial_Distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='General_Gaussian_Binomial_Distributions',
+      version='0.1',
+      description='General, Gaussian, Binomial and Distributions',
+      packages=['General_Gaussian_Binomial_Distributions'],
+      author = 'Maria Milman and Udacity',
+      zip_safe=False)
