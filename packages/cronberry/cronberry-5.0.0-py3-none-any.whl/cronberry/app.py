f6f5f9a93b39efa148708e4d04a7427168a7b0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 16:06:15 +0000

    Add package cronberry/5.0.0/cronberry-5.0.0-py3-none-any.whl

---
+++ packages/cronberry/cronberry-5.0.0-py3-none-any.whl/cronberry/app.py
@@ -0,0 +1,239 @@
+# SPDX-FileCopyrightText: 2024 Alec Delaney
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""CLI for working with cronberry library."""
+
+import click
+
+import cronberry
+
+
+@click.group()
+@click.version_option()
+def cli() -> None:
+    """Entry point for the CLI."""
+
+
+@cli.command()
+@click.argument(
+    "crontab",
+    type=click.Path(dir_okay=False, resolve_path=True),
+)
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+@click.option(
+    "-o",
+    "--overwrite/--no-overwrite",
+    default=False,
+    help="Overwrite if the job currently exists in the crontab",
+)
+@click.option(
+    "-t", "--title", default="", help="Title of specific job to add from CRONTAB"
+)
+def add(crontab: str, filepath: str | None, overwrite: bool, title: str) -> None:
+    """Add a job from CRONTAB to the crontab being used."""
+    jobs = cronberry.parse_crontab(crontab)
+    if title:
+        jobs = [job for job in jobs if job.title == title]
+    try:
+        cronberry.add_cronjobs(jobs, filepath, overwrite=overwrite)
+    except (ValueError, RuntimeError) as err:
+        raise click.ClickException(err.args[0]) from err
+
+
+@cli.command()
+@click.argument("job_title")
+@click.argument("cronjob")
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+@click.option(
+    "-o",
+    "--overwrite/--no-overwrite",
+    default=False,
+    help="Overwrite if the job currently exists in the crontab",
+)
+@click.option(
+    "-mt",
+    "--mailto",
+    default=None,
+    help=f"MAILTO environment variable for the job (default is {cronberry.CURRENT_USER})",
+)
+@click.option(
+    "-mf",
+    "--mailfrom",
+    default=None,
+    help="MAILFROM environment variable for the job (default is 'root')",
+)
+@click.option(
+    "-p",
+    "--path",
+    default=None,
+    help=f"PATH environment variable for the job (default is {cronberry.CURRENT_USER_PATH})",
+)
+@click.option(
+    "-s",
+    "--shell",
+    default=None,
+    help="SHELL environment variable for the job (default is '/bin/sh/)",
+)
+@click.option(
+    "-t",
+    "--cron-tz",
+    default=None,
+    help=f"CRON_TZ environment variable for the job (default is 'Etc/UTC)",
+)
+def enter(
+    job_title: str, cronjob: str, filepath: str | None, overwrite: bool, **kwargs
+) -> None:
+    """Add CRONJOB with the name JOB_TITLE to the crontab."""
+    job_kwargs = {key: value for key, value in kwargs.items() if value is not None}
+    timing, command = cronberry.CronJob.parse_cron_text(cronjob)
+    job = cronberry.CronJob(job_title, timing, command, **job_kwargs)
+    try:
+        cronberry.add_cronjobs((job,), filepath, overwrite=overwrite)
+    except (ValueError, RuntimeError) as err:
+        raise click.ClickException(err.args[0]) from err
+
+
+@cli.command()
+@click.argument(
+    "job_titles",
+    nargs=-1,
+)
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+@click.option(
+    "-i",
+    "--ignore-missing/--no-ignore-missing",
+    default=False,
+    help="Ignore jobs missing from the crontab",
+)
+def remove(job_titles: tuple[str], filepath: str | None, ignore_missing: bool) -> None:
+    """Remove JOB_TITLES from the crontab being used."""
+    unique_titles = set(job_titles)
+    if len(unique_titles) != len(job_titles):
+        raise click.ClickException("Duplicate job titles provided")
+    for job_title in job_titles:
+        try:
+            cronberry.remove_cronjob(job_title, filepath, ignore_missing=ignore_missing)
+        except ValueError:
+            raise click.ClickException(
+                f"Job title {job_title} does not exist in the crontab"
+            )
+
+
+@cli.command()
+@click.argument(
+    "filepath",
+    type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+)
+def save(filepath: str) -> None:
+    """Save the current crontab being used to FILEPATH."""
+    cronberry.save_crontab(filepath)
+
+
+@cli.command()
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+def clear(filepath: str | None) -> None:
+    """Clear the current crontab being used."""
+    cronberry.clear_cronjobs(filepath)
+
+
+@cli.command()
+@click.argument("job_title")
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+@click.option(
+    "-v",
+    "--variables",
+    is_flag=True,
+    default=False,
+    help="See the environment variables associated with the job",
+)
+def view(job_title: str, filepath: str | None, variables: bool) -> None:
+    """View the cronbjob JOB_TITLE from a crontab."""
+    all_jobs = cronberry.parse_crontab(filepath)
+    selected_jobs = [any_job for any_job in all_jobs if any_job.title == job_title]
+    if len(selected_jobs) < 1:
+        raise click.ClickException(
+            f"Job title {job_title} was not present in the crontab"
+        )
+    elif len(selected_jobs) > 1:
+        raise click.ClickException(
+            f"Job title {job_title} was found multiple times in the crontab"
+        )
+    selected_job = selected_jobs[0]
+    if variables:
+        formatted_command = selected_job.to_file_text()[:-1]
+    else:
+        formatted_command = str(selected_job)
+    click.echo(formatted_command)
+
+
+@cli.command()
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+@click.option(
+    "-v",
+    "--variables",
+    is_flag=True,
+    default=False,
+    help="See the environment variables associated with the job",
+)
+def list(filepath: str | None, variables: bool) -> None:
+    """List all the jobs in a given cron table."""
+    all_jobs = cronberry.parse_crontab(filepath)
+    for index, selected_job in enumerate(all_jobs):
+        if index != 0:
+            click.echo()
+        if variables:
+            formatted_command = selected_job.to_file_text()[:-1]
+        else:
+            formatted_command = f"# [{selected_job.title}]\n{str(selected_job)}"
+        click.echo(formatted_command)
+
+
+@cli.command()
+@click.option(
+    "-f",
+    "--filepath",
+    type=click.Path(dir_okay=False, resolve_path=True),
+    default=None,
+    help="Filepath to use instead of the user's crontab",
+)
+def jobs(filepath: str | None) -> None:
+    """Get the jobs from a specific crontab."""
+    jobs = cronberry.parse_crontab(filepath)
+    for job in jobs:
+        click.echo(job.title)
---
+++ packages/cronberry/cronberry-5.0.0-py3-none-any.whl/cronberry/fields.py
@@ -0,0 +1,105 @@
+# SPDX-FileCopyrightText: 2024 Alec Delaney
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+"""Classes and type annotations for cronjob fields."""
+
+from enum import Enum
+from typing import Any, NamedTuple, TypeAlias
+
+
+def _cron_str(comp: "DiscreteValue") -> str:
+    """Return the cron equivalent string of a discrete value (field value)."""
+    return "*" if comp is None else str(comp)
+
+
+class ShorthandSyntax(Enum):
+    """Enum for shorthand syntax of timing options."""
+
+    HOURLY = "@hourly"
+    DAILY = "@daily"
+    WEEKLY = "@weekly"
+    ANNUALLY = "@annually"
+    YEARLY = "@yearly"
+    REBOOT = "@reboot"
+
+
+AnyValue: TypeAlias = None
+"""Type alias for any value (*)."""
+
+ExactValue: TypeAlias = int
+"""Type alias for an exact value (2)."""
+
+
+class ValueRange(NamedTuple):
+    """Named tuple of a range of values (3-5)."""
+
+    start: ExactValue
+    end: ExactValue
+
+    def __eq__(self, other: Any) -> bool:
+        """Check equality."""
+        if isinstance(other, ValueRange):
+            return self.start == other.start and self.end == other.end
+        return False
+
+    def __str__(self) -> str:
+        """Representation of the value range as a string (x-y)."""
+        return f"{self.start}-{self.end}"
+
+
+class StepValues(NamedTuple):
+    """Named tuple of a set of step values (8/3)."""
+
+    duration: AnyValue | ExactValue | ValueRange
+    frequency: ExactValue
+
+    def __eq__(self, other: Any) -> bool:
+        """Check equality."""
+        if isinstance(other, StepValues):
+            return self.duration == other.duration and self.frequency == other.frequency
+        return False
+
+    def __str__(self) -> str:
+        """Representation of the step values as a string (x/y)."""
+        return f"{_cron_str(self.duration)}/{self.frequency}"
+
+
+ListValue: TypeAlias = AnyValue | ExactValue | ValueRange | StepValues
+"""Type alias of any single list item."""
+
+ListValues: TypeAlias = tuple[ListValue, ...]
+"""Type alias for a list of items."""
+
+DiscreteValue: TypeAlias = ListValues | ListValue
+"""Type alias for a single discrete value (field value)."""
+
+
+class ExplicitTiming(NamedTuple):
+    """Named tuple for an explicit timing setup."""
+
+    minute: DiscreteValue
+    hour: DiscreteValue
+    day_of_month: DiscreteValue
+    month: DiscreteValue
+    day_of_week: DiscreteValue
+
+    def __eq__(self, __value: object) -> bool:
+        """Check equality."""
+        if isinstance(__value, ExplicitTiming):
+            return (
+                self.minute == __value.minute
+                and self.hour == __value.hour
+                and self.day_of_month == __value.day_of_month
+                and self.month == __value.month
+                and self.day_of_week == __value.day_of_week
+            )
+        return False
+
+    def __str__(self) -> str:
+        """Representation of the explicit timing as a string (a, b, c, d, e)."""
+        minute_comp = ",".join(_cron_str(comp) for comp in self.minute)
+        hour_comp = ",".join(_cron_str(comp) for comp in self.hour)
+        dom_comp = ",".join(_cron_str(comp) for comp in self.day_of_month)
+        month_comp = ",".join(_cron_str(comp) for comp in self.month)
+        dow_comp = ",".join(_cron_str(comp) for comp in self.day_of_week)
+        return f"{minute_comp} {hour_comp} {dom_comp} {month_comp} {dow_comp}"
