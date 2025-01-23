7f7ec4af475337f3ca48631f5fad2a4c80941fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:16 +0000

    Add package aims-chain/0.1.3/aims_chain-0.1.3-py3-none-any.whl

---
+++ packages/aims-chain/aims_chain-0.1.3-py3-none-any.whl/aimsChain/gs_path.py
@@ -0,0 +1,356 @@
+"""
+The growing string path
+"""
+import numpy as np
+from aimsChain.utility import vmag, vunit, vproj
+from aimsChain.path import Path
+from aimsChain.node import Node
+from aimsChain.optimizer.optimize import FDOptimize
+
+import pickle as cp
+class GrowingStringPath(Path):
+
+    def __init__(self, 
+             nodes = [],
+             control = None):
+
+        super(GrowingStringPath,self).__init__(nodes,control)
+
+        self.lower_end = 0
+
+    @property
+    def upper_end(self):
+        return self.lower_end + 1
+    @property
+    def linsep(self):
+#        print "using " + str(self.control.nimage) + " images\n"
+        return 1.0/(self.control.gs_nimage + 1)
+
+    @property
+    def params(self):
+        t = []
+        for node in self.nodes:
+            t.append(node.param)
+        return t
+    @property
+    def lower(self):
+        return self.nodes[self.lower_end]
+    @property
+    def upper(self):
+        return self.nodes[self.upper_end]
+
+
+    def add_lower(self):
+        from aimsChain.interpolate import spline_pos
+        import copy
+        import os
+        if ((self.upper.param - self.lower.param)
+            <= self.linsep * 1.5):
+            return False
+
+        self.insert_node(self.lower.param + self.linsep)
+        self.lower_end += 1
+
+#        if self.control.gs_global_optimizer:
+#            save = os.path.join(self.nodes[0].dir_pre, "pgs.opt")
+#            opt = self.get_optimizer("dampedBFGS", save)
+#            opt.initialize()
+ #           opt.load()
+ #           opt.insert_node(self.lower_end, #loc to insert
+ #                           len(self.nodes[0].geometry.atoms)) #num of atoms
+
+
+        if ((self.upper.param - self.lower.param)
+            <= self.linsep * 1.5):
+            return False
+
+        return True
+
+        
+    def add_upper(self):    
+        import copy
+        import os
+        from aimsChain.interpolate import spline_pos
+        if ((self.upper.param - self.lower.param)
+            <= self.linsep * 1.5):
+            return False
+
+        self.insert_node(self.upper.param - self.linsep)
+        
+        if ((self.upper.param - self.lower.param)
+            <= self.linsep * 1.5):
+            return False
+
+ #       if self.control.gs_global_optimizer:
+ #           save = os.path.join(self.nodes[0].dir_pre, "pgs.opt")
+ #           opt = self.get_optimizer("dampedBFGS", save)
+ #           opt.initialize()
+ #           opt.load()
+ #           opt.insert_node(self.lower_end, #loc to insert
+ #                           len(self.nodes[0].geometry.atoms)) #num of atoms
+        
+
+        return True
+
+
+    def lower_tangent(self):
+        from aimsChain.interpolate import spline_pos
+        if (self.upper.param - self.lower.param) <= self.linsep*1.5:
+            return self.lower.get_tangent()
+        positions = []
+        param = self.params
+        loc = self.lower_end
+        for i in self.nodes:
+            positions.append(i.positions)
+          
+        derv = spline_pos(positions, param, param, 3, 1)[loc]
+        tangent = derv
+        
+        return vunit(tangent)
+
+    def upper_tangent(self):
+        if (self.upper.param - self.lower.param) <= self.linsep*1.5:
+            return self.upper.get_tangent()
+        from aimsChain.interpolate import spline_pos
+        positions = []
+        param = self.params
+        loc = self.upper_end
+        for i in self.nodes:
+            positions.append(i.positions)
+          
+        derv = spline_pos(positions, param, param, 3, 1)[loc]
+        tangent = derv
+        
+        return vunit(tangent)
+
+    def lower_force(self):
+        forces = self.lower.forces
+        tangent = self.lower_tangent()
+        forces -= vproj(forces,tangent)
+        return forces
+
+    def upper_force(self):
+        forces = self.upper.forces
+        tangent = self.upper_tangent()
+        forces -= vproj(forces,tangent)
+        return forces
+
+
+
+    def move_nodes(self):
+        forces = None
+        if (self.upper.param - self.lower.param) <= self.linsep*1.5:
+            forces = self.move_nodes_combined()
+        else:
+            forces = self.move_nodes_separated()
+
+        return forces
+
+    def move_nodes_separated(self):
+        from aimsChain.interpolate import spline_pos, get_total_length, get_t
+        import os
+        
+        positions = []
+        forces = []
+        new_pos = []
+        new_pos2= []
+        total_length = 0
+        low_force = self.lower_force()
+        high_force = self.upper_force()
+        for node in self.nodes:
+            positions.append(node.positions)
+
+            if node is self.upper:
+                forces.append(self.upper_force())
+            elif node is self.lower:
+                forces.append(self.lower_force())
+            else:
+                forces.append(node.normal_forces)
+
+        forces = np.array(forces)
+        positions = np.array(positions)
+        
+        if self.control.gs_global_optimizer:
+            save = os.path.join(self.nodes[0].dir_pre, "gs.opt")
+            new_pos,opt = self.g_opt(
+                self.control.gs_optimizer,
+                positions,
+                forces,
+                save)
+        else:
+            new_pos,opt = self.nong_opt(
+                self.control.gs_optimizer,
+                self.nodes,
+            positions,
+                forces,
+                ".gs.opt")
+
+        lower_length = get_total_length(new_pos[:self.lower_end+1])
+        upper_length = get_total_length(new_pos[self.upper_end:])
+        total_length = get_total_length(spline_pos(np.array(new_pos), np.linspace(0,1,100)))
+
+
+        #decide whether to reparm locally or globally
+        lower_ideal = self.lower.param*total_length
+        upper_ideal = total_length - self.upper.param*total_length
+
+
+        if (abs(lower_length-lower_ideal) > 0.1*lower_ideal or 
+            abs(upper_length - upper_ideal) > 0.1 * upper_ideal):
+            if not (isinstance(opt, FDOptimize) and opt.finite_diff):
+                new_pos2 = spline_pos(np.array(new_pos), self.params)
+            else:
+                new_pos2 = new_pos
+
+            
+        else:
+            temp_pos = new_pos[:self.lower_end+1]
+            old_t = get_t(temp_pos)
+            new_t = np.linspace(0.0,1.0,len(temp_pos))
+            
+            if not (isinstance(opt, FDOptimize) and opt.finite_diff):
+                temp_pos = spline_pos(temp_pos, new_t, old_t)
+            else:
+                pass
+            new_pos2.extend(temp_pos)
+
+            temp_pos = new_pos[self.upper_end:]
+            old_t = get_t(temp_pos)
+            new_t = np.linspace(0.0,1.0,len(temp_pos))
+            
+            if not (isinstance(opt, FDOptimize) and opt.finite_diff):
+                temp_pos = spline_pos(temp_pos, new_t, old_t)
+            else:
+                pass
+            new_pos2.extend(temp_pos)
+
+
+        for i,position in enumerate(new_pos2):
+            self.nodes[i].positions = position
+
+
+        high_force = np.reshape(high_force, (-1,3))
+        high_force = np.sum(high_force**2,1)**0.5
+
+        low_force = np.reshape(low_force, (-1,3))
+        low_force = np.sum(low_force**2,1)**0.5
+
+        forces = np.reshape(forces, (-1,3))
+        forces = np.sum(forces**2,1)**0.5
+        return np.nanmax(forces),np.nanmax(low_force),np.nanmax(high_force)
+
+
+
+
+    def move_nodes_combined(self):
+        from aimsChain.interpolate import spline_pos, get_total_length
+        import os
+        
+        positions = []
+        forces = []
+        new_t = self.params
+        new_pos = []
+        low_force = self.lower_force()
+        high_force = self.upper_force()
+
+        for i,node in enumerate(self.nodes):
+            positions.append(node.positions)
+            forces.append(node.normal_forces)
+
+        forces = np.array(forces)
+        positions = np.array(positions)
+        if self.control.gs_global_optimizer:
+            save = os.path.join(self.nodes[0].dir_pre, "pgs.opt")
+            new_pos,opt = self.g_opt(
+                self.control.gs_optimizer,
+                positions,
+                forces,
+                save)
+
+        else:
+            new_pos,opt = self.nong_opt(
+                self.control.gs_optimizer,
+                self.nodes,
+                positions,
+                forces,
+                ".gs.opt")
+
+        if not (isinstance(opt, FDOptimize) and opt.finite_diff):
+            new_pos = spline_pos(new_pos, new_t)
+
+
+        for i,position in enumerate(new_pos):
+            self.nodes[i].positions = position
+
+
+
+        
+        high_force = np.reshape(high_force, (-1,3))
+        high_force = np.sum(high_force**2,1)**0.5
+
+        low_force = np.reshape(low_force, (-1,3))
+        low_force = np.sum(low_force**2,1)**0.5
+
+        forces = np.reshape(forces, (-1,3))
+        forces = np.sum(forces**2,1)**0.5
+        return np.nanmax(forces),np.nanmax(low_force),np.nanmax(high_force)
+
+
+
+    def write_path(self, filename="path.dat"):
+        """
+        Write the current path into a file
+        Will contain:
+        current runs
+        each node's param, dir, and whether they are fixed or not
+        This was preferred over pickling for the sake of easy debugging
+        May change to pickling in the production level
+        """
+        data = {}
+        data["runs"] = self.runs
+        params = []
+        dirs = []
+        fix = []
+        climb = []
+        for node in self.nodes:
+            params.append(node.param)
+            dirs.append(node.dir)
+            fix.append(node.fixed)
+            climb.append(node.climb)
+
+        data["param"] = params
+        data["dirs"] = dirs
+        data["fix"] = fix
+        data["climb"] = climb
+        data["lower"] = self.lower_end
+
+        save = open(filename,'wb')
+        cp.dump(data,save)
+        save.close()
+
+    def read_path(self, filename="path.dat"):
+        """
+        Read the file into current path
+        """
+        nodes = []
+        save = open(filename, 'rb')
+        print() 
+        data = cp.load(save)
+        save.close()
+
+        self.runs = data["runs"]
+        params = data["param"]
+        dirs = data["dirs"]
+        fix = data["fix"]
+        climb = data["climb"]
+        self.lower_end = data["lower"]
+
+        for i,param in enumerate(params):
+            tmp_node = Node(param, path=self)
+            tmp_node.dir = dirs[i]
+            tmp_node.fixed = fix[i]
+            tmp_node.climb = climb[i]
+            
+            nodes.append(tmp_node)
+    
+        self.nodes = nodes
---
+++ packages/aims-chain/aims_chain-0.1.3-py3-none-any.whl/aimsChain/node.py
@@ -0,0 +1,270 @@
+"""
+This module defines the Node
+Node is intended to be the fundamental unit in string method
+Unlike geometry, it maintains parametric values, which remains 
+unchanged through the entire calculation
+"""
+
+import numpy as np
+from aimsChain.utility import vmag, vunit, vproj
+from aimsChain.config import Control
+
+
+
+class Node(object):
+    """
+    Class for a single Node
+    """
+    def __init__(self, 
+                 param = 0,
+                 geometry = None,
+                 path = None,
+                 dir = None,
+                 fixed = False):
+        from aimsChain.atom import Atoms
+        self.__param = float(param)
+        self.__geometry = geometry
+        if self.__geometry == None:
+            self.__geometry = Atoms()
+        self.__path = path
+        self.__dir = dir
+        self.__fixed = fixed
+        self.__climb = False
+        self.__dir_pre = "iterations"
+        self.__previous_dir = None
+
+    @property
+    def param(self):
+        return self.__param
+    @param.setter
+    def param(self, t):
+        self.__param = float(t)
+    @property
+    def geometry(self):
+        return self.__geometry
+    @geometry.setter
+    def geometry(self, atoms):
+        self.__geometry = atoms
+    @property
+    def climb(self):
+        return self.__climb
+    @climb.setter
+    def climb(self, climb):
+        self.__climb = bool(climb)
+    
+    @property
+    def control(self):
+        if self.path:
+            return self.path.control
+        else:
+            return Control()
+
+    @property
+    def path(self):
+        return self.__path
+    @path.setter
+    def path(self, path):
+        self.__path = path
+
+    @property
+    def dir(self):
+        return self.__dir
+    @dir.setter
+    def dir(self, dir):
+        self.__dir = dir
+    
+    @property
+    def dir_pre(self):
+        return self.__dir_pre
+    @dir_pre.setter
+    def dir_pre(self,dir_pre):
+        self.__dir_pre = dir_pre
+    @property
+    def ener(self):
+        return float(self.__geometry.ener)
+    @ener.setter
+    def ener(self, ener):
+        self.__geometry.ener = float(ener)
+    
+    @property
+    def positions(self):
+        return self.__geometry.positions
+    @positions.setter
+    def positions(self, positions):
+        if not self.__fixed:
+            self.__geometry.positions = positions
+    
+    @property
+    def forces(self):
+        if self.fixed:
+            return np.zeros(np.shape(self.geometry.forces))
+        else:
+            return self.geometry.forces
+    @forces.setter
+    def forces(self, forces):
+        self.__geometry.forces = forces
+
+    @property
+    def climb_forces(self):
+        if self.fixed:
+            return np.zeros(np.shape(self.forces))
+        elif self.climb:
+            forces = self.forces
+            tangent = self.get_tangent(for_climb=True, test_tangent = False)
+            return forces - 2*vproj(forces,tangent)
+        else:
+            if self.control.method == "neb":
+                return self.spring_forces
+            else:
+                return self.normal_forces
+
+    @property
+    def normal_forces(self):
+        if self.fixed:
+            return np.zeros(np.shape(self.geometry.forces))
+        elif self.prev == None or self.next == None:
+            return self.forces
+        forces = self.forces
+        tangent = self.get_tangent()
+        forces -= vproj(forces, tangent)
+        return forces
+
+    @property
+    def spring_forces(self):
+        k = self.control.spring_k
+        if self.fixed:
+            return np.zeros(np.shape(self.positions))
+        elif self.prev==None or self.next==None:
+            return self.forces
+        forces = self.normal_forces
+        tangent = self.get_tangent()
+        tan1 = self.next.positions - self.positions
+        tan2 = self.positions - self.prev.positions
+        mag = (vmag(tan1) - vmag(tan2))
+        forces += k * mag * tangent
+        return forces
+    
+   
+    @property
+    def ener(self):
+        return self.__geometry.ener
+    @ener.setter
+    def ener(self, energy):
+        self.__geometry.ener = energy
+
+    @property
+    def fixed(self):
+        return self.__fixed
+    @fixed.setter
+    def fixed(self, value):
+        self.__fixed = bool(value)
+
+    @property
+    def prev(self):
+        """get the previous element in path"""
+        if not self.path:
+            return None
+        i = self.path.nodes.index(self)
+        if i == 0:
+            return None
+        else:
+            return self.path.nodes[i-1]
+    @property
+    def next(self):
+        """get the next element in path"""
+        if not self.path:
+            return None
+        i = self.path.nodes.index(self)
+        if i+1 == len(self.path.nodes):
+            return None
+        else:
+            return self.path.nodes[i+1]
+
+    @property
+    def previous_dir(self):
+        """store the previous dir for this node"""
+        return self.__previous_dir
+    @previous_dir.setter
+    def previous_dir(self, value):
+        self.__previous_dir = value
+
+
+    def get_tangent(self, for_climb=False, unit=True, test_tangent = False):
+        from aimsChain.utility import vunit
+        from aimsChain.interpolate import get_t, spline_pos
+        prev = self.prev
+        next = self.next
+        tangent = None
+        if not prev:
+            tangent = (next.positions - self.positions)
+        elif not next:
+            tangent = (self.positions - prev.positions)
+        else:
+            tan1 = next.positions - self.positions
+            tan2 = self.positions - prev.positions
+
+            if next.ener >= self.ener and self.ener >= prev.ener and not for_climb:
+                tangent = tan1
+            elif next.ener < self.ener and self.ener < prev.ener and not for_climb:
+                tangent = tan2
+            elif not test_tangent:
+                max = np.nanmax((abs(next.ener-self.ener), abs(self.ener-prev.ener)))
+                min = np.nanmin((abs(next.ener-self.ener), abs(self.ener-prev.ener)))
+
+                if next.ener >= prev.ener:
+                    tangent = (max * tan1 + min * tan2)
+                else:
+                    tangent = (min * tan1 + max * tan2)
+            else:
+                positions = [prev.positions, self.positions, next.positions]
+                loc = 1
+                if prev.prev:
+                    positions.insert(0,prev.prev.positions)
+                    loc += 1
+                if next.next:
+                    positions.append(next.next.positions)
+                param = get_t(positions)
+                derv = spline_pos(positions, param, param, 3, 1)[loc]
+                tangent = derv
+        if unit:
+            return vunit(tangent)
+        
+
+        return tangent
+
+    def write_node(self, write_fixed = False, control_file = "control.in"):
+        """
+        create a directory for each node
+        including geometry and control
+        """
+        import glob, os, shutil
+        from aimsChain.aimsio import write_aims
+        dir = os.path.join(self.dir_pre, self.dir)
+        if (not self.fixed) or write_fixed:
+            if not os.path.isdir(dir):
+                os.makedirs(dir)
+            write_aims(os.path.join(dir, "geometry.in"), self.geometry)
+            shutil.copy(control_file, os.path.join(dir, "control.in"))
+            if (self.path and self.control.aims_restart
+                and self.previous_dir):
+                if self.previous_dir != self.dir:
+                    restart_file = self.control.aims_restart + "*"
+                    files = glob.glob(os.path.join(self.dir_pre, self.previous_dir, restart_file))
+                    for file in files:
+                        if os.path.isfile(file):
+                            shutil.copy(file, dir)
+            return dir
+        else:
+            return None
+    
+    def update_dir(self, write_fixed = False):
+        """
+        update the dirs for all the nodes
+        if write_fixed = True, 
+        even fixed nodes will get updates
+        """
+        if (not self.fixed) or write_fixed:
+            self.previous_dir = self.dir
+            self.dir = "iteration%04d/" % self.path.runs + "aims-chain-node-%.5f" % (self.param)
+                
+
