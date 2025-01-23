92045b96eced1f907c1903cff2567cfbd3af232e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:29 +0000

    Add package types-aiobotocore-firehose/2.19.0/types_aiobotocore_firehose-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-firehose/types_aiobotocore_firehose-2.19.0.tar.gz/types_aiobotocore_firehose-2.19.0/types_aiobotocore_firehose/__init__.py
@@ -0,0 +1,28 @@
+"""
+Main interface for firehose service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_firehose import (
+        Client,
+        FirehoseClient,
+    )
+
+    session = get_session()
+    async with session.create_client("firehose") as client:
+        client: FirehoseClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import FirehoseClient
+
+Client = FirehoseClient
+
+
+__all__ = ("Client", "FirehoseClient")
