035802379984278f5440e9728eea8fe2ac561f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:06 +0000

    Add package imas-python/1.1.2.dev54/imas_python-1.1.2.dev54.tar.gz

---
+++ packages/imas-python/imas_python-1.1.2.dev54.tar.gz/imas_python-1.1.2.dev54/ci/build_dd_zip.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Bamboo CI script to build IDSDef.zip
+# Note: this script should be run from the root of the git repository
+
+# Debuggging:
+set -e -o pipefail
+echo "Loading modules..."
+
+# Set up environment such that module files can be loaded
+source /etc/profile.d/modules.sh
+module purge
+# Modules are supplied as arguments in the CI job:
+module load $@
+
+# Debuggging:
+echo "Done loading modules"
+set -x
+
+# Build the DD zip
+rm -rf venv  # Environment should be clean, but remove directory to be sure
+python -m venv venv
+source venv/bin/activate
+pip install gitpython saxonche packaging
+python imas/dd_helpers.py
+deactivate
