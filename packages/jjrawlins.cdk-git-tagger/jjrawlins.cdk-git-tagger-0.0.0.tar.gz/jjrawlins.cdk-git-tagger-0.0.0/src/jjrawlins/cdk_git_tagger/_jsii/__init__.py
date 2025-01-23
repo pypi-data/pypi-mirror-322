2a6021625a4db733bde14621a489e19d98d67c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:05:40 +0000

    Add package jjrawlins.cdk-git-tagger/0.0.0/jjrawlins.cdk-git-tagger-0.0.0.tar.gz

---
+++ packages/jjrawlins.cdk-git-tagger/jjrawlins.cdk-git-tagger-0.0.0.tar.gz/jjrawlins.cdk-git-tagger-0.0.0/src/jjrawlins/cdk_git_tagger/_jsii/__init__.py
@@ -0,0 +1,31 @@
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@jjrawlins/cdk-git-tagger",
+    "0.0.0",
+    __name__[0:-6],
+    "cdk-git-tagger@0.0.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
