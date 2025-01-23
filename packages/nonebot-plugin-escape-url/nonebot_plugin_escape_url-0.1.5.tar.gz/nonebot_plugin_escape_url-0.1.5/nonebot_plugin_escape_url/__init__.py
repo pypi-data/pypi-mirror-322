c967d73a32bba9582a95bcaf7fd52a9151e82294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:04:59 +0000

    Add package nonebot-plugin-escape-url/0.1.5/nonebot_plugin_escape_url-0.1.5.tar.gz

---
+++ packages/nonebot-plugin-escape-url/nonebot_plugin_escape_url-0.1.5.tar.gz/nonebot_plugin_escape_url-0.1.5/nonebot_plugin_escape_url/__init__.py
@@ -0,0 +1,16 @@
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="链接防夹",
+    description="自动转换发送消息中的URL，逃避URL检测",
+    usage="无",
+    type="application",
+    homepage="https://github.com/bot-ssttkkl/nonebot-plugin-escape-url",
+    config=Config,
+    supported_adapters={"~onebot.v11"}
+)
+
+from . import onebot_v11
+from . import qq
