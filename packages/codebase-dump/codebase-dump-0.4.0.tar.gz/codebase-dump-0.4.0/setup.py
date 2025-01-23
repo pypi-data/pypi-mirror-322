34c24d5ac567b2191fadc33f2b201cb7c53cbf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:05:13 +0000

    Add package codebase-dump/0.4.0/codebase-dump-0.4.0.tar.gz

---
+++ packages/codebase-dump/codebase-dump-0.4.0.tar.gz/codebase-dump-0.4.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="codebase-dump",
+    version="0.4.0",
+    description="Generate a single-file dump of your repository, so you can use it as LLM input",
+    author="Mirek Stanek, Kamil Stanuch",
+    author_email="mirek@practicalengineering.management, kamil@stanuch.eu",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=["tiktoken", "gitignore_parser"],  # List core dependencies here
+    extras_require={
+        "dev": ["pytest", "twine"]  # Development dependencies
+    },
+    entry_points={
+    'console_scripts': [
+        'codebase-dump=codebase_dump.app:main',
+    ]},
+    python_requires=">=3.7",
+)
