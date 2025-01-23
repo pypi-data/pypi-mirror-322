cc267cb292b11c6f10cb989b104ecc7a90189c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 04:04:57 +0000

    Add package nonebot_plugin_BR/0.0.7/nonebot_plugin_br-0.0.7-py3-none-any.whl

---
+++ packages/nonebot_plugin_BR/nonebot_plugin_br-0.0.7-py3-none-any.whl/nonebot_plugin_BR/__init__.py
@@ -0,0 +1,28 @@
+from nonebot import require
+from nonebot.plugin import PluginMetadata, inherit_supported_adapters
+
+require("nonebot_plugin_uninfo")
+# require("nonebot_plugin_alconna")
+require("nonebot_plugin_waiter")
+require("nonebot_plugin_session")
+
+from . import __main__ as __main__  # noqa: E402
+from .config import ConfigModel  # noqa: E402
+
+__version__ = "0.0.7"
+__plugin_meta__ = PluginMetadata(
+    name="恶魔轮盘赌",
+    description="与机器人交互实现恶魔轮盘赌规则",
+    usage="br help呼出帮助",
+    type="application",
+    config=ConfigModel,
+    homepage="https://github.com/Agnes4m/nonebot_plugin_BR",
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_uninfo",
+        "nonebot_plugin_session",
+    ),
+    extra={
+        "version": __version__,
+        "author": "Agnes4m <Z735803792@163.com>",
+    },
+)
