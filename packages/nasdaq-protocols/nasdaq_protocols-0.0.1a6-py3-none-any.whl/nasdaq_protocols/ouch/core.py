ab7f03c07854d9fbf926969e06ba78c1332a20ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 05:05:44 +0000

    Add package nasdaq-protocols/0.0.1a6/nasdaq_protocols-0.0.1a6-py3-none-any.whl

---
+++ packages/nasdaq-protocols/nasdaq_protocols-0.0.1a6-py3-none-any.whl/nasdaq_protocols/ouch/core.py
@@ -0,0 +1,49 @@
+import attrs
+from nasdaq_protocols.common import Serializable, Byte, CommonMessage, logable
+
+
+__all__ = [
+    'OuchMessageId',
+    'Message'
+]
+APP_NAME = 'OUCH'
+
+
+@attrs.define(auto_attribs=True, hash=True)
+class OuchMessageId(Serializable):
+    indicator: int
+    direction: str = attrs.field(default="", eq=False, hash=False)
+
+    @classmethod
+    def from_bytes(cls, bytes_: bytes) -> tuple[int, 'OuchMessageId']:
+        return 1, OuchMessageId(Byte.from_bytes(bytes_)[1])
+
+    def to_bytes(self) -> tuple[int, bytes]:
+        return Byte.to_bytes(self.indicator)
+
+    def __str__(self):
+        return f'indicator={self.indicator}'
+
+
+@attrs.define
+@logable
+class Message(CommonMessage, msg_id_cls=OuchMessageId, app_name=APP_NAME):
+
+    IncomingMsgClasses = []
+    OutgoingMsgsClasses = []
+
+    def __init_subclass__(cls, *args, **kwargs):
+        cls.log.debug('ouch.core.Message subclassing %s, params = %s', cls.__name__, str(kwargs))
+
+        if 'app_name' not in kwargs:
+            kwargs['app_name'] = APP_NAME
+
+        kwargs['msg_id_cls'] = OuchMessageId
+
+        if all(k in kwargs for k in ['direction', 'indicator']):
+            kwargs['msg_id'] = OuchMessageId(kwargs['indicator'], kwargs['direction'])
+            if kwargs['direction'] == 'incoming':
+                Message.IncomingMsgClasses.append(cls)
+            elif kwargs['direction'] == 'outgoing':
+                Message.OutgoingMsgsClasses.append(cls)
+        super().__init_subclass__(**kwargs)
