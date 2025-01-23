b28902e12ebeef826b44a9227c2cd916e0c309e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:26 +0000

    Add package types-aiobotocore-osis/2.19.0/types_aiobotocore_osis-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-osis/types_aiobotocore_osis-2.19.0.tar.gz/types_aiobotocore_osis-2.19.0/types_aiobotocore_osis/__init__.pyi
@@ -0,0 +1,27 @@
+"""
+Main interface for osis service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_osis import (
+        Client,
+        OpenSearchIngestionClient,
+    )
+
+    session = get_session()
+    async with session.create_client("osis") as client:
+        client: OpenSearchIngestionClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import OpenSearchIngestionClient
+
+Client = OpenSearchIngestionClient
+
+__all__ = ("Client", "OpenSearchIngestionClient")
