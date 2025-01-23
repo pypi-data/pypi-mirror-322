de68bc9eec1ed7f01ef4024960553f0d38d5de98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:26 +0000

    Add package types-aiobotocore-mediapackage/2.19.0/types_aiobotocore_mediapackage-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-mediapackage/types_aiobotocore_mediapackage-2.19.0-py3-none-any.whl/types_aiobotocore_mediapackage/__init__.pyi
@@ -0,0 +1,41 @@
+"""
+Main interface for mediapackage service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_mediapackage import (
+        Client,
+        ListChannelsPaginator,
+        ListHarvestJobsPaginator,
+        ListOriginEndpointsPaginator,
+        MediaPackageClient,
+    )
+
+    session = get_session()
+    async with session.create_client("mediapackage") as client:
+        client: MediaPackageClient
+        ...
+
+
+    list_channels_paginator: ListChannelsPaginator = client.get_paginator("list_channels")
+    list_harvest_jobs_paginator: ListHarvestJobsPaginator = client.get_paginator("list_harvest_jobs")
+    list_origin_endpoints_paginator: ListOriginEndpointsPaginator = client.get_paginator("list_origin_endpoints")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import MediaPackageClient
+from .paginator import ListChannelsPaginator, ListHarvestJobsPaginator, ListOriginEndpointsPaginator
+
+Client = MediaPackageClient
+
+__all__ = (
+    "Client",
+    "ListChannelsPaginator",
+    "ListHarvestJobsPaginator",
+    "ListOriginEndpointsPaginator",
+    "MediaPackageClient",
+)
