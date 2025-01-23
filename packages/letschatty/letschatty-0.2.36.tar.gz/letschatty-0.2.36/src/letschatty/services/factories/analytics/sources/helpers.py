e0a0d01f0f4d73dcca2c6c25b4327faf6a14b0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:04:50 +0000

    Add package letschatty/0.2.36/letschatty-0.2.36.tar.gz

---
+++ packages/letschatty/letschatty-0.2.36.tar.gz/letschatty-0.2.36/src/letschatty/services/factories/analytics/sources/helpers.py
@@ -0,0 +1,15 @@
+from typing import Type
+from .....models.utils.types.source_types import SourceType
+from .....models.analytics.sources import OtherSource, PureAd, WhatsAppDefaultSource, TopicDefaultSource, UTMSource, Source, PureAdUtmSource
+
+class SourceFactoryHelpers:
+    @staticmethod
+    def source_type_to_class(source_type: SourceType) -> Type[Source]:
+        return {
+            SourceType.OTHER_SOURCE: OtherSource,
+            SourceType.PURE_AD: PureAd,
+            SourceType.WHATSAPP_DEFAULT_SOURCE: WhatsAppDefaultSource,
+            SourceType.TOPIC_DEFAULT_SOURCE: TopicDefaultSource,
+            SourceType.UTM_SOURCE: UTMSource,
+            SourceType.PURE_AD_UTM_SOURCE: PureAdUtmSource
+        }[source_type]
