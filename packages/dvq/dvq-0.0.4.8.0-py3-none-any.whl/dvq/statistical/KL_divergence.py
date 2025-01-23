8e33a21ef0003f64af1907d679633c4d1338a14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:04:59 +0000

    Add package dvq/0.0.4.8.0/dvq-0.0.4.8.0-py3-none-any.whl

---
+++ packages/dvq/dvq-0.0.4.8.0-py3-none-any.whl/dvq/statistical/KL_divergence.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+from typing import List, Dict
+from collections import Counter
+
+
+def calculate_probabilities(seq: str, chunk_size: int = 10) -> Dict[str, float]:
+    """
+    Calculate the probability distribution of chunks in a sequence.
+    
+    Parameters:
+    seq (str): The input sequence.
+    chunk_size (int): The size of each chunk.
+    
+    Returns:
+    Dict[str, float]: A dictionary with chunks as keys and their probabilities as values.
+    """
+    seq_len = len(seq)
+    chunks = [seq[i:i+chunk_size] for i in range(len(seq) - chunk_size + 1)]
+    chunk_counter = Counter(chunks)
+    probabilities = {chunk: count / seq_len for chunk, count in chunk_counter.items()}
+    return probabilities
+
+
+def kl_divergence(seq_P: str, seq_Q: str) -> float:
+    """
+    Calculate the KL divergence between two probability distributions.
+    
+    Parameters:
+    seq_P (str): first sequence.
+    seq_Q (str): second sequence.
+    
+    Returns:
+    float: The KL divergence.
+    """
+    P = calculate_probabilities(seq_P)
+    Q = calculate_probabilities(seq_Q)
+
+    kl_div = 0.0
+    for chunk, p_prob in P.items():
+        q_prob = Q.get(chunk)
+        if q_prob is None or q_prob == 0:
+            continue  # Skip the term if q_prob is zero or the chunk is not in Q
+        kl_div += p_prob * np.log2(p_prob / q_prob)
+    return kl_div
+
---
+++ packages/dvq/dvq-0.0.4.8.0-py3-none-any.whl/dvq/statistical/wens_method.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+WEIGHTS = {'0100': 1/6, '0101': 2/6, '1100' : 3/6, '0110':3/6, '1101': 4/6, '1110': 5/6,'0111':5/6, '1111': 6/6}
+LOWEST_LENGTH = 5000
+
+def _get_subsequences(sequence):
+    return {nuc: [i+1 for i, x in enumerate(sequence) if x == nuc] for nuc in 'ACTG'}
+
+def _calculate_coordinates_fixed(subsequence, L=LOWEST_LENGTH):
+    return [((2 * np.pi / (L - 1)) * (K-1), np.sqrt((2 * np.pi / (L - 1)) * (K-1))) for K in subsequence]
+
+def _calculate_weighting_full(sequence, WEIGHTS, L=LOWEST_LENGTH, E=0.0375):
+    weightings = [0]
+    for i in range(1, len(sequence) - 1):
+        if i < len(sequence) - 2:
+            subsequence = sequence[i-1:i+3]
+            comparison_pattern = f"{'1' if subsequence[0] == subsequence[1] else '0'}1{'1' if subsequence[2] == subsequence[1] else '0'}{'1' if subsequence[3] == subsequence[1] else '0'}"
+            weight = WEIGHTS.get(comparison_pattern, 0)
+            weight = weight * E if i > L else weight
+        else:
+            weight = 0
+        weightings.append(weight)
+    weightings.append(0)
+    return weightings
+
+def _centre_of_mass(polar_coordinates, weightings):
+    x, y = _calculate_standard_coordinates(polar_coordinates)
+    return sum(weightings[i] * ((x[i] - (x[i]*weightings[i]))**2 + (y[i] - y[i]*weightings[i])**2) for i in range(len(x)))
+
+def _normalised_moment_of_inertia(polar_coordinates, weightings):
+    moment = _centre_of_mass(polar_coordinates, weightings)
+    return np.sqrt(moment / sum(weightings))
+
+def _calculate_standard_coordinates(polar_coordinates):
+    return [rho * np.cos(theta) for theta, rho in polar_coordinates], [rho * np.sin(theta) for theta, rho in polar_coordinates]
+
+
+def _moments_of_inertia(polar_coordinates, weightings):
+    return [_normalised_moment_of_inertia(indices, weightings) for subsequence, indices in polar_coordinates.items()]
+
+def moment_of_inertia(sequence, WEIGHTS=WEIGHTS, L=5000, E=0.0375):
+    subsequences = _get_subsequences(sequence)
+    polar_coordinates = {subsequence: _calculate_coordinates_fixed(indices, len(sequence)) for subsequence, indices in subsequences.items()}
+    weightings = _calculate_weighting_full(sequence, WEIGHTS, L=L, E=E)
+    return _moments_of_inertia(polar_coordinates, weightings)
+
+
+def similarity_wen(sequence1, sequence2, WEIGHTS=WEIGHTS, L=5000, E=0.0375):
+    inertia1 = moment_of_inertia(sequence1, WEIGHTS, L=L, E=E)
+    inertia2 = moment_of_inertia(sequence2, WEIGHTS, L=L, E=E)
+    similarity = np.sqrt(sum((x - y)**2 for x, y in zip(inertia1, inertia2)))
+    return similarity
