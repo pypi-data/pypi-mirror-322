5c5428825c05ef77bbe09ce4a9a8c22c447cebd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:20 +0000

    Add package types-aiobotocore-iot-data/2.19.0/types_aiobotocore_iot_data-2.19.0.tar.gz

---
+++ packages/types-aiobotocore-iot-data/types_aiobotocore_iot_data-2.19.0.tar.gz/types_aiobotocore_iot_data-2.19.0/types_aiobotocore_iot_data/__init__.pyi
@@ -0,0 +1,31 @@
+"""
+Main interface for iot-data service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_iot_data import (
+        Client,
+        IoTDataPlaneClient,
+        ListRetainedMessagesPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("iot-data") as client:
+        client: IoTDataPlaneClient
+        ...
+
+
+    list_retained_messages_paginator: ListRetainedMessagesPaginator = client.get_paginator("list_retained_messages")
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import IoTDataPlaneClient
+from .paginator import ListRetainedMessagesPaginator
+
+Client = IoTDataPlaneClient
+
+__all__ = ("Client", "IoTDataPlaneClient", "ListRetainedMessagesPaginator")
