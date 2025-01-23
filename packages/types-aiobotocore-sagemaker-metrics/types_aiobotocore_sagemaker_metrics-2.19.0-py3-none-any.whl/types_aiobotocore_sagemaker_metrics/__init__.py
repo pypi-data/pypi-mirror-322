c19027f50020aaa571617429c922676d36bc066f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 17:05:29 +0000

    Add package types-aiobotocore-sagemaker-metrics/2.19.0/types_aiobotocore_sagemaker_metrics-2.19.0-py3-none-any.whl

---
+++ packages/types-aiobotocore-sagemaker-metrics/types_aiobotocore_sagemaker_metrics-2.19.0-py3-none-any.whl/types_aiobotocore_sagemaker_metrics/__init__.py
@@ -0,0 +1,28 @@
+"""
+Main interface for sagemaker-metrics service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_sagemaker_metrics import (
+        Client,
+        SageMakerMetricsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("sagemaker-metrics") as client:
+        client: SageMakerMetricsClient
+        ...
+
+    ```
+
+Copyright 2025 Vlad Emelianov
+"""
+
+from .client import SageMakerMetricsClient
+
+Client = SageMakerMetricsClient
+
+
+__all__ = ("Client", "SageMakerMetricsClient")
