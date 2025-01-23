0253da972a9fffb70ca952e34f4805bc66f71e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 05:05:33 +0000

    Add package SUN-DIC/0.0.6/SUN_DIC-0.0.6-py3-none-any.whl

---
+++ packages/SUN-DIC/SUN_DIC-0.0.6-py3-none-any.whl/sundic/version.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.6"
+# 0.0.1 - Initial release
+# 0.0.2 - Changed underlying data structure and added parallel processing
+# 0.0.3 - Added support for multiple input files
+# 0.0.4 - Created an settings object and am now serializing the settings object
+#         and the results object from the analysis to a msgpack binary file that
+#         is used for post-processing
+# 0.0.5 - Minor change to gradient calculations
+# 0.0.6 - Publishing to PyPi
