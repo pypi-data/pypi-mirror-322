e61c155f9b47a925cea91bdf71399a328444faf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 14:05:28 +0000

    Add package eegprep/0.0.4/eegprep-0.0.4.tar.gz

---
+++ packages/eegprep/eegprep-0.0.4.tar.gz/eegprep-0.0.4/README.md
@@ -0,0 +1,36 @@
+## Build docker
+
+docker run --rm -it -v $(pwd):/usr/src/project dtyoung/eegprep /bin/bash
+docker run -u root --rm -it -v $(pwd):/usr/src/project dtyoung/eegprep /bin/bash
+
+# remove
+
+docker rmi dtyoung/eegprep
+
+Mounted folder in /usr/src/project
+
+## How to convert a function from MATLAB to Python
+
+1. Get a MATLAB file to load and process an EEG file
+
+2. Convert the code using GPT4 or when short Copilot and test in a Notebook. Once the code runs without erroring, move to 3.
+
+3. Use the Jupyter code to create a Python file (not notebook) to load the same file as MATLAB and process it as well (in plain Python, not in a subfunction)
+
+4. Start the debugger in both and compare. Note that it is better to use the debugger on Python file than Jupyter Notebook (could not get it to stop)
+
+5. Once the result is the same, package the Python code in a function with the same name as MATLAB
+
+6. Write the function to compare (see example) and the helper Python function to load the file (note that there could be a general Python helper function)
+
+## Install package
+Packaging was done following the tutorial: https://packaging.python.org/en/latest/tutorials/packaging-projects/ with setuptools
+
+To install the package, run:
+```
+pip install eegprep
+```
+
+## Test
+
+use tests/main_compare.m
---
+++ packages/eegprep/eegprep-0.0.4.tar.gz/eegprep-0.0.4/src/eegprep/pop_reref.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+def pop_reref(EEG, ref):
+    
+    # check if ref is not empty and not none
+    if ref is not None and ref != []:
+        raise ValueError('Feature not implemented: The ref parameter must be empty or None')
+            
+    # Check if the number of channels in EEG['icachansind'] is the same as the number of channels in EEG['nbchan']
+    if len(EEG['icachansind']) == EEG['nbchan']:
+        # Subtract mean from EEG data using broadcasting in NumPy
+        EEG['data'] = EEG['data'] - np.mean(EEG['data'], axis=0)
+        
+        #print(np.array2string(EEG['icaweights'][:10, :10], precision=3, suppress_small=True))
+
+        # Subtract mean from EEG icawinv using broadcasting in NumPy
+        EEG['icawinv'] = EEG['icawinv'] - np.mean(EEG['icawinv'], axis=0)
+        
+        # show the first 10 rows and columns of EEG['icawinv']
+        # print it pretty line by line with 4 digit after the decimal point
+        
+        # Compute the pseudoinverse of EEG['icawinv']
+        EEG['icaweights'] = np.linalg.pinv(EEG['icawinv'])
+        #print(' ')
+        #print(np.array2string(EEG['icaweights'][:10, :10], precision=3, suppress_small=True))
+
+        EEG['icasphere'] = np.eye(EEG['nbchan'])
+
+        # Compute the ICA activations        
+        # data = EEG['data'].reshape(EEG['data'].shape[0], -1)
+        # EEG['icaact'] = np.dot(EEG['icaweights'], data)
+        # EEG['icaact'] = EEG['icaact'].reshape(EEG['icaweights'].shape[0], EEG['pnts'], EEG['trials'])
+                
+        # Set EEG['ref'] to 'average'
+        EEG['ref'] = 'average'
+        
+        # Update the reference for each channel in EEG['chanlocs']
+        for iChan in range(len(EEG['chanlocs'])):
+            EEG['chanlocs'][iChan]['ref'] = 'average'
+        
+        return EEG
+            
+    else:
+        raise ValueError('Feature not implemented: The number of channels in EEG[''icachansind''] must be the same as the number of channels in EEG[''nbchan'']')
