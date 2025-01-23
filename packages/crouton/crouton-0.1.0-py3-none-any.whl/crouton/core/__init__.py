900c9b6e702b455a118d7952b5383d0521c69f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-23 18:06:30 +0000

    Add package crouton/0.1.0/crouton-0.1.0-py3-none-any.whl

---
+++ packages/crouton/crouton-0.1.0-py3-none-any.whl/crouton/core/__init__.py
@@ -0,0 +1,18 @@
+from . import _utils
+from ._base import NOT_FOUND, CRUDGenerator
+from .databases import DatabasesCRUDRouter
+from .mem import MemoryCRUDRouter
+from .ormar import OrmarCRUDRouter
+from .sqlalchemy import SQLAlchemyCRUDRouter
+from .tortoise import TortoiseCRUDRouter
+
+__all__ = [
+    "_utils",
+    "CRUDGenerator",
+    "NOT_FOUND",
+    "MemoryCRUDRouter",
+    "SQLAlchemyCRUDRouter",
+    "DatabasesCRUDRouter",
+    "TortoiseCRUDRouter",
+    "OrmarCRUDRouter",
+]
