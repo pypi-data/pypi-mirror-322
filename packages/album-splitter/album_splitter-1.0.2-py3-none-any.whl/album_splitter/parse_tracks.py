9b8342c72266aed766180c032ab9253260c54930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:06:45 +0000

    Add package album-splitter/1.0.2/album_splitter-1.0.2-py3-none-any.whl

---
+++ packages/album-splitter/album_splitter-1.0.2-py3-none-any.whl/album_splitter/parse_tracks.py
@@ -0,0 +1,58 @@
+import re
+from collections import namedtuple
+
+import typing
+
+Track = namedtuple("Track", ["title", "start_timestamp"])
+
+
+def parse_time_string(time: str):
+    parts = time.strip().split(":")
+    seconds = None
+    if len(parts) == 3:  # h:m:s
+        seconds = int(parts[0]) * 3600 + int(parts[1]) * 60 + int(parts[2])
+    elif len(parts) == 2:  # m:s
+        seconds = int(parts[0]) * 60 + int(parts[1])
+    else:
+        raise Exception(f"Unknown time format: {time}")
+    return seconds
+
+
+def parse_tracks(tracks_content: str, duration: bool = False) -> typing.List[Track]:
+    lines = tracks_content.splitlines()
+    tracks: typing.List[Track] = []
+    current_time = 0
+    for line in lines:
+        line = line.strip()
+        if line.startswith("#") or len(line) == 0:
+            continue
+        track_time, title = parse_line(line)
+        track_time_seconds = parse_time_string(track_time)
+        if not duration:
+            current_time = track_time_seconds
+        tracks.append(Track(title=title, start_timestamp=current_time))
+        if duration:
+            current_time += track_time_seconds
+    return tracks
+
+
+def parse_line(line: str) -> typing.Tuple[str, str]:
+    line = line.strip()
+    # match [HHH:]MM:SS
+    timestamp_regex = r"(?:\d+:)?(?:0[0-9]|[1-5][0-9]):(?:0[0-9]|[1-5][0-9])"
+    timestamp_regex_beginning = rf"^{timestamp_regex}\b"
+    timestamp_regex_end = rf"\b{timestamp_regex}$"
+    match_beginning = re.search(timestamp_regex_beginning, line)
+    match_end = re.search(timestamp_regex_end, line)
+    if match_beginning:
+        timestamp = match_beginning.group(0)
+        title = re.sub(timestamp_regex_beginning, "", line)
+    elif match_end:
+        timestamp = match_end.group(0)
+        title = re.sub(timestamp_regex_end, "", line)
+    else:
+        raise ValueError(
+            f"Can't find a valid timestamp (HH:MM:SS or MM:SS) at the beginning or at the end of line: {line}"
+        )
+    title = title.strip(" -|")
+    return timestamp, title
---
+++ packages/album-splitter/album_splitter-1.0.2-py3-none-any.whl/album_splitter/utils/secure_filename.py
@@ -0,0 +1,55 @@
+import os
+import re
+import unicodedata
+
+
+# Taken and adapted from https://github.com/pallets/werkzeug/blob/e6c908f36c68eff4ecc10c405d8c2512f27e65a0/src/werkzeug/utils.py#L432
+def secure_filename(filename: str) -> str:
+    r"""Pass it a filename and it will return a secure version of it. This
+    filename can then safely be stored on a regular file system and passed
+    to :func:`os.path.join`.  The filename returned is an ASCII only string
+    for maximum portability.
+    >>> secure_filename("My cool movie.mov")
+    'My_cool_movie.mov'
+    >>> secure_filename("../../../etc/passwd")
+    'etc_passwd'
+    >>> secure_filename('i contain cool \xfcml\xe4uts.txt')
+    'i_contain_cool_umlauts.txt'
+
+    The function might return an empty filename.  It's your responsibility
+    to ensure that the filename is unique and that you abort or
+    generate a random filename if the function returned an empty one.
+    """
+    filename = unicodedata.normalize("NFKD", filename)
+    filename = filename.encode("ascii", "ignore").decode("ascii")
+    for sep in os.path.sep, os.path.altsep:
+        if sep:
+            filename = filename.replace(sep, " ")
+    _filename_ascii_strip_re = re.compile(r"[^A-Za-z0-9_.-]")
+    filename = str(_filename_ascii_strip_re.sub("", "_".join(filename.split()))).strip(
+        "._"
+    )
+    # on nt a couple of special files are present in each folder.  We
+    # have to ensure that the target file is not such a filename.  In
+    # this case we prepend an underline
+    _windows_device_files = (
+        "CON",
+        "AUX",
+        "COM1",
+        "COM2",
+        "COM3",
+        "COM4",
+        "LPT1",
+        "LPT2",
+        "LPT3",
+        "PRN",
+        "NUL",
+    )
+    if (
+        os.name == "nt"
+        and filename
+        and filename.split(".")[0].upper() in _windows_device_files
+    ):
+        filename = f"_{filename}"
+
+    return filename
