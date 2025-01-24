a60144f7deed2a2a5b6575e0b906d17bb1eb36e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:06:53 +0000

    Add package ontologist/0.1.1/ontologist-0.1.1.tar.gz

---
+++ packages/ontologist/ontologist-0.1.1.tar.gz/ontologist-0.1.1/ontologist/__init__.py
@@ -0,0 +1,7 @@
+from .entrypoints import validate
+
+# version compliant with https://www.python.org/dev/peps/pep-0440/
+__version__ = "0.1.1"
+# Don't forget to change the version number in pyproject.toml, Dockerfile, and CITATION.cff along with this one
+
+__all__ = ["validate"]
