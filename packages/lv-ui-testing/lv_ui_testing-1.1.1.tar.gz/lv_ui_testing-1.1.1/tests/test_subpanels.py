96e75fb768e1ef7b899b8d211adcf7bac9d80247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:06:17 +0000

    Add package lv-ui-testing/1.1.1/lv_ui_testing-1.1.1.tar.gz

---
+++ packages/lv-ui-testing/lv_ui_testing-1.1.1.tar.gz/lv_ui_testing-1.1.1/tests/test_subpanels.py
@@ -0,0 +1,56 @@
+# Start the VI "example 3 - subpnael" first then run that script
+import importlib.util
+import sys
+import pytest
+
+spec = importlib.util.spec_from_file_location("lv_ui_testing", "../src/lv_ui_testing/ui_testing.py")
+ui_testing = importlib.util.module_from_spec(spec)
+sys.modules["lv_ui_testing"] = ui_testing
+spec.loader.exec_module(ui_testing)
+
+
+
+def test_front_most_vi():
+    # Ask the tester daemon what is the front most VI
+    front_most_vi = ui_testing.FMV_get_vi_name()
+    assert front_most_vi ==  "example 3 - subpanel.vi"
+
+
+def test_subpanel1():
+    # Get the subpanel to load the subpanel example 1
+    ui_testing.FMV_click_on_button("sub1")
+    sub1 = ui_testing.SP_get_vi_name("mySub")
+    assert sub1 == "example 3 - subpanel1.vi"
+
+@pytest.mark.parametrize("number, expected_bool", [ (x, x > 5) for x in range(1, 11)])
+
+def test_set_value(number,expected_bool):
+    # Set value to 0
+    ui_testing.SP_set_value_DBL("mySub", "myNumber", number)
+    data_number = ui_testing.SP_get_value_DBL("mySub", "myNumber")
+    data_bool = ui_testing.SP_get_value_bool("mySub", "greater")
+
+    assert data_bool == expected_bool
+    assert data_number == number
+
+def test_subpanel2():
+    # Get the subpanel to load the subpanel example 1
+    ui_testing.FMV_click_on_button("sub2")
+    sub1 = ui_testing.SP_get_vi_name("mySub")
+    assert sub1 == "example 3 - subpanel2.vi"
+
+
+def test_random_plot():
+    # Generate a random plot and query the data insisde it
+    ui_testing.SP_click_on_button("mySub","Random")
+    data = ui_testing.SP_get_value("mySub", "Plot")
+    print(f"data from plot : {data}")
+
+    # Repeate the previous step
+    ui_testing.SP_click_on_button("mySub","Random")
+    data2 = ui_testing.SP_get_value("mySub", "Plot")
+    print(f"data from plot : {data2}")
+
+    # Compare the results, they should be differnet
+    assert data != data2
+
---
+++ packages/lv-ui-testing/lv_ui_testing-1.1.1.tar.gz/lv_ui_testing-1.1.1/tests/test_values.py
@@ -0,0 +1,25 @@
+# Start the VI "example 2 - value" first then run that script
+
+import pytest
+import importlib.util
+import sys
+spec = importlib.util.spec_from_file_location("lv_ui_testing", "../src/lv_ui_testing/ui_testing.py")
+ui_testing = importlib.util.module_from_spec(spec)
+sys.modules["lv_ui_testing"] = ui_testing
+spec.loader.exec_module(ui_testing)
+
+def test_front_most_vi():
+    # Ask the tester daemon what is the front most VI
+    front_most_vi = ui_testing.FMV_get_vi_name()
+    assert front_most_vi == "example 2 - value.vi"
+
+@pytest.mark.parametrize("number, expected_bool", [ (x, x > 5) for x in range(1, 11)])
+
+def test_set_value(number,expected_bool):
+    # Set value to 0
+    ui_testing.FMV_set_value_DBL("myNumber", number)
+    data_number = ui_testing.FMV_get_value_DBL("myNumber")
+    data_bool = ui_testing.FMV_get_value_bool("greater")
+
+    assert data_bool == expected_bool
+    assert data_number == number
