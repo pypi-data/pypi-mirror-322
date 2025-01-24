c8c1b494b43a890b6bea65f205965cae58cb185c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:04 +0000

    Add package arpakitlib/1.7.132/arpakitlib-1.7.132.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.7.132.tar.gz/arpakitlib-1.7.132/arpakitlib/_arpakit_project_template/src/operation_execution/start_scheduled_operation_creator_worker.py
@@ -0,0 +1,19 @@
+from arpakitlib.ar_operation_execution_util import ScheduledOperationCreatorWorker
+from src.core.settings import get_cached_settings
+from src.core.util import setup_logging
+from src.db.util import get_cached_sqlalchemy_db
+from src.operation_execution.scheduled_operations import SCHEDULED_OPERATIONS
+
+
+def start_scheduled_operation_creator_worker():
+    setup_logging()
+    get_cached_settings().raise_if_mode_type_prod()
+    worker = ScheduledOperationCreatorWorker(
+        sqlalchemy_db=get_cached_sqlalchemy_db(),
+        scheduled_operations=SCHEDULED_OPERATIONS
+    )
+    worker.sync_safe_run()
+
+
+if __name__ == '__main__':
+    start_scheduled_operation_creator_worker()
