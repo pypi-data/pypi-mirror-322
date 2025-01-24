ad6dc22bb84d293670d7d4294df4cbb5a18e842d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:28 +0000

    Add package contrast-agent/10.0.0/contrast_agent-10.0.0-cp312-cp312-musllinux_1_2_aarch64.whl

---
+++ packages/contrast-agent/contrast_agent-10.0.0-cp312-cp312-musllinux_1_2_aarch64.whl/contrast/agent/middlewares/route_coverage/bottle_routes.py
@@ -0,0 +1,27 @@
+# Copyright © 2025 Contrast Security, Inc.
+# See https://www.contrastsecurity.com/enduser-terms-0317a for more details.
+from typing import Set
+from contrast.agent.middlewares.route_coverage.common import (
+    build_signature,
+    DEFAULT_ROUTE_METHODS,
+    get_normalized_uri,
+)
+from contrast_fireball import DiscoveredRoute
+
+DEFAULT_ROUTE_METHODS = DEFAULT_ROUTE_METHODS + ("PUT", "PATCH", "DELETE")
+
+
+def create_bottle_routes(app) -> Set[DiscoveredRoute]:
+    """
+    Returns all the routes registered to a Bottle app.
+    """
+    return {
+        DiscoveredRoute(
+            verb=method_type,
+            url=get_normalized_uri(str(route.rule)),
+            signature=build_signature(route.rule, route.callback),
+            framework="Bottle",
+        )
+        for route in app.routes
+        for method_type in DEFAULT_ROUTE_METHODS
+    }
