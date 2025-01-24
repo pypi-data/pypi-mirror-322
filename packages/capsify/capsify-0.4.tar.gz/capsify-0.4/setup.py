9b4d7fa825a5140f412654d9c660ccdc487c5edc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:05:47 +0000

    Add package capsify/0.4/capsify-0.4.tar.gz

---
+++ packages/capsify/capsify-0.4.tar.gz/capsify-0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='capsify',
+    version='0.4',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.6',
+    author='Alpha',
+    author_email='imr@outlook.in',
+    description='A Python package for Capsify Font.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://t.me/North_Yankton',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
