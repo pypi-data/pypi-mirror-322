bcc66aa287a7286d2cfe8e91f2bb1080f4efe077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:49 +0000

    Add package gpuhunt/0.0.19/gpuhunt-0.0.19-py3-none-any.whl

---
+++ packages/gpuhunt/gpuhunt-0.0.19-py3-none-any.whl/gpuhunt/_internal/utils.py
@@ -0,0 +1,25 @@
+from typing import Callable, Optional, Union
+
+
+def empty_as_none(value: Optional[str], loader: Optional[Callable] = None):
+    if value is None or value == "":
+        return None
+    if loader is not None:
+        return loader(value)
+    return value
+
+
+def parse_compute_capability(
+    value: Optional[Union[str, tuple[int, int]]],
+) -> Optional[tuple[int, int]]:
+    if isinstance(value, str):
+        major, minor = value.split(".")
+        return int(major), int(minor)
+    return value
+
+
+def to_camel_case(snake_case: str) -> str:
+    words = snake_case.split("_")
+    words = list(filter(None, words))
+    words[1:] = [word[:1].upper() + word[1:] for word in words[1:]]
+    return "".join(words)
