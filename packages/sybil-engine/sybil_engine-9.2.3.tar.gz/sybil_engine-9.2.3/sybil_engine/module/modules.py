594d74fb25cbb5b723c3bc3d3228782aba7888a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:42 +0000

    Add package sybil-engine/9.2.3/sybil_engine-9.2.3.tar.gz

---
+++ packages/sybil-engine/sybil_engine-9.2.3.tar.gz/sybil_engine-9.2.3/sybil_engine/module/modules.py
@@ -0,0 +1,40 @@
+from sybil_engine.config.app_config import get_network
+from sybil_engine.data.networks import get_networks
+
+
+class Modules:
+    def __init__(self, module_map, swap_facade):
+        self.module_map = module_map
+        self.swap_facade = swap_facade
+
+    def get_module_config_by_name(self, module_name, module_map):
+        for key, (module_class, config) in self.get_module_map().items():
+            if module_class and (module_class.module_name == module_name):
+                if config is None:
+                    return {}
+                elif isinstance(config, str):
+                    if config in module_map:
+                        return module_map[config]
+                    else:
+                        return "{}"
+                else:
+                    return config
+        return 0
+
+    def get_module_class_by_name(self, module_name):
+        for key, (module_class, config) in self.get_module_map().items():
+            if module_class and (module_class.module_name == module_name):
+                return module_class
+        return 0
+
+    def get_modules(self):
+        return [module[0] for module in self.get_module_map().values()]
+
+    def get_module_map(self):
+        return {key: (module_class, module_class.module_config) for key, module_class in self.module_map.items()}
+
+    def get_swap_apps(self):
+        return self.swap_facade.get_swap_apps()
+
+    def get_supported_chains(self):
+        return list(get_networks(get_network()).copy().keys())
