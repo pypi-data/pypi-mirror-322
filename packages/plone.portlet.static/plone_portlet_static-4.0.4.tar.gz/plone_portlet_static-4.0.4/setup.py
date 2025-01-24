2b8631a19a7031edc5dd64606e2b79dd013947f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:07:01 +0000

    Add package plone.portlet.static/4.0.4/plone_portlet_static-4.0.4.tar.gz

---
+++ packages/plone.portlet.static/plone_portlet_static-4.0.4.tar.gz/plone_portlet_static-4.0.4/setup.py
@@ -0,0 +1,67 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+version = "4.0.4"
+
+long_description = read("README.rst") + "\n" + read("CHANGES.rst") + "\n"
+
+
+setup(
+    name="plone.portlet.static",
+    version=version,
+    description="An editable static HTML portlet for Plone.",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plone portlet static",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.portlet.static",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone", "plone.portlet"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    extras_require=dict(
+        test=[
+            "plone.app.testing",
+        ]
+    ),
+    install_requires=[
+        "setuptools",
+        "plone.base",
+        "plone.portlets",
+        "plone.app.portlets",
+        "plone.app.textfield",
+        "plone.app.z3cform",
+        "plone.autoform",
+        "plone.i18n",
+        "Products.GenericSetup",
+        "Zope",
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
