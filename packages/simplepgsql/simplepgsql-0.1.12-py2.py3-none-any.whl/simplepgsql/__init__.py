a509e5774ea0a79bf0c8e2f67c59120b93a17c54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:05:50 +0000

    Add package simplepgsql/0.1.12/simplepgsql-0.1.12-py2.py3-none-any.whl

---
+++ packages/simplepgsql/simplepgsql-0.1.12-py2.py3-none-any.whl/simplepgsql/__init__.py
@@ -0,0 +1,6 @@
+from .build_query import Query
+from .simplepgsql import SimplePgSQL
+from .labels import Params, StandardKeywords
+from .pgsql_wizard import PgSQLWizard
+
+__all__ = ["SimplePgSQL", "Params", "Query", "StandardKeywords", "PgSQLWizard"]
