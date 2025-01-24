e0e1039265d70fdedf90c2ec662ceb6558a8b17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:07:13 +0000

    Add package pygro/1.0.0/pygro-1.0.0.tar.gz

---
+++ packages/pygro/pygro-1.0.0.tar.gz/pygro-1.0.0/pygro/__init__.py
@@ -0,0 +1,11 @@
+from pygro.metric_engine import Metric
+from pygro.geodesic_engine import GeodesicEngine
+from pygro.geodesic import Geodesic
+
+from pygro.observer import Observer
+
+import logging
+
+PYGRO_LOGGING = logging.INFO
+
+logging.basicConfig(level=PYGRO_LOGGING, format="(PyGRO) {levelname}: {message}", style="{")
