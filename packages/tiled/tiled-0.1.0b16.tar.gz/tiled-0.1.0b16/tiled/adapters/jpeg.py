801c07f6592430cd08cb03044b1eb4058c6553b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:06:31 +0000

    Add package tiled/0.1.0b16/tiled-0.1.0b16.tar.gz

---
+++ packages/tiled/tiled-0.1.0b16.tar.gz/tiled-0.1.0b16/tiled/adapters/jpeg.py
@@ -0,0 +1,133 @@
+import builtins
+from typing import Any, List, Optional, Tuple, Union
+
+import numpy as np
+from numpy._typing import NDArray
+from PIL import Image
+
+from ..structures.array import ArrayStructure, BuiltinDtype
+from ..structures.core import Spec, StructureFamily
+from ..type_aliases import JSON, NDSlice
+from ..utils import path_from_uri
+from .protocols import AccessPolicy
+from .resource_cache import with_resource_cache
+from .sequence import FileSequenceAdapter
+
+
+class JPEGAdapter:
+    """
+    Read a JPEG file.
+
+    Examples
+    --------
+
+    >>> JPEGAdapter("path/to/file.jpeg")
+    """
+
+    structure_family = StructureFamily.array
+
+    def __init__(
+        self,
+        data_uri: str,
+        *,
+        structure: Optional[ArrayStructure] = None,
+        metadata: Optional[JSON] = None,
+        specs: Optional[List[Spec]] = None,
+        access_policy: Optional[AccessPolicy] = None,
+    ) -> None:
+        """
+
+        Parameters
+        ----------
+        data_uri :
+        structure :
+        metadata :
+        specs :
+        access_policy :
+        """
+        if not isinstance(data_uri, str):
+            raise Exception
+        filepath = path_from_uri(data_uri)
+        cache_key = (Image.open, filepath)
+        self._file = with_resource_cache(cache_key, Image.open, filepath)
+        self.specs = specs or []
+        self._provided_metadata = metadata or {}
+        self.access_policy = access_policy
+        if structure is None:
+            arr = np.asarray(self._file)
+            structure = ArrayStructure(
+                shape=arr.shape,
+                chunks=tuple((dim,) for dim in arr.shape),
+                data_type=BuiltinDtype.from_numpy_dtype(arr.dtype),
+            )
+        self._structure = structure
+
+    def metadata(self) -> JSON:
+        """
+
+        Returns
+        -------
+
+        """
+        return self._provided_metadata.copy()
+
+    def read(self, slice: Optional[NDSlice] = None) -> NDArray[Any]:
+        """
+
+        Parameters
+        ----------
+        slice :
+
+        Returns
+        -------
+
+        """
+        arr = np.asarray(self._file)
+        if slice is not None:
+            arr = arr[slice]
+        return arr
+
+    def read_block(
+        self, block: Tuple[int, ...], slice: Optional[builtins.slice] = None
+    ) -> NDArray[Any]:
+        """
+
+        Parameters
+        ----------
+        block :
+        slice :
+
+        Returns
+        -------
+
+        """
+        if sum(block) != 0:
+            raise IndexError(block)
+
+        arr = np.asarray(self._file)
+        if slice is not None:
+            arr = arr[slice]
+        return arr
+
+    def structure(self) -> ArrayStructure:
+        """
+
+        Returns
+        -------
+
+        """
+        return self._structure
+
+
+class JPEGSequenceAdapter(FileSequenceAdapter):
+    def _load_from_files(
+        self, slice: Union[builtins.slice, int] = slice(None)
+    ) -> NDArray[Any]:
+        from PIL import Image
+
+        if isinstance(slice, int):
+            return np.asarray(Image.open(self.filepaths[slice]))[None, ...]
+        else:
+            return np.asarray(
+                [np.asarray(Image.open(file)) for file in self.filepaths[slice]]
+            )
---
+++ packages/tiled/tiled-0.1.0b16.tar.gz/tiled-0.1.0b16/tiled/adapters/parquet.py
@@ -0,0 +1,166 @@
+from pathlib import Path
+from typing import Any, List, Optional, Union
+
+import dask.dataframe
+import pandas
+
+from ..server.schemas import Asset
+from ..structures.core import Spec, StructureFamily
+from ..structures.table import TableStructure
+from ..type_aliases import JSON
+from ..utils import path_from_uri
+from .dataframe import DataFrameAdapter
+from .protocols import AccessPolicy
+
+
+class ParquetDatasetAdapter:
+    """ """
+
+    structure_family = StructureFamily.table
+
+    def __init__(
+        self,
+        data_uris: List[str],
+        structure: TableStructure,
+        metadata: Optional[JSON] = None,
+        specs: Optional[List[Spec]] = None,
+        access_policy: Optional[AccessPolicy] = None,
+    ) -> None:
+        """
+
+        Parameters
+        ----------
+        data_uris :
+        structure :
+        metadata :
+        specs :
+        access_policy :
+        """
+        # TODO Store data_uris instead and generalize to non-file schemes.
+        self._partition_paths = [path_from_uri(uri) for uri in data_uris]
+        self._metadata = metadata or {}
+        self._structure = structure
+        self.specs = list(specs or [])
+        self.access_policy = access_policy
+
+    def metadata(self) -> JSON:
+        """
+
+        Returns
+        -------
+
+        """
+        return self._metadata
+
+    @property
+    def dataframe_adapter(self) -> DataFrameAdapter:
+        """
+
+        Returns
+        -------
+
+        """
+        partitions = []
+        for path in self._partition_paths:
+            if not Path(path).exists():
+                partition = None
+            else:
+                partition = dask.dataframe.read_parquet(path)
+            partitions.append(partition)
+        return DataFrameAdapter(partitions, self._structure)
+
+    @classmethod
+    def init_storage(cls, data_uri: str, structure: TableStructure) -> List[Asset]:
+        """
+
+        Parameters
+        ----------
+        data_uri :
+        structure :
+
+        Returns
+        -------
+
+        """
+        directory = path_from_uri(data_uri)
+        directory.mkdir(parents=True, exist_ok=True)
+        assets = [
+            Asset(
+                data_uri=f"{data_uri}/partition-{i}.parquet",
+                is_directory=False,
+                parameter="data_uris",
+                num=i,
+            )
+            for i in range(structure.npartitions)
+        ]
+        return assets
+
+    def write_partition(
+        self, data: Union[dask.dataframe.DataFrame, pandas.DataFrame], partition: int
+    ) -> None:
+        """
+
+        Parameters
+        ----------
+        data :
+        partition :
+
+        Returns
+        -------
+
+        """
+        uri = self._partition_paths[partition]
+        data.to_parquet(uri)
+
+    def write(self, data: Union[dask.dataframe.DataFrame, pandas.DataFrame]) -> None:
+        """
+
+        Parameters
+        ----------
+        data :
+
+        Returns
+        -------
+
+        """
+        if self.structure().npartitions != 1:
+            raise NotImplementedError
+        uri = self._partition_paths[0]
+        data.to_parquet(uri)
+
+    def read(self, *args: Any, **kwargs: Any) -> pandas.DataFrame:
+        """
+
+        Parameters
+        ----------
+        args :
+        kwargs :
+
+        Returns
+        -------
+
+        """
+        return self.dataframe_adapter.read(*args, **kwargs)
+
+    def read_partition(self, *args: Any, **kwargs: Any) -> pandas.DataFrame:
+        """
+
+        Parameters
+        ----------
+        args :
+        kwargs :
+
+        Returns
+        -------
+
+        """
+        return self.dataframe_adapter.read_partition(*args, **kwargs)
+
+    def structure(self) -> TableStructure:
+        """
+
+        Returns
+        -------
+
+        """
+        return self._structure
