bcb9fa7809264e60079e8ef5c1edfa5daeeff68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:07 +0000

    Add package slandroid/0.3.5/slandroid-0.3.5-py3-none-any.whl

---
+++ packages/slandroid/slandroid-0.3.5-py3-none-any.whl/slandroid/__init__.py
@@ -0,0 +1,9 @@
+# Package version
+__version__ = "0.3.5"
+
+# Package description
+__description__ = "A universal script runner for multiple programming languages."
+
+# Package author
+__author__ = "Ishan Oshada (SL ANDROID TEAM)"
+__email__ = "ishan.kodithuwakku.offical@gmail.com"
