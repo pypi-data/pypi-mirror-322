3ce7aacbd60220f9075b4d7dfd40c666c18a7b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:07:27 +0000

    Add package metaflow-stubs/2.13.6/metaflow_stubs-2.13.6-py2.py3-none-any.whl

---
+++ packages/metaflow-stubs/metaflow_stubs-2.13.6-py2.py3-none-any.whl/metaflow-stubs/plugins/kubernetes/spot_monitor_sidecar.pyi
@@ -0,0 +1,20 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.13.6                                                                                 #
+# Generated on 2025-01-23T12:09:52.994458                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+
+class SpotTerminationMonitorSidecar(object, metaclass=type):
+    def __init__(self):
+        ...
+    def process_message(self, msg):
+        ...
+    @classmethod
+    def get_worker(cls):
+        ...
+    ...
+
