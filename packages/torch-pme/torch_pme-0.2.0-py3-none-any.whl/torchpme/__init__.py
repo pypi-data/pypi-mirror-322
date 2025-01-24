ac77c7340f947fee7febf25039fc8bfc9b26e114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:41 +0000

    Add package torch-pme/0.2.0/torch_pme-0.2.0-py3-none-any.whl

---
+++ packages/torch-pme/torch_pme-0.2.0-py3-none-any.whl/torchpme/__init__.py
@@ -0,0 +1,27 @@
+import contextlib
+
+from . import calculators, lib, potentials, prefactors, tuning  # noqa
+from ._version import __version__, __version_tuple__  # noqa
+from .calculators import Calculator, EwaldCalculator, P3MCalculator, PMECalculator
+from .potentials import (
+    CombinedPotential,
+    CoulombPotential,
+    InversePowerLawPotential,
+    Potential,
+    SplinePotential,
+)
+
+with contextlib.suppress(ImportError):
+    from . import metatensor  # noqa
+
+__all__ = [
+    "Calculator",
+    "EwaldCalculator",
+    "P3MCalculator",
+    "PMECalculator",
+    "CoulombPotential",
+    "Potential",
+    "InversePowerLawPotential",
+    "SplinePotential",
+    "CombinedPotential",
+]
