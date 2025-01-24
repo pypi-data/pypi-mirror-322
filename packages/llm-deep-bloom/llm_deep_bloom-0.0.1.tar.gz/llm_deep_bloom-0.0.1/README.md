ee8b39fb69f661809bb65b9ab0dd7f2d47cf51bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:07:02 +0000

    Add package llm-deep-bloom/0.0.1/llm_deep_bloom-0.0.1.tar.gz

---
+++ packages/llm-deep-bloom/llm_deep_bloom-0.0.1.tar.gz/llm_deep_bloom-0.0.1/README.md
@@ -0,0 +1,3 @@
+# llm-deep-bloom
+
+This is a placeholder package to reserve the llm-deep-bloom name on PyPI.
---
+++ packages/llm-deep-bloom/llm_deep_bloom-0.0.1.tar.gz/llm_deep_bloom-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="llm-deep-bloom",
+    version="0.0.1",
+    author="Thomas Hughes",
+    author_email="irthomasthomas@gmail.com",
+    description="Placeholder package to reserve the llm-deep-bloom name on PyPI",
+    long_description="This is a placeholder package to reserve the llm-deep-bloom name on PyPI.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/irthomasthomas/llm-deep-bloom",
+    packages=["llm_deep_bloom"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
