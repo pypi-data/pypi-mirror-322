678b09d10447fba408234b439c05384a026d1233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:40 +0000

    Add package alidanish-package-two/0.1.1/alidanish_package_two-0.1.1.tar.gz

---
+++ packages/alidanish-package-two/alidanish_package_two-0.1.1.tar.gz/alidanish_package_two-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="alidanish_package_two",
+    version="0.1.1",
+    author="Danish Ali",
+    author_email="danish.ali@protonshub.in",
+    description="A simple example package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/danish-protonshub/alidanish_package_two",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
