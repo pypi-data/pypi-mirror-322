f148bf2252990a2c53b6aff036b8a493fd1290c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:25 +0000

    Add package abc-test-ti-client/1.0.0/abc_test_ti_client-1.0.0.tar.gz

---
+++ packages/abc-test-ti-client/abc_test_ti_client-1.0.0.tar.gz/abc_test_ti_client-1.0.0/abc_test_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from abc_test_ti_client.api.pet_api import PetApi
+from abc_test_ti_client.api.store_api import StoreApi
+from abc_test_ti_client.api.user_api import UserApi
+
