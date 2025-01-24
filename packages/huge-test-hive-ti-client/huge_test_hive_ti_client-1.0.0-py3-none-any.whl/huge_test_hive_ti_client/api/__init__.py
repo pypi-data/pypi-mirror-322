f00773a99a5b5c719aa2e7e16aea30b76dd0b59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:41 +0000

    Add package huge-test-hive-ti-client/1.0.0/huge_test_hive_ti_client-1.0.0-py3-none-any.whl

---
+++ packages/huge-test-hive-ti-client/huge_test_hive_ti_client-1.0.0-py3-none-any.whl/huge_test_hive_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from huge_test_hive_ti_client.api.pet_api import PetApi
+from huge_test_hive_ti_client.api.store_api import StoreApi
+from huge_test_hive_ti_client.api.user_api import UserApi
+
