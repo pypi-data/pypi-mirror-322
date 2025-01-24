d8f5f2db27597fed0c393a9c1d92a08c216adf26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:07:44 +0000

    Add package dataanalysts/0.2.2/dataanalysts-0.2.2.tar.gz

---
+++ packages/dataanalysts/dataanalysts-0.2.2.tar.gz/dataanalysts-0.2.2/setup.py
@@ -0,0 +1,33 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='dataanalysts',
+    version='0.2.2',
+    description='An advanced Python data analysis library with enhanced cleaning, transformation, and visualization.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Venkata Govind Neelapu',
+    author_email='venkatagovindneelapu@gmail.com',
+    license='MIT',
+    url='https://github.com/yourusername/dataanalysts',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scikit-learn',
+        'seaborn'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
+
+
+
+
