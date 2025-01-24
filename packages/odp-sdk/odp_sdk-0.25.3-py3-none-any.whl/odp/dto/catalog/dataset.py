d29887ff5dd7aa6c1f0f30839d5a52478a34f5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:07:26 +0000

    Add package odp-sdk/0.25.3/odp_sdk-0.25.3-py3-none-any.whl

---
+++ packages/odp-sdk/odp_sdk-0.25.3-py3-none-any.whl/odp/dto/catalog/dataset.py
@@ -0,0 +1,72 @@
+from typing import Any, Dict, List, Optional, Set
+
+from pydantic import BaseModel, Field
+
+from ..common.contact_info import ContactInfo
+from ..resource import ResourceDto, ResourceSpec
+from ..resource_registry import DEFAULT_RESOURCE_TYPE_REGISTRY
+
+
+class Citation(BaseModel):
+    """Citation information"""
+
+    cite_as: Optional[str] = None
+    """Directions on how to cite the dataset"""
+
+    doi: Optional[str] = None
+
+
+class Attribute(BaseModel):
+    """Dataset attribute"""
+
+    name: str
+    """Attribute name. This can be a column name in a table, a dimension in an array, etc."""
+
+    description: Optional[str] = None
+    """Attribute description"""
+
+    traits: list[str]
+    """List of traits. Traits are used to describe the attribute in more detail.
+
+    Traits are based on Microsoft Common Data Model (CDM) traits. See the [CDM documentation]
+    (https://learn.microsoft.com/en-us/common-data-model/sdk/trait-concepts-and-use-cases#what-are-traits)
+    for more information.
+    """
+
+
+class DatasetSpec(ResourceSpec):
+    storage_class: str
+    """Storage class qualified name"""
+
+    storage_controller: Optional[str] = None
+    """Storage controller qualified name"""
+
+    data_collection: Optional[str] = None
+    """Data collection qualified name"""
+
+    maintainer: ContactInfo
+    """Active maintainer information"""
+
+    citation: Optional[Citation] = None
+    """Citation information"""
+
+    documentation: List[str] = Field(default_factory=list)
+    """Links to any relevant documentation"""
+
+    attributes: List[Attribute] = Field(default_factory=list)
+    """Dataset attributes"""
+
+    facets: Optional[Dict[str, Any]] = None
+    """Facets for the dataset"""
+
+    tags: Set[str] = Field(default_factory=set)
+
+
+class DatasetDto(ResourceDto):
+    _kind: str = "catalog.hubocean.io/dataset"
+    _version: str = "v1alpha3"
+
+    spec: DatasetSpec
+
+
+DEFAULT_RESOURCE_TYPE_REGISTRY.add(DatasetDto)
