beafe83e2fe4cb52d0cc2bd5f347afeb83027cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:30 +0000

    Add package pypaya-python-tools/0.11.1/pypaya_python_tools-0.11.1.tar.gz

---
+++ packages/pypaya-python-tools/pypaya_python_tools-0.11.1.tar.gz/pypaya_python_tools-0.11.1/pypaya_python_tools/object_access/exceptions.py
@@ -0,0 +1,30 @@
+
+
+class ObjectAccessError(Exception):
+    """Base exception for object access errors."""
+    pass
+
+
+class AccessError(ObjectAccessError):
+    """Base class for access operation errors."""
+    pass
+
+
+class InstantiationError(AccessError):
+    """Error during object instantiation."""
+    pass
+
+
+class CallError(AccessError):
+    """Error during object calling."""
+    pass
+
+
+class BuilderError(ObjectAccessError):
+    """Base class for builder errors."""
+    pass
+
+
+class InvalidStateError(BuilderError):
+    """Raised when builder is used in invalid state."""
+    pass
