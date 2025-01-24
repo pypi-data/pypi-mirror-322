72f2cad206ca3ad6d931d8518a8af51ffa833168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:06:01 +0000

    Add package gnukek/2.0.0a1/gnukek-2.0.0a1.tar.gz

---
+++ packages/gnukek/gnukek-2.0.0a1.tar.gz/gnukek-2.0.0a1/gnukek/backends/decryption.py
@@ -0,0 +1,54 @@
+import io
+from abc import ABCMeta, abstractmethod
+from typing import Iterator
+
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+from .. import constants
+from ..constants import KekAlgorithmVersion
+
+
+class DecryptionBackend(metaclass=ABCMeta):
+    version: KekAlgorithmVersion
+
+    def __init__(self, data: bytes, private_key: rsa.RSAPrivateKey) -> None:
+        self._data = data
+        self._private_key = private_key
+
+    @abstractmethod
+    def decrypt(self) -> bytes: ...
+
+
+class StreamDecryptionBackend(metaclass=ABCMeta):
+    version: KekAlgorithmVersion
+
+    def __init__(
+        self,
+        buffer: io.BufferedIOBase,
+        private_key: rsa.RSAPrivateKey,
+    ) -> None:
+        self._buffer = buffer
+        self._private_key = private_key
+
+    @abstractmethod
+    def decrypt_stream(
+        self,
+        *,
+        chunk_length: int = constants.CHUNK_LENGTH,
+    ) -> Iterator[bytes]: ...
+
+
+class DecryptionBackendFactory(metaclass=ABCMeta):
+    version: KekAlgorithmVersion
+
+    @staticmethod
+    @abstractmethod
+    def get_decryptor(
+        data: bytes, *, private_key: rsa.RSAPrivateKey
+    ) -> DecryptionBackend: ...
+
+    @staticmethod
+    @abstractmethod
+    def get_stream_decryptor(
+        buffer: io.BufferedIOBase, *, private_key: rsa.RSAPrivateKey
+    ) -> StreamDecryptionBackend: ...
