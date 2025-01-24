a3e76ef119c417db043ffe446a97e017f3af95c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:07:26 +0000

    Add package nonebot-plugin-suggarchat/1.8.0/nonebot_plugin_suggarchat-1.8.0.tar.gz

---
+++ packages/nonebot-plugin-suggarchat/nonebot_plugin_suggarchat-1.8.0.tar.gz/nonebot_plugin_suggarchat-1.8.0/tests/example.py
@@ -0,0 +1,20 @@
+from nonebot.plugin import require
+require("nonebot_plugin_suggarchat")
+#先require再作导入！！！！
+from nonebot import logger
+from nonebot_plugin_suggarchat.on_event import on_chat,on_poke
+from nonebot_plugin_suggarchat.event import ChatEvent
+from nonebot_plugin_suggarchat.matcher import SuggarMatcher
+from nonebot.adapters.onebot.v11 import MessageSegment
+
+@on_chat().handle()
+async def chat_logic(event:ChatEvent,matcher:SuggarMatcher):
+    logger.info("收到消息事件")
+    logger.info(f"{event.get_event_type()}")
+    matcher.append_message(MessageSegment.text("\n测试插件"))
+
+@on_poke().handle()
+async def poke_logic(event:ChatEvent,matcher:SuggarMatcher):
+    logger.info("收到戳一戳事件")
+    logger.info(f"{event.get_event_type()}")
+    matcher.append_message(MessageSegment.text("\n测试插件"))
