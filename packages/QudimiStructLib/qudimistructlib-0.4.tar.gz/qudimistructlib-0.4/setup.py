8679667313dbbb1500bbd76cf2cc41450d5dd944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:13 +0000

    Add package QudimiStructLib/0.4/qudimistructlib-0.4.tar.gz

---
+++ packages/QudimiStructLib/qudimistructlib-0.4.tar.gz/qudimistructlib-0.4/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup , find_packages
+setup(
+    name="QudimiStructLib",
+    version="0.4",
+    author="Abdulaziz AL-qudimi",
+    author_email="eng7mi@gmail.com",
+    description="Data Structures",
+    packages=find_packages(),
+    install_requires=["matplotlib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    )
