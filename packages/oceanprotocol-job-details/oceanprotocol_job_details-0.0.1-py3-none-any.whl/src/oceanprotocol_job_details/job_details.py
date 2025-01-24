9891e1f90b689aed8674ac46925f904bd7dfdd39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:07:00 +0000

    Add package oceanprotocol-job-details/0.0.1/oceanprotocol_job_details-0.0.1-py3-none-any.whl

---
+++ packages/oceanprotocol-job-details/oceanprotocol_job_details-0.0.1-py3-none-any.whl/src/oceanprotocol_job_details/job_details.py
@@ -0,0 +1,20 @@
+from typing import Literal, Optional
+from src.oceanprotocol_job_details.dataclasses.job_details import JobDetails
+from src.oceanprotocol_job_details.loaders.loader import Loader
+from src.oceanprotocol_job_details.loaders.impl.environment import EnvironmentLoader
+
+_Implementations = Literal["env"]
+
+
+class OceanProtocolJobDetails(Loader[JobDetails]):
+    """Decorator that loads the JobDetails from the given implementation"""
+
+    def __init__(self, implementation: Optional[_Implementations], *args, **kwargs):
+        # As there are not more implementations, we can use the EnvironmentLoader directly
+        self._loader = lambda: EnvironmentLoader(*args, **kwargs)
+
+    def load(self) -> JobDetails:
+        return self._loader().load()
+
+
+del _Implementations
