33f1d28e72a1ee6aaeb87ed2c1a40b162e3acba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:06:48 +0000

    Add package WrightTools/3.5.5/wrighttools-3.5.5.tar.gz

---
+++ packages/WrightTools/wrighttools-3.5.5.tar.gz/wrighttools-3.5.5/WrightTools/kit/_unicode.py
@@ -0,0 +1,63 @@
+"""Unicode."""
+
+# --- import --------------------------------------------------------------------------------------
+
+
+import collections
+
+
+# --- define --------------------------------------------------------------------------------------
+
+
+__all__ = ["unicode_dictionary"]
+
+
+unicode_dictionary = collections.OrderedDict()
+unicode_dictionary["Alpha"] = "\u0391"
+unicode_dictionary["Beta"] = "\u0392"
+unicode_dictionary["Gamma"] = "\u0392"
+unicode_dictionary["Delta"] = "\u0394"
+unicode_dictionary["Epsilon"] = "\u0395"
+unicode_dictionary["Zeta"] = "\u0396"
+unicode_dictionary["Eta"] = "\u0397"
+unicode_dictionary["Theta"] = "\u0398"
+unicode_dictionary["Iota"] = "\u0399"
+unicode_dictionary["Kappa"] = "\u039A"
+unicode_dictionary["Lamda"] = "\u039B"
+unicode_dictionary["Mu"] = "\u039C"
+unicode_dictionary["Nu"] = "\u039D"
+unicode_dictionary["Xi"] = "\u039E"
+unicode_dictionary["Omicron"] = "\u039F"
+unicode_dictionary["Pi"] = "\u03A0"
+unicode_dictionary["Rho"] = "\u03A1"
+unicode_dictionary["Sigma"] = "\u03A3"
+unicode_dictionary["Tau"] = "\u03A4"
+unicode_dictionary["Upsilon"] = "\u03A5"
+unicode_dictionary["Phi"] = "\u03A6"
+unicode_dictionary["Chi"] = "\u03A7"
+unicode_dictionary["Psi"] = "\u03A8"
+unicode_dictionary["Omega"] = "\u03A9"
+unicode_dictionary["alpha"] = "\u03B1"
+unicode_dictionary["beta"] = "\u03B2"
+unicode_dictionary["gamma"] = "\u03B3"
+unicode_dictionary["delta"] = "\u03B4"
+unicode_dictionary["epsilon"] = "\u03B5"
+unicode_dictionary["zeta"] = "\u03B6"
+unicode_dictionary["eta"] = "\u03B7"
+unicode_dictionary["theta"] = "\u03B8"
+unicode_dictionary["iota"] = "\u03B9"
+unicode_dictionary["kappa"] = "\u03BA"
+unicode_dictionary["lamda"] = "\u03BB"
+unicode_dictionary["mu"] = "\u03BC"
+unicode_dictionary["nu"] = "\u03BD"
+unicode_dictionary["xi"] = "\u03BE"
+unicode_dictionary["omicron"] = "\u03BF"
+unicode_dictionary["pi"] = "\u03C0"
+unicode_dictionary["rho"] = "\u03C1"
+unicode_dictionary["sigma"] = "\u03C3"
+unicode_dictionary["tau"] = "\u03C4"
+unicode_dictionary["upsilon"] = "\u03C5"
+unicode_dictionary["phi"] = "\u03C6"
+unicode_dictionary["chi"] = "\u03C7"
+unicode_dictionary["psi"] = "\u03C8"
+unicode_dictionary["omega"] = "\u03C9"
