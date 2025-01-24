f9203e4fa60a9e9b8bd351ae70445bdb67f65b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:06:08 +0000

    Add package infercraft/0.1.4/infercraft-0.1.4.tar.gz

---
+++ packages/infercraft/infercraft-0.1.4.tar.gz/infercraft-0.1.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+x = 0
+
+y = 1
+
+z = 4
+
+version = "{}.{}.{}".format(x, y, z)
+
+
+setup(
+    name='infercraft',
+    version=version,
+    description='A python framework for distributed inference by arbitrary platform',
+    url='',
+    author='WD',
+    author_email='',
+    license='MIT',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    requires=[
+        "torch",
+        "onnxruntime"
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
