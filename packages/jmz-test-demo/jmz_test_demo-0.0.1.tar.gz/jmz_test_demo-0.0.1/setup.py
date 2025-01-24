1dd6ed8c4e0a70bc50a3e9b89277c862ee61a66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:50 +0000

    Add package jmz-test-demo/0.0.1/jmz_test_demo-0.0.1.tar.gz

---
+++ packages/jmz-test-demo/jmz_test_demo-0.0.1.tar.gz/jmz_test_demo-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages,find_namespace_packages
+print(find_packages())
+
+setup(
+    name='jmz_test_demo',
+    version='0.0.1',
+    author='Your Name',
+    author_email='17816765317@163.com',
+    description='A simple Python package',
+    packages=find_packages(),
+
+)
