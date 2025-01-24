4c5e28998dcf530f4b087ac5b01abff854458d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:05:50 +0000

    Add package panimg/0.15.1/panimg-0.15.1-py3-none-any.whl

---
+++ packages/panimg/panimg-0.15.1-py3-none-any.whl/panimg/exceptions.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+class ValidationError(Exception):
+    pass
+
+
+class UnconsumedFilesException(Exception):  # noqa: N818
+    """
+    Raised on completion of an image builder and there are unconsumed files.
+    Contains a dictionary with a map of the errors encountered when loading
+    the unconsumed file.
+    """
+
+    def __init__(self, *args, file_errors: dict[Path, list[str]]):
+        super().__init__(*args)
+        self.file_errors = file_errors
