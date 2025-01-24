bba45c7ec7268cd0f98c80e129b27c1ecfa96a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:44 +0000

    Add package dsptoolbox/0.5.1/dsptoolbox-0.5.1.tar.gz

---
+++ packages/dsptoolbox/dsptoolbox-0.5.1.tar.gz/dsptoolbox-0.5.1/dsptoolbox/plots/__init__.py
@@ -0,0 +1,25 @@
+"""
+Plots
+-----
+This module contains plotting templates that use matplotlib and seaborn for
+styling.
+
+- `general_matrix_plot()`
+- `general_plot()`
+- `general_subplots_line()`
+- `show()`
+
+"""
+from .plots import (
+    general_matrix_plot,
+    general_plot,
+    general_subplots_line,
+    show,
+)
+
+__all__ = [
+    "general_matrix_plot",
+    "general_plot",
+    "general_subplots_line",
+    "show",
+]
