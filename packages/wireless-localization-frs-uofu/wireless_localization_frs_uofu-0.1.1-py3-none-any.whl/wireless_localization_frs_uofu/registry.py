322c471289bf384c8fdff1fa9b59f7f4de67f3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:05:57 +0000

    Add package wireless-localization-frs-uofu/0.1.1/wireless_localization_frs_uofu-0.1.1-py3-none-any.whl

---
+++ packages/wireless-localization-frs-uofu/wireless_localization_frs_uofu-0.1.1-py3-none-any.whl/wireless_localization_frs_uofu/registry.py
@@ -0,0 +1,32 @@
+DATASETS = {
+    "frs-dataset": {
+        "file_name": "json_dataset.json",
+        "file_url": "https://raw.githubusercontent.com/mhrafi66/wireless_localization_frs_uofu/refs/heads/main/Full_Dataset/powder_462.7_rss_data.json",
+        "format": "json",
+        "description": "Full dataset in json format",
+    },
+    "single_tx_json": {
+        "file_name": "single_tx.json",
+        "file_url": "https://raw.githubusercontent.com/mhrafi66/wireless_localization_frs_uofu/refs/heads/main/Separated_Dataset_JSON/all_data/single_tx.json",
+        "format": "json",
+        "description": "Only Single transmitter files in json",
+    },
+    "two_tx_json": {
+        "file_name": "two_tx.json",
+        "file_url": "https://raw.githubusercontent.com/mhrafi66/wireless_localization_frs_uofu/refs/heads/main/Separated_Dataset_JSON/all_data/two_tx.json",
+        "format": "json",
+        "description": "Only Two transmitter files in json",
+    },
+    "single_tx_csv": {
+        "file_name": "single_tx.csv",
+        "file_url": "https://raw.githubusercontent.com/mhrafi66/wireless_localization_frs_uofu/refs/heads/main/Separated_Dataset_CSV/single_tx.csv",
+        "format": "csv",
+        "description": "Only Single transmitter files in csv",
+    },
+    "two_tx_csv": {
+        "file_name": "two_tx.csv",
+        "file_url": "https://raw.githubusercontent.com/mhrafi66/wireless_localization_frs_uofu/refs/heads/main/Separated_Dataset_CSV/two_tx.csv",
+        "format": "csv",
+        "description": "Only Two transmitter files in csv",
+    },
+}
