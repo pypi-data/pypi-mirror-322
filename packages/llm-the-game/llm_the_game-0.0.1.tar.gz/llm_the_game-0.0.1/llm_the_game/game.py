658a8080d4f57e5ed66473e71b6ea938cfb9356e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:03 +0000

    Add package llm-the-game/0.0.1/llm_the_game-0.0.1.tar.gz

---
+++ packages/llm-the-game/llm_the_game-0.0.1.tar.gz/llm_the_game-0.0.1/llm_the_game/game.py
@@ -0,0 +1,3 @@
+def placeholder():
+    """Placeholder function for LLM The Game"""
+    return "This is a placeholder for LLM The Game"
---
+++ packages/llm-the-game/llm_the_game-0.0.1.tar.gz/llm_the_game-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llm-the-game",
+    version="0.0.1",
+    author="Thomas Hughes",
+    author_email="irthomasthomas@gmail.com",
+    description="A placeholder package for LLM The Game",
+    long_description="This is a placeholder package for LLM The Game",
+    long_description_content_type="text/markdown",
+    url="https://github.com/irthomasthomas/llm-the-game",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
