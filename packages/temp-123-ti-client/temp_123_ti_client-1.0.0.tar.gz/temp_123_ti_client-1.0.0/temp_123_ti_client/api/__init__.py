5088a39b0c4b180cfc1457e3b57981d20336a5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:06 +0000

    Add package temp-123-ti-client/1.0.0/temp_123_ti_client-1.0.0.tar.gz

---
+++ packages/temp-123-ti-client/temp_123_ti_client-1.0.0.tar.gz/temp_123_ti_client-1.0.0/temp_123_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from temp_123_ti_client.api.pet_api import PetApi
+from temp_123_ti_client.api.store_api import StoreApi
+from temp_123_ti_client.api.user_api import UserApi
+
