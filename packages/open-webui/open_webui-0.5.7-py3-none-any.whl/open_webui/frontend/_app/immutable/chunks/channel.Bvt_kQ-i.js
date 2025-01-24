ea0e6548331ae9483f5f7b4c7de4721818f6e32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:07:02 +0000

    Add package open-webui/0.5.7/open_webui-0.5.7-py3-none-any.whl

---
+++ packages/open-webui/open_webui-0.5.7-py3-none-any.whl/open_webui/frontend/_app/immutable/chunks/channel.Bvt_kQ-i.js
@@ -0,0 +1,2 @@
+import{U as a,C as n}from"./Markdown.BbZedqzQ.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
+//# sourceMappingURL=channel.Bvt_kQ-i.js.map
---
+++ packages/open-webui/open_webui-0.5.7-py3-none-any.whl/open_webui/frontend/_app/immutable/entry/start.B1ibnFLA.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.B71ECrEP.js";export{t as start};
+//# sourceMappingURL=start.B1ibnFLA.js.map
