8e18d7fcd44c4e6866bf19e1d93e4d1aa1386b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:06:58 +0000

    Add package nonebot-plugin-suggarchat/1.8.1/nonebot_plugin_suggarchat-1.8.1.tar.gz

---
+++ packages/nonebot-plugin-suggarchat/nonebot_plugin_suggarchat-1.8.1.tar.gz/nonebot_plugin_suggarchat-1.8.1/src/nonebot_plugin_suggarchat/__init__.py
@@ -0,0 +1,28 @@
+
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+from .conf import __KERNEL_VERSION__
+from .config import Config
+from .conf import *
+from .resources import *
+from .suggar import *
+from .API import *
+from .event import *
+from .matcher import *
+from.on_event import *
+from .  import *
+__plugin_meta__ = PluginMetadata(
+    name="SuggarChat OpenAI协议聊天插件" ,
+    description="强大的插件，支持OpenAI协议，多模型切换，完全的上下文支持，智能化的聊天。适配Nonebot2-Onebot-V11适配器",
+    usage="按照Readme.md修改配置文件后使用，默认enable为false！",
+    config=Config,
+    homepage="https://github.com/JohnRichard4096/nonebot_plugin_suggarchat/",
+    type="application",
+    supported_adapters={"~onebot.v11"}
+)
+
+config = get_plugin_config(Config)
+
+
+
+
