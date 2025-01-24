1d099db5b6fcf3ae3265decc26862446b14840d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:05:55 +0000

    Add package quotesfromthematrix/1.0.6/quotesfromthematrix-1.0.6-py3-none-any.whl

---
+++ packages/quotesfromthematrix/quotesfromthematrix-1.0.6-py3-none-any.whl/quotesfromthematrix/__init__.py
@@ -0,0 +1,16 @@
+import sys
+
+# The function that displays quotes based on input
+def load(input_value=None):
+    if input_value == "2.103.3.4":
+        print("What is real? How do you define 'real'? If you're talking about what you can feel, what you can smell, what you can taste and see, then 'real' is simply electrical signals interpreted by your brain.")
+    else:
+        print("There is no spoon.")
+
+# Directly expose the `quote` function in the module's namespace
+
+print("This is your last chance. After this, there is no turning back. "
+    "You take the blue pill - the story ends, you wake up in your bed "
+    "and believe whatever you want to believe. You take the red pill - "
+    "you stay in Wonderland and I show you how deep the rabbit hole goes.")
+
