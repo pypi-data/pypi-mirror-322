a1abf4e94222db6fe61d1691fec3280fa6685f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:06:35 +0000

    Add package ak-test-ti-client/1.0.0/ak_test_ti_client-1.0.0-py3-none-any.whl

---
+++ packages/ak-test-ti-client/ak_test_ti_client-1.0.0-py3-none-any.whl/ak_test_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from ak_test_ti_client.api.pet_api import PetApi
+from ak_test_ti_client.api.store_api import StoreApi
+from ak_test_ti_client.api.user_api import UserApi
+
