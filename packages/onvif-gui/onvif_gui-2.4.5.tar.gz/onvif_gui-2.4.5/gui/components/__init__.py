c2af91f7eb0d62ff08e521a611a32b0d7d217fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:07:02 +0000

    Add package onvif-gui/2.4.5/onvif_gui-2.4.5.tar.gz

---
+++ packages/onvif-gui/onvif_gui-2.4.5.tar.gz/onvif_gui-2.4.5/gui/components/__init__.py
@@ -0,0 +1,7 @@
+from .fileselector import FileSelector
+from .directoryselector import DirectorySelector
+from .thresholdslider import ThresholdSlider
+from .comboselector import ComboSelector
+from .progress import Progress
+from .warningbar import WarningBar, Indicator
+from .target import Target, TargetList, TargetDialog, TargetSelector
