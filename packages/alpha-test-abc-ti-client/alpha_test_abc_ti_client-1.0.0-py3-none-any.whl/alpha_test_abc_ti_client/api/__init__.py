65b73e8fb102854b42a188499aeacf497254edb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:12 +0000

    Add package alpha-test-abc-ti-client/1.0.0/alpha_test_abc_ti_client-1.0.0-py3-none-any.whl

---
+++ packages/alpha-test-abc-ti-client/alpha_test_abc_ti_client-1.0.0-py3-none-any.whl/alpha_test_abc_ti_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from alpha_test_abc_ti_client.api.pet_api import PetApi
+from alpha_test_abc_ti_client.api.store_api import StoreApi
+from alpha_test_abc_ti_client.api.user_api import UserApi
+
