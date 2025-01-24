e011d4f745325030fdc4eb70cf19d0bb36c0f468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:06 +0000

    Add package flask-squeeze/3.3.1/flask_squeeze-3.3.1-py3-none-any.whl

---
+++ packages/flask-squeeze/flask_squeeze-3.3.1-py3-none-any.whl/flask_squeeze/compress.py
@@ -0,0 +1,50 @@
+import gzip
+import time
+import zlib
+from dataclasses import dataclass
+
+import brotli
+from flask import Response
+
+from flask_squeeze.models import Encoding
+
+
+@dataclass(frozen=True)
+class CompressionResult:
+	data: bytes
+	encoding: Encoding
+	quality: int
+	duration: float
+	ratio: float
+
+
+def compress(
+	data: bytes,
+	encode_choice: Encoding,
+	quality: int,
+) -> CompressionResult:
+	t0 = time.perf_counter()
+
+	if encode_choice == Encoding.br:
+		compressed_data = brotli.compress(data, quality=quality)
+	elif encode_choice == Encoding.deflate:
+		compressed_data = zlib.compress(data, level=quality)
+	elif encode_choice == Encoding.gzip:
+		compressed_data = gzip.compress(data, compresslevel=quality)
+	else:
+		raise ValueError(f"Invalid encoding choice {encode_choice}")
+
+	return CompressionResult(
+		data=compressed_data,
+		encoding=encode_choice,
+		quality=quality,
+		duration=time.perf_counter() - t0,
+		ratio=len(data) / len(compressed_data),
+	)
+
+
+def update_response_with_compressed_data(response: Response, result: CompressionResult) -> None:
+	response.set_data(result.data)
+	response.headers["X-Flask-Squeeze-Compression-Quality"] = str(result.quality)
+	response.headers["X-Flask-Squeeze-Compression-Duration"] = f"{result.duration * 1000:.1f}ms"
+	response.headers["X-Flask-Squeeze-Compression-Ratio"] = f"{result.ratio:.1f}x"
