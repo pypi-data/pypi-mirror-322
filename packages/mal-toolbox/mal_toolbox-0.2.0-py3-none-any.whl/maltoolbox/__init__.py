f62065bbf7d3911f26d46aa2bb360ed5932f5722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:06:10 +0000

    Add package mal-toolbox/0.2.0/mal_toolbox-0.2.0-py3-none-any.whl

---
+++ packages/mal-toolbox/mal_toolbox-0.2.0-py3-none-any.whl/maltoolbox/__init__.py
@@ -0,0 +1,94 @@
+# -*- encoding: utf-8 -*-
+# MAL Toolbox v0.2.0
+# Copyright 2024, Andrei Buhaiu.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+"""
+MAL-Toolbox Framework
+"""
+
+__title__ = 'maltoolbox'
+__version__ = '0.2.0'
+__authors__ = ['Andrei Buhaiu',
+    'Giuseppe Nebbione',
+    'Nikolaos Kakouros',
+    'Jakob Nyberg',
+    'Joakim Loxdal']
+__license__ = 'Apache 2.0'
+__docformat__ = 'restructuredtext en'
+
+__all__ = ()
+
+import os
+import configparser
+import logging
+
+ERROR_INCORRECT_CONFIG = 1
+
+CONFIGFILE = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "default.conf"
+)
+
+config = configparser.ConfigParser()
+config.read(CONFIGFILE)
+
+if 'logging' not in config:
+    raise ValueError('Config file is missing essential information, cannot proceed.')
+
+if 'log_file' not in config['logging']:
+    raise ValueError('Config file is missing a log_file location, cannot proceed.')
+
+log_configs = {
+    'log_file': config['logging']['log_file'],
+    'log_level': config['logging']['log_level'],
+    'attackgraph_file': config['logging']['attackgraph_file'],
+    'model_file': config['logging']['model_file'],
+    'langspec_file': config['logging']['langspec_file'],
+}
+
+os.makedirs(os.path.dirname(log_configs['log_file']), exist_ok = True)
+
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s', datefmt='%m-%d %H:%M')
+file_handler = logging.FileHandler(log_configs['log_file'], mode='w')
+file_handler.setFormatter(formatter)
+
+logger = logging.getLogger(__name__)
+logger.addHandler(file_handler)
+
+log_level = log_configs['log_level']
+if log_level != '':
+    level = logging.getLevelName(log_level)
+    logger.setLevel(level)
+    logger.info('Set loggin level of %s to %s.', __name__, log_level)
+
+if 'neo4j' in config:
+    for term in ['uri', 'username', 'password', 'dbname']:
+        if term not in config['neo4j']:
+
+            msg = (
+                'Config file is missing essential Neo4J '
+                f'information: {term}, cannot proceed.'
+            )
+            logger.critical(msg)
+            raise ValueError(msg)
+
+    neo4j_configs = {
+        'uri': config['neo4j']['uri'],
+        'username': config['neo4j']['username'],
+        'password': config['neo4j']['password'],
+        'dbname': config['neo4j']['dbname'],
+    }
---
+++ packages/mal-toolbox/mal_toolbox-0.2.0-py3-none-any.whl/maltoolbox/attackgraph/node.py
@@ -0,0 +1,199 @@
+"""
+MAL-Toolbox Attack Graph Node Dataclass
+"""
+
+from __future__ import annotations
+import copy
+from dataclasses import field, dataclass
+from functools import cached_property
+from typing import Any, Optional
+
+from . import Attacker
+from ..language import LanguageGraphAttackStep
+
+@dataclass
+class AttackGraphNode:
+    """Node part of AttackGraph"""
+    type: str
+    lang_graph_attack_step: LanguageGraphAttackStep
+    name: str
+    ttc: Optional[dict] = None
+    id: Optional[int] = None
+    asset: Optional[Any] = None
+    children: list[AttackGraphNode] = field(default_factory=list)
+    parents: list[AttackGraphNode] = field(default_factory=list)
+    defense_status: Optional[float] = None
+    existence_status: Optional[bool] = None
+    is_viable: bool = True
+    is_necessary: bool = True
+    compromised_by: list[Attacker] = field(default_factory=list)
+    tags: set[str] = field(default_factory=set)
+    attributes: Optional[dict] = None
+
+    # Optional extra metadata for AttackGraphNode
+    extras: dict = field(default_factory=dict)
+
+
+    def to_dict(self) -> dict:
+        """Convert node to dictionary"""
+        node_dict: dict = {
+            'id': self.id,
+            'type': self.type,
+            'lang_graph_attack_step': self.lang_graph_attack_step.full_name,
+            'name': self.name,
+            'ttc': self.ttc,
+            'children': {},
+            'parents': {},
+            'compromised_by': [attacker.name for attacker in \
+                self.compromised_by]
+        }
+
+        for child in self.children:
+            node_dict['children'][child.id] = child.full_name
+        for parent in self.parents:
+            node_dict['parents'][parent.id] = parent.full_name
+        if self.asset is not None:
+            node_dict['asset'] = str(self.asset.name)
+        if self.defense_status is not None:
+            node_dict['defense_status'] = str(self.defense_status)
+        if self.existence_status is not None:
+            node_dict['existence_status'] = str(self.existence_status)
+        if self.is_viable is not None:
+            node_dict['is_viable'] = str(self.is_viable)
+        if self.is_necessary is not None:
+            node_dict['is_necessary'] = str(self.is_necessary)
+        if self.tags:
+            node_dict['tags'] = list(self.tags)
+        if self.extras:
+            node_dict['extras'] = self.extras
+
+        return node_dict
+
+
+    def __repr__(self) -> str:
+        return str(self.to_dict())
+
+
+    def __deepcopy__(self, memo) -> AttackGraphNode:
+        """Deep copy an attackgraph node
+
+        The deepcopy will copy over node specific information, such as type,
+        name, etc., but it will not copy attack graph relations such as
+        parents, children, or attackers it is compromised by. These references
+        should be recreated when deepcopying the attack graph itself.
+
+        """
+
+        # Check if the object is already in the memo dictionary
+        if id(self) in memo:
+            return memo[id(self)]
+
+        copied_node = AttackGraphNode(
+            self.type,
+            self.lang_graph_attack_step,
+            self.name,
+            self.ttc,
+            self.id,
+            self.asset,
+            [],
+            [],
+            self.defense_status,
+            self.existence_status,
+            self.is_viable,
+            self.is_necessary,
+            [],
+            set(),
+            {},
+            {}
+        )
+
+        copied_node.tags = copy.deepcopy(self.tags, memo)
+        copied_node.attributes = copy.deepcopy(self.attributes, memo)
+        copied_node.extras = copy.deepcopy(self.extras, memo)
+
+        # Remember that self was already copied
+        memo[id(self)] = copied_node
+
+        return copied_node
+
+
+    def is_compromised(self) -> bool:
+        """
+        Return True if any attackers have compromised this node.
+        False, otherwise.
+        """
+        return len(self.compromised_by) > 0
+
+
+    def is_compromised_by(self, attacker: Attacker) -> bool:
+        """
+        Return True if the attacker given as an argument has compromised this
+        node.
+        False, otherwise.
+
+        Arguments:
+        attacker    - the attacker we are interested in
+        """
+        return attacker in self.compromised_by
+
+
+    def compromise(self, attacker: Attacker) -> None:
+        """
+        Have the attacker given as a parameter compromise this node.
+
+        Arguments:
+        attacker    - the attacker that will compromise the node
+        """
+        attacker.compromise(self)
+
+
+    def undo_compromise(self, attacker: Attacker) -> None:
+        """
+        Remove the attacker given as a parameter from the list of attackers
+        that have compromised this node.
+
+        Arguments:
+        attacker    - the attacker that we wish to remove from the compromised
+                      list.
+        """
+        attacker.undo_compromise(self)
+
+
+    def is_enabled_defense(self) -> bool:
+        """
+        Return True if this node is a defense node and it is enabled and not
+        suppressed via tags.
+        False, otherwise.
+        """
+        return self.type == 'defense' and \
+            'suppress' not in self.tags and \
+            self.defense_status == 1.0
+
+
+    def is_available_defense(self) -> bool:
+        """
+        Return True if this node is a defense node and it is not fully enabled
+        and not suppressed via tags. False otherwise.
+        """
+        return self.type == 'defense' and \
+            'suppress' not in self.tags and \
+            self.defense_status != 1.0
+
+
+    @property
+    def full_name(self) -> str:
+        """
+        Return the full name of the attack step. This is a combination of the
+        asset name to which the attack step belongs and attack step name
+        itself.
+        """
+        if self.asset:
+            full_name = self.asset.name + ':' + self.name
+        else:
+            full_name = str(self.id) + ':' + self.name
+        return full_name
+
+
+    @cached_property
+    def info(self) -> dict[str, str]:
+        return self.lang_graph_attack_step.info
