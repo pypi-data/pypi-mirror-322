67882ec37101f01150b4a7400eee3554acb4ea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 05:07:02 +0000

    Add package onvif-gui/2.4.5/onvif_gui-2.4.5-py3-none-any.whl

---
+++ packages/onvif-gui/onvif_gui-2.4.5-py3-none-any.whl/gui/panels/__init__.py
@@ -0,0 +1,5 @@
+from .camerapanel import CameraPanel
+from .filepanel import FilePanel
+from .videopanel import VideoPanel
+from .settingspanel import SettingsPanel
+from .audiopanel import AudioPanel
