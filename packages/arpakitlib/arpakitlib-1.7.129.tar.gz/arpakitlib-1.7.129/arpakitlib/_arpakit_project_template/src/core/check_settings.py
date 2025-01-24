3e49f089c0317a1f33638ca54f43f98446d9043b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:05:46 +0000

    Add package arpakitlib/1.7.129/arpakitlib-1.7.129.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.7.129.tar.gz/arpakitlib-1.7.129/arpakitlib/_arpakit_project_template/src/core/check_settings.py
@@ -0,0 +1,12 @@
+from arpakitlib.ar_json_util import safely_transfer_obj_to_json_str
+from src.core.settings import get_cached_settings
+from src.core.util import setup_logging
+
+
+def command():
+    setup_logging()
+    print(safely_transfer_obj_to_json_str(get_cached_settings().model_dump(mode="json")))
+
+
+if __name__ == '__main__':
+    command()
