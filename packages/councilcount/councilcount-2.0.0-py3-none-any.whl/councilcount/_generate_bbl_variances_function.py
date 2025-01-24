c9e6e2131a91fd1781f346e2747346f4385ff778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:16 +0000

    Add package councilcount/2.0.0/councilcount-2.0.0-py3-none-any.whl

---
+++ packages/councilcount/councilcount-2.0.0-py3-none-any.whl/councilcount/_generate_bbl_variances_function.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pygris
+from pygris.data import get_census
+from _gen_proportion_MOE_function import _gen_proportion_MOE
+
+def _generate_bbl_variances(acs_year, demo_dict, pop_est_df, census_api_key, total_pop_code = None, total_house_code = None):
+
+    """
+    This function retrieves ACS 5-Year data for specified demographic variables, calculates the variances at the 
+    census tract level, and generates proportion MOEs for demographic estimates (with total population or households 
+    as the denominator). Called in `generate_new_estimates()`.
+    
+    Parameters:
+    -----------
+    acs_year : int
+        The ACS 5-Year dataset end year (e.g., 2022 for the 2018-2022 ACS 5-Year dataset).
+    demo_dict : dict
+        Dictionary pairing each demographic variable name with its category ('person' or 'household').
+    pop_est_df : dataframe
+        DataFrame containing population estimates by BBL (Building Block Lot).
+    census_api_key : str
+        API key for accessing the U.S. Census Bureau's API.
+    total_pop_code : str, optional
+        API code for total population. Required if generating person-level estimates.
+    total_house_code : str, optional
+        API code for total households. Required if generating household-level estimates.
+
+    Returns:
+    -----------
+    DataFrame: A DataFrame containing variances for all specified variables, with columns:
+        - '{variable}_variance': Variance of the demographic variable proportion.
+
+    Notes:
+        - Census Tract raw number MOEs are converted to proportions using a census formula
+        - Proportion MOEs are converted to variances using the formula: variance = (MOE / 1.645)^2.
+    """
+    
+    # setting census year (the year census tracts are associated with) 
+    
+    if (acs_year < 2020) and (acs_year >= 2010): # censuses from these years use 2010 census tracts 
+        census_year = 2010
+    elif acs_year >= 2020: # censuses from these years use 2020 census tracts   
+        census_year = 2020
+        
+    # picking which denoms to include
+    denom_list = [code for code in (total_pop_code, total_house_code) if code is not None]
+    denom_moe_list = [denom_code[:-1] + 'M' for denom_code in denom_list]
+        
+    var_code_list = list(demo_dict.keys()) + denom_list # list of all variable codes entered in the demo_dict
+    MOE_code_list = [var_code[:-1] + 'M' for var_code in var_code_list] # converting to codes that access a variable's MOE (ending in M calls variable's MOE)
+
+        # retrieving the MOE and estimate data by census tract (need this data for calculating MOE of proportion in gen_proportion_MOE)
+
+    variance_df = get_census(dataset = "acs/acs5/profile", # acs 5yr link
+                     variables = var_code_list + MOE_code_list, 
+                     year = acs_year, # 5-year acs
+                     params = {
+                           "for": "tract:*", # tract-level
+                           "in": "state:36&in=county:005,047,081,085,061", # NYC
+                           "key": census_api_key}, # census user key
+                     guess_dtypes = True,
+                     return_geoid = True)
+
+    # adding unique identifier column: '2020_TRACT_ID'
+
+    variance_df['TRACTCE'] = variance_df['GEOID'].str[5:]
+    variance_df['COUNTYFP'] = variance_df['GEOID'].str[3:5]
+
+    variance_df['COUNTYFP'] = variance_df['COUNTYFP'].astype(int) # removing leading zeroes
+    variance_df['COUNTYFP'] = variance_df['COUNTYFP'].astype(str) 
+    variance_df['TRACTCE'] = variance_df['TRACTCE'].astype(int) # removing leading zeroes
+    variance_df['TRACTCE'] = variance_df['TRACTCE'].astype(str) 
+
+    variance_df[str(census_year) + '_tract_id'] = variance_df['TRACTCE'] +'-'+ variance_df['COUNTYFP'] # adding unique identifier
+    variance_df = variance_df.drop(columns=['GEOID','TRACTCE','COUNTYFP'])
+    
+    for MOE_code in MOE_code_list: # for each code in the list, convert to proportion
+        
+        if MOE_code not in denom_moe_list: # exclude total population and total households because they are the denominators for the other variables
+       
+            # turning raw number MOE to MOE of proportion (total population or total households = denominator)
+            variance_df = _gen_proportion_MOE(demo_dict, variance_df, MOE_code, total_pop_code, total_house_code) 
+
+        var_code = MOE_code[:-1] + 'E' # creating column name based on estimate code
+        
+        variance_df[var_code + '_variance'] = (variance_df[MOE_code] / 1.645) ** 2 # converting MOE to variance
+        
+    variance_df = variance_df.drop(columns=var_code_list + MOE_code_list) # removing unnecesary columns
+        
+    return variance_df
+                    
