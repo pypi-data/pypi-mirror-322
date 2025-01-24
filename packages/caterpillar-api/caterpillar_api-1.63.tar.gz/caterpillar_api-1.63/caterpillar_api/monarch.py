54e3d5781d536a5818862329d85cee38669472db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 07:06:59 +0000

    Add package caterpillar-api/1.63/caterpillar_api-1.63.tar.gz

---
+++ packages/caterpillar-api/caterpillar_api-1.63.tar.gz/caterpillar_api-1.63/caterpillar_api/monarch.py
@@ -0,0 +1,41 @@
+from caterpillar_api import util
+
+import json, logging
+
+def succ( request, objs={} ):
+    objs['successful'] = True
+
+    # If we have no request, then just return the objects
+    if request is None:
+        return objs
+
+    callback = request.GET['callback'] if 'callback' in request.GET else None
+    return util.raw( json.dumps( objs ), status=200, content='application/json', callback=callback )
+
+# Json response
+def resp( request, objs={} ):
+    objs['successful'] = True
+
+    # If we have no request, then just return the objects
+    if request is None:
+        return objs
+
+    callback = request.GET['callback'] if 'callback' in request.GET else None
+    return util.raw( json.dumps( objs ), status=200, content='application/json', callback=callback )
+
+
+# Return an error response
+def fail( request, reason, code="", extra={} ):
+    # Provide logging for errors
+    logging.warning( reason )
+
+    # Add my extra data
+    objs = { 'successful': False, 'reason': reason, 'code': code }
+    objs.update( extra )
+
+    # If we have no request, then just return the objects
+    if request is None:
+        return objs
+
+    callback = request.GET['callback'] if 'callback' in request.GET else None
+    return util.raw( json.dumps( objs ), status=201, content='application/json', callback=callback )
