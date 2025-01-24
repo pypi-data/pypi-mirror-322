de31a2d312c105bfefe12059cabd7836f55ee336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:38 +0000

    Add package nbprg-auth/0.0.1/nbprg_auth-0.0.1.tar.gz

---
+++ packages/nbprg-auth/nbprg_auth-0.0.1.tar.gz/nbprg_auth-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="nbprg_auth",
+    version="0.0.1",
+    packages=["nbprg_auth"],
+    description="",
+    author="Saifur Rahman",
+    author_email="novbh091@gmail.com",
+    license="MIT",
+    install_requires=[
+        "requests"
+    ],
+)
