985546793bc9ba6bfdd25997e76b948cf9d87496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 03:07:37 +0000

    Add package gdsfactoryplus/0.29.1/gdsfactoryplus-0.29.1-cp313-cp313-win_amd64.whl

---
+++ packages/gdsfactoryplus/gdsfactoryplus-0.29.1-cp313-cp313-win_amd64.whl/gdsfactoryplus/serve/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.29.1"
+__author__ = "GDSFactory"
+
+from .app import app as app
+from .dosax import *
+from .info import *
+from .schematic import *
+from .viewer import *
+from .watcher import *
+from .yaml import *
