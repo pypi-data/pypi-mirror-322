156e30881aae3d6989a4e0dcc086b2d809d0451c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 06:05:06 +0000

    Add package sendmail-docx/0.1.2/sendmail_docx-0.1.2-py3-none-any.whl

---
+++ packages/sendmail-docx/sendmail_docx-0.1.2-py3-none-any.whl/tests/test_email_sender_02.py
@@ -0,0 +1,30 @@
+"""
+Para ejecutar el ejemplo:
+python -m unittest tests.test_email_sender_02
+"""
+
+
+import unittest
+from sendmail-docx import enviar_correo_electronico
+import os
+
+class TestEnviarCorreoElectronico(unittest.TestCase):
+    def test_enviar_correo_electronico(self):
+        # Enviar un mail de prueba
+        resultado = enviar_correo_electronico(
+            template_path=os.path.join("tests", "templates", "plantilla-ejemplo.docx"),
+            datos={"nombre": "John Doe", "saldo": "1.235,50 €"},
+            asunto="Comunicación de saldo",
+            destinatarios=["jmsanchez.ibiza@gmail.com"],
+            cc=None,
+            cco=None,
+            adjuntos=['README.md', 'LICENSE']
+        )
+
+        print(f"TEST_EMAIL_SENDER_02.PY: {resultado=}")
+        self.assertEqual(resultado['status'], 'success')  # Cambié de 1 a 'success'
+        self.assertEqual(resultado['message'], 'Correo enviado con éxito.')  # Cambié de [] a 'Correo enviado con éxito.'
+
+
+if __name__ == '__main__':
+    unittest.main()
