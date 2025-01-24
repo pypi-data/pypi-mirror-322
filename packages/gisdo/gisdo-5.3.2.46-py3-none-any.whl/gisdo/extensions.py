781ff991fb095785cf4c9f52c5a5e930da4b831c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-24 04:06:00 +0000

    Add package gisdo/5.3.2.46/gisdo-5.3.2.46-py3-none-any.whl

---
+++ packages/gisdo/gisdo-5.3.2.46-py3-none-any.whl/gisdo/extensions.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from .constants import PERM_RELATED_TO_MO
+
+from typing import Any
+
+
+def get_gisdo_unit_pack_perm_dict(ext_result: Any = None) -> dict[str, str]:
+    """Возвращает словарь с правами для добавления к EditUnitDictPack
+
+    :param ext_result: Результат выполнения предыдущего
+        обработчика точки расширения.
+
+    :return: Словарь с данными формата: {Код права: Наименование права}
+
+    """
+    return {PERM_RELATED_TO_MO: 'Поле "Относится к МО"'}
